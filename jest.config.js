b42f434c1e5c937f32df6ce43324380f46ecbde7
Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2020-07-24 16:44:56 +0800

    feat: add jest basic config

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  preset: 'ts-jest',
+  globals: {
+    // work around: https://github.com/kulshekhar/ts-jest/issues/748#issuecomment-423528659
+    'ts-jest': {
+      diagnostics: {
+        ignoreCodes: [151001],
+      },
+    },
+  },
+  testEnvironment: 'jsdom',
+  transform: {
+    "^.+\\.vue$": "vue-jest"
+  },
+  moduleFileExtensions: ['vue', 'json', 'ts', 'tsx', 'js', 'json']
+}
---
+++ packages/button/__tests__/button.spec.ts
@@ -0,0 +1,6 @@
+describe('Button.vue', () => {
+  test('hello world', () => {
+    const bestGirl = 'Rem'
+    expect(bestGirl).not.toMatch('Emilia')
+  })
+})
