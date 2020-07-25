7f82633322407ba8d9801d2cf83d3de0ed3fb96b
Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2020-07-25 18:33:49 +0800

    feat: add story source

--- .storybook/export-loader.js
+++ .storybook/export-loader.js
@@ -1,7 +1,10 @@
-module.exports = function (content) {
-  return content.replace(
-  /export ?{\s* default as (.+?)}(.+)/g,`
+module.exports = function (content, map, meta) {
+  content = content.replace(
+  /^export\s*{\s*default as (.+?)}(.+)/mg,`
 import _$1 $2
 export const $1 = () => _$1
-`)
+`.trim())
+  this.callback(null, content, map, meta)
+  return
 }
+
