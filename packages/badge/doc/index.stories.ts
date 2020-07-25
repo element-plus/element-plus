00eeef6137b1415aac20ad2da55ea06c0d80bf9a
Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2020-07-25 18:33:49 +0800

    feat: add story source

--- packages/badge/doc/index.stories.ts
+++ packages/badge/doc/index.stories.ts
@@ -3,3 +3,9 @@ export { default as BasicUsage} from './basic.vue'
 export default {
   title: "Badge",
 }
+
+export const AAA = () => `
+<el-badge :value="333">
+  <el-button> eee</el-button>
+</el-badge>
+`
