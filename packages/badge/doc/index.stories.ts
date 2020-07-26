09e0a05c19b1b93fd7786f96ca1a086b44a8aea8
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2020-07-26 09:51:16 +0800

    fix(core): remove test code in test

--- packages/badge/doc/index.stories.ts
+++ packages/badge/doc/index.stories.ts
@@ -3,9 +3,3 @@ export { default as BasicUsage} from './basic.vue'
 export default {
   title: "Badge",
 }
-
-export const AAA = () => `
-<el-badge :value="333">
-  <el-button> eee</el-button>
-</el-badge>
-`
