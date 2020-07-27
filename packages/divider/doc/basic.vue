77856770d312d056d907aebe0b978ad927300f44
Caaalabash <1121062986@qq.com>
2020-07-27 23:22:31 +0800

    feat: el-divider

---
+++ packages/divider/doc/basic.vue
@@ -0,0 +1,31 @@
+<template>
+  <el-divider class="item" />
+  <el-divider class="item" @click="handleClick">click me</el-divider>
+  <el-divider class="item" content-position="left">撸起袖子加油干</el-divider>
+  <el-divider class="item" content-position="right">一张蓝图绘到底</el-divider>
+  <el-divider class="item"><i class="el-icon-mobile-phone"></i></el-divider>
+  <div class="item">
+    <span>雨纷纷</span>
+    <el-divider direction="vertical" />
+    <span>旧故里</span>
+    <el-divider direction="vertical" />
+    <span>草木深</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Basic',
+  methods: {
+    handleClick() {
+      alert('click')
+    },
+  },
+}
+</script>
+
+<style scoped>
+.item {
+  margin-bottom: 40px;
+}
+</style>
---
+++ packages/divider/src/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div
+    v-bind="$attrs"
+    :class="['el-divider', `el-divider--${direction}`]"
+  >
+    <div
+      v-if="$slots.default && direction !== 'vertical'"
+      :class="['el-divider__text', `is-${contentPosition}`]"
+    >
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'ElDivider',
+  props: {
+    direction: {
+      type: String,
+      default: 'horizontal',
+      validator(val: string): boolean {
+        return ['horizontal', 'vertical'].indexOf(val) !== -1
+      },
+    },
+    contentPosition: {
+      type: String,
+      default: 'center',
+      validator(val: string): boolean {
+        return ['left', 'center', 'right'].indexOf(val) !== -1
+      },
+    },
+  },
+}
+</script>
