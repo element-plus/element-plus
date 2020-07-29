989d0c23e0178881e897850d4a2967e672ba82e4
JeremyWuuuuu <591449570@qq.com>
2020-07-29 21:25:29 +0800

    feat/card - add new component card and it's tests, docs

---
+++ packages/card/doc/complex.vue
@@ -0,0 +1,19 @@
+<template>
+  <el-card class="box-card">
+    <template #header class="clearfix">
+      <span>卡片名称</span>
+      <el-button style="float: right; padding: 3px 0" type="text" @click="alert">点我</el-button>
+    </template>
+    <div v-for="o in 4" :key="o" class="text item">{{ '列表内容 ' + o }}</div>
+  </el-card>
+</template>
+
+<script lang="ts">
+export default {
+  methods: {
+    alert(): void {
+      alert('点我干嘛')
+    },
+  },
+}
+</script>
--- packages/element-plus/index.ts
+++ packages/element-plus/index.ts
@@ -1,6 +1,7 @@
 import type { App } from 'vue'
 import ElButton from '@element-plus/button'
 import ElBadge from '@element-plus/badge'
+import ElCard from '@element-plus/card'
 import ElTag from '@element-plus/tag'
 import ElDivider from '@element-plus/divider'
 
@@ -11,6 +12,7 @@ export {
 export default function install(app: App): void {
   ElButton(app)
   ElBadge(app)
+  ElCard(app)
   ElTag(app)
   ElDivider(app)
 }
