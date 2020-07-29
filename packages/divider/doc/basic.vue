28bb568c0c8ea4bcffe150bb3a2b12e2d7ce6875
Caaalabash <1121062986@qq.com>
2020-07-29 20:34:38 +0800

    test: change test string

--- packages/divider/doc/basic.vue
+++ packages/divider/doc/basic.vue
@@ -1,8 +1,8 @@
 <template>
   <el-divider class="item" />
   <el-divider class="item" @click="handleClick">click me</el-divider>
-  <el-divider class="item" content-position="left">撸起袖子加油干</el-divider>
-  <el-divider class="item" content-position="right">一张蓝图绘到底</el-divider>
+  <el-divider class="item" content-position="left">清风徐来</el-divider>
+  <el-divider class="item" content-position="right">水波不兴</el-divider>
   <el-divider class="item"><i class="el-icon-mobile-phone"></i></el-divider>
   <div class="item">
     <span>雨纷纷</span>
