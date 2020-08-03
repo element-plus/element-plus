90ae785bebbcd8d90458375d8c4a5df90117cfc9
zazzaz <izazzaz@hotmail.com>
2020-08-03 21:32:59 +0800

    test: update test

--- packages/locale/__test__/locale.spec.ts
+++ packages/locale/__test__/locale.spec.ts
@@ -2,6 +2,7 @@ import { mount } from '@vue/test-utils'
 import { t, use } from '../index'
 import localeMixin from '../mixin'
 import zhCn from '../lang/zh-CN'
+import en from '../lang/en'
 
 describe('Locale', () => {
   test('t', () => {
@@ -15,13 +16,14 @@ describe('Locale', () => {
   test('use', () => {
     use(zhCn)
     expect(t('el.popconfirm.confirmButtonText')).toBe('确定')
+    use(en)
   })
 
   test('mixin', () => {
     const component = {
       template: `<p>{{ t('el.popconfirm.cancelButtonText') }}</p>`,
       mixins: [localeMixin],
-    }
+    } as any
     const wrapper = mount(component)
     expect(wrapper.text()).toContain('No')
   })
