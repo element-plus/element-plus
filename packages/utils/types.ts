bbbe50e6b847ce8e4de0e83dc1dbbe1e359499be
zazzaz <izazzaz@hotmail.com>
2020-08-04 10:49:49 +0800

    ci: add lint to ci

--- packages/utils/types.ts
+++ packages/utils/types.ts
@@ -1,16 +1,16 @@
-type OptionalKeys<T extends object> = {
+type OptionalKeys<T extends Record<string, unknown>> = {
   [K in keyof T]: T extends Record<K, T[K]>
     ? never
     : K
 }[keyof T]
 
-type RequiredKeys<T extends object> = Exclude<keyof T, OptionalKeys<T>>
+type RequiredKeys<T extends Record<string, unknown>> = Exclude<keyof T, OptionalKeys<T>>
 
 type MonoArgEmitter<T, Keys extends keyof T> = <K extends Keys>(evt: K, arg?: T[K]) => void
 
 type BiArgEmitter<T, Keys extends keyof T> = <K extends Keys>(evt: K, arg: T[K]) => void
 
-export type EventEmitter<T extends object> =
+export type EventEmitter<T extends Record<string, unknown>> =
   MonoArgEmitter<T, OptionalKeys<T>> & BiArgEmitter<T, RequiredKeys<T>>
 
-type A = Required<{}>
+type A = Required<Record<string, unknown>>
