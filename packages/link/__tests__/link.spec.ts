441da522e91780ad29e23938dd9f758f562085ce
JeremyWuuuuu <591449570@qq.com>
2020-08-05 14:43:25 +0800

    address PR comments

--- packages/link/__tests__/link.spec.ts
+++ packages/link/__tests__/link.spec.ts
@@ -13,14 +13,10 @@ describe('Link.vue', () => {
     expect(wrapper.text()).toEqual(AXIOM)
   })
 
-  test('it should handle click event when href were given and is not disabled', async () => {
-    const href = 'https://target.com'
+  test('it should handle click event when link is not disabled', async () => {
     const wrapper = mount(Link, {
       slots: {
         default: AXIOM,
-      },
-      props: {
-        href,
       },
     })
 
@@ -28,14 +24,12 @@ describe('Link.vue', () => {
     expect(wrapper.emitted('click')).toHaveLength(1)
   })
 
-  test('it should disable click when it\'s disabled', async () => {
-    const href = 'https://target.com'
+  test('it should disable click when link is disabled', async () => {
     const wrapper = mount(Link, {
       slots: {
         default: AXIOM,
       },
       props: {
-        href,
         disabled: true,
       },
     })
@@ -45,5 +39,16 @@ describe('Link.vue', () => {
   })
 
 
+  test('icon slots', () => {
+    const linkName = 'test link'
+    const wrapper = mount(Link, {
+      slots: {
+        default: linkName,
+        icon: AXIOM,
+      },
+    })
+    expect(wrapper.text()).toContain(linkName)
+    expect(wrapper.text()).toContain(AXIOM)
+  })
 
 })
