cc7ec461d3a2e4c8b0dc20c1cc281d1dbaa18289
Caaalabash <1121062986@qq.com>
2020-08-06 23:35:23 +0800

    style(backtop): indent & space

--- packages/backtop/__tests__/backtop.spec.ts
+++ packages/backtop/__tests__/backtop.spec.ts
@@ -9,20 +9,22 @@ const _mount = (template: string) => mount({
 }, { attachTo: document.body })
 
 describe('Backtop.vue', () => {
-  test('render',async () => {
+  test('render', async () => {
     const wrapper = _mount(`
-       <div class="target" style="height: 100px; overflow: auto">
-         <div style="height: 10000px; width: 100%">
-           <el-backtop target=".target" :visibilityHeight="2000" :right="100" :bottom="200" />
-         </div>
-       </div>
+      <div class="target" style="height: 100px; overflow: auto">
+        <div style="height: 10000px; width: 100%">
+          <el-backtop target=".target" :visibilityHeight="2000" :right="100" :bottom="200" />
+        </div>
+      </div>
     `)
     expect(wrapper.find('.el-backtop').exists()).toBe(false)
     wrapper.element.scrollTop = 2000
     await wrapper.trigger('scroll')
     expect(wrapper.find('.el-backtop').exists()).toBe(true)
+
     expect(wrapper.find('.el-backtop').attributes('style')).toBe('right: 100px; bottom: 200px;')
     expect(wrapper.find('.el-icon-caret-top').exists()).toBe(true)
+
     await wrapper.trigger('click')
     expect(wrapper.emitted()).toBeDefined()
   })
