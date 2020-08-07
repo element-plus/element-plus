9dc7aeff287196be49ae81d9195454b4d78463c6
Caaalabash <1121062986@qq.com>
2020-08-07 13:45:02 +0800

    fix(backtop): code style

--- packages/utils/util.ts
+++ packages/utils/util.ts
@@ -146,4 +146,10 @@ export function rafThrottle(fn: (args: Record<string, unknown>) => unknown): (..
 
 export const objToArray = castArray
 
+export const cubic = (value: number): number => Math.pow(value, 3)
+
+export const easeInOutCubic = (value: number): number => value < 0.5
+  ? cubic(value * 2) / 2
+  : 1 - cubic((1 - value) * 2) / 2
+
 export { isVNode } from 'vue'
