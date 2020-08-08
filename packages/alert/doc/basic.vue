01cadb6d9bc0be00a018214861c6f05c1c5f056c
homer <homer6wang@gmail.com>
2020-08-08 22:46:57 +0800

    feat: add doc & test

---
+++ packages/alert/doc/basic.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div>
+      <el-alert
+        title="success alert"
+        type="success"
+      />
+      <el-alert
+        title="info alert"
+        type="info"
+      />
+      <el-alert
+        title="warning alert"
+        type="warning"
+      />
+      <el-alert
+        title="error alert"
+        type="error"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.el-alert {
+  margin: 6px;
+}
+</style>
+
