cbb362e3ab272eaf8196240b36bff407a55c89c1
homer <homer6wang@gmail.com>
2020-08-09 23:42:06 +0800

    feat: storybook

--- packages/alert/doc/basic.vue
+++ packages/alert/doc/basic.vue
@@ -18,6 +18,28 @@
         type="error"
       />
     </div>
+    <div>
+      <el-alert
+        title="success alert"
+        type="success"
+        show-icon
+      />
+      <el-alert
+        title="info alert"
+        type="info"
+        effect="dark"
+      />
+      <el-alert
+        title="warning alert"
+        type="warning"
+        :closable="false"
+      />
+      <el-alert
+        title="error alert"
+        type="error"
+        center
+      />
+    </div>
   </div>
 </template>
 
--- packages/element-plus/index.ts
+++ packages/element-plus/index.ts
@@ -1,4 +1,5 @@
 import type { App } from 'vue'
+import ElAlert from '@element-plus/alert'
 import ElAvatar from '@element-plus/avatar'
 import ElBacktop from '@element-plus/backtop'
 import ElButton from '@element-plus/button'
@@ -23,6 +24,7 @@ import ElScrollBar from '@element-plus/scrollbar'
 import ElCollapse from '@element-plus/collapse'
 
 export {
+  ElAlert,
   ElAvatar,
   ElBacktop,
   ElLayout,
@@ -48,6 +50,7 @@ export {
 }
 
 export default function install(app: App): void {
+  ElAlert(app)
   ElAvatar(app)
   ElBacktop(app)
   ElButton(app)
