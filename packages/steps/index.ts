6763e6d895f855162edbebe2a85d914b24277dbd
Caaalabash <1121062986@qq.com>
2020-08-12 13:50:44 +0800

    feat(steps): add steps component

---
+++ packages/steps/index.ts
@@ -0,0 +1,7 @@
+import { App } from 'vue'
+import Steps from './src/index.vue'
+import Step from './src/item.vue'
+export default (app: App): void => {
+  app.component(Steps.name, Steps)
+  app.component(Step.name, Step)
+}
