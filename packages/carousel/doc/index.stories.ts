54743936083b9b0168a2d2c6a8ce3d209f80074a
liik <linyunqianpp@126.com>
2020-08-18 15:30:03 +0800

    feat(carousel): more test cases

--- packages/carousel/doc/index.stories.ts
+++ packages/carousel/doc/index.stories.ts
@@ -1,4 +1,6 @@
 export { default as BasicUsage } from './carousel.vue'
+export { default as CardUsage } from './carousel-card.vue'
+export { default as VerticalUsage } from './carousel-vertical.vue'
 
 export default {
   title: 'Carousel',
