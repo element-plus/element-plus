e237f70515d322b167d8f64fcf781392aad98863
Ryan <ryanzhao2128@gmail.com>
2020-08-24 22:29:13 +0800

    docs(slider): add storybook docs

---
+++ packages/slider/doc/basic-usage.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <div class="block">
+      <span class="demonstration">默认</span>
+      <el-slider v-model="value1" />
+    </div>
+    <div class="block">
+      <span class="demonstration">自定义初始值</span>
+      <el-slider v-model="value2" />
+    </div>
+    <div class="block">
+      <span class="demonstration">隐藏 Tooltip</span>
+      <el-slider v-model="value3" :show-tooltip="false" />
+    </div>
+    <div class="block">
+      <span class="demonstration">格式化 Tooltip</span>
+      <el-slider v-model="value4" :format-tooltip="formatTooltip" />
+    </div>
+    <div class="block">
+      <span class="demonstration">禁用</span>
+      <el-slider v-model="value5" disabled />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { reactive, toRefs } from 'vue'
+
+export default {
+  setup() {
+    const values = reactive({
+      value1: 30,
+      value2: 50,
+      value3: 36,
+      value4: 48,
+      value5: 42,
+    })
+    const formatTooltip = (val:number) => `当前: ${val}`
+    return {
+      slider,
+      ...toRefs(values),
+      formatTooltip,
+      click,
+    }
+  },
+}
+</script>
+<style>
+.demonstration {
+  font-size: 14px;
+  color: #8492a6;
+  line-height: 44px;
+}
+</style>
--- packages/slider/doc/index.stories.ts
+++ packages/slider/doc/index.stories.ts
@@ -1,6 +1,14 @@
-import ElSlider from '../index'
-
 export default {
   title: 'Slider',
 }
+
+export { default as BasicUsage } from './basic-usage.vue'
+
+export { default as DiscreteValues } from './discrete-values.vue'
+
+export { default as RangeSelection } from './range-selection.vue'
+
+export { default as VerticalMode } from './vertical-mode.vue'
+
+export { default as ShowMarks } from './show-marks.vue'
 
