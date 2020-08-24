ac0e897e16ee97d83d7b71fb102f08b81625545e
Ryan <ryanzhao2128@gmail.com>
2020-08-24 22:28:58 +0800

    feat(utils): add jestMock.ts

---
+++ packages/utils/jestMock.ts
@@ -0,0 +1,16 @@
+
+
+
+export const jestMock = (object:any, method: string, implementation:() => any, accessType?: 'get' | 'set') => {
+  let mockSpy = null
+  if(!accessType){
+    mockSpy = jest.spyOn(object, method)
+      .mockImplementation(implementation)
+  }
+  if(accessType === 'get' || accessType === 'set'){
+    // must use `as 'get'` or `as 'set'` or `as 'any'`.
+    mockSpy = global.jest.spyOn(object, method, accessType as 'get')
+      .mockImplementation(implementation)
+  }
+  return mockSpy
+}
