20b9dd63da71a2bb85d0f706689f011aa87efb9d
plusli <121318100@qq.com>
2020-08-30 23:43:58 +0800

    feat(color-picker): create color-picker component

--- packages/color-picker/doc/basic.vue
+++ packages/color-picker/doc/basic.vue
@@ -1,12 +1,32 @@
 <template>
   <div>
-    <el-color-picker />
+    <el-color-picker v-model="color" :show-alpha="true" :predefine="predefineColors" />
   </div>
 </template>
 
-<style scoped>
-.el-alert {
-  margin: 6px;
-}
-</style>
+<script>
+export default {
+  data() {
+    return {
+      color: '#333333',
+      predefineColors: [
+        '#ff4500',
+        '#ff8c00',
+        '#ffd700',
+        '#90ee90',
+        '#00ced1',
+        '#1e90ff',
+        '#c71585',
+        'rgba(255, 69, 0, 0.68)',
+        'rgb(255, 120, 0)',
+        'hsv(51, 100, 98)',
+        'hsva(120, 40, 94, 0.5)',
+        'hsl(181, 100%, 37%)',
+        'hsla(209, 100%, 56%, 0.73)',
+        '#c7158577',
+      ],
 
+    }
+  },
+}
+</script>
