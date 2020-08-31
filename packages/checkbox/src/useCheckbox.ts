a8d825bce8c0097b95a7189883f1583945584bed
zouhang <zouhang@didiglobal.com>
2020-08-31 09:51:33 +0800

    feat(checkbox): optimize code logic

--- packages/checkbox/src/useCheckbox.ts
+++ packages/checkbox/src/useCheckbox.ts
@@ -1,23 +1,60 @@
-import { ref, computed, inject } from 'vue'
+import { ref, computed, inject, getCurrentInstance, SetupContext } from 'vue'
+import { UPDATE_MODEL_EVENT } from '@element-plus/utils/constants'
+import { ICheckboxGroupInstance, ICheckboxProps } from './checkbox'
 
-export const useCheckbox = () => {
+export const useCheckboxGroup = () => {
   //todo: ELEMENT
   const ELEMENT = null
   const elForm = inject('elForm', {})
   const elFormItem = inject('elFormItem', {}) as any
-  const _checkboxGroup = inject('CheckboxGroup', {}) as any
+  const checkboxGroup = inject<ICheckboxGroupInstance>('CheckboxGroup', {})
   const focus = ref(false)
-  const isGroup = computed(() => _checkboxGroup && _checkboxGroup.name === 'ElCheckboxGroup')
-  const _elFormItemSize = computed(() => {
+  const isGroup = computed(() => checkboxGroup && checkboxGroup.name === 'ElCheckboxGroup')
+  const elFormItemSize = computed(() => {
     return (elFormItem || {} as any).elFormItemSize
   })
   return {
     isGroup,
     focus,
-    _checkboxGroup,
+    checkboxGroup,
     elForm,
     ELEMENT,
-    _elFormItemSize,
+    elFormItemSize,
     elFormItem,
+  }
+}
+
+export const useCheckbox = (props: ICheckboxProps, { emit }: SetupContext) => {
+  let selfModel = false
+  const { elForm, isGroup, checkboxGroup, elFormItemSize, elFormItem, ELEMENT } = useCheckboxGroup()
+  const instance = getCurrentInstance()
+  const focus = ref(false)
+  const isLimitExceeded = ref(false)
+  const store = computed(() => checkboxGroup ? checkboxGroup.modelValue.value : props.modelValue)
+  const model = computed({
+    get() {
+      return isGroup.value ? store.value : props.modelValue !== undefined ? props.modelValue : selfModel
+    },
+
+    set(val: any) {
+      if (isGroup.value) {
+        isLimitExceeded.value = false
+
+        if (checkboxGroup.min !== undefined && val.length < checkboxGroup.min) {
+          isLimitExceeded.value = true
+        }
+        if (checkboxGroup.max !== undefined && val.length > checkboxGroup.max) {
+          isLimitExceeded.value = true
+        }
+
+        isLimitExceeded.value === false && checkboxGroup.changeEvent?.(val)
+      } else {
+        emit('update:modelValue', val)
+        selfModel = val
+      }
+    },
+  })
+  return {
+    instance,
   }
 }
