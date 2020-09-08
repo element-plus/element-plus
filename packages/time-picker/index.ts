03c76c1215466c4fa9372abf5b46b581e8292a9a
zazzaz <izazzaz@hotmail.com>
2020-09-08 17:37:46 +0800

    chore: update

--- packages/time-picker/index.ts
+++ packages/time-picker/index.ts
@@ -1,7 +1,5 @@
 import { App } from 'vue'
 import TimePicker from './src/time-picker'
-import DatePicker from './src/date-picker'
 export default (app: App): void => {
-  app.component(DatePicker.name, DatePicker)
   app.component(TimePicker.name, TimePicker)
 }
--- website/play/index.vue
+++ website/play/index.vue
@@ -1,37 +1,15 @@
 <template>
   <div>
-    <el-time-picker
-      v-model="value"
-      is-range
-      :enabled-hours="enabledHours"
-    />
-    value1: {{ value }}
-    <br>
-    // left ['08:00:00 - 12:59:59'] right ['11:00:00 - 16:59:59']
+    <el-button>{{ value1 }}</el-button>
   </div>
 </template>
 
 <script>
-const makeRange = (start, end) => {
-  const result = []
-  for (let i = start; i <= end; i++) {
-    result.push(i)
-  }
-  return result
-}
 export default {
   data() {
     return {
-      value: '',
+      value1: 'TEST',
     }
-  },
-  methods: {
-    enabledHours(role) {
-      if (role === 'start') {
-        return makeRange(8, 12)
-      }
-      return makeRange(11, 16)
-    },
   },
 }
 </script>
