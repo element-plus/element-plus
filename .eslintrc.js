7c2ccfeba2f78764a3fc8ebaf252657e91f5495b
liik <linyunqianpp@126.com>
2020-09-08 22:52:05 +0800

    feat(carousel): revert eslint modify

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,9 @@ module.exports = {
     browser: true,
     node: true,
   },
-  plugins: ['@typescript-eslint'],
+  plugins: [
+    '@typescript-eslint',
+  ],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:vue/vue3-recommended',
@@ -19,11 +21,7 @@ module.exports = {
     'no-trailing-spaces': 'error',
     'comma-style': ['error', 'last'],
     'comma-dangle': ['error', 'always-multiline'],
-    quotes: [
-      'error',
-      'single',
-      { avoidEscape: true, allowTemplateLiterals: true },
-    ],
+    quotes: ['error', 'single', { avoidEscape: true, allowTemplateLiterals: true }],
     camelcase: ['error', { properties: 'never' }],
     semi: ['error', 'never'],
     indent: ['error', 2, { SwitchCase: 1 }],
@@ -47,38 +45,18 @@ module.exports = {
     // vue
     'vue/no-v-html': 'off',
     'vue/singleline-html-element-content-newline': 'off',
-    'vue/html-self-closing': [
-      'error',
-      {
-        html: {
-          void: 'never',
-          normal: 'never',
-          component: 'always',
-        },
+    'vue/html-self-closing': ['error', {
+      html: {
+        void: 'never',
+        normal: 'never',
+        component: 'always',
       },
-    ],
-    'vue/max-attributes-per-line': [
-      'error',
-      {
-        singleline: 3,
-        multiline: 1,
-      },
-    ],
-    '@typescript-eslint/member-delimiter-style': [
-      2,
-      {
-        multiline: {
-          delimiter: 'none',
-          requireLast: false,
-        },
-        singleline: {
-          delimiter: 'semi',
-          requireLast: true,
-        },
-      },
-    ],
+    }],
+    'vue/max-attributes-per-line': ['error', {
+      singleline: 3,
+      multiline: 1,
+    }],
     'vue/require-default-prop': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
     'vue/html-closing-bracket-spacing': 'error',
   },
 }
