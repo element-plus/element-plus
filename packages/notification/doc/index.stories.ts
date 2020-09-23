eb105aa574ba8f4804a57fc97bac6a4643e4c5b3
JeremyWuuuuu <591449570@qq.com>
2020-09-23 20:12:18 +0800

    chore(core): deprecate storybook

--- packages/notification/doc/index.stories.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  title: 'Notification',
-}
-
-export { default as BasicNotification } from './standealone.vue'
-export { default as AdvancedNotification } from './vnode.vue'
-export { default as Closable } from './closable.vue'
