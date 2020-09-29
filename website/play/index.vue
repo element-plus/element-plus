55d368ccbe029bc24a460ab9c1b35d582922a33f
jeremywu <591449570@qq.com>
2020-09-29 16:44:48 +0800

    fix: making use popper universal (#371)
    * staging work
    
    * refactor(popper): making usePopper portable

--- website/play/index.vue
+++ website/play/index.vue
@@ -1,6 +1,17 @@
 <template>
-  <div>
-  </div>
+  <el-popper
+    placement="bottom-end"
+    effect="light"
+    pure
+    trigger="click"
+  >
+    <template #trigger>
+      <el-button>Button</el-button>
+    </template>
+    <div>
+      I am the content
+    </div>
+  </el-popper>
 </template>
 
 <script>
