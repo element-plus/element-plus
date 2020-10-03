387946ace756f0baa2c394b2d33b187487caa72d
Enguerrand des Vaux <enguerrand@des-vaux.fr>
2020-10-03 14:08:43 +0200

    fix(form): fix label-wrap impor

--- packages/form/index.ts
+++ packages/form/index.ts
@@ -1,7 +1,7 @@
 import { App } from 'vue'
 import Form from './src/form.vue'
 import FormItem from './src/form-item.vue'
-import LabelWrap from './src/label-wrap.vue'
+import LabelWrap from './src/label-wrap'
 
 export default (app: App): void => {
   app.component(Form.name, Form)
