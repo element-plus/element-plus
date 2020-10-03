355a778a2cee8d95cf8dbcebd790b1f0de69e130
zazzaz <izazzaz@hotmail.com>
2020-10-03 20:13:19 +0800

    Feat/datepicker && datetimepicker (#326)
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * Feat/form (#342)
    
    * feat(form): add form component
    
    fix #125
    
    * test(form): add test code
    
    * docs(form): add form doc
    
    * feat: add uitls merge
    
    * fix(form): fix style
    
    * test(form): add form test code
    
    * refactor(form): review changes
    
    * test(form): use idiomatic vue-test-util methods
    
    * feat(core): bump vue version
    
    * feat(form): rewrite label wrap
    
    * feat(form): fix tons of bugs
    
    * fix(form): reuse ts extension
    
    * refactor(form): move out label width computation
    
    * fix(form): fix tons of bugs
    
    * fix(form): test
    
    Co-authored-by: 286506460 <286506460@qq.com>
    
    * Feat/select (#381)
    
    * fix: resove conflict
    
    * feat: select basic usage
    
    * feat: select basic usage
    
    * feat: select feature create item
    
    * fix: fix option data insert
    
    * refactor: select
    
    * fix: fix parse error
    
    * feat: select test
    
    * fix: select add popper
    
    * fix: update select option
    
    * fix: add select dependency
    
    * fix: add index.ts file
    
    * fix(select): clean up
    
    * fix(select): some refactor
    
    * fix(select): some update
    
    * fix(select): fix all test cases
    
    Co-authored-by: helen <yinhelen.hlj@qq.com>
    
    Co-authored-by: Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
    Co-authored-by: 286506460 <286506460@qq.com>
    Co-authored-by: helen <yinhelen.hlj@qq.com>

--- packages/element-plus/index.ts
+++ packages/element-plus/index.ts
@@ -28,6 +28,7 @@ import ElSteps from '@element-plus/steps'
 import ElCollapse from '@element-plus/collapse'
 import ElPopper from '@element-plus/popper'
 import ElTimePicker from '@element-plus/time-picker'
+import ElDatePicker from '@element-plus/date-picker'
 import ElTabs from '@element-plus/tabs'
 import ElTooltip from '@element-plus/tooltip'
 import ElSlider from '@element-plus/slider'
@@ -71,6 +72,7 @@ export {
   ElRadio,
   ElCollapse,
   ElTimePicker,
+  ElDatePicker,
   ElTabs,
   ElTooltip,
   ElSlider,
@@ -116,6 +118,7 @@ const install = (app: App): void =>  {
   ElCollapse(app)
   ElPopper(app)
   ElTimePicker(app)
+  ElDatePicker(app)
   ElTabs(app)
   ElTooltip(app)
   ElSlider(app)
