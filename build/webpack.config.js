a432ae396631b199e7f64ead33d20992d6a854fe
plantainX <oobakoyo@gmail.com>
2020-10-16 14:14:36 +0800

    fix(build): vue external config (#380)

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -80,7 +80,12 @@ module.exports = {
     extensions: ['.ts', '.tsx', '.js', '.vue', '.json'],
   },
   externals: {
-    vue: 'Vue',
+    vue: {
+      root: 'Vue',
+      commonjs: 'vue',
+      commonjs2: 'vue',
+      amd: 'vue',
+    },
   },
   plugins: [
     new VueLoaderPlugin(),
