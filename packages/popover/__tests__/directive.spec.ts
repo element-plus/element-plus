50021c1523b478d8bfcea91bb02e6c499979927e
JeremyWuuuuu <591449570@qq.com>
2020-10-18 17:57:59 +0800

    feat(popover): popover component

--- packages/popover/__tests__/directive.spec.ts
+++ packages/popover/__tests__/directive.spec.ts
@@ -1,20 +1,29 @@
+import { nextTick } from 'vue'
 import Popover from '../src/index.vue'
 import PopoverDirective, { VPopover } from '../src/directive'
 import makeMount from '@element-plus/test-utils/make-mount'
+
+import type { ComponentPublicInstance } from 'vue'
 
 
 const AXIOM = 'Rem is the best girl'
 
 const Comp = {
   template: `
-  <el-popover title="title" content="some content" ref="popover"/>
-  <div v-popover:popover>
+  <el-popover title="title" :content="content" ref="popover"/>
+  <div v-popover:popover v-if="visible" id="reference-node">
     ${AXIOM}
   </div>
   `,
   components: {
     [Popover.name]: Popover,
-  }
+  },
+  data() {
+    return {
+      visible: false,
+      content: AXIOM,
+    }
+  },
 }
 
 const mount = makeMount(Comp, {
@@ -24,8 +33,8 @@ const mount = makeMount(Comp, {
   global: {
     directives: {
       [VPopover]: PopoverDirective,
-    }
-  }
+    },
+  },
 })
 
 describe('v-popover', () => {
@@ -34,5 +43,20 @@ describe('v-popover', () => {
     const wrapper = mount()
 
     expect(wrapper.text()).toContain(AXIOM)
+
+  })
+
+  test('should show popover when reference is mounted', async () => {
+    const wrapper = mount()
+    const refNode = '#reference-node'
+    expect(wrapper.find(refNode).exists()).toBe(false);
+
+    (wrapper.vm as ComponentPublicInstance<{visible: boolean;}>).visible = true
+    await nextTick()
+
+    expect(wrapper.find(refNode).exists()).toBe(true)
+    expect(wrapper.find('.el-popover').attributes('style')).toContain('display: none')
+    await wrapper.find(refNode).trigger('click')
+    expect(wrapper.find('.el-popover').attributes('style')).not.toContain('display: none')
   })
 })
