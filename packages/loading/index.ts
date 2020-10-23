ee3b42fb096b41d99e1171a1a193ba74b8939071
bastarder <85257684@qq.com>
2020-10-23 10:15:49 +0800

    feat(loading): add loading component (#447)
    * refactor(loading): refactored loading component
    
    * refactor(loading): use render & createVNode instead of createApp
    
    Co-authored-by: Ryan <ryanzhao2128@gmail.com>

---
+++ packages/loading/index.ts
@@ -0,0 +1,8 @@
+import { App } from 'vue'
+import Loading from './src/index'
+import vLoading from './src/directive'
+
+export default (app: App): void => {
+  app.directive('loading', vLoading)
+  app.config.globalProperties.$loading = Loading
+}
