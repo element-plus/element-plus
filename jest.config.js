b1cd496684c684e8a95b14932bd2c992145e5015
Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2020-10-27 16:52:43 +0800

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -30,7 +30,7 @@ module.exports = {
       },
     ],
   },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'vue'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
   // u can change this option to a more specific folder for test single component or util when dev
   // for example, ['<rootDir>/packages/input']
   roots: ['<rootDir>'],
