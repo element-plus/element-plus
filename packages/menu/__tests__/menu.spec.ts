eab75c13ea631a64db358448f4a30d61974a0173
liik <linyunqianpp@126.com>
2020-11-11 10:30:09 +0800

    feat(menu): fix spec return valule

--- packages/menu/__tests__/menu.spec.ts
+++ packages/menu/__tests__/menu.spec.ts
@@ -1,11 +1,9 @@
-import { mount } from '@vue/test-utils'
-import Menu from '../src/menu.vue'
-
-const AXIOM = 'Rem is the best girl'
+// import { mount } from '@vue/test-utils'
+// import Menu from '../src/menu.vue'
 
 describe('Menu.vue', () => {
   test('render test', () => {
     // TODO: compelete test case
-    return true
+    return undefined
   })
 })
