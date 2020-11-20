5726ad2c3d4061937e295f105bb3a537ecaf6244
zazzaz <izazzaz@hotmail.com>
2020-11-20 23:29:50 +0800

    docs: update upload doc

--- packages/dropdown/index.ts
+++ packages/dropdown/index.ts
@@ -1,7 +1,5 @@
 import { App } from 'vue'
 import Dropdown from './src/dropdown.vue'
-import DropdownItem from './src/dropdown-item.vue'
-import DropdownMenu from './src/dropdown-menu.vue'
 
 Dropdown.install = (app: App): void => {
   app.component(Dropdown.name, Dropdown)
