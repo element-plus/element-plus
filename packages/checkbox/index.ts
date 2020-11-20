95d28d48ea2a54ebcd8aead95cbcfaca56fd02d7
zazzaz <izazzaz@hotmail.com>
2020-11-20 23:29:50 +0800

    docs: update upload doc

--- packages/checkbox/index.ts
+++ packages/checkbox/index.ts
@@ -1,7 +1,5 @@
 import { App } from 'vue'
 import Checkbox from './src/checkbox.vue'
-import CheckboxButton from './src/checkbox-button.vue'
-import CheckboxGroup from './src/checkbox-group.vue'
 
 Checkbox.install = (app: App): void => {
   app.component(Checkbox.name, Checkbox)
