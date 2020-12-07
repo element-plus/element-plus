536c43a57133b0eadc8bea3e0c5454e27c025c82
Simona <36978416+SimonaliaChen@users.noreply.github.com>
2020-12-07 13:21:23 +0800

    fix(input): non-emit events such as keyup not work (#866)
    * fix(input): non-emit events such as keyup not work
    
    fix #837
    
    * fix: remove debug code
    
    * test(input): add test case

--- packages/hooks/__tests__/use-attrs.spec.ts
+++ packages/hooks/__tests__/use-attrs.spec.ts
@@ -6,6 +6,7 @@ const CLASS = 'a'
 const WIDTH = '50px'
 const TEST_KEY = 'test'
 const TEST_VALUE = 'fake'
+const ANOTHER_TEST_VALUE = 'fake1'
 
 const handleClick = jest.fn()
 
@@ -54,7 +55,7 @@ afterEach(() => {
 
 describe('useAttrs', () => {
   test('class and style should not bind to child node', async () => {
-    const wrapper = _mount(genComp(true))
+    const wrapper = _mount(genComp())
     const container = wrapper.element as HTMLDivElement
     const span = wrapper.find('span')
 
@@ -67,6 +68,14 @@ describe('useAttrs', () => {
     await span.trigger('click')
 
     expect(handleClick).toBeCalledTimes(2)
+  })
+
+  test('child node\'s attributes should update when prop change', async () => {
+    const wrapper = _mount(genComp())
+    const span = wrapper.find('span')
+
+    await wrapper.setProps({ [TEST_KEY]: ANOTHER_TEST_VALUE })
+    expect(span.attributes(TEST_KEY)).toBe(ANOTHER_TEST_VALUE)
   })
 
   test('excluded listeners should not bind to child node', async () => {
