bee7f6535ba87d41e0138f79b7242cc5f8a26136
hukeqin <lamysafari@163.com>
2020-12-09 19:12:32 +0800

    fix(directives): fix click touchpad once but the value changes twice

--- packages/directives/repeat-click/index.ts
+++ packages/directives/repeat-click/index.ts
@@ -3,23 +3,33 @@ import { on, once } from '@element-plus/utils/dom'
 import type { ObjectDirective } from 'vue'
 export default {
   beforeMount(el, binding) {
-    let interval = null
-    let startTime: number
+    const DELAY = 300
+    const INTERVAL = 100
+    let intervalTimer = null
+    let delayTimer = null
     const handler = () => binding.value && binding.value()
     const clear = () => {
-      if (Date.now() - startTime < 100) {
-        handler()
-      }
-      clearInterval(interval)
-      interval = null
+      clearInterval(intervalTimer)
+      clearTimeout(delayTimer)
+      intervalTimer = null
+      delayTimer = null
     }
 
     on(el, 'mousedown', e => {
       if ((e as any).button !== 0) return
-      startTime = Date.now()
-      once(document as any, 'mouseup', clear)
-      clearInterval(interval)
-      interval = setInterval(handler, 100)
+
+      handler()
+      once(el, 'mouseup', clear)
+      clearInterval(intervalTimer)
+
+      if (delayTimer) {
+        clearTimeout(delayTimer)
+      }
+
+      delayTimer = setTimeout(() => {
+        intervalTimer = setInterval(handler, INTERVAL)
+        delayTimer = null
+      }, DELAY)
     })
   },
 } as ObjectDirective
