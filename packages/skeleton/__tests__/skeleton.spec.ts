914246a0793407470f4f3a042a932432572a37f2
wujinze <wujinze@hellobike.com>
2020-12-10 15:37:34 +0800

    fix: fix unit test

--- packages/skeleton/__tests__/skeleton.spec.ts
+++ packages/skeleton/__tests__/skeleton.spec.ts
@@ -1,15 +1,9 @@
 import { mount } from '@vue/test-utils'
 import Skeleton from '../src/index.vue'
 
-const AXIOM = 'Rem is the best girl'
-
 describe('Skeleton.vue', () => {
   test('render test', () => {
-    const wrapper = mount(Skeleton, {
-      slots: {
-        default: AXIOM,
-      },
-    })
-    expect(wrapper.text()).toEqual(AXIOM)
+    const wrapper = mount(Skeleton)
+    expect(wrapper.classes()).toContain('el-skeleton')
   })
 })
