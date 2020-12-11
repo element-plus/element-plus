52ded1a6fdc6b07a79dcab896b5cd2f942ea4e12
zouhang <zouhang@didiglobal.com>
2020-12-11 14:34:27 +0800

    fix(build): fix duplicate exports

--- packages/popper/index.ts
+++ packages/popper/index.ts
@@ -10,5 +10,4 @@ export default Popper
 export { default as defaultProps, Effect, Placement, Options } from './src/use-popper/defaults'
 export type { TriggerType, IPopperOptions, PopperInstance } from './src/use-popper/defaults'
 export { default as usePopper } from './src/use-popper/index'
-export { Options, Placement } from './src/use-popper/defaults'
 export * from './src/renderers/index'
