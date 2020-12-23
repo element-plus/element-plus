f76d292f6f1be7730fcc2231cf7ba9cc7b968c85
JeremyWuuuuu <591449570@qq.com>
2020-12-23 21:23:22 +0800

    fix(project): fix/release-automation
    - Fix publish scripts

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,9 +3,6 @@
 # Do not run this file without setting the environment variables, you will end up fatal error
 # If you wish to run this locally, please change the env variable before running this.
 
-WORKING_PATH="../$(dirname $0)"
-cd ..
-
-cat package.json | grep -v '"private":' | grep -v '"version":' | sed "s/\(\"name\": \"@element-plus\/icons\"\)/\1,\n  \"version\": \"${TAG_VERSION}\"/g" > package.json
+cat package.json | grep -v '"private":' | grep -v '"version":' | sed "s/\(\"name\": \"element-plus\"\)/\1,\n  \"version\": \"${TAG_VERSION}\"/g" > package.json
 
 npm publish --registry ${REGISTRY} --access public
