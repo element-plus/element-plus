5dbfb9b5785637ac3b6a728f30d3742e7e2b116d
kooriookami <38392315+kooriookami@users.noreply.github.com>
2020-12-23 11:07:14 +0800

    fix(color-picker): fix ColorPicker drag (#1084)
    fix #1077

--- packages/color-picker/src/draggable.ts
+++ packages/color-picker/src/draggable.ts
@@ -1,5 +1,6 @@
 import isServer from '@element-plus/utils/isServer'
 import { on, off } from '@element-plus/utils/dom'
+
 let isDragging = false
 
 export declare interface IOptions {
@@ -8,31 +9,28 @@ export declare interface IOptions {
   end?: (event: Event) => void
 }
 
-const noopFunc = () => false
-
-export default function(element: HTMLElement, options: IOptions) {
+export default function (element: HTMLElement, options: IOptions) {
   if (isServer) return
 
-  const moveFn = function(event: Event) {
+  const moveFn = function (event: Event) {
     options.drag?.(event)
   }
 
-  const upFn = function(event: Event) {
-
-    off(document, 'mousemove',moveFn)
+  const upFn = function (event: Event) {
+    off(document, 'mousemove', moveFn)
     off(document, 'mouseup', upFn)
-    off(document, 'selectstart', noopFunc)
-    off(document, 'dragstart', noopFunc)
+    document.onselectstart = null
+    document.ondragstart = null
 
     isDragging = false
 
     options.end?.(event)
   }
 
-  on(element, 'mousedown', function(event) {
+  on(element, 'mousedown', function (event) {
     if (isDragging) return
-    on(document, 'selectstart', noopFunc)
-    on(document, 'dragstart', noopFunc)
+    document.onselectstart = () => false
+    document.ondragstart = () => false
     on(document, 'mousemove', moveFn)
     on(document, 'mouseup', upFn)
 
