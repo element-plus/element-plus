246aba3ee2891cbd506dec8f24b1360e7d97e513
JeremyWuuuuu <591449570@qq.com>
2020-12-23 16:16:49 +0800

    feat(project): feat/release-automation
    - Fix version generation issue

--- build/gen-version.js
+++ build/gen-version.js
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const fs = require('fs')
 const path = require('path')
-const { version } = require('../package.json')
+const tagVer = process.env.TAG_VERSION
+const version = tagVer.startsWith('v') ? tagVer.slice(1) : tagVer
 fs.writeFileSync(path.resolve(__dirname, '../packages/element-plus/version.ts'), `export const version = '${version}'
 `)
