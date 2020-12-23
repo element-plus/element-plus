97e8848617e0c177ff64bdab1263ed94c8618c11
jeremywu <591449570@qq.com>
2020-12-23 15:59:58 +0800

    build(project): feat/release-automation (#1094)
    - Add publish.sh for automated publishing

--- packages/space/index.ts
+++ packages/space/index.ts
@@ -1,8 +1,11 @@
 import { App } from 'vue'
 import Space from './src/index'
+import type { SFCWithInstall } from '@element-plus/utils/types'
 
 Space.install = (app: App): void => {
   app.component(Space.name, Space)
 }
 
-export default Space
+const _Space: SFCWithInstall<typeof Space> = Space as any
+
+export default _Space
