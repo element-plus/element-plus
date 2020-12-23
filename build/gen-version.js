473d1ed0fda24ca4c7f0516cda0b667769bed81b
JeremyWuuuuu <591449570@qq.com>
2020-12-23 16:37:09 +0800

    feat(project): release-automation
    - Fix env variable

--- build/gen-version.js
+++ build/gen-version.js
@@ -2,6 +2,13 @@
 const fs = require('fs')
 const path = require('path')
 const tagVer = process.env.TAG_VERSION
-const version = tagVer.startsWith('v') ? tagVer.slice(1) : tagVer
-fs.writeFileSync(path.resolve(__dirname, '../packages/element-plus/version.ts'), `export const version = '${version}'
-`)
+if (tagVer) {
+  version = tagVer.startsWith('v') ? tagVer.slice(1) : tagVer
+} else {
+  version = require('../package.json').version
+}
+fs.writeFileSync(
+  path.resolve(__dirname, '../packages/element-plus/version.ts'),
+  `export const version = '${version}'
+`,
+)
