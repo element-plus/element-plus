3cfd50a156a5be237526b6fa5d10a6497b6f8299
JeremyWuuuuu <591449570@qq.com>
2020-12-27 13:58:38 +0800

    build(core): core/bundling-opt
    - Modify publish scripts and github actions workflow

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,8 +2,12 @@
 
 # Do not run this file without setting the environment variables, you will end up fatal error
 # If you wish to run this locally, please change the env variable before running this.
-echo "//${REGISTRY}/:_authToken=${TOKEN}" > .npmrc
+# echo "//${REGISTRY}/:_authToken=${TOKEN}" > .npmrc
 
-cat package.json | grep -v '"private":' | grep -v '"version":' | sed "s/\(\"name\": \"element-plus\"\)/\1,\n  \"version\": \"${TAG_VERSION}\"/g" > package.json.bak
+cat package.json \
+| grep -v '"private":' \
+| grep -v '"version":' \
+| sed "s/\(\"name\": \"element-plus\"\)/\"name\": \"${PKG_NAME}\",\n  \"version\": \"${TAG_VERSION}\"/g" > package.json.bak
+
 cat package.json.bak > package.json
 npm publish --registry ${REGISTRY}
