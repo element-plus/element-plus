756c6556b4d23d070ed660b2921b6d12362e0a08
zazzaz <izazzaz@hotmail.com>
2021-01-02 18:16:21 +0800

    chore: update

--- website/play.js
+++ website/play.js
@@ -2,10 +2,8 @@ import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import App from './play/index.vue'
 import '../packages/theme-chalk/src/index.scss'
-import 'dayjs/locale/zh-cn'
 
-import locale from '../packages/locale/lang/zh-cn'
 const app = createApp(App)
-app.use(ElementPlus, { locale })
+app.use(ElementPlus)
 app.mount('#app')
 
