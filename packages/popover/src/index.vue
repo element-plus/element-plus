ce4f3078c07753c526a7fd02412cdf01cfab99cc
VijayAdam <765139628@qq.com>
2021-01-04 14:13:10 +0800

    fix(popover): clickoutside not working (#1171)
    * fix(cascader): fix: cascader-panel
    
    * fix click-outsie: change the capture phase
    
    * Revert "fix click-outsie: change the capture phase"
    
    This reverts commit 4f21ea4cb575693bddc7fd467e65f36d86822dc6.
    
    * 'fix(popover)'
    
    * Update index.vue
    
    Co-authored-by: vijay <vijayniubi@gmail.com>

--- packages/popover/src/index.vue
+++ packages/popover/src/index.vue
@@ -6,7 +6,6 @@ import { renderPopper, renderTrigger, renderArrow } from '@element-plus/popper'
 import { ClickOutside } from '@element-plus/directives'
 import { warn } from '@element-plus/utils/error'
 import { renderBlock, renderIf, PatchFlags } from '@element-plus/utils/vnode'
-import { stop } from '@element-plus/utils/dom'
 import usePopover, { SHOW_EVENT, HIDE_EVENT } from './usePopover'
 
 import type { PropType } from 'vue'
@@ -111,8 +110,6 @@ export default defineComponent({
     const _trigger = trigger ? renderTrigger(trigger, {
       ariaDescribedby: popperId,
       ref: 'triggerRef',
-      onMouseDown: stop,
-      onMouseUp: stop,
       ...events,
     }) : createCommentVNode('v-if', true)
 
