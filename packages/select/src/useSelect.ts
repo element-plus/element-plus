0d18c627a914cf0dc853a704bd254290c0ba19d2
Ryan2128 <33176053+Ryan2128@users.noreply.github.com>
2021-02-26 22:45:51 +0800

    fix(select): fix noMatchText error show

--- packages/select/src/useSelect.ts
+++ packages/select/src/useSelect.ts
@@ -191,6 +191,7 @@ export const useSelect = (props, states: States, ctx) => {
       popper.value?.update?.()
 
       if (props.filterable) {
+        states.filteredOptionsCount = states.optionsCount
         states.query = props.remote ? '' : states.selectedLabel
         if (props.multiple) {
           input.value.focus()
@@ -595,7 +596,6 @@ export const useSelect = (props, states: States, ctx) => {
       if (props.automaticDropdown || props.filterable) {
         states.visible = true
         if (props.filterable) {
-          states.filteredOptionsCount = states.optionsCount
           states.menuVisibleOnFocus = true
         }
       }
