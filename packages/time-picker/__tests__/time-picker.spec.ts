d7731e9f793235a5e4f54cc61930e1d560a00ffa
inottn <inottn@outlook.com>
2021-03-06 14:52:31 +0800

    fix(time-picker): replace `it.only` with `it` in test

--- packages/time-picker/__tests__/time-picker.spec.ts
+++ packages/time-picker/__tests__/time-picker.spec.ts
@@ -144,7 +144,7 @@ describe('TimePicker', () => {
     expect(secondsDom).toBeUndefined()
   })
 
-  it.only('event change, focus, blur', async () => {
+  it('event change, focus, blur', async () => {
     const changeHandler = jest.fn()
     const focusHandler = jest.fn()
     const blurHandler = jest.fn()
