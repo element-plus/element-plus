f62d1246327e9095b8b21c170f1b8764e9f9ea6d
Ryan <ryanzhao2128@gmail.com>
2021-03-22 19:12:30 +0800

    fix: fix tooltip's export

--- packages/tooltip/index.ts
+++ packages/tooltip/index.ts
@@ -1,8 +1,11 @@
 import { App } from 'vue'
 import Tooltip from './src/index'
+import { SFCWithInstall } from '@element-plus/utils/types'
 
 Tooltip.install = (app: App): void => {
   app.component(Tooltip.name, Tooltip)
 }
 
-export default Tooltip
+const _Tooltip: SFCWithInstall<typeof Tooltip> = Tooltip as SFCWithInstall<typeof Tooltip>
+
+export default _Tooltip
