dcd33207a59aa1ff8f9ec86c37e18be500f8253e
HakurouKen <ticshot@gmail.com>
2021-03-31 00:35:16 +0800

    test(time-select): add v-model test cases

--- packages/time-select/__tests__/time-select.spec.ts
+++ packages/time-select/__tests__/time-select.spec.ts
@@ -2,16 +2,20 @@ import { mount } from '@vue/test-utils'
 import { nextTick } from 'vue'
 import TimeSelect from '../src/time-select.vue'
 
-const _mount = (template: string, data, otherObj?) => mount({
-  components: {
-    'el-time-select': TimeSelect,
-  },
-  template,
-  data,
-  ...otherObj,
-},{
-  attachTo: 'body',
-})
+const _mount = (template: string, data, otherObj?) =>
+  mount(
+    {
+      components: {
+        'el-time-select': TimeSelect,
+      },
+      template,
+      data,
+      ...otherObj,
+    },
+    {
+      attachTo: 'body',
+    },
+  )
 
 afterEach(() => {
   document.documentElement.innerHTML = ''
@@ -19,20 +23,21 @@ afterEach(() => {
 
 describe('TimeSelect', () => {
   it('create', async () => {
-    const wrapper = _mount(`<el-time-select
-    :style="{color:'red'}"
-    class="customClass"
-  />`, () => ({
-      readonly: true }))
-    const outterInput = wrapper.find('.el-select')
-    expect(outterInput.classes()).toContain('customClass')
-    expect(outterInput.attributes().style).toBeDefined()
+    const wrapper = _mount(
+      `<el-time-select :style="{color:'red'}" class="customClass" />`,
+      () => ({
+        readonly: true,
+      }),
+    )
+    const outerInput = wrapper.find('.el-select')
+    expect(outerInput.classes()).toContain('customClass')
+    expect(outerInput.attributes().style).toBeDefined()
   })
 
   it('set default value', async () => {
-    const wrapper = _mount(`<el-time-select
-    v-model="value"
-  />`, () => ({ value: '14:30' }))
+    const wrapper = _mount(`<el-time-select v-model="value" />`, () => ({
+      value: '14:30',
+    }))
     const input = wrapper.find('input')
     input.trigger('blur')
     input.trigger('focus')
@@ -42,9 +47,7 @@ describe('TimeSelect', () => {
   })
 
   it('set minTime', async () => {
-    const wrapper = _mount(`<el-time-select
-    minTime='14:30'
-    />`, () => ({}))
+    const wrapper = _mount(`<el-time-select minTime='14:30' />`, () => ({}))
     const input = wrapper.find('input')
     input.trigger('blur')
     input.trigger('focus')
@@ -55,9 +58,7 @@ describe('TimeSelect', () => {
   })
 
   it('set maxTime', async () => {
-    const wrapper = _mount(`<el-time-select
-    maxTime='14:30'
-    />`, () => ({}))
+    const wrapper = _mount(`<el-time-select maxTime='14:30' />`, () => ({}))
     const input = wrapper.find('input')
     input.trigger('blur')
     input.trigger('focus')
@@ -65,5 +66,25 @@ describe('TimeSelect', () => {
     const elm = document.querySelector('.is-disabled')
     expect(elm.textContent).toBe('14:30')
   })
-})
 
+  it('set value update', async () => {
+    const wrapper = _mount(`<el-time-select v-model="value" />`, () => ({
+      value: '10:00',
+    }))
+    await nextTick()
+
+    const input = document.querySelector(
+      'input.el-input__inner',
+    ) as HTMLInputElement
+
+    expect(input).toBeDefined()
+    expect((input as HTMLInputElement).value).toBe('10:00')
+    // wrapper.setData is not supported until version 2.0.0-beta.8
+    // change value directly on `wrapper.vm`
+    const vm = wrapper.vm as any
+    vm.value = '10:30'
+    await nextTick()
+    expect(vm.value).toBe('10:30')
+    expect(input.value).toBe('10:30')
+  })
+})
