fe3807b4f7705aa53f60945612124bd6fb1596d1
神楽坂みずき <45122329+CokeMine@users.noreply.github.com>
2021-04-01 09:27:38 +0800

    refactor(card): remove needless div tag (#1732)
    fix #1717

--- packages/card/src/index.vue
+++ packages/card/src/index.vue
@@ -1,17 +1,16 @@
 <template>
-  <div>
-    <div class="el-card" :class="shadow ? 'is-' + shadow + '-shadow' : 'is-always-shadow'">
-      <div v-if="$slots.header || header" class="el-card__header">
-        <slot name="header">{{ header }}</slot>
-      </div>
-      <div class="el-card__body" :style="bodyStyle">
-        <slot></slot>
-      </div>
+  <div class="el-card" :class="shadow ? 'is-' + shadow + '-shadow' : 'is-always-shadow'">
+    <div v-if="$slots.header || header" class="el-card__header">
+      <slot name="header">{{ header }}</slot>
+    </div>
+    <div class="el-card__body" :style="bodyStyle">
+      <slot></slot>
     </div>
   </div>
 </template>
 <script lang='ts'>
 import { defineComponent } from 'vue'
+
 export default defineComponent({
   name: 'ElCard',
   props: {
