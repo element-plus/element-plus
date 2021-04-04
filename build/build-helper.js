72e7e5bd6aadae3990ed2f3dcb7bdc624eb711c7
tolking <qw13131wang@gmail.com>
2021-04-04 15:11:22 +0800

    chore: replace Accepted Values for icon

--- build/build-helper.js
+++ build/build-helper.js
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const helper = require('components-helper')
 const { name, version } = require('../package.json')
+const icon = require('../website/icon.json')
+const icons = icon.map(item => 'el-icon-' + item).join('/')
 const tagVer = process.env.TAG_VERSION
 const _version = tagVer ? tagVer.startsWith('v') ? tagVer.slice(1) : tagVer : version
 
@@ -29,25 +31,33 @@ function reDocUrl(fileName, header) {
   return docs + fileName + (_header ? '#' + _header : '')
 }
 
-function reAttribute(value, key) {
+function reAttribute(value, key, item) {
   const _value = value.match(/^\*\*(.*)\*\*$/)
   const str = _value ? _value[1]: value
 
-  if (str === '') {
+  if (key === 'Accepted Values' && /icon/i.test(item[0])) {
+    return icons
+  } else if (key === 'Name' && /^(-|—)$/.test(str)) {
+    return 'default'
+  } else if (str === '' || /^(-|—)$/.test(str)) {
     return undefined
-  } else if (/^(-|—)$/.test(str)) {
-    return key === 'Name' ? 'default' : undefined
-  } else if (/v-model:(.+)/.test(str)){
+  } else if (key === 'Attribute' && /v-model:(.+)/.test(str)){
     const _str = str.match(/v-model:(.+)/)
     return _str ? _str[1] : undefined
-  } else if (/v-model/.test(str)) {
+  } else if (key === 'Attribute' && /v-model/.test(str)) {
     return 'model-value'
   } else if (key === 'Attribute') {
     return str.replace(/\B([A-Z])/g, '-$1').toLowerCase()
   } else if (key === 'Type') {
-    return str.replace(/\s*\/\s*/g, '|').replace(/\(.*\)/g, '').toLowerCase()
+    return str
+      .replace(/\s*\/\s*/g, '|')
+      .replace(/\s*,\s*/g, '|')
+      .replace(/\(.*\)/g, '')
+      .toLowerCase()
   } else if (key === 'Accepted Values') {
-    return /\[.+\]\(.+\)/.test(str) || /^\*$/.test(str) ? undefined : str
+    return /\[.+\]\(.+\)/.test(str) || /^\*$/.test(str)
+      ? undefined
+      : str.replace(/`/g, '')
   } else {
     return str
   }
