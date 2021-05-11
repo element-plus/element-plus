a0ae8974ce869610ae4e25f8e14c2fe68e36666f
JeremyWuuuuu <591449570@qq.com>
2021-05-11 10:52:24 +0800

    fix(date-picker): fix/date-picker-popper-options-expose
    - Expose popper options API for date picker

--- packages/date-picker/src/date-picker.ts
+++ packages/date-picker/src/date-picker.ts
@@ -1,10 +1,11 @@
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref, provide, h } from 'vue'
+import dayjs from 'dayjs'
+
 import { DEFAULT_FORMATS_DATE, DEFAULT_FORMATS_DATEPICKER } from '@element-plus/time-picker'
 import { CommonPicker, defaultProps } from '@element-plus/time-picker'
 import DatePickPanel from './date-picker-com/panel-date-pick.vue'
 import DateRangePickPanel from './date-picker-com/panel-date-range.vue'
 import MonthRangePickPanel from './date-picker-com/panel-month-range.vue'
-import dayjs from 'dayjs'
 import customParseFormat from 'dayjs/plugin/customParseFormat'
 import advancedFormat from 'dayjs/plugin/advancedFormat'
 import localeData from 'dayjs/plugin/localeData'
@@ -13,7 +14,7 @@ import weekYear from 'dayjs/plugin/weekYear'
 import dayOfYear from 'dayjs/plugin/dayOfYear'
 import isSameOrAfter from 'dayjs/plugin/isSameOrAfter'
 import isSameOrBefore from 'dayjs/plugin/isSameOrBefore'
-import { h } from 'vue'
+
 dayjs.extend(localeData)
 dayjs.extend(advancedFormat)
 dayjs.extend(customParseFormat)
@@ -44,6 +45,7 @@ export default defineComponent({
   },
   emits: ['update:modelValue'],
   setup(props, ctx) {
+    provide('ElPopperOptions', props.popperOptions)
     const commonPicker = ref(null)
     const format = DEFAULT_FORMATS_DATEPICKER[props.type] || DEFAULT_FORMATS_DATE
     const refProps = {
