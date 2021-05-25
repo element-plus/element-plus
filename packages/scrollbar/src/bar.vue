4e60c3f470c60bd1dbd91e8ef652a54af661b32c
kooriookami <bingshuanglingluo@163.com>
2021-05-25 17:57:12 +0800

    perf(scrollbar): perf behavior of bar click
    re #2064

--- packages/scrollbar/src/bar.vue
+++ packages/scrollbar/src/bar.vue
@@ -46,6 +46,7 @@ export default defineComponent({
       if (e.ctrlKey || [1, 2].includes(e.button)) {
         return
       }
+      window.getSelection().removeAllRanges()
       startDrag(e)
       barStore.value[bar.value.axis] = (e.currentTarget[bar.value.offset] - (e[bar.value.client] - e.currentTarget.getBoundingClientRect()[bar.value.direction]))
     }
