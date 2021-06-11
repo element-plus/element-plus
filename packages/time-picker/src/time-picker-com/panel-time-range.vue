8206e34a66f6a9d685e3d7c1b7b14751f3391e93
jeremywu <15975785+JeremyWuuuuu@users.noreply.github.com>
2021-06-11 18:20:49 +0800

    fix(project): fix/upgrade-to-vue-3-1-1-and-with-bug-fix (#2220)
    - Upgrade Vue to 3.1.1 and fix the bug alone with the upgrade

--- packages/time-picker/src/time-picker-com/panel-time-range.vue
+++ packages/time-picker/src/time-picker-com/panel-time-range.vue
@@ -276,6 +276,7 @@ export default defineComponent({
     }
 
     const formatToString = value => {
+      console.log(value, props.format)
       if (!value) return null
       if (Array.isArray(value)) {
         return value.map(_=> _.format(props.format))
