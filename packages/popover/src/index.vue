45982cff8a65a57928e21598e2234b3ecdb41be0
jeremywu <15975785+JeremyWuuuuu@users.noreply.github.com>
2021-06-23 14:35:42 +0800

    refactor(popper): refactor extract common props for props (#2300)
    - Extract some common props out of prop
    
    Co-authored-by: jeremywuuuuu <jeremywuuuuu@no-reply.github.com>

--- packages/popover/src/index.vue
+++ packages/popover/src/index.vue
@@ -1,15 +1,17 @@
 <script lang="ts">
 import { defineComponent, Fragment, createTextVNode, renderSlot, toDisplayString, createCommentVNode, withDirectives, Teleport, h } from 'vue'
 import ElPopper from '@element-plus/popper'
-import { defaultProps, Effect } from '@element-plus/popper'
 import { renderPopper, renderTrigger, renderArrow } from '@element-plus/popper'
 import { ClickOutside } from '@element-plus/directives'
 import { warn } from '@element-plus/utils/error'
+import defaultProps from '@element-plus/internal/props/use-popper-props'
+import { LIGHT_EFFECT } from '@element-plus/constants/popper.constants'
 import { renderIf, PatchFlags } from '@element-plus/utils/vnode'
+
 import usePopover, { SHOW_EVENT, HIDE_EVENT } from './usePopover'
 
 import type { PropType } from 'vue'
-import type { TriggerType } from '@element-plus/popper'
+import type { TriggerType } from '@element-plus/internal/props/use-popper-props'
 
 const emits = ['update:visible', 'after-enter', 'after-leave', SHOW_EVENT, HIDE_EVENT]
 const NAME = 'ElPopover'
@@ -89,7 +91,7 @@ export default defineComponent({
     ].join(' ')
 
     let popover = renderPopper({
-      effect: Effect.LIGHT,
+      effect: LIGHT_EFFECT,
       name: transition,
       popperClass: kls,
       popperStyle: popperStyle,
--- packages/popper/index.ts
+++ packages/popper/index.ts
@@ -10,8 +10,6 @@ const _Popper: SFCWithInstall<typeof Popper> = Popper
 
 export default _Popper
 
-export { default as defaultProps, Effect } from './src/use-popper/defaults'
 export type { Placement, Options } from '@popperjs/core'
-export type { TriggerType, IPopperOptions, PopperInstance } from './src/use-popper/defaults'
 export { default as usePopper } from './src/use-popper/index'
 export * from './src/renderers/index'
