47e4a0284411a26fdd7add34b3c89870cfe6c1e9
Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2021-07-02 19:20:36 +0800

    fix: remove duplicate cell-contextmenu
    duplicate commits

--- packages/table/src/table.vue
+++ packages/table/src/table.vue
@@ -302,7 +302,6 @@ export default defineComponent({
     'current-change',
     'header-dragend',
     'expand-change',
-    'cell-contextmenu',
   ],
   setup(props) {
     type Row = typeof props.data[number]
