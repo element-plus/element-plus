92c911f071a0f6f593cc5c102801ff790412fca7
qige2016 <286882998@qq.com>
2021-07-05 13:58:40 +0800

    fix(avatar): fix fit prop validator

--- packages/avatar/src/index.vue
+++ packages/avatar/src/index.vue
@@ -46,6 +46,9 @@ export default defineComponent({
     srcSet: String,
     fit: {
       type: String,
+      validator(this: never, val: string) {
+        return ['fill', 'contain', 'cover', 'none', 'scale-down'].includes(val)
+      },
       default: 'cover',
     },
   },
