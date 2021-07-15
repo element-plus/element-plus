0e2a309236a33f9f9d39975a3b071429accb5d3e
jeremywu <15975785+JeremyWuuuuu@users.noreply.github.com>
2021-07-15 14:08:13 +0800

    feat(hooks): redesign popper apis (#2529)
    * feat(hooks): redesign popper apis
    
    - Redesign use-popper APIs to make it more adaptable.
    
    * - Reorganize the popper APIs

--- packages/hooks/use-popper/use-popper-options.ts
+++ packages/hooks/use-popper/use-popper-options.ts
@@ -1,108 +1,50 @@
-import { computed, getCurrentInstance } from 'vue'
 
-import type { Ref } from 'vue'
-import type {
-  Options,
-  Placement,
-  StrictModifiers,
-  PositioningStrategy,
-} from '@popperjs/core'
+import type { PropType } from 'vue'
+import type { Options } from '@popperjs/core'
 
-interface IUsePopperProps {
-  arrowOffset: number
-  fallbackPlacements: Array<Placement>
-  gpuAcceleration: boolean
-  offset: number
-  popperOptions: Options
-  placement: Placement
-  strategy: PositioningStrategy
-}
+export const DEFAULT_FALLBACK_PLACEMENTS = []
 
-export const usePopperOptions = (arrowRef: Ref<HTMLElement>) => {
-  const vm = getCurrentInstance()
+export const defaultModifiers = [
+  {
+    name: 'offset',
+    options: {
+      offset: [0, 12],
+    },
+  },
+  {
+    name: 'preventOverflow',
+    options: {
+      padding: {
+        top: 2,
+        bottom: 2,
+        left: 5,
+        right: 5,
+      },
+    },
+  },
+  {
+    name: 'flip',
+    options: {
+      padding: 5,
+      fallbackPlacements: [],
+    },
+  },
+  {
+    name: 'computeStyles',
+    options: {
+      gpuAcceleration: true,
+      adaptive: true,
+    },
+  },
+]
 
-  const props = vm.props as unknown as IUsePopperProps
-
-  return computed(() => {
+export const defaultPopperOptions = {
+  type: Object as PropType<Options>,
+  default: () => {
     return {
-      placement: props.placement,
-      strategy: props.strategy,
-      ...props.popperOptions,
-      // Avoiding overriding modifiers.
-      modifiers: buildModifiers({
-        arrow: arrowRef.value,
-        arrowOffset: props.arrowOffset,
-        offset: props.offset,
-        gpuAcceleration: props.gpuAcceleration,
-        fallbackPlacements: props.fallbackPlacements,
-      }, props.popperOptions?.modifiers),
+      fallbackPlacements: DEFAULT_FALLBACK_PLACEMENTS,
+      strategy: 'fixed',
+      modifiers: defaultModifiers,
     }
-  })
-}
-
-interface ModifierProps {
-  offset?: number
-  arrow?: HTMLElement
-  arrowOffset?: number
-  gpuAcceleration?: boolean
-  fallbackPlacements?: Array<Placement>
-}
-
-function buildModifiers(props: ModifierProps, externalModifiers: StrictModifiers[] = []) {
-
-  const {
-    arrow,
-    arrowOffset,
-    offset,
-    gpuAcceleration,
-    fallbackPlacements,
-  } = props
-
-  const modifiers: Array<StrictModifiers> = [
-    {
-      name: 'offset',
-      options: {
-        offset: [0, offset ?? 12],
-      },
-    },
-    {
-      name: 'preventOverflow',
-      options: {
-        padding: {
-          top: 2,
-          bottom: 2,
-          left: 5,
-          right: 5,
-        },
-      },
-    },
-    {
-      name: 'flip',
-      options: {
-        padding: 5,
-        fallbackPlacements: fallbackPlacements ?? [],
-      },
-    },
-    {
-      name: 'computeStyles',
-      options: {
-        gpuAcceleration,
-        adaptive: gpuAcceleration,
-      },
-    },
-    // tippyModifier,
-  ]
-
-  if (arrow) {
-    modifiers.push({
-      name: 'arrow',
-      options: {
-        element: arrow,
-        padding: arrowOffset ?? 5,
-      },
-    })
-  }
-
-  modifiers.push(...(externalModifiers))
-  return modifiers
+  },
 }
