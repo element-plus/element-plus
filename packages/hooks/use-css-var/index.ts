f478ad7a0666b50578e74c7df4b26c8f1577f73a
zouhang <zouhang@didiglobal.com>
2021-07-18 14:58:22 +0800

    perf: optimize usecssvar logic

--- packages/hooks/use-css-var/index.ts
+++ packages/hooks/use-css-var/index.ts
@@ -1,43 +1,105 @@
-import { watch, isRef } from 'vue'
-import type { Ref, WatchStopHandle } from 'vue'
+import {
+  watch,
+  isRef,
+  onMounted,
+  computed,
+  unref,
+  inject,
+  onUnmounted,
+  provide,
+  ref,
+} from 'vue'
+import type { Ref, WatchStopHandle, InjectionKey } from 'vue'
 
-let stopWatchCssVar: WatchStopHandle | null = null
+type VarsType = Ref<Record<string, string>> | Record<string, string>
 
+const VAR_PREFIX = '--el-'
+
+const setVars = (
+  target: HTMLElement,
+  val: Record<string, string>,
+) => {
+  Object.keys(val).forEach(key => {
+    if (key.startsWith(VAR_PREFIX)) {
+      target?.style.setProperty(key, val[key])
+    } else {
+      target?.style.setProperty(VAR_PREFIX + key, val[key])
+    }
+  })
+}
+
+export const themeVarsKey: InjectionKey<VarsType> = 'themeVars' as any
+
+/**
+ * @param vars
+ * @param target
+ * @returns stopWatchHandler
+ * @example
+ *
+ * If you pass a Ref vars param, it will generate a watch handler.
+ *
+ * In main.ts:
+ *
+ * ```ts
+ * const themeVars = {
+ *  '--el-color-primary': '#f44336',
+ *  '--el-color-white': '#2196f3',
+ * }
+ * app.provide(themeVarsKey, themeVars)
+ * ```
+ * Usually you need to use this function in the root component.
+ * In components' steup:
+ *
+ * ```ts
+ * setup() {
+ *  const themeVars = ref({
+ *    '--el-button-default-background-color': '#f44336',
+ *    '--el-button-default-font-color': '#2196f3',
+ *  })
+ *  useCssVar(themeVars)
+ * }
+ * ```
+ */
 export function useCssVar(
-  vars:
-    | Ref<Record<string, string>>
-    | Record<string, string>,
-  target?: HTMLElement,
+  vars: VarsType,
+  target?: Ref<HTMLElement> | HTMLElement,
 ) {
-  const elRef = target || window?.document?.documentElement
-  const VAR_PREFIX = '--el-'
+  let stopWatchCssVar: WatchStopHandle | null = null
 
-  const setVars = (val: Record<string, string>) => {
-    Object.keys(val).forEach(key => {
-      if (key.startsWith(VAR_PREFIX)) {
-        elRef.style.setProperty(key, val[key])
-      } else {
-        elRef.style.setProperty(VAR_PREFIX + key, val[key])
-      }
-    })
-  }
+  const elRef = computed(
+    () => unref(target) || window?.document?.documentElement,
+  )
 
-  if (stopWatchCssVar) {
-    stopWatchCssVar()
-    stopWatchCssVar = null
-  }
+  const themeVars = useThemeVars()
+  const customVars = { ...themeVars, ...unref(vars) }
 
-  isRef(vars)
-    ? (stopWatchCssVar = watch(
-      vars,
-      val => {
-        setVars(val)
-      },
-      {
-        immediate: true,
-      },
-    ))
-    : setVars(vars)
+  provide(themeVarsKey, ref(customVars))
 
-  return stopWatchCssVar
+  onMounted(() => {
+    isRef(vars)
+      ? (stopWatchCssVar = watch(
+        vars,
+        val => {
+          setVars(elRef.value, {
+            ...unref(themeVars),
+            ...val,
+          })
+        },
+        {
+          immediate: true,
+          deep: true,
+        },
+      ))
+      : setVars(elRef.value, {
+        ...unref(themeVars),
+        ...vars,
+      })
+  })
+
+  onUnmounted(() => stopWatchCssVar && stopWatchCssVar())
+}
+
+export const useThemeVars = () => {
+  const themeVars = inject(themeVarsKey, {})
+  return themeVars
 }
