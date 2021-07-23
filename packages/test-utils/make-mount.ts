e882c50114f1e59caa97bb1fafeb2b66ff56184b
msidolphin <msidolphin@outlook.com>
2021-07-23 10:56:13 +0800

    test(select-v2): add some test cases & fix selectLabel/multiple-limit issue

--- packages/test-utils/make-mount.ts
+++ packages/test-utils/make-mount.ts
@@ -5,4 +5,22 @@ const makeMount = <C,O, E>(element: C, defaultOptions: O) => {
   return (props: (E | O) | (E & O)= {} as E) => mount(element, merge({}, defaultOptions, props))
 }
 
+interface Options {
+  data?: () => {
+    [key: string]: any
+  }
+  methods?: {
+    [key: string]: (...args) => any
+  }
+}
+
+export const makeMountFunc = defaultOptions => {
+  return (template: string, options: Options) => {
+    return mount({
+      ...merge({}, defaultOptions, options),
+      template,
+    })
+  }
+}
+
 export default makeMount
