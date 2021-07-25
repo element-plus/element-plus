c14fa4ba55b76830e8fb023202e4b5cd4f3f0140
YunYouJun <me@yunyoujun.cn>
2021-07-25 15:59:39 +0800

    refactor(border): migrate css var & simplify docs

---
+++ website/components/demo/border/radius-box.vue
@@ -0,0 +1,33 @@
+<template>
+  <el-row :gutter="12" class="demo-radius">
+    <el-col
+      v-for="(radius, i) in radiusGroup"
+      :key="i"
+      :span="6"
+      :xs="{span: 12}"
+    >
+      <div class="title">{{ radius.name }}</div>
+      <div class="value"><code>border-radius: {{ getValue(radius.type) || '0px' }}</code></div>
+      <div class="radius" :style="{ borderRadius: radius.type ? `var(--el-border-radius-${radius.type})` : '' }"></div>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+import { getCssVarValue } from '../../../util'
+export default {
+  props: {
+    radiusGroup: {
+      type: Array,
+      default() {
+        return []
+      },
+    },
+  },
+  methods: {
+    getValue(type) {
+      return getCssVarValue('border-radius', type)
+    },
+  },
+}
+</script>
