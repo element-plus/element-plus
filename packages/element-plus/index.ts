c8d65bf43db0bd0d2e9c4e6e9a7829acebb69905
jeremywu <15975785+JeremyWuuuuu@users.noreply.github.com>
2021-07-25 15:26:00 +0800

    feat(locale): locale-provider (#2662)
    - Add a component for providing configs
    - Add `use-locale` hooks into hooks for composable usage
    - Add `use-locale` for `config-provider` to allow user to provide locale config via `react-like` API
    - Add test cases against these new features

--- packages/element-plus/index.ts
+++ packages/element-plus/index.ts
@@ -14,6 +14,7 @@ import type { InstallOptions } from '@element-plus/utils/config'
 
 export * from './components'
 export * from './plugins'
+export * from './composables'
 
 type DWindow = Window & typeof globalThis & {
   dayjs?: typeof dayjs
