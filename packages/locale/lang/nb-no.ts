c96b43e14bb1a2b76af570d9e3963dc83e7234f3
Adrian Andersen <adrianandersen@protonmail.com>
2021-08-03 03:12:51 +0200

    fix(locale): update Norwegian translation file (#2789)

--- packages/locale/lang/nb-no.ts
+++ packages/locale/lang/nb-no.ts
@@ -13,14 +13,14 @@ export default {
       confirm: 'OK',
       selectDate: 'Velg dato',
       selectTime: 'Velg tidspunkt',
-      startDate: 'Start Dato',
-      startTime: 'Start Tidspunkt',
+      startDate: 'Startdato',
+      startTime: 'Starttidspunkt',
       endDate: 'Sluttdato',
       endTime: 'Sluttidspunkt',
-      prevYear: 'Previous Year', // to be translated
-      nextYear: 'Next Year', // to be translated
-      prevMonth: 'Previous Month', // to be translated
-      nextMonth: 'Next Month', // to be translated
+      prevYear: 'I fjor',
+      nextYear: 'Neste år',
+      prevMonth: 'Forrige Måned',
+      nextMonth: 'Neste Måned',
       year: '',
       month1: 'Januar',
       month2: 'Februar',
@@ -34,7 +34,7 @@ export default {
       month10: 'Oktober',
       month11: 'November',
       month12: 'Desember',
-      // week: 'week',
+      week: 'uke',
       weeks: {
         sun: 'Søn',
         mon: 'Man',
@@ -61,15 +61,15 @@ export default {
     },
     select: {
       loading: 'Laster',
-      noMatch: 'Ingen samsvarende data',
-      noData: 'Ingen data',
+      noMatch: 'Ingen samsvarende resulater',
+      noData: 'Ingen resulater',
       placeholder: 'Velg',
     },
     cascader: {
-      noMatch: 'Ingen samsvarende data',
+      noMatch: 'Ingen samsvarende resultater',
       loading: 'Laster',
       placeholder: 'Velg',
-      noData: 'Ingen data',
+      noData: 'Ingen resultater',
     },
     pagination: {
       goto: 'Gå til',
@@ -83,7 +83,7 @@ export default {
       error: 'Ugyldig input',
     },
     upload: {
-      deleteTip: 'press delete to remove', // to be translated
+      deleteTip: 'trykk på x for å slette',
       delete: 'Slett',
       preview: 'Forhåndsvisning',
       continue: 'Fortsett',
@@ -93,7 +93,7 @@ export default {
       confirmFilter: 'Bekreft',
       resetFilter: 'Tilbakestill',
       clearFilter: 'Alle',
-      sumText: 'Sum', // to be translated
+      sumText: 'Sum',
     },
     tree: {
       emptyText: 'Ingen Data',
@@ -101,20 +101,20 @@ export default {
     transfer: {
       noMatch: 'Ingen samsvarende data',
       noData: 'Ingen data',
-      titles: ['List 1', 'List 2'], // to be translated
-      filterPlaceholder: 'Enter keyword', // to be translated
-      noCheckedFormat: '{total} items', // to be translated
-      hasCheckedFormat: '{checked}/{total} checked', // to be translated
+      titles: ['Liste 1', 'Liste 2'],
+      filterPlaceholder: 'Skriv inn nøkkelord',
+      noCheckedFormat: '{total} gjenstander',
+      hasCheckedFormat: '{checked}/{total} valgt',
     },
     image: {
-      error: 'FAILED', // to be translated
+      error: 'FEILET',
     },
     pageHeader: {
-      title: 'Back', // to be translated
+      title: 'Tilbake',
     },
     popconfirm: {
-      confirmButtonText: 'Yes', // to be translated
-      cancelButtonText: 'No', // to be translated
+      confirmButtonText: 'Ja',
+      cancelButtonText: 'Nei',
     },
   },
 }
