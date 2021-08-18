918ec49bcf1c4aebe729c7b1c9cc8f3bc3065e4f
JeremyWuuuuu <15975785+JeremyWuuuuu@users.noreply.github.com>
2021-08-18 13:58:58 +0800

    uncomment publish script

--- scripts/monorepo.sh
+++ scripts/monorepo.sh
@@ -23,19 +23,19 @@ yarn build:tokens
 yarn build:full-bundle
 yarn build:helper
 
-echo 'copy index.css'
+echo "copy index.css"
 cp dist/element-plus/theme-chalk/index.css dist/element-plus/dist/index.css
 
-echo 'syncing style.css'
+echo "syncing style.css"
 rsync -a dist/styles/es/ dist/element-plus/es/components/
 rsync -a dist/styles/lib/ dist/element-plus/lib/components/
 
 cp -R packages dist/element-plus
 cp packages/element-plus/package.json dist/element-plus/package.json
 
-exit 0
 
-# cd dist/element-plus
-# npm publish --access public
-# cd -
+cd dist/element-plus
+npm publish --access public
+cd -
 
+echo "Publish completed"
