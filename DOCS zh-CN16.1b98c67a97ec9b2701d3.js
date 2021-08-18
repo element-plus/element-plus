5403e76053faa17d8bb04625a9cf0c33a95c15b3
ElementPlusBot <hello@element-plus.org>
2021-08-18 03:31:46 +0000

    website deploy

---
+++ DOCS zh-CN16.1b98c67a97ec9b2701d3.js
@@ -0,0 +1,170 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[297],{
+
+/***/ 465:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXTERNAL MODULE: ./node_modules/vue/dist/vue.esm-browser.prod.js
+var vue_esm_browser_prod = __webpack_require__(0);
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib!./node_modules/vue-loader/dist/templateLoader.js??ref--6!./node_modules/vue-loader/dist??ref--2-0!./website/md-loader!./website/docs/zh-CN/config-provider.md?vue&type=template&id=306484ec
+
+var config_providervue_type_template_id_306484ec_hoisted_1 = {
+  class: "content element-doc"
+};
+
+var _hoisted_2 = /*#__PURE__*/Object(vue_esm_browser_prod["p" /* createVNode */])("h2", {
+  id: "config-provider"
+}, [/*#__PURE__*/Object(vue_esm_browser_prod["p" /* createVNode */])("a", {
+  class: "header-anchor",
+  href: "#config-provider"
+}, "¶"), /*#__PURE__*/Object(vue_esm_browser_prod["o" /* createTextVNode */])(" Config Provider")], -1);
+
+var _hoisted_3 = /*#__PURE__*/Object(vue_esm_browser_prod["p" /* createVNode */])("p", null, "Config Provider 被用来提供全局的配置选项，让你的配置能够在全局都能够被访问到。", -1);
+
+var _hoisted_4 = /*#__PURE__*/Object(vue_esm_browser_prod["p" /* createVNode */])("h3", {
+  id: "guo-ji-hua-xiang-guan-pei-zhi"
+}, [/*#__PURE__*/Object(vue_esm_browser_prod["p" /* createVNode */])("a", {
+  class: "header-anchor",
+  href: "#guo-ji-hua-xiang-guan-pei-zhi"
+}, "¶"), /*#__PURE__*/Object(vue_esm_browser_prod["o" /* createTextVNode */])(" 国际化相关配置")], -1);
+
+var _hoisted_5 = /*#__PURE__*/Object(vue_esm_browser_prod["p" /* createVNode */])("p", null, "通过 Config Provider 来配置国际化相关的服务，能让你的应用完成语言切换和获取。", -1);
+
+var _hoisted_6 = /*#__PURE__*/Object(vue_esm_browser_prod["p" /* createVNode */])("div", null, [/*#__PURE__*/Object(vue_esm_browser_prod["p" /* createVNode */])("p", null, "通过两个属性来提供国际化相关的语言配置")], -1);
+
+var _hoisted_7 = /*#__PURE__*/Object(vue_esm_browser_prod["p" /* createVNode */])("pre", null, [/*#__PURE__*/Object(vue_esm_browser_prod["p" /* createVNode */])("code", {
+  class: "html"
+}, "<div>\n  <el-config-provider :locale=\"locale1\">\n    <el-color-picker :modelValue=\"''\" style=\"vertical-align: middle;\" />\n  </el-config-provider>\n  <el-button @click=\"toggle\" style=\"margin-left: 8px; vertical-align: middle;\">\n    切换语言\n  </el-button>\n</div>\n\n<script>\n  // import { ConfigProvider } from 'element-plus'\n  export default {\n    data() {\n      return {\n        locale1: {\n          name: 'zh-cn',\n          el: {\n            colorpicker: {\n              confirm: '确定',\n              clear: '清空',\n            },\n          },\n        },\n        locale2: {\n          name: 'en',\n          el: {\n            colorpicker: {\n              confirm: 'Confirm',\n              clear: 'Clear',\n            },\n          },\n        },\n      }\n    },\n    methods: {\n      toggle() {\n        const temp = this.locale1\n        this.locale1 = this.locale2\n        this.locale2 = temp\n      },\n    },\n  }\n</script>\n")], -1);
+
+var _hoisted_8 = /*#__PURE__*/Object(vue_esm_browser_prod["n" /* createStaticVNode */])("<h3 id=\"configprovider-shu-xing\"><a class=\"header-anchor\" href=\"#configprovider-shu-xing\">¶</a> ConfigProvider 属性</h3><table><thead><tr><th>参数</th><th>说明</th><th>类型</th><th>可选值</th><th>默认值</th></tr></thead><tbody><tr><td>locale</td><td>翻译文本对象</td><td>Object&lt;Language&gt;</td><td><a href=\"https://github.com/element-plus/element-plus/tree/dev/packages/locale/lang\">languages</a></td><td>English</td></tr><tr><td>i18n</td><td>外部的翻译方法，当该方法被提供时，会优先使用该方法进行翻译的操作，若返回空值便会落回到自带翻译方法</td><td>Function&lt;(...args: []) =&gt; string&gt;</td><td>-</td><td>-</td></tr></tbody></table>", 2);
+
+function config_providervue_type_template_id_306484ec_render(_ctx, _cache, $props, $setup, $data, $options) {
+  var _component_element_demo0 = Object(vue_esm_browser_prod["Q" /* resolveComponent */])("element-demo0");
+
+  var _component_demo_block = Object(vue_esm_browser_prod["Q" /* resolveComponent */])("demo-block");
+
+  var _component_right_nav = Object(vue_esm_browser_prod["Q" /* resolveComponent */])("right-nav");
+
+  return Object(vue_esm_browser_prod["H" /* openBlock */])(), Object(vue_esm_browser_prod["k" /* createBlock */])("section", config_providervue_type_template_id_306484ec_hoisted_1, [_hoisted_2, _hoisted_3, _hoisted_4, _hoisted_5, Object(vue_esm_browser_prod["p" /* createVNode */])(_component_demo_block, null, {
+    source: Object(vue_esm_browser_prod["fb" /* withCtx */])(function () {
+      return [Object(vue_esm_browser_prod["p" /* createVNode */])(_component_element_demo0)];
+    }),
+    highlight: Object(vue_esm_browser_prod["fb" /* withCtx */])(function () {
+      return [_hoisted_7];
+    }),
+    default: Object(vue_esm_browser_prod["fb" /* withCtx */])(function () {
+      return [_hoisted_6];
+    }),
+    _: 1
+  }), _hoisted_8, Object(vue_esm_browser_prod["p" /* createVNode */])(_component_right_nav)]);
+}
+// CONCATENATED MODULE: ./website/docs/zh-CN/config-provider.md?vue&type=template&id=306484ec
+
+// EXTERNAL MODULE: ./node_modules/@babel/runtime/helpers/extends.js
+var helpers_extends = __webpack_require__(4);
+var extends_default = /*#__PURE__*/__webpack_require__.n(helpers_extends);
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib!./node_modules/vue-loader/dist??ref--2-0!./website/md-loader!./website/docs/zh-CN/config-provider.md?vue&type=script&lang=ts
+
+
+/* harmony default export */ var config_providervue_type_script_lang_ts = ({
+  name: 'component-doc',
+  components: {
+    "element-demo0": function () {
+      var _resolveComponent = vue_esm_browser_prod["Q" /* resolveComponent */],
+          _createVNode = vue_esm_browser_prod["p" /* createVNode */],
+          _withCtx = vue_esm_browser_prod["fb" /* withCtx */],
+          _createTextVNode = vue_esm_browser_prod["o" /* createTextVNode */],
+          _openBlock = vue_esm_browser_prod["H" /* openBlock */],
+          _createBlock = vue_esm_browser_prod["k" /* createBlock */];
+
+      var _hoisted_1 = /*#__PURE__*/_createTextVNode(" 切换语言 ");
+
+      function render(_ctx, _cache) {
+        var _component_el_color_picker = _resolveComponent("el-color-picker");
+
+        var _component_el_config_provider = _resolveComponent("el-config-provider");
+
+        var _component_el_button = _resolveComponent("el-button");
+
+        return _openBlock(), _createBlock("div", null, [_createVNode("div", null, [_createVNode(_component_el_config_provider, {
+          locale: _ctx.locale1
+        }, {
+          default: _withCtx(function () {
+            return [_createVNode(_component_el_color_picker, {
+              modelValue: '',
+              style: {
+                "vertical-align": "middle"
+              }
+            })];
+          }),
+          _: 1
+        }, 8, ["locale"]), _createVNode(_component_el_button, {
+          onClick: _ctx.toggle,
+          style: {
+            "margin-left": "8px",
+            "vertical-align": "middle"
+          }
+        }, {
+          default: _withCtx(function () {
+            return [_hoisted_1];
+          }),
+          _: 1
+        }, 8, ["onClick"])])]);
+      } // const { ConfigProvider } = require('element-plus')
+
+
+      var democomponentExport = {
+        data: function data() {
+          return {
+            locale1: {
+              name: 'zh-cn',
+              el: {
+                colorpicker: {
+                  confirm: '确定',
+                  clear: '清空'
+                }
+              }
+            },
+            locale2: {
+              name: 'en',
+              el: {
+                colorpicker: {
+                  confirm: 'Confirm',
+                  clear: 'Clear'
+                }
+              }
+            }
+          };
+        },
+        methods: {
+          toggle: function toggle() {
+            var temp = this.locale1;
+            this.locale1 = this.locale2;
+            this.locale2 = temp;
+          }
+        }
+      };
+      return extends_default()({
+        render: render
+      }, democomponentExport);
+    }()
+  }
+});
+// CONCATENATED MODULE: ./website/docs/zh-CN/config-provider.md?vue&type=script&lang=ts
+ 
+// CONCATENATED MODULE: ./website/docs/zh-CN/config-provider.md
+
+
+
+config_providervue_type_script_lang_ts.render = config_providervue_type_template_id_306484ec_render
+
+/* harmony default export */ var config_provider = __webpack_exports__["default"] = (config_providervue_type_script_lang_ts);
+
+/***/ })
+
+}]);
---
+++ DOCS zh-CN5.1b98c67a97ec9b2701d3.js
@@ -0,0 +1,240 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[334],{
+
+/***/ 454:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXTERNAL MODULE: ./node_modules/vue/dist/vue.esm-browser.prod.js
+var vue_esm_browser_prod = __webpack_require__(0);
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib!./node_modules/vue-loader/dist/templateLoader.js??ref--6!./node_modules/vue-loader/dist??ref--2-0!./website/md-loader!./website/docs/zh-CN/border.md?vue&type=template&id=08e0fa0c
+
+var _hoisted_1 = {
+  class: "content element-doc"
+};
+
+var _hoisted_2 = /*#__PURE__*/Object(vue_esm_browser_prod["n" /* createStaticVNode */])("<h2 id=\"border-bian-kuang\"><a class=\"header-anchor\" href=\"#border-bian-kuang\">¶</a> Border 边框</h2><p>我们对边框进行统一规范，可用于按钮、卡片、弹窗等组件里。</p><h3 id=\"bian-kuang\"><a class=\"header-anchor\" href=\"#bian-kuang\">¶</a> 边框</h3><p>我们提供了以下几种边框样式，以供选择。</p><table class=\"demo-border\"><tbody><tr><td class=\"text\">名称</td><td class=\"text\">粗细</td><td class=\"line\">举例</td></tr><tr><td class=\"text\">\b实线</td><td class=\"text\">1px</td><td class=\"line\"><div></div></td></tr><tr><td class=\"text\">\b虚线</td><td class=\"text\">2px</td><td class=\"line\"><div class=\"dashed\"></div></td></tr></tbody></table><h3 id=\"yuan-jiao\"><a class=\"header-anchor\" href=\"#yuan-jiao\">¶</a> 圆角</h3><p>我们提供了以下几种圆角样式，以供选择。</p>", 7);
+
+var _hoisted_9 = /*#__PURE__*/Object(vue_esm_browser_prod["p" /* createVNode */])("h3", {
+  id: "tou-ying"
+}, [/*#__PURE__*/Object(vue_esm_browser_prod["p" /* createVNode */])("a", {
+  class: "header-anchor",
+  href: "#tou-ying"
+}, "¶"), /*#__PURE__*/Object(vue_esm_browser_prod["o" /* createTextVNode */])(" 投影")], -1);
+
+var _hoisted_10 = /*#__PURE__*/Object(vue_esm_browser_prod["p" /* createVNode */])("p", null, "我们提供了以下几种投影样式，以供选择。", -1);
+
+function render(_ctx, _cache, $props, $setup, $data, $options) {
+  var _component_radius_box = Object(vue_esm_browser_prod["Q" /* resolveComponent */])("radius-box");
+
+  var _component_shadow_box = Object(vue_esm_browser_prod["Q" /* resolveComponent */])("shadow-box");
+
+  var _component_right_nav = Object(vue_esm_browser_prod["Q" /* resolveComponent */])("right-nav");
+
+  return Object(vue_esm_browser_prod["H" /* openBlock */])(), Object(vue_esm_browser_prod["k" /* createBlock */])("section", _hoisted_1, [_hoisted_2, Object(vue_esm_browser_prod["p" /* createVNode */])(_component_radius_box, {
+    "radius-group": $data.radiusGroup
+  }, null, 8, ["radius-group"]), _hoisted_9, _hoisted_10, Object(vue_esm_browser_prod["p" /* createVNode */])(_component_shadow_box, {
+    "shadow-group": $data.shadowGroup
+  }, null, 8, ["shadow-group"]), Object(vue_esm_browser_prod["p" /* createVNode */])(_component_right_nav)]);
+}
+// CONCATENATED MODULE: ./website/docs/zh-CN/border.md?vue&type=template&id=08e0fa0c
+
+// EXTERNAL MODULE: ./website/components/demo/border/radius-box.vue + 4 modules
+var radius_box = __webpack_require__(819);
+
+// EXTERNAL MODULE: ./website/components/demo/border/shadow-box.vue + 4 modules
+var shadow_box = __webpack_require__(820);
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib!./node_modules/vue-loader/dist??ref--2-0!./website/md-loader!./website/docs/zh-CN/border.md?vue&type=script&lang=js
+
+
+var radiusGroup = [{
+  name: '无圆角',
+  type: ''
+}, {
+  name: '小圆角',
+  type: 'small'
+}, {
+  name: '大圆角',
+  type: 'base'
+}, {
+  name: '圆形圆角',
+  type: 'round'
+}];
+var shadowGroup = [{
+  name: '基础投影',
+  type: 'base'
+}, {
+  name: '浅色投影',
+  type: 'light'
+}];
+/* harmony default export */ var bordervue_type_script_lang_js = ({
+  components: {
+    RadiusBox: radius_box["a" /* default */],
+    ShadowBox: shadow_box["a" /* default */]
+  },
+  data: function data() {
+    return {
+      radiusGroup: radiusGroup,
+      shadowGroup: shadowGroup
+    };
+  }
+});
+// CONCATENATED MODULE: ./website/docs/zh-CN/border.md?vue&type=script&lang=js
+ 
+// CONCATENATED MODULE: ./website/docs/zh-CN/border.md
+
+
+
+bordervue_type_script_lang_js.render = render
+
+/* harmony default export */ var border = __webpack_exports__["default"] = (bordervue_type_script_lang_js);
+
+/***/ }),
+
+/***/ 819:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+
+// EXTERNAL MODULE: ./node_modules/vue/dist/vue.esm-browser.prod.js
+var vue_esm_browser_prod = __webpack_require__(0);
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib!./node_modules/vue-loader/dist/templateLoader.js??ref--6!./node_modules/vue-loader/dist??ref--11-0!./website/components/demo/border/radius-box.vue?vue&type=template&id=e81661e0
+
+var _hoisted_1 = {
+  class: "title"
+};
+var _hoisted_2 = {
+  class: "value"
+};
+function render(_ctx, _cache, $props, $setup, $data, $options) {
+  var _component_el_col = Object(vue_esm_browser_prod["Q" /* resolveComponent */])("el-col");
+
+  var _component_el_row = Object(vue_esm_browser_prod["Q" /* resolveComponent */])("el-row");
+
+  return Object(vue_esm_browser_prod["H" /* openBlock */])(), Object(vue_esm_browser_prod["k" /* createBlock */])(_component_el_row, {
+    gutter: 12,
+    class: "demo-radius"
+  }, {
+    default: Object(vue_esm_browser_prod["fb" /* withCtx */])(function () {
+      return [(Object(vue_esm_browser_prod["H" /* openBlock */])(true), Object(vue_esm_browser_prod["k" /* createBlock */])(vue_esm_browser_prod["b" /* Fragment */], null, Object(vue_esm_browser_prod["O" /* renderList */])($props.radiusGroup, function (radius, i) {
+        return Object(vue_esm_browser_prod["H" /* openBlock */])(), Object(vue_esm_browser_prod["k" /* createBlock */])(_component_el_col, {
+          key: i,
+          span: 6,
+          xs: {
+            span: 12
+          }
+        }, {
+          default: Object(vue_esm_browser_prod["fb" /* withCtx */])(function () {
+            return [Object(vue_esm_browser_prod["p" /* createVNode */])("div", _hoisted_1, Object(vue_esm_browser_prod["U" /* toDisplayString */])(radius.name), 1), Object(vue_esm_browser_prod["p" /* createVNode */])("div", _hoisted_2, [Object(vue_esm_browser_prod["p" /* createVNode */])("code", null, "border-radius: " + Object(vue_esm_browser_prod["U" /* toDisplayString */])($options.getValue(radius.type) || '0px'), 1)]), Object(vue_esm_browser_prod["p" /* createVNode */])("div", {
+              class: "radius",
+              style: {
+                borderRadius: radius.type ? "var(--el-border-radius-" + radius.type + ")" : ''
+              }
+            }, null, 4)];
+          }),
+          _: 2
+        }, 1024);
+      }), 128))];
+    }),
+    _: 1
+  });
+}
+// CONCATENATED MODULE: ./website/components/demo/border/radius-box.vue?vue&type=template&id=e81661e0
+
+// EXTERNAL MODULE: ./website/util.js
+var util = __webpack_require__(25);
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib!./node_modules/vue-loader/dist??ref--11-0!./website/components/demo/border/radius-box.vue?vue&type=script&lang=js
+
+/* harmony default export */ var radius_boxvue_type_script_lang_js = ({
+  props: {
+    radiusGroup: {
+      type: Array,
+      default: function _default() {
+        return [];
+      }
+    }
+  },
+  methods: {
+    getValue: function getValue(type) {
+      return Object(util["c" /* getCssVarValue */])('border-radius', type);
+    }
+  }
+});
+// CONCATENATED MODULE: ./website/components/demo/border/radius-box.vue?vue&type=script&lang=js
+ 
+// CONCATENATED MODULE: ./website/components/demo/border/radius-box.vue
+
+
+
+radius_boxvue_type_script_lang_js.render = render
+
+/* harmony default export */ var radius_box = __webpack_exports__["a"] = (radius_boxvue_type_script_lang_js);
+
+/***/ }),
+
+/***/ 820:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+
+// EXTERNAL MODULE: ./node_modules/vue/dist/vue.esm-browser.prod.js
+var vue_esm_browser_prod = __webpack_require__(0);
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib!./node_modules/vue-loader/dist/templateLoader.js??ref--6!./node_modules/vue-loader/dist??ref--11-0!./website/components/demo/border/shadow-box.vue?vue&type=template&id=20d741bc
+
+var _hoisted_1 = {
+  class: "demo-shadow-text"
+};
+function render(_ctx, _cache, $props, $setup, $data, $options) {
+  return Object(vue_esm_browser_prod["H" /* openBlock */])(), Object(vue_esm_browser_prod["k" /* createBlock */])("div", null, [(Object(vue_esm_browser_prod["H" /* openBlock */])(true), Object(vue_esm_browser_prod["k" /* createBlock */])(vue_esm_browser_prod["b" /* Fragment */], null, Object(vue_esm_browser_prod["O" /* renderList */])($props.shadowGroup, function (shadow, i) {
+    return Object(vue_esm_browser_prod["H" /* openBlock */])(), Object(vue_esm_browser_prod["k" /* createBlock */])(vue_esm_browser_prod["b" /* Fragment */], {
+      key: i
+    }, [Object(vue_esm_browser_prod["p" /* createVNode */])("div", {
+      class: "demo-shadow",
+      style: {
+        boxShadow: "var(--el-box-shadow-" + shadow.type + ")"
+      }
+    }, null, 4), Object(vue_esm_browser_prod["p" /* createVNode */])("span", _hoisted_1, [Object(vue_esm_browser_prod["o" /* createTextVNode */])(Object(vue_esm_browser_prod["U" /* toDisplayString */])(shadow.name) + " ", 1), Object(vue_esm_browser_prod["p" /* createVNode */])("code", null, "box-shadow: " + Object(vue_esm_browser_prod["U" /* toDisplayString */])($options.getValue(shadow.type)), 1)])], 64);
+  }), 128))]);
+}
+// CONCATENATED MODULE: ./website/components/demo/border/shadow-box.vue?vue&type=template&id=20d741bc
+
+// EXTERNAL MODULE: ./website/util.js
+var util = __webpack_require__(25);
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib!./node_modules/vue-loader/dist??ref--11-0!./website/components/demo/border/shadow-box.vue?vue&type=script&lang=js
+
+/* harmony default export */ var shadow_boxvue_type_script_lang_js = ({
+  props: {
+    shadowGroup: {
+      type: Array,
+      default: function _default() {
+        return [];
+      }
+    }
+  },
+  methods: {
+    getValue: function getValue(type) {
+      return Object(util["c" /* getCssVarValue */])('box-shadow', type);
+    }
+  }
+});
+// CONCATENATED MODULE: ./website/components/demo/border/shadow-box.vue?vue&type=script&lang=js
+ 
+// CONCATENATED MODULE: ./website/components/demo/border/shadow-box.vue
+
+
+
+shadow_boxvue_type_script_lang_js.render = render
+
+/* harmony default export */ var shadow_box = __webpack_exports__["a"] = (shadow_boxvue_type_script_lang_js);
+
+/***/ })
+
+}]);
