a9a1c890cad513e2b254f9da15801c33c55e1a3c
imswk <syfzxx@vip.qq.com>
2021-09-02 00:10:39 +0800

    fix(components): tabs dynamic text (#3174)

--- packages/components/tabs/src/tabs.ts
+++ packages/components/tabs/src/tabs.ts
@@ -78,6 +78,13 @@ export default defineComponent({
       paneStatesMap[pane.uid] = pane
     })
 
+    provide<UpdatePaneStateCallback>('updatePanes', (pane: Pane) => {
+      panes.value = panes.value.map((val: Pane)=>{
+        if(val.uid === pane.uid) val = pane
+        return val
+      })
+    })
+
     watch(
       () => props.activeName,
       modelValue => {
