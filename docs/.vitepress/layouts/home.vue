32ba735e013aced73987be569151d23c26005841
JeremyWuuuuu <15975785+JeremyWuuuuu@users.noreply.github.com>
2021-09-06 10:18:36 +0800

    fix homepage layout issue

---
+++ docs/.vitepress/layouts/home.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import HomeFooter from './footer.vue'
+</script>
+
+<template>
+  <div class="home-wrapper">
+    <Content />
+    <HomeFooter />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.home-wrapper {
+  padding-top: var(--header-height);
+}
+</style>
