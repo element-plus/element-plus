f77ccdd0243dbc662cd88b19b9fb6677ce1567ae
Kevin <sxzz@sxzz.moe>
2021-09-06 12:35:51 +0800

    refactor(components): refactor tooltip

--- packages/components/tooltip/src/index.ts
+++ packages/components/tooltip/src/index.ts
@@ -1,11 +1,40 @@
 import { defineComponent, h, ref, cloneVNode } from 'vue'
 import {
   default as ElPopper,
-  popperDefaultProps,
+  popperProps,
 } from '@element-plus/components/popper'
 import { UPDATE_MODEL_EVENT } from '@element-plus/utils/constants'
 import { throwError } from '@element-plus/utils/error'
 import { getFirstValidNode } from '@element-plus/utils/vnode'
+import { buildProp } from '@element-plus/utils/props'
+
+import type { ExtractPropTypes } from 'vue'
+
+const tooltipProps = {
+  ...popperProps,
+  manual: {
+    type: Boolean,
+    default: false,
+  },
+  modelValue: buildProp<boolean | undefined>({
+    type: Boolean,
+    default: undefined,
+  }),
+  // This API should be decaprecate since it's confusing with close-delay
+  openDelay: {
+    type: Number,
+    default: 0,
+  },
+  visibleArrow: {
+    type: Boolean,
+    default: true,
+  },
+  tabindex: buildProp<string | number>({
+    type: [String, Number],
+    default: '0',
+  }),
+} as const
+export type TooltipProps = ExtractPropTypes<typeof tooltipProps>
 
 /**
  * ElTooltip
@@ -15,105 +44,71 @@ import { getFirstValidNode } from '@element-plus/utils/vnode'
  */
 export default defineComponent({
   name: 'ElTooltip',
-  components: {
-    ElPopper,
-  },
-  props: {
-    ...popperDefaultProps,
-    manual: {
-      type: Boolean,
-      default: false,
-    },
-    modelValue: {
-      type: Boolean,
-      validator: (val: unknown) => {
-        return typeof val === 'boolean'
-      },
-      default: undefined,
-    },
-    // This API should be decaprecate since it's confusing with close-delay
-    openDelay: {
-      type: Number,
-      default: 0,
-    },
-    visibleArrow: {
-      type: Boolean,
-      default: true,
-    },
-    tabindex: {
-      type: [String, Number],
-      default: '0',
-    },
-  },
+
+  props: tooltipProps,
   emits: [UPDATE_MODEL_EVENT],
-  setup(props, ctx) {
+
+  setup(props, { emit, slots, expose }) {
     // when manual mode is true, v-model must be passed down
-    if (props.manual && typeof props.modelValue === 'undefined') {
+    if (props.manual && typeof props.modelValue === undefined) {
       throwError(
         '[ElTooltip]',
         'You need to pass a v-model to el-tooltip when `manual` is true'
       )
     }
 
-    const popper = ref(null)
+    const popper = ref()
 
-    const onUpdateVisible = (val) => {
-      ctx.emit(UPDATE_MODEL_EVENT, val)
+    const onUpdateVisible = (val: boolean) => {
+      emit(UPDATE_MODEL_EVENT, val)
     }
 
-    const updatePopper = () => {
-      return popper.value.update()
-    }
+    const updatePopper = () => popper.value.update()
 
-    return {
-      popper,
-      onUpdateVisible,
+    expose({
       updatePopper,
-    }
-  },
-  render() {
-    const {
-      $slots,
-      content,
-      manual,
-      openDelay,
-      onUpdateVisible,
-      showAfter,
-      visibleArrow,
-      modelValue,
-      tabindex,
-    } = this
+    })
 
-    const throwErrorTip = () => {
-      throwError('[ElTooltip]', 'you need to provide a valid default slot.')
-    }
+    return () => {
+      const {
+        content,
+        manual,
+        openDelay,
+        showAfter,
+        visibleArrow,
+        modelValue,
+        tabindex,
+      } = props
 
-    const popper = h(
-      ElPopper,
-      {
-        ...Object.keys(popperDefaultProps).reduce((result, key) => {
-          return { ...result, [key]: this[key] }
-        }, {}),
-        ref: 'popper',
-        manualMode: manual,
-        showAfter: openDelay || showAfter, // this is for mapping API due to we decided to rename the current openDelay API to showAfter for better readability,
-        showArrow: visibleArrow,
-        visible: modelValue,
-        'onUpdate:visible': onUpdateVisible,
-      },
-      {
-        default: () => ($slots.content ? $slots.content() : content),
-        trigger: () => {
-          if ($slots.default) {
-            const firstVnode = getFirstValidNode($slots.default(), 1)
-            if (!firstVnode) throwErrorTip()
-            return cloneVNode(firstVnode, { tabindex }, true)
-          }
-          throwErrorTip()
-        },
+      function throwErrorTip(): never {
+        throwError('[ElTooltip]', 'you need to provide a valid default slot.')
       }
-    )
 
-    return popper
+      return h(
+        ElPopper,
+        {
+          ref: popper,
+          ...Object.fromEntries(
+            Object.entries(popperProps).map(([key]) => [key, props[key]])
+          ),
+          manualMode: manual,
+          showAfter: openDelay || showAfter, // this is for mapping API due to we decided to rename the current openDelay API to showAfter for better readability,
+          showArrow: visibleArrow,
+          visible: modelValue,
+          'onUpdate:visible': onUpdateVisible,
+        },
+        {
+          default: () => slots.content?.() ?? content,
+          trigger: () => {
+            if (slots.default) {
+              const firstVnode = getFirstValidNode(slots.default(), 1)
+              if (!firstVnode) throwErrorTip()
+              return cloneVNode(firstVnode, { tabindex }, true)
+            }
+            throwErrorTip()
+          },
+        }
+      )
+    }
   },
 })
