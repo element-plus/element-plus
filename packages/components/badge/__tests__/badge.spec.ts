03f24937b2d55fc2c35420eec7f251a8b605d2f7
Aex <spryti@qq.com>
2021-09-07 08:22:23 +0000

    feat(components): [el-badge] apply color of type when isDot

--- packages/components/badge/__tests__/badge.spec.ts
+++ packages/components/badge/__tests__/badge.spec.ts
@@ -26,6 +26,17 @@ describe('Badge', () => {
     expect(wrapper.find('.el-badge__content.is-dot').exists()).toBe(true)
   })
 
+  test('is dot with type', () => {
+    const wrapper = mount(Badge, {
+      props: { isDot: true, type: 'success' },
+      slots: { default: AXIOM },
+    })
+    expect(wrapper.find('.el-badge__content.is-dot').exists()).toBe(true)
+    expect(
+      wrapper.find('.el-badge__content.el-badge__content--success').exists()
+    ).toBe(true)
+  })
+
   test('max', async () => {
     const wrapper = mount(Badge, {
       props: { max: 100, value: 200 },
