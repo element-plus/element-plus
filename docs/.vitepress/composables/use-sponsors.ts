aea30ea8bd861d2dc5528a8918320755dee7b2fd
JeremyWuuuuu <15975785+JeremyWuuuuu@users.noreply.github.com>
2021-09-08 00:02:21 +0800

    reorganized files and fix issue that causes demo unavailable after build

--- docs/.vitepress/composables/use-sponsors.ts
+++
@@ -1,9 +0,0 @@
-import { computed } from 'vue'
-import { useData } from 'vitepress'
-
-export const useSponsors = () => {
-  const { site } = useData()
-  return computed(() => {
-    return site.value.themeConfig.sponsors
-  })
-}
