950926c80952c7d00d840eb8801d7d8922fecee8
0song <0song@gmail.com>
2021-09-08 21:11:45 +0800

    chore: update doc

--- docs/examples/alert/basic.vue
+++ docs/examples/alert/basic.vue
@@ -1,6 +1,6 @@
 <template>
-  <el-alert title="success alert" type="success"> </el-alert>
-  <el-alert title="info alert" type="info"> </el-alert>
-  <el-alert title="warning alert" type="warning"> </el-alert>
-  <el-alert title="error alert" type="error"> </el-alert>
+  <el-alert title="success alert" type="success" />
+  <el-alert title="info alert" type="info" />
+  <el-alert title="warning alert" type="warning" />
+  <el-alert title="error alert" type="error" />
 </template>
