6e3aa2a1fdfbc4146c73e8cf1f6b32d2caad662c
Aex <spryti@qq.com>
2021-09-08 17:27:54 +0800

    docs: [date-picker] migrate document  (#3289)
    * docs: [date-picker] migrate document
    
    * fix: typo

---
+++ docs/examples/date-picker/date-formats.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="demo-date-picker">
+    <div class="block">
+      <span class="demonstration">Emits Date object</span>
+      <div class="demonstration">Value: {{ value1 }}</div>
+      <el-date-picker
+        v-model="value1"
+        type="date"
+        placeholder="Pick a Date"
+        format="YYYY/MM/DD"
+      >
+      </el-date-picker>
+    </div>
+    <div class="block">
+      <span class="demonstration">Use value-format</span>
+      <div class="demonstration">Value：{{ value2 }}</div>
+      <el-date-picker
+        v-model="value2"
+        type="date"
+        placeholder="Pick a Date"
+        format="YYYY/MM/DD"
+        value-format="YYYY-MM-DD"
+      >
+      </el-date-picker>
+    </div>
+    <div class="block">
+      <span class="demonstration">Timestamp</span>
+      <div class="demonstration">Value：{{ value3 }}</div>
+      <el-date-picker
+        v-model="value3"
+        type="date"
+        placeholder="Pick a Date"
+        format="YYYY/MM/DD"
+        value-format="x"
+      >
+      </el-date-picker>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  setup() {
+    const value1 = ref('')
+    const value2 = ref('')
+    const value3 = ref('')
+
+    return {
+      value1,
+      value2,
+      value3,
+    }
+  },
+})
+</script>
