ba2538506562a5bed43d58a9d4f12097f4a5e417
Aex <spryti@qq.com>
2021-09-08 15:33:20 +0000

    chore(utils): allow console.warn when test

--- packages/utils/error.ts
+++ packages/utils/error.ts
@@ -10,7 +10,8 @@ export default (scope: string, m: string) => {
 }
 
 export function debugWarn(scope: string, message: string): void {
-  if (process.env.NODE_ENV === 'development') {
+  console.error('process.env.NODE_ENV', `==${process.env.NODE_ENV}==`)
+  if (['development', 'test'].includes(process.env.NODE_ENV)) {
     // eslint-disable-next-line no-console
     console.warn(new ElementPlusError(`[${scope}] ${message}`))
   }
