f47151c13e4bb4a28ebf62bb4b2528845e5b1e46
Aex <spryti@qq.com>
2021-09-08 15:39:21 +0000

    chore(utils): remove uesless code

--- packages/utils/error.ts
+++ packages/utils/error.ts
@@ -10,7 +10,6 @@ export default (scope: string, m: string) => {
 }
 
 export function debugWarn(scope: string, message: string): void {
-  console.error('process.env.NODE_ENV', `==${process.env.NODE_ENV}==`)
   if (['development', 'test'].includes(process.env.NODE_ENV)) {
     // eslint-disable-next-line no-console
     console.warn(new ElementPlusError(`[${scope}] ${message}`))
