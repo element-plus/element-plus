d99aceb2dc04e60d43614fa3ab7d3249fde2d14b
Aex <spryti@qq.com>
2021-09-08 15:22:24 +0000

    chore(utils): ignore no-console in debugWarn

--- packages/utils/error.ts
+++ packages/utils/error.ts
@@ -11,6 +11,7 @@ export default (scope: string, m: string) => {
 
 export function debugWarn(scope: string, message: string): void {
   if (process.env.NODE_ENV === 'development') {
+    // eslint-disable-next-line no-console
     console.warn(new ElementPlusError(`[${scope}] ${message}`))
   }
 }
