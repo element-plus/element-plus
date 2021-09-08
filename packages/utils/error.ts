d0675fd668b2d6b29d1d190372cf67bb911f3c75
Aex <spryti@qq.com>
2021-09-08 14:38:05 +0000

    feat(utils): add debugWarn

--- packages/utils/error.ts
+++ packages/utils/error.ts
@@ -12,3 +12,9 @@ export default (scope: string, m: string) => {
 export function warn(scope: string, m: string) {
   console.warn(new ElementPlusError(`[${scope}] ${m}`))
 }
+
+export function debugWarn(scope: string, m: string): void {
+  if (process.env.NODE_ENV === 'development') {
+    console.warn(new ElementPlusError(`[${scope}] ${m}`))
+  }
+}
