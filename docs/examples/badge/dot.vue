6fd7d9c0e5cb757201de64d064a2806267e50041
jeremywu <15975785+JeremyWuuuuu@users.noreply.github.com>
2021-09-09 00:03:38 +0800

    chore: update doc (#3305)
    Co-authored-by: 0song <0song@gmail.com>

---
+++ docs/examples/badge/dot.vue
@@ -0,0 +1,17 @@
+<template>
+  <el-badge is-dot class="item">query</el-badge>
+  <el-badge is-dot class="item">
+    <el-button
+      class="share-button"
+      icon="el-icon-share"
+      type="primary"
+    ></el-button>
+  </el-badge>
+</template>
+
+<style scoped>
+.item {
+  margin-top: 10px;
+  margin-right: 40px;
+}
+</style>
