b468f8e4296f6ff9e1f3ee0de15c9e6155ba9c52
Kevin <sxzz@sxzz.moe>
2021-09-12 03:14:32 +0800

    refactor(components): refactor button

---
+++ packages/components/button/src/button.ts
@@ -0,0 +1,53 @@
+import { buildProp } from '@element-plus/utils/props'
+
+import type { ExtractPropTypes } from 'vue'
+
+export const buttonType = [
+  'default',
+  'primary',
+  'success',
+  'warning',
+  'info',
+  'danger',
+  'text',
+] as const
+export const buttonSize = ['', 'large', 'medium', 'small', 'mini'] as const
+export const buttonNativeType = ['button', 'submit', 'reset'] as const
+
+export const buttonProps = {
+  type: buildProp({
+    type: String,
+    values: buttonType,
+    default: 'default',
+  } as const),
+  size: buildProp({
+    type: String,
+    values: buttonSize,
+    default: '',
+  } as const),
+  icon: {
+    type: String,
+    default: '',
+  },
+  nativeType: buildProp({
+    type: String,
+    values: buttonNativeType,
+    default: 'button',
+  } as const),
+  loading: Boolean,
+  disabled: Boolean,
+  plain: Boolean,
+  autofocus: Boolean,
+  round: Boolean,
+  circle: Boolean,
+} as const
+
+export const buttonEmits = {
+  click: (evt: MouseEvent) => evt instanceof MouseEvent,
+}
+
+export type ButtonProps = ExtractPropTypes<typeof buttonProps>
+export type ButtonEmits = typeof buttonEmits
+
+export type ButtonType = ButtonProps['type']
+export type ButtonNativeType = ButtonProps['nativeType']
