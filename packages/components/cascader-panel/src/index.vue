5978ebdabd3c520c7f8b4c1004f4118162ed3876
Alan Wang <948467222@qq.com>
2021-09-15 05:25:08 +0800

    fix: [el-cascader] multiple search with v-model can't work (#3350)
    Co-authored-by: Kevin <sxzz@sxzz.moe>

--- packages/components/cascader-panel/src/index.vue
+++ packages/components/cascader-panel/src/index.vue
@@ -100,23 +100,36 @@ export default defineComponent({
     )
     const renderLabelFn = computed(() => props.renderLabel || slots.default)
 
+    let oldConfig: typeof config
+    let oldOptions: CascaderOption[]
     const initStore = () => {
       const { options } = props
       const cfg = config.value
 
-      manualChecked = false
-      store.value = new Store(options, cfg)
-      menus.value = [store.value.getNodes()]
+      const configTemp = config
+      if (
+        oldOptions === undefined ||
+        oldOptions !== options ||
+        oldConfig === undefined ||
+        configTemp !== oldConfig
+      ) {
+        manualChecked = false
+        store.value = new Store(options, cfg)
+        menus.value = [store.value.getNodes()]
 
-      if (cfg.lazy && isEmpty(props.options)) {
-        initialLoaded = false
-        lazyLoad(null, () => {
-          initialLoaded = true
+        if (cfg.lazy && isEmpty(props.options)) {
+          initialLoaded = false
+          lazyLoad(null, () => {
+            initialLoaded = true
+            syncCheckedValue(false, true)
+          })
+        } else {
           syncCheckedValue(false, true)
-        })
-      } else {
-        syncCheckedValue(false, true)
+        }
       }
+
+      oldConfig = configTemp
+      oldOptions = options
     }
 
     const lazyLoad: ElCascaderPanelContext['lazyLoad'] = (node, cb) => {
@@ -322,7 +335,7 @@ export default defineComponent({
       })
     )
 
-    watch([config, () => props.options], initStore, {
+    watch([config, () => props.options], () => initStore(), {
       deep: true,
       immediate: true,
     })
