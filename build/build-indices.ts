0b546eafdae5cb9e28b231ad47d487fb566055dc
JeremyWuuuuu <15975785+JeremyWuuuuu@users.noreply.github.com>
2021-09-15 17:46:39 +0800

    fix preview-build error

--- build/build-indices.ts
+++ build/build-indices.ts
@@ -28,27 +28,29 @@ const langs = {
 ;['zh-CN', 'en-US', 'es', 'fr-FR', 'jp'].forEach((lang) => {
   const indexName = langs[lang]
   const index = client.initIndex(indexName)
-  index.clearObjects().then(() => {
-    const files = fg.sync(`website/docs/${lang}/*.md`)
-    let indices: Index[] = []
-    files.forEach((file) => {
-      const regExp = new RegExp(`website\/docs\/${lang}\/(.*).md`)
-      const pathContent = file.match(regExp)!
-      const path = pathContent[1]
-      const index = path.lastIndexOf('/')
-      const names = index !== -1 ? path.split('/') : []
-      const component = names.length ? names[names.length - 1] : path
-      const content = fs.readFileSync(file, 'utf8')
-      const matches = content
-        .replace(/:::[\s\S]*?:::/g, '')
-        .replace(/```[\s\S]*?```/g, '')
-        .match(/#{2,4}[^#]*/g)!
-        .map((match) =>
-          match
-            .replace(/\n+/g, '\n')
-            .split('\n')
-            .filter((part) => !!part)
-        )
+  index
+    .clearObjects()
+    .then(() => {
+      const files = fg.sync(`website/docs/${lang}/*.md`)
+      let indices: Index[] = []
+      files.forEach((file) => {
+        const regExp = new RegExp(`website\/docs\/${lang}\/(.*).md`)
+        const pathContent = file.match(regExp)!
+        const path = pathContent[1]
+        const index = path.lastIndexOf('/')
+        const names = index !== -1 ? path.split('/') : []
+        const component = names.length ? names[names.length - 1] : path
+        const content = fs.readFileSync(file, 'utf8')
+        const matches = content
+          .replace(/:::[\s\S]*?:::/g, '')
+          .replace(/```[\s\S]*?```/g, '')
+          .match(/#{2,4}[^#]*/g)!
+          .map((match) =>
+            match
+              .replace(/\n+/g, '\n')
+              .split('\n')
+              .filter((part) => !!part)
+          )
       })
 
       index
