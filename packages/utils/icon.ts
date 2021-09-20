8d7e40df8d36de27abe29b285056ae600af262ca
Aex <adaex@qq.com>
2021-09-20 08:43:12 +0000

    feat(utils): add icon util

---
+++ packages/utils/icon.ts
@@ -0,0 +1,36 @@
+import ElIcon from '@element-plus/components/icon'
+import {
+  SuccessFilled,
+  InfoFilled,
+  WarningFilled,
+  CircleCloseFilled,
+  Close,
+} from '@element-plus/icons'
+
+export const TYPE_NAME_MAP = {
+  success: SuccessFilled,
+  warning: WarningFilled,
+  error: CircleCloseFilled,
+  info: InfoFilled,
+}
+
+export const TYPE_MAP = {
+  success: 'SuccessFilled',
+  warning: 'WarningFilled',
+  error: 'CircleCloseFilled',
+  info: 'InfoFilled',
+}
+
+export const CloseComponents = {
+  ElIcon,
+  Close,
+}
+
+export const TypeComponents = {
+  ElIcon,
+  Close,
+  SuccessFilled,
+  InfoFilled,
+  WarningFilled,
+  CircleCloseFilled,
+}
