62561891002a0c19f71cb1649d73f104f1de6740
三咲智子 <sxzz@sxzz.moe>
2021-09-22 01:19:04 +0800

    refactor(components): refactor pagination (#3526)
    * refactor(components): refactor pagination
    
    * fix: tests
    
    * fix: emits
    
    * refactor: improve props

--- packages/utils/props.ts
+++ packages/utils/props.ts
@@ -1,3 +1,4 @@
+import type { Mutable } from './types'
 import type { PropType } from 'vue'
 
 /**
@@ -69,5 +70,7 @@ export function buildProp<
 }
 
 export const keyOf = <T>(arr: T) => Object.keys(arr) as Array<keyof T>
+export const mutable = <T extends readonly any[]>(val: T) =>
+  val as Mutable<typeof val>
 
 export const componentSize = ['large', 'medium', 'small', 'mini'] as const
