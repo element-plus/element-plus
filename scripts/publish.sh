0ed28cc41d7d65f2e993c0401427aa16bdc0d804
JeremyWuuuuu <15975785+JeremyWuuuuu@users.noreply.github.com>
2021-09-23 08:52:07 +0800

    fix(build): release script error
    - Fix release script error copying node_modules

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -7,6 +7,8 @@ pnpm update:version
 
 sh scripts/build.sh
 
+find dist/element-plus/packages -type d -name node_modules -print0 | xargs -0 -I {} rm -rf {}
+
 cd dist/element-plus
 npm publish --access public
 cd -
