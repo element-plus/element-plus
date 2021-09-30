05c928d1f9e47f359b7544cd77290b61f774ff88
Kevin <sxzz@sxzz.moe>
2021-09-30 20:38:06 +0800

    refactor(popper): popper refactor props and emits

--- packages/components/popper/src/popper.ts
+++ packages/components/popper/src/popper.ts
@@ -11,6 +11,7 @@ import {
   withDirectives,
   h,
 } from 'vue'
+import { buildProps, definePropType, mutable } from '@element-plus/utils/props'
 import { ClickOutside } from '@element-plus/directives'
 import { throwError } from '@element-plus/utils/error'
 
@@ -18,19 +19,150 @@ import usePopper from './use-popper/index'
 import defaultProps from './use-popper/defaults'
 import { renderPopper, renderTrigger, renderArrow } from './renderers'
 
+import type { ExtractPropTypes } from 'vue'
+import type { Placement, PositioningStrategy, Options } from '@popperjs/core'
+
+const triggers = ['click', 'hover', 'focus', 'manual'] as const
+export type Trigger = typeof triggers[number]
+
+export const popperProps = buildProps({
+  // the arrow size is an equilateral triangle with 10px side length, the 3rd side length ~ 14.1px
+  // adding a offset to the ceil of 4.1 should be 5 this resolves the problem of arrow overflowing out of popper.
+  arrowOffset: {
+    type: Number,
+    default: 5,
+  } as const,
+  appendToBody: {
+    type: Boolean,
+    default: true,
+  } as const,
+  autoClose: {
+    type: Number,
+    default: 0,
+  } as const,
+  boundariesPadding: {
+    type: Number,
+    default: 0,
+  } as const,
+  content: {
+    type: String,
+    default: '',
+  } as const,
+  class: {
+    type: String,
+    default: '',
+  } as const,
+  style: {
+    type: [Object, String],
+    default: '',
+  } as const,
+  hideAfter: {
+    type: Number,
+    default: 200,
+  } as const,
+  cutoff: {
+    type: Boolean,
+    default: false,
+  } as const,
+  disabled: {
+    type: Boolean,
+    default: false,
+  } as const,
+  effect: {
+    type: String,
+    values: ['dark', 'light'],
+    default: 'dark',
+  } as const,
+  enterable: {
+    type: Boolean,
+    default: true,
+  } as const,
+  manualMode: {
+    type: Boolean,
+    default: false,
+  } as const,
+  showAfter: {
+    type: Number,
+    default: 0,
+  } as const,
+  offset: {
+    type: Number,
+    default: 12,
+  } as const,
+  placement: {
+    type: definePropType<Placement>(String),
+    default: 'bottom',
+  } as const,
+  popperClass: {
+    type: String,
+    default: '',
+  } as const,
+  pure: {
+    type: Boolean,
+    default: false,
+  } as const,
+  // Once this option were given, the entire popper is under the users' control, top priority
+  popperOptions: {
+    type: definePropType<Partial<Options>>(Object),
+    default: () => mutable({} as const),
+  },
+  showArrow: {
+    type: Boolean,
+    default: true,
+  } as const,
+  strategy: {
+    type: definePropType<PositioningStrategy>(String),
+    default: 'fixed',
+  } as const,
+  transition: {
+    type: String,
+    default: 'el-fade-in-linear',
+  } as const,
+  trigger: {
+    type: [String, Array],
+    values: triggers,
+    default: 'hover',
+    validator: (values: unknown): values is Trigger[] =>
+      Array.isArray(values) && values.every((val) => triggers.includes(val)),
+  },
+  visible: {
+    type: definePropType<boolean | undefined>(Boolean),
+    default: undefined,
+  },
+  stopPopperMouseEvent: {
+    type: Boolean,
+    default: true,
+  } as const,
+  gpuAcceleration: {
+    type: Boolean,
+    default: true,
+  } as const,
+  fallbackPlacements: {
+    type: definePropType<Placement[]>(Array),
+    default: () => mutable([] as const),
+  },
+} as const)
+export type PopperProps = ExtractPropTypes<typeof popperProps>
+export type PopperEffect = PopperProps['effect']
+
+export const popperEmits = {
+  'update:visible': (val: boolean) => typeof val === 'boolean',
+  'after-enter': () => true,
+  'after-leave': () => true,
+  'before-enter': () => true,
+  'before-leave': () => true,
+}
+export type PopperEmits = typeof popperEmits
+
 const compName = 'ElPopper'
 const UPDATE_VISIBLE_EVENT = 'update:visible'
 
 export default defineComponent({
   name: compName,
-  props: defaultProps,
-  emits: [
-    UPDATE_VISIBLE_EVENT,
-    'after-enter',
-    'after-leave',
-    'before-enter',
-    'before-leave',
-  ],
+
+  props: popperProps,
+  emits: popperEmits,
+
   setup(props, ctx) {
     if (!ctx.slots.trigger) {
       throwError(compName, 'Trigger must be provided')
