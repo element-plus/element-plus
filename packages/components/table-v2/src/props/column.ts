6a233ffaa8c20f64aa931b5b5d9b6516fc0b1043
jeremywuuuuu <15975785+JeremyWuuuuu@users.noreply.github.com>
2021-09-30 11:10:30 +0800

    Add partial column props

--- packages/components/table-v2/src/props/column.ts
+++ packages/components/table-v2/src/props/column.ts
@@ -1,4 +1,8 @@
-import { buildProp, definePropType, mutable } from '@element-plus/utils/props'
+import {
+  buildProps,
+  buildProp,
+  definePropType,
+} from '@element-plus/utils/props'
 
 import type { CSSProperties, ExtractPropTypes, VNodeChild } from 'vue'
 
@@ -32,6 +36,59 @@ export const tableV2ColumnRendererProps = {
 
 export const tableV2ColumnProps = {
   ...tableV2ColumnRendererProps,
-}
+  ...buildProps({
+    align: {
+      type: String,
+      values: ['left', 'center', 'right'],
+      default: 'left',
+    },
+
+    /**
+     * @description user defined data getter.
+     */
+    dataGetter: {
+      type: definePropType<
+        (props: {
+          columns: any[]
+          column: any
+          columnIndex: number
+          rowData: any
+          rowIndex: number
+        }) => any
+      >(Function),
+    },
+
+    flexGrow: {
+      type: Number,
+      default: 0,
+    },
+    flexShrink: {
+      type: Number,
+      default: 0,
+    },
+
+    width: {
+      type: Number,
+      required: true,
+    },
+  } as const),
+  /**
+   * @description
+   * dataKey is used by column rendering internally
+   * when dataGetter is not presented in the props, cell rendering will
+   * use _.get(cellData, dataKey) to retrieve data.
+   */
+  dataKey: String,
+
+  fixed: Boolean,
+  headerClassName: String,
+  hidden: Boolean,
+
+  sortable: Boolean,
+  title: String,
+
+  maxWidth: Number,
+  minWidth: Number,
+} as const
 
 export type TableV2ColumnProps = ExtractPropTypes<typeof tableV2ColumnProps>
