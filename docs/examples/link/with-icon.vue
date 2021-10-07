60624c643da63418c77d3d4024eb23f1ce11a144
Aex <adaex@qq.com>
2021-10-07 11:41:44 +0000

    refactor: update example

--- docs/examples/link/with-icon.vue
+++ docs/examples/link/with-icon.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <el-link icon="el-icon-edit">Edit</el-link>
-    <el-link>Check<i class="el-icon-view el-icon--right"></i> </el-link>
+    <el-link icon="Edit">Edit</el-link>
+    <el-link>
+      Check<el-icon class="el-icon--right"><view /></el-icon>
+    </el-link>
   </div>
 </template>
