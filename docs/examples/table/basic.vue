e57b5d78bcc334e6ae0e0299da2d2c24e0b46354
lhtuling <44140500+lhtuling@users.noreply.github.com>
2021-10-08 23:39:12 +0800

    Update basic.vue
    自闭合会导致显示异常!

--- docs/examples/table/basic.vue
+++ docs/examples/table/basic.vue
@@ -1,8 +1,8 @@
 <template>
   <el-table :data="tableData" style="width: 100%">
-    <el-table-column prop="date" label="Date" width="180" />
-    <el-table-column prop="name" label="Name" width="180" />
-    <el-table-column prop="address" label="Address" />
+    <el-table-column prop="date" label="Date" width="180" ></el-table-column>
+    <el-table-column prop="name" label="Name" width="180" ></el-table-column>
+    <el-table-column prop="address" label="Address" ></el-table-column>
   </el-table>
 </template>
 
