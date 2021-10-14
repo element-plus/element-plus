5d0cb5a2f78cafa97dae94c72a2fe922dd4a98dd
徐志伟 <zwxu01@wisedu.com>
2021-10-14 14:37:22 +0800

    docs(other): modify vp-sidebar and vp-content component scrollbar

--- docs/.vitepress/vitepress/components/vp-content.vue
+++ docs/.vitepress/vitepress/components/vp-content.vue
@@ -1,5 +1,13 @@
 <script setup lang="ts">
-import { computed, onUpdated, watch, ref, nextTick } from 'vue'
+import {
+  computed,
+  onUpdated,
+  watch,
+  ref,
+  nextTick,
+  onMounted,
+  onBeforeUnmount,
+} from 'vue'
 import nprogress from 'nprogress'
 import { useData, useRoute } from 'vitepress'
 import { useSidebar } from '../composables/sidebar'
@@ -17,6 +25,18 @@ const props = defineProps<{ isSidebarOpen: boolean }>()
 
 const shouldUpdateProgress = ref(true)
 
+const clientWidth = ref(document.documentElement.clientWidth)
+const update = () => {
+  clientWidth.value = document.documentElement.clientWidth
+}
+const screenMinWidth = 960 // less than 960px .sub-nav element display, so need subtract .sub-nav element height
+// .sub-nav element height 41px; .navbar element height 55px
+const maxHeight = computed(() =>
+  clientWidth.value < screenMinWidth
+    ? `calc(100vh - 96px)`
+    : 'calc(100vh - 55px)'
+)
+
 watch(
   () => props.isSidebarOpen,
   (val) => {
@@ -32,15 +52,25 @@ onUpdated(() => {
     nprogress.done()
   }
 })
+
+onMounted(() => {
+  addEventListener('resize', update)
+})
+
+onBeforeUnmount(() => {
+  removeEventListener('resize', update)
+})
 </script>
 
 <template>
   <main :class="{ 'page-content': true, 'has-sidebar': hasSidebar }">
-    <VPNotFound v-if="isNotFound" />
-    <VPHeroContent v-else-if="isHeroPost" />
-    <VPDocContent v-else>
-      <template #content-top><slot name="content-top" /></template>
-      <template #content-bottom><slot name="content-bottom" /></template>
-    </VPDocContent>
+    <el-scrollbar :max-height="maxHeight">
+      <VPNotFound v-if="isNotFound" />
+      <VPHeroContent v-else-if="isHeroPost" />
+      <VPDocContent v-else>
+        <template #content-top><slot name="content-top" /></template>
+        <template #content-bottom><slot name="content-bottom" /></template>
+      </VPDocContent>
+    </el-scrollbar>
   </main>
 </template>
--- docs/.vitepress/vitepress/components/vp-sidebar.vue
+++ docs/.vitepress/vitepress/components/vp-sidebar.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import { computed, ref, onMounted, onBeforeUnmount } from 'vue'
 import { useSidebar } from '../composables/sidebar'
 
 import VPSidebarLink from './sidebar/vp-sidebar-link.vue'
@@ -8,24 +9,54 @@ defineEmits(['close'])
 
 // const isHome = useIsHome()
 const { sidebars, hasSidebar } = useSidebar()
+
+const clientWidth = ref(document.documentElement.clientWidth)
+const appMaxWidth = 1376 // --app-max-width: 1376px;
+const wrapStyle = computed(() =>
+  clientWidth.value >= appMaxWidth
+    ? `padding: 24px 32px 24px ${(clientWidth.value - appMaxWidth) / 2}px;`
+    : ''
+)
+
+const update = () => {
+  clientWidth.value = document.documentElement.clientWidth
+}
+
+onMounted(() => {
+  addEventListener('resize', update)
+})
+
+onBeforeUnmount(() => {
+  removeEventListener('resize', update)
+})
 </script>
 
 <template>
   <aside v-if="hasSidebar" :class="{ sidebar: true, open }">
-    <slot name="top" />
-    <div class="sidebar-groups">
-      <section v-for="(item, key) of sidebars" :key="key" class="sidebar-group">
-        <p class="sidebar-group__title">
-          {{ item.text }}
-        </p>
-        <VPSidebarLink
-          v-for="(child, childKey) in item.children"
-          :key="childKey"
-          :item="child"
-          @close="$emit('close')"
-        />
-      </section>
-    </div>
-    <slot name="bottom" />
+    <el-scrollbar
+      :key="open"
+      :wrap-style="wrapStyle"
+      :height="open ? '100vh' : 'calc(100vh - 55px)'"
+    >
+      <slot name="top" />
+      <div class="sidebar-groups">
+        <section
+          v-for="(item, key) of sidebars"
+          :key="key"
+          class="sidebar-group"
+        >
+          <p class="sidebar-group__title">
+            {{ item.text }}
+          </p>
+          <VPSidebarLink
+            v-for="(child, childKey) in item.children"
+            :key="childKey"
+            :item="child"
+            @close="$emit('close')"
+          />
+        </section>
+      </div>
+      <slot name="bottom" />
+    </el-scrollbar>
   </aside>
 </template>
