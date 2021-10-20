62346d6b50ee453e6889f75011488b06a3bc940a
Aex <adaex@qq.com>
2021-10-20 16:23:14 +0800

    refactor(components): el-input icon (#3565)
    * refactor(components): input icon
    
    * docs: update example
    
    * refactor(components): el-select icon
    
    * docs: icon usage
    
    * fix: input style
    
    * fix: add scoped
    
    * docs: fix icon
    
    * fix: icon align
    
    * fix: icon align
    
    * fix: typo
    
    * fix: typo

--- packages/utils/icon.ts
+++ packages/utils/icon.ts
@@ -4,6 +4,9 @@ import {
   WarningFilled,
   CircleCloseFilled,
   Close,
+  Loading,
+  CircleCheck,
+  CircleClose,
 } from '@element-plus/icons'
 import { definePropType } from './props'
 import type { Component } from 'vue'
@@ -27,4 +30,10 @@ export const TypeComponentsMap = {
   warning: WarningFilled,
   error: CircleCloseFilled,
   info: InfoFilled,
+}
+
+export const ValidateComponentsMap = {
+  validating: Loading,
+  success: CircleCheck,
+  error: CircleClose,
 }
