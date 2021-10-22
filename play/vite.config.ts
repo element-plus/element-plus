b5db48dfc1acec0626b7e281fa044020c2d37d32
三咲智子 <sxzz@sxzz.moe>
2021-10-22 15:59:29 +0800

    feat(dev): play add unplugin-vue-components (#3931)

--- play/vite.config.ts
+++ play/vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import './vite.init'
 
 export default defineConfig({
   server: {
     host: true,
   },
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      resolvers: ElementPlusResolver(),
+    }),
+  ],
 })
