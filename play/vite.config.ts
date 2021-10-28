07801e1cc53c9f985c25a3c3e1cd2907c1c9edfa
Kevin <sxzz@sxzz.moe>
2021-10-28 18:27:41 +0800

    refactor: reorder import

--- play/vite.config.ts
+++ play/vite.config.ts
@@ -3,10 +3,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-import './vite.init'
 import Inspect from 'vite-plugin-inspect'
 import glob from 'fast-glob'
 import { epRoot, pkgRoot, projRoot } from '../build/utils/paths'
+import './vite.init'
 
 export default defineConfig(async () => {
   const optimizeDeps = (
