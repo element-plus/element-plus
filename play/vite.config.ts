ab562240a622e6dc22d8a234453c32b6af795da9
Kevin <sxzz@sxzz.moe>
2021-10-28 18:26:01 +0800

    fix(dev): fix playground and docs alias

--- play/vite.config.ts
+++ play/vite.config.ts
@@ -4,29 +4,54 @@ import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import './vite.init'
-import { epRoot, pkgRoot } from '../build/utils/paths'
+import Inspect from 'vite-plugin-inspect'
+import glob from 'fast-glob'
+import { epRoot, pkgRoot, projRoot } from '../build/utils/paths'
 
-export default defineConfig({
-  resolve: {
-    alias: [
-      {
-        find: /^element-plus$/,
-        replacement: path.resolve(epRoot, 'index.ts'),
-      },
-      {
-        find: /^element-plus\/(es|lib)\/(.*)$/,
-        replacement: `${pkgRoot}/$2`,
-      },
+export default defineConfig(async () => {
+  const optimizeDeps = (
+    await glob(['lodash/*.js', 'dayjs/(locale|plugin)/*.js'], {
+      cwd: path.resolve(projRoot, 'node_modules'),
+    })
+  ).map((dep) => dep.replace(/\.js$/, ''))
+
+  return {
+    resolve: {
+      alias: [
+        {
+          find: /^element-plus(\/(es|lib))?$/,
+          replacement: path.resolve(epRoot, 'index.ts'),
+        },
+        {
+          find: /^element-plus\/(es|lib)\/(.*)$/,
+          replacement: `${pkgRoot}/$2`,
+        },
+      ],
+    },
+    server: {
+      host: true,
+    },
+    plugins: [
+      vue(),
+      Components({
+        include: `${__dirname}/**`,
+        resolvers: ElementPlusResolver({ importStyle: 'sass' }),
+      }),
+      Inspect(),
     ],
-  },
-  server: {
-    host: true,
-  },
-  plugins: [
-    vue(),
-    Components({
-      include: `${__dirname}/**`,
-      resolvers: ElementPlusResolver({ importStyle: 'sass' }),
-    }),
-  ],
+
+    optimizeDeps: {
+      include: [
+        '@vue/shared',
+        '@vueuse/core',
+        'async-validator',
+        'memoize-one',
+        'normalize-wheel-es',
+        '@popperjs/core',
+        'dayjs',
+        '@element-plus/icons',
+        ...optimizeDeps,
+      ],
+    },
+  }
 })
