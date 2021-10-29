abd266c4c17f82c096c57d5a2977ea52787c25de
Aex <adaex@qq.com>
2021-10-29 03:43:21 +0000

    revert(play): component use

--- play/main.ts
+++ play/main.ts
@@ -1,9 +1,8 @@
 import { createApp } from 'vue'
 import App from './src/App.vue'
-import { useComponent } from './src/use'
 
 import '@element-plus/theme-chalk/src/index.scss'
 
 const app = createApp(App)
-useComponent(app)
+
 app.mount('#play')
