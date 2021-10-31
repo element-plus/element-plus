c33d86179790b4508f68ad9b51e62393f86fd717
btea <2356281422@qq.com>
2021-10-31 07:11:16 -0500

    fix(components): [el-tree-v2] indent can not set zero (#3881)
    * fix(components): [el-tree-v2] indent can not set zero
    
    * chore: simplify code

--- packages/components/tree-v2/src/tree-node.vue
+++ packages/components/tree-v2/src/tree-node.vue
@@ -76,7 +76,7 @@ export default defineComponent({
     const tree = inject(ROOT_TREE_INJECTION_KEY)
 
     const indent = computed(() => {
-      return tree?.props.indent || 16
+      return tree?.props.indent ?? 16
     })
 
     const icon = computed(() => {
