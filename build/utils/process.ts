b5adbf73288aa10c5661da4c654c46c4ee658e33
Chen_Z <565302968@qq.com>
2021-11-02 13:43:57 +0800

    fix(build): windows executes build correctly

--- build/utils/process.ts
+++ build/utils/process.ts
@@ -4,7 +4,37 @@ import { projRoot } from './paths'
 
 export const run = async (command: string, cwd: string = projRoot) =>
   new Promise<void>((resolve, reject) => {
-    const [cmd, ...args] = command.split(' ')
+    let [cmd, ...args] = command.split(' ')
+    args.forEach((item, index) => {
+      console.log(item)
+      args[index] = item.replace(/\//g, '\\')
+    })
+    if (process.platform === 'win32') {
+      switch (cmd) {
+        case 'cp':
+          cmd = 'copy'
+          break
+        case 'mv':
+          cmd = 'del'
+          break
+        case 'rmdir':
+          cmd = 'del'
+          break
+        case 'rsync':
+          cmd = 'xcopy'
+          args.map((item, index) => {
+            if (item === '-a') args.splice(index, 1)
+          })
+          break
+        case 'mkdir':
+          args.map((item, index) => {
+            if (item === '-p') args.splice(index, 1)
+          })
+          break
+        default:
+          break
+      }
+    }
     console.log(`run: ${green(`${cmd} ${args.join(' ')}`)}`)
     const app = spawn(cmd, args, {
       cwd,
