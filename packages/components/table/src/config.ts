69d47430c1b63640c672b9bbedbe06df0f15dcf0
msidolphin <msidolphin@outlook.com>
2021-11-02 10:48:04 +0800

    fix(components): [el-table] development environment warning issues
    fix #4166

--- packages/components/table/src/config.ts
+++ packages/components/table/src/config.ts
@@ -117,7 +117,17 @@ export const cellForced = {
           class: classes,
           onClick: callback,
         },
-        [h(ElIcon, null, [h(ArrowRight)])]
+        {
+          default: () => {
+            return [
+              h(ElIcon, null, {
+                default: () => {
+                  return [h(ArrowRight)]
+                },
+              }),
+            ]
+          },
+        }
       )
     },
     sortable: false,
@@ -183,7 +193,19 @@ export function treeCellPrefix<T>({
           class: expandClasses,
           onClick: callback,
         },
-        [h(ElIcon, { class: { 'is-loading': treeNode.loading } }, [h(icon)])]
+        {
+          default: () => {
+            return [
+              h(
+                ElIcon,
+                { class: { 'is-loading': treeNode.loading } },
+                {
+                  default: () => [h(icon)],
+                }
+              ),
+            ]
+          },
+        }
       )
     )
   } else {
