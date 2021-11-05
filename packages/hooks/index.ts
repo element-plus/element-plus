cad3bf7c1dbe605328ec37e2b392198ff570c46c
Kevin <sxzz@sxzz.moe>
2021-11-06 03:55:55 +0800

    refactor(hooks): remove use-events

--- packages/hooks/index.ts
+++ packages/hooks/index.ts
@@ -1,5 +1,4 @@
 export { default as useAttrs } from './use-attrs'
-export { default as useEvents } from './use-events'
 export { default as useLockScreen } from './use-lockscreen'
 export { default as useRestoreActive } from './use-restore-active'
 export { default as useModal } from './use-modal'
