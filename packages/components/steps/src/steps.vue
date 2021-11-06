5c7c37a569482721db3ffb7cd003317f534d2476
undefined-design <1207145565@qq.com>
2021-11-06 17:07:54 +0800

    refactor(components): refactor steps/step

---
+++ packages/components/steps/src/steps.vue
@@ -0,0 +1,44 @@
+<template>
+  <div
+    :class="[
+      'el-steps',
+      simple ? 'el-steps--simple' : `el-steps--${direction}`,
+    ]"
+  >
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, watch, ref, provide } from 'vue'
+import { CHANGE_EVENT } from '@element-plus/utils/constants'
+import { stepsProps, stepsEmits } from './steps'
+
+export default defineComponent({
+  name: 'ElSteps',
+  props: stepsProps,
+  emits: stepsEmits,
+  setup(props, { emit }) {
+    const steps = ref([])
+
+    watch(steps, () => {
+      steps.value.forEach((instance, index) => {
+        instance.setIndex(index)
+      })
+    })
+
+    provide('ElSteps', { props, steps })
+
+    watch(
+      () => props.active,
+      (newVal, oldVal) => {
+        emit(CHANGE_EVENT, newVal, oldVal)
+      }
+    )
+
+    return {
+      steps,
+    }
+  },
+})
+</script>
