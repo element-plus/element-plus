edc48804b9eb8051dfeaabe8b38598f535cd0b85
Kevin <sxzz@sxzz.moe>
2021-11-12 05:38:50 +0800

    refactor(components): refactor tabs

--- packages/components/tabs/src/tab-bar.ts
+++ packages/components/tabs/src/tab-bar.ts
@@ -1,11 +1,11 @@
-import { buildProps, definePropType } from '@element-plus/utils/props'
-import type { Pane } from './token'
+import { buildProps, definePropType, mutable } from '@element-plus/utils/props'
+import type { TabsPaneContext } from '@element-plus/tokens'
 import type { ExtractPropTypes } from 'vue'
 
 export const tabBar = buildProps({
   tabs: {
-    type: definePropType<Pane[]>(Array),
-    default: () => [],
+    type: definePropType<TabsPaneContext[]>(Array),
+    default: () => mutable([] as const),
   },
 } as const)
 
