9aa3b2d52d2e2c5e57945e2b8c5d1c4916b16aa1
muhammadcahya <muhammadcahyax@gmail.com>
2021-11-20 22:22:17 +0700

    Improve Indonesian (Bahasa) translations

--- docs/examples/button/custom.vue
+++ docs/examples/button/custom.vue
@@ -1,6 +1,6 @@
 <template>
   <el-row>
-    <el-button color="#626aef" style="color: white">Cutsom</el-button>
-    <el-button color="#626aef" plain>Cutsom</el-button>
+    <el-button color="#626aef" style="color: white">Custom</el-button>
+    <el-button color="#626aef" plain>Custom</el-button>
   </el-row>
 </template>
