352aebb990767bd79c9c1a550df4e5de06c3394c
YunYouJun <me@yunyoujun.cn>
2021-11-24 18:42:38 +0800

    refactor(components): adjust tag size

--- docs/examples/tag/sizes.vue
+++ docs/examples/tag/sizes.vue
@@ -1,6 +1,5 @@
 <template>
+  <el-tag size="large">Large</el-tag>
   <el-tag>Default</el-tag>
-  <el-tag size="medium">Medium</el-tag>
   <el-tag size="small">Small</el-tag>
-  <el-tag size="mini">Mini</el-tag>
 </template>
