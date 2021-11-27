28f9b8590ed0c00cca34bb1ee427a3a386214038
ElementPlusBot <hello@element-plus.org>
2021-11-27 14:17:21 +0000

    website deploy

--- assets/Sketch-Template.db0894ab.js
+++
@@ -1,1 +0,0 @@
-var s="/images/Axure-Components.svg",a="/images/Sketch-Template.svg";export{s as _,a};
---
+++ assets/figma.b0a39244.js
@@ -0,0 +1,1 @@
+var a="/images/Axure-Components.svg",s="/images/Sketch-Template.svg",e="/images/figma.png";export{a as _,s as a,e as b};
