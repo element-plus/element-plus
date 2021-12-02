f7c15a491b61400db0f2af41cd7fe776c3b7eaac
cokemine <aptx4561@gmail.com>
2021-12-02 20:48:27 +0800

    revert play/main.ts

--- play/main.ts
+++ play/main.ts
@@ -1,11 +1,8 @@
 import { createApp } from 'vue'
-import ElementPlus from 'element-plus'
 import App from './src/App.vue'
 
 import '@element-plus/theme-chalk/src/index.scss'
 
 const app = createApp(App)
-
-app.use(ElementPlus)
 
 app.mount('#play')
