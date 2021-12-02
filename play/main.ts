5ef44f6e144219d6d677ac90c131da727ffd4004
cokemine <aptx4561@gmail.com>
2021-12-02 17:40:29 +0800

    chore(play): fix dev environment

--- play/main.ts
+++ play/main.ts
@@ -1,8 +1,11 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
 import App from './src/App.vue'
 
 import '@element-plus/theme-chalk/src/index.scss'
 
 const app = createApp(App)
+
+app.use(ElementPlus)
 
 app.mount('#play')
