214ecdfcbc6b59a5fa7ffef53a385ecb81808dbd
Chen <1207145565@qq.com>
2021-12-09 17:33:09 +0800

    feat(components): [ElCheckboxGroup] add Fragment

--- packages/components/checkbox/src/checkbox-group.vue
+++ packages/components/checkbox/src/checkbox-group.vue
@@ -1,9 +1,3 @@
-<template>
-  <div class="el-checkbox-group" role="group" aria-label="checkbox-group">
-    <slot></slot>
-  </div>
-</template>
-
 <script lang="ts">
 import {
   defineComponent,
@@ -12,6 +6,8 @@ import {
   provide,
   nextTick,
   toRefs,
+  h,
+  renderSlot,
 } from 'vue'
 import { UPDATE_MODEL_EVENT } from '@element-plus/utils/constants'
 import { isValidComponentSize } from '@element-plus/utils/validators'
@@ -49,20 +45,24 @@ export default defineComponent({
       type: String,
       default: undefined,
     },
+    tag: {
+      type: String,
+      default: 'div',
+    },
   },
 
   emits: [UPDATE_MODEL_EVENT, 'change'],
 
-  setup(props, ctx) {
+  setup(props, { emit, slots }) {
     const { elFormItem, elFormItemSize, ELEMENT } = useCheckboxGroup()
     const checkboxGroupSize = computed(
       () => props.size || elFormItemSize.value || ELEMENT.size
     )
 
     const changeEvent = (value) => {
-      ctx.emit(UPDATE_MODEL_EVENT, value)
+      emit(UPDATE_MODEL_EVENT, value)
       nextTick(() => {
-        ctx.emit('change', value)
+        emit('change', value)
       })
     }
 
@@ -89,6 +89,17 @@ export default defineComponent({
         elFormItem.validate?.('change')
       }
     )
+    return () => {
+      return h(
+        props.tag,
+        {
+          class: 'el-checkbox-group',
+          role: 'group',
+          'aria-label': 'checkbox-group',
+        },
+        [renderSlot(slots, 'default')]
+      )
+    }
   },
 })
 </script>
