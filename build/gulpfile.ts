3e12db55a2554f60d50e1aa50a01a4513c0ee91e
Kevin <sxzz@sxzz.moe>
2021-12-09 21:38:35 +0800

    ci: add windows build test

--- build/gulpfile.ts
+++ build/gulpfile.ts
@@ -56,6 +56,7 @@ export const copyFullStyle = async () => {
 
 export default series(
   withTaskName('clean', () => run('pnpm run clean')),
+  withTaskName('createOutput', () => mkdir(epOutput, { recursive: true })),
 
   parallel(
     runTask('buildModules'),
