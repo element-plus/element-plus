dda60bd200c17acf97f0397149167bccd181d558
btea <2356281422@qq.com>
2021-12-12 23:44:29 -0600

    chore(utils): remove unnecessary as string (#4805)

--- packages/utils/validators.ts
+++ packages/utils/validators.ts
@@ -3,13 +3,12 @@ import { isNumber } from './util'
 export const isValidWidthUnit = (val: string | number): boolean => {
   if (isNumber(val)) {
     return true
-  } else {
-    return (
-      ['px', 'rem', 'em', 'vw', '%', 'vmin', 'vmax'].some((unit) =>
-        (val as string).endsWith(unit)
-      ) || (val as string).startsWith('calc')
-    )
   }
+  return (
+    ['px', 'rem', 'em', 'vw', '%', 'vmin', 'vmax'].some((unit) =>
+      val.endsWith(unit)
+    ) || val.startsWith('calc')
+  )
 }
 
 export const isValidComponentSize = (val: string) =>
