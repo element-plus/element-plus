43086287484a3aae3888fd2236e1b1f418b6a298
C.Y.Kun <30518686+emojiiii@users.noreply.github.com>
2021-12-16 09:08:02 +0800

    docs(components): use setup sugar simplify `progress` demo

--- docs/examples/progress/dashboard-progress-bar.vue
+++ docs/examples/progress/dashboard-progress-bar.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="demo-progress">
-    <el-progress type="dashboard" :percentage="percentage" :color="colors" />
-    <el-progress type="dashboard" :percentage="percentage2" :color="colors" />
+    <el-progress
+      type="dashboard"
+      :percentage="state.percentage"
+      :color="state.colors"
+    />
+    <el-progress
+      type="dashboard"
+      :percentage="state.percentage2"
+      :color="state.colors"
+    />
     <div>
       <el-button-group>
         <el-button :icon="Minus" @click="decrease" />
@@ -11,48 +19,37 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent, reactive, toRefs, onMounted } from 'vue'
+<script lang="ts" setup>
+import { reactive, onMounted } from 'vue'
 import { Minus, Plus } from '@element-plus/icons-vue'
 
-export default defineComponent({
-  setup() {
-    const state = reactive({
-      percentage: 10,
-      percentage2: 0,
-      colors: [
-        { color: '#f56c6c', percentage: 20 },
-        { color: '#e6a23c', percentage: 40 },
-        { color: '#5cb87a', percentage: 60 },
-        { color: '#1989fa', percentage: 80 },
-        { color: '#6f7ad3', percentage: 100 },
-      ],
-    })
-    const increase = () => {
-      state.percentage += 10
-      if (state.percentage > 100) {
-        state.percentage = 100
-      }
-    }
-    const decrease = () => {
-      state.percentage -= 10
-      if (state.percentage < 0) {
-        state.percentage = 0
-      }
-    }
-    onMounted(() => {
-      setInterval(() => {
-        state.percentage2 = (state.percentage2 % 100) + 10
-      }, 500)
-    })
+const state = reactive({
+  percentage: 10,
+  percentage2: 0,
+  colors: [
+    { color: '#f56c6c', percentage: 20 },
+    { color: '#e6a23c', percentage: 40 },
+    { color: '#5cb87a', percentage: 60 },
+    { color: '#1989fa', percentage: 80 },
+    { color: '#6f7ad3', percentage: 100 },
+  ],
+})
 
-    return {
-      ...toRefs(state),
-      increase,
-      decrease,
-      Minus,
-      Plus,
-    }
-  },
+const increase = () => {
+  state.percentage += 10
+  if (state.percentage > 100) {
+    state.percentage = 100
+  }
+}
+const decrease = () => {
+  state.percentage -= 10
+  if (state.percentage < 0) {
+    state.percentage = 0
+  }
+}
+onMounted(() => {
+  setInterval(() => {
+    state.percentage2 = (state.percentage2 % 100) + 10
+  }, 500)
 })
 </script>
