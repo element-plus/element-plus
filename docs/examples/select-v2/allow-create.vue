ec415aab25a9d6eae22d16349c8291be7a44e819
Ryan <ryanzhao2128@gmail.com>
2021-12-21 23:12:05 +0800

    docs: [select-v2] fix demo render error

--- docs/examples/select-v2/allow-create.vue
+++ docs/examples/select-v2/allow-create.vue
@@ -22,6 +22,7 @@
 
 <script lang="ts" setup>
 import { ref } from 'vue'
+const initials = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
 
 const value1 = ref([])
 const value2 = ref('')
@@ -29,5 +30,4 @@ const options = Array.from({ length: 1000 }).map((_, idx) => ({
   value: `Option ${idx + 1}`,
   label: `${initials[idx % 10]}${idx}`,
 }))
-const initials = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
 </script>
