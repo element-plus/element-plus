f7fbf9c6250ae034126aeb42bf051f7d2ebbe0a4
Satrong <satrong@users.noreply.github.com>
2021-12-28 15:01:24 +0800

    Add `type="password"`

--- docs/examples/input/password.vue
+++ docs/examples/input/password.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-input v-model="input" placeholder="Please input password" show-password />
+  <el-input v-model="input" type="password" placeholder="Please input password" show-password />
 </template>
 
 <script lang="ts" setup>
