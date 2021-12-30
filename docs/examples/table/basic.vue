fce6acb5cab55d1af833e96154847ab687fda61b
iamkun <kunhello@outlook.com>
2021-12-30 17:32:24 +0800

    refactor(style): adjust table size to large/default/small (#5036)
    * feat: update table css
    
    * feat: update table css
    
    * feat: update table css
    
    * feat: update table css

--- docs/examples/table/basic.vue
+++ docs/examples/table/basic.vue
@@ -1,9 +1,21 @@
 <template>
-  <el-table :data="tableData" style="width: 100%">
-    <el-table-column prop="date" label="Date" width="180" />
-    <el-table-column prop="name" label="Name" width="180" />
-    <el-table-column prop="address" label="Address" />
-  </el-table>
+  <div>
+    <el-table :data="tableData" style="width: 100%">
+      <el-table-column prop="date" label="Date" width="180" />
+      <el-table-column prop="name" label="Name" width="180" />
+      <el-table-column prop="address" label="Address" />
+    </el-table>
+    <el-table size="small" :data="tableData" style="width: 100%">
+      <el-table-column prop="date" label="Date" width="180" />
+      <el-table-column prop="name" label="Name" width="180" />
+      <el-table-column prop="address" label="Address" />
+    </el-table>
+    <el-table size="large" :data="tableData" style="width: 100%">
+      <el-table-column prop="date" label="Date" width="180" />
+      <el-table-column prop="name" label="Name" width="180" />
+      <el-table-column prop="address" label="Address" />
+    </el-table>
+  </div>
 </template>
 
 <script lang="ts" setup>
