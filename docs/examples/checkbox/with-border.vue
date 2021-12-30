047dbccf3d2cbc0c506bd0280590baf8ed72acfa
云游君 <me@yunyoujun.cn>
2021-12-30 17:26:32 +0800

    fix: adjust tag icon size & add checkbox with-border demo (#5033)

--- docs/examples/checkbox/with-border.vue
+++ docs/examples/checkbox/with-border.vue
@@ -13,13 +13,19 @@
       border
     ></el-checkbox>
   </div>
-  <div class="demo-with-border my-4">
+  <div class="mt-4">
     <el-checkbox v-model="checked3" label="Option1" border></el-checkbox>
     <el-checkbox v-model="checked4" label="Option2" border></el-checkbox>
   </div>
-  <div class="demo-with-border">
+  <div class="mt-4">
     <el-checkbox-group v-model="checkboxGroup1" size="small">
       <el-checkbox label="Option1" border></el-checkbox>
+      <el-checkbox label="Option2" border></el-checkbox>
+    </el-checkbox-group>
+  </div>
+  <div class="mt-4">
+    <el-checkbox-group v-model="checkboxGroup1" size="small">
+      <el-checkbox label="Option1" border disabled></el-checkbox>
       <el-checkbox label="Option2" border disabled></el-checkbox>
     </el-checkbox-group>
   </div>
@@ -32,5 +38,5 @@ const checked1 = ref(true)
 const checked2 = ref(false)
 const checked3 = ref(false)
 const checked4 = ref(true)
-const checkboxGroup1 = ref([])
+const checkboxGroup1 = ref(['Option1'])
 </script>
