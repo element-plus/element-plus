fe0bcd16ce6d8539febc3a697f975156e5488497
jeremywu <15975785+JeremyWuuuuu@users.noreply.github.com>
2021-12-30 17:48:34 +0800

    feat(docs): update demo block playground icon (#5052)
    - Update playground icon and update the icon for github, copy, source-code for alignment

--- docs/.vitepress/vitepress/components/icons/copy-icon.vue
+++ docs/.vitepress/vitepress/components/icons/copy-icon.vue
@@ -1,8 +1,13 @@
 <template>
-  <svg viewBox="0 0 24 24">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    width="24"
+    height="24"
+  >
+    <path fill="none" d="M0 0h24v24H0z" />
     <path
       d="M7 6V3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1h-3v3c0 .552-.45 1-1.007 1H4.007A1.001 1.001 0 0 1 3 21l.003-14c0-.552.45-1 1.007-1H7zm2 0h8v10h2V4H9v2zm-2 5v2h6v-2H7zm0 4v2h6v-2H7z"
-      fill="currentColor"
-    ></path>
+    />
   </svg>
 </template>
