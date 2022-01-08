26b9ec49ee81feb3578ff57c4564f6d5023c0373
Kevin <sxzz@sxzz.moe>
2022-01-08 17:10:46 +0800

    revert: play

--- play/main.ts
+++ play/main.ts
@@ -1,12 +1,8 @@
 import { createApp } from 'vue'
-import ElementPlus from 'element-plus'
-import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import App from './src/App.vue'
 
 import '@element-plus/theme-chalk/src/index.scss'
 
-const app = createApp(App).use(ElementPlus, {
-  locale: zhCn,
-})
+const app = createApp(App)
 
 app.mount('#play')
