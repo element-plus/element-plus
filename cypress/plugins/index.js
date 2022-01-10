e1b15ffbc725d780cd00dbb08923bb0312e7ae9f
JeremyWuuuuu <15975785+JeremyWuuuuu@users.noreply.github.com>
2022-01-10 23:25:31 +0800

    fix(ci): cypress ci issue
    - Replace localhost with 127.0.0.1
    - Add feature flag for headless Chrome

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -17,7 +17,15 @@
  * @type {Cypress.PluginConfig}
  */
 // eslint-disable-next-line no-unused-vars
-module.exports = (_on, _config) => {
+module.exports = (on, _config) => {
+  on('before:browser:launch', (browser = {}, args) => {
+    if (browser.name === 'chrome') {
+      // ^ make sure this is your browser name, you may
+      // be using 'canary' or 'chromium' for example, so change it to match!
+      args.push('--proxy-bypass-list=<-loopback>')
+      return args
+    }
+  })
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 }
