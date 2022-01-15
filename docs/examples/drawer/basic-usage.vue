eb41233b6781db81a67704838915ecd8ea2fb82f
Alanscut <wp_scut@163.com>
2022-01-14 19:35:55 -0800

    feat: [el-drawer]add footer slot

--- docs/examples/drawer/basic-usage.vue
+++ docs/examples/drawer/basic-usage.vue
@@ -9,6 +9,9 @@
   <el-button type="primary" style="margin-left: 16px" @click="drawer = true">
     open
   </el-button>
+  <el-button type="primary" style="margin-left: 16px" @click="drawer2 = true">
+    with footer
+  </el-button>
 
   <el-drawer
     v-model="drawer"
@@ -18,6 +21,24 @@
   >
     <span>Hi, there!</span>
   </el-drawer>
+  <el-drawer
+    v-model="drawer2"
+    :direction="direction"
+    :before-close="handleClose"
+  >
+    <template #title>
+      <h4>set title by slot</h4>
+    </template>
+    <template #default>
+      <p>set default slot</p>
+    </template>
+    <template #footer>
+      <div style="flex: auto">
+        <el-button>cancel</el-button>
+        <el-button type="primary">confirm</el-button>
+      </div>
+    </template>
+  </el-drawer>
 </template>
 
 <script lang="ts" setup>
@@ -25,6 +46,7 @@ import { ref } from 'vue'
 import { ElMessageBox } from 'element-plus'
 
 const drawer = ref(false)
+const drawer2 = ref(false)
 const direction = ref('rtl')
 const handleClose = (done: () => void) => {
   ElMessageBox.confirm('Are you sure you want to close this?')
