87b3c60b4309c6a6bde6f802f6f817e6465ee8c6
Valar103769 <36926073+Valar103769@users.noreply.github.com>
2022-01-16 19:44:11 +0800

    Update gulpfile.ts
    删除多余的task

--- build/gulpfile.ts
+++ build/gulpfile.ts
@@ -45,7 +45,6 @@ export const copyFullStyle = async () => {
 
 export default series(
   withTaskName('clean', () => run('pnpm run clean')),
-  withTaskName('createOutput', () => mkdir(epOutput, { recursive: true })),
 
   parallel(
     runTask('buildModules'),
