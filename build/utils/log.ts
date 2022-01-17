a51dafffcd4f0147e25ce51b51febfcb7cef3eb6
JeremyWuuuuu <15975785+JeremyWuuuuu@users.noreply.github.com>
2022-01-17 11:43:29 +0800

    - Refactor command like function

--- build/utils/log.ts
+++ build/utils/log.ts
@@ -4,33 +4,33 @@ import chalk from 'chalk'
 /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
 const [command, info, error] = ['command', 'info', 'error'].map(
   (symbol: string) => {
-    return (msg: string) => console.log(`[${symbol}] ${msg}`)
+    return (msg: string) => `[${symbol}] ${msg}`
   }
 )
 /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
 const [group, endGroup] = ['group', 'endgroup'].map((symbol) => {
-  return (groupMsg: string) => {
-    console.log(`##[${symbol}] ${groupMsg}`)
-  }
+  return (groupMsg: string) => `##[${symbol}] ${groupMsg}`
 })
 
 export function cyan(str: string) {
-  command(chalk.cyan(str))
+  console.log(command(chalk.cyan(str)))
 }
 
 export function yellow(str: string) {
-  command(chalk.yellow(str))
+  console.log(command(chalk.yellow(str)))
 }
 
 export function green(str: string) {
-  command(chalk.green(str))
+  console.log(command(chalk.green(str)))
 }
 
 export function red(str: string) {
-  error(chalk.red(str))
+  console.log(error(chalk.red(str)))
 }
 
 export function errorAndExit(e: Error): never {
   red(e.stack ?? e.message)
   process.exit(1)
 }
+
+export { command, info, error, group, endGroup }
