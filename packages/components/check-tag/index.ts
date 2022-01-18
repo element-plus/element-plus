2e3cc1995632cf64823ca13ad2e17159dfe7049b
Kevin <sxzz@sxzz.moe>
2022-01-18 14:36:37 +0800

    refactor(components): refactor check-tag

--- packages/components/check-tag/index.ts
+++ packages/components/check-tag/index.ts
@@ -1,5 +1,7 @@
 import { withInstall } from '@element-plus/utils/with-install'
-import CheckTag from './src/index.vue'
+import CheckTag from './src/check-tag.vue'
 
 export const ElCheckTag = withInstall(CheckTag)
 export default ElCheckTag
+
+export * from './src/check-tag'
