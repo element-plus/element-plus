4cc113e15aaa26dc494571326ff9fa3a7c46f325
三咲智子 <sxzz@sxzz.moe>
2022-01-18 15:00:46 +0800

    refactor(components): [result] add namespace & instance type (#5453)

--- packages/components/result/src/result.ts
+++ packages/components/result/src/result.ts
@@ -6,6 +6,7 @@ import {
   InfoFilled,
 } from '@element-plus/icons-vue'
 import type { Component, ExtractPropTypes } from 'vue'
+import type Result from './result.vue'
 
 export const IconMap = {
   success: 'icon-success',
@@ -40,3 +41,5 @@ export const resultProps = buildProps({
 } as const)
 
 export type ResultProps = ExtractPropTypes<typeof resultProps>
+
+export type ResultInstance = InstanceType<typeof Result>
