62a38138f67f883546a304eb7bcf36e1692fe560
btea <2356281422@qq.com>
2022-01-19 16:02:05 -0600

    refactor(components): [container] use useNamespace (#5498)

--- packages/components/container/src/main.vue
+++ packages/components/container/src/main.vue
@@ -1,12 +1,20 @@
 <template>
-  <main class="el-main">
+  <main :class="ns.b()">
     <slot></slot>
   </main>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { useNamespace } from '@element-plus/hooks'
 
 export default defineComponent({
   name: 'ElMain',
+  setup() {
+    const ns = useNamespace('main')
+
+    return {
+      ns,
+    }
+  },
 })
 </script>
