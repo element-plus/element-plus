930b7937d1e4f8a553d812d64e842af301155823
Delyan Haralanov <purepear@gmail.com>
2022-01-23 09:40:16 +0200

    feat(components): [el-dialog] remove width validator (#5329)

--- packages/utils/validators.ts
+++ packages/utils/validators.ts
@@ -1,16 +1,3 @@
-import { isNumber } from './util'
-
-export const isValidWidthUnit = (val: string | number): boolean => {
-  if (isNumber(val)) {
-    return true
-  }
-  return (
-    ['px', 'rem', 'em', 'vw', '%', 'vmin', 'vmax'].some((unit) =>
-      val.endsWith(unit)
-    ) || val.startsWith('calc')
-  )
-}
-
 export const isValidComponentSize = (val: string) =>
   ['', 'large', 'default', 'small'].includes(val)
 
