95f09e0bb21e9746d3cb65faf543dfb48a014738
Corgis <738782092@qq.com>
2022-01-25 14:25:59 +0800

    Update date-and-time-formats.vue
    Change to Setup mode

--- docs/examples/datetime-picker/date-and-time-formats.vue
+++ docs/examples/datetime-picker/date-and-time-formats.vue
@@ -38,20 +38,10 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent, ref } from 'vue'
+<script lang="ts" setup>
+import { ref } from 'vue'
 
-export default defineComponent({
-  setup() {
-    const value1 = ref('')
-    const value2 = ref('')
-    const value3 = ref('')
-
-    return {
-      value1,
-      value2,
-      value3,
-    }
-  },
-})
+const value1 = ref('')
+const value2 = ref('')
+const value3 = ref('')
 </script>
