e09b4eece0e22a094807f30c3b310e46fba08dac
msidolphin <msidolphin@outlook.com>
2022-01-26 08:58:14 +0800

    refactor(components): [el-pagination] adjust component size

--- docs/examples/pagination/small-pagination.vue
+++ docs/examples/pagination/small-pagination.vue
@@ -1,3 +1,5 @@
 <template>
   <el-pagination small layout="prev, pager, next" :total="50"> </el-pagination>
+  <el-pagination small background layout="prev, pager, next" :total="50">
+  </el-pagination>
 </template>
