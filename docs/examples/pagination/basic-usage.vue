2030b4c1bf228a0420f2be24550768ee18b7316b
xiaoxian521 <1923740402@qq.com>
2022-01-26 18:28:13 +0800

    docs: fix all style contamination

--- docs/examples/pagination/basic-usage.vue
+++ docs/examples/pagination/basic-usage.vue
@@ -1,10 +1,19 @@
 <template>
-  <div class="block">
-    <span class="demonstration">When you have few pages</span>
+  <div class="example-block">
+    <span class="example-demonstration">When you have few pages</span>
     <el-pagination layout="prev, pager, next" :total="50"></el-pagination>
   </div>
-  <div class="block">
-    <span class="demonstration">When you have more than 7 pages</span>
+  <div class="example-block">
+    <span class="example-demonstration">When you have more than 7 pages</span>
     <el-pagination layout="prev, pager, next" :total="1000"></el-pagination>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.example-block {
+  margin: 1rem;
+}
+.example-demonstration {
+  margin: 1rem;
+}
+</style>
