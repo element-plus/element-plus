9db86d7f5568e42668b50d332741f2a21e9a1573
Thriven-and-Thro <78684021+Thriven-and-Thro@users.noreply.github.com>
2022-02-03 12:22:08 +0800

    第8行组件引入应该为'element-plus/lib/components'
    第8行组件引入应该为'element-plus/lib/components'，否则在编译阶段会报错找不到依赖

--- docs/examples/message/basic.vue
+++ docs/examples/message/basic.vue
@@ -5,7 +5,7 @@
 
 <script lang="ts" setup>
 import { h } from 'vue'
-import { ElMessage } from 'element-plus'
+import { ElMessage } from 'element-plus/lib/components'
 
 const open = () => {
   ElMessage('this is a message.')
