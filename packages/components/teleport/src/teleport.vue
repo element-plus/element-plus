4db23d24266f65e446859460fd9130145fafd44e
bqy <1743369777@qq.com>
2022-02-08 05:44:50 +0800

    refactor(components): [teleport] use useNamespace (#5751)

--- packages/components/teleport/src/teleport.vue
+++ packages/components/teleport/src/teleport.vue
@@ -1,6 +1,6 @@
 <template>
   <teleport v-if="container" :to="container" :disabled="disabled">
-    <div ref="containerRef" class="el-teleport" :style="containerStyle">
+    <div ref="containerRef" :class="ns.b()" :style="containerStyle">
       <slot />
     </div>
   </teleport>
@@ -8,11 +8,13 @@
 
 <script lang="ts">
 import { computed, defineComponent, ref } from 'vue'
+import { useNamespace } from '@element-plus/hooks'
 import { elTeleportProps } from './teleport'
 
 export default defineComponent({
   props: elTeleportProps,
   setup(props) {
+    const ns = useNamespace('teleport')
     const containerRef = ref<HTMLElement>()
     const containerStyle = computed(() => {
       return props.container === 'body'
@@ -28,6 +30,7 @@ export default defineComponent({
         : {}
     })
     return {
+      ns,
       containerRef,
       containerStyle,
     }
