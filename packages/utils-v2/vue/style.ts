81bca48fd401fc376db77bb989172904483a2b82
啝裳 <1923740402@qq.com>
2022-02-10 23:29:04 +0800

    fix: value does not exist, warning appears (#5906)

--- packages/utils-v2/vue/style.ts
+++ packages/utils-v2/vue/style.ts
@@ -4,11 +4,11 @@ import { debugWarn } from '../error'
 const SCOPE = 'utils-v2/vue/style'
 
 export function addUnit(value: string | number, defaultUnit = 'px') {
+  if (!value) return ''
   if (isString(value)) {
     return value
   } else if (isNumber(value)) {
     return `${value}${defaultUnit}`
   }
   debugWarn(SCOPE, 'binding value must be a string or number')
-  return ''
 }
