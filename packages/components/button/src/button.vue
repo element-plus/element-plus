e3e81616fa04d111456522838a7ac1585e80f174
Kevin <sxzz@sxzz.moe>
2022-02-10 21:05:44 +0800

    refactor(components): refactor button

--- packages/components/button/src/button.vue
+++ packages/components/button/src/button.vue
@@ -35,8 +35,8 @@
   </button>
 </template>
 
-<script lang="ts">
-import { computed, inject, defineComponent, Text, ref } from 'vue'
+<script lang="ts" setup>
+import { computed, inject, Text, ref, useSlots } from 'vue'
 import { useCssVar } from '@vueuse/core'
 import { TinyColor } from '@ctrl/tinycolor'
 import { ElIcon } from '@element-plus/components/icon'
@@ -48,120 +48,89 @@ import {
   useSize,
 } from '@element-plus/hooks'
 import { buttonGroupContextKey } from '@element-plus/tokens'
-import { Loading } from '@element-plus/icons-vue'
-
 import { buttonEmits, buttonProps } from './button'
 
-export default defineComponent({
+defineOptions({
   name: 'ElButton',
-
-  components: {
-    ElIcon,
-    Loading,
-  },
-
-  props: buttonProps,
-  emits: buttonEmits,
-
-  setup(props, { emit, slots }) {
-    const buttonRef = ref()
-    const buttonGroupContext = inject(buttonGroupContextKey, undefined)
-    const globalConfig = useGlobalConfig('button')
-    const ns = useNamespace('button')
-    const autoInsertSpace = computed(
-      () =>
-        props.autoInsertSpace ?? globalConfig.value?.autoInsertSpace ?? false
-    )
-
-    // add space between two characters in Chinese
-    const shouldAddSpace = computed(() => {
-      const defaultSlot = slots.default?.()
-      if (autoInsertSpace.value && defaultSlot?.length === 1) {
-        const slot = defaultSlot[0]
-        if (slot?.type === Text) {
-          const text = slot.children
-          return /^\p{Unified_Ideograph}{2}$/u.test(text as string)
-        }
-      }
-      return false
-    })
-
-    const { form } = useFormItem()
-    const buttonSize = useSize(computed(() => buttonGroupContext?.size))
-    const buttonDisabled = useDisabled()
-    const buttonType = computed(
-      () => props.type || buttonGroupContext?.type || ''
-    )
-
-    // calculate hover & active color by color
-    const typeColor = computed(
-      () => useCssVar(`--el-color-${props.type}`).value
-    )
-    const buttonStyle = computed(() => {
-      let styles = {}
-
-      const buttonColor = props.color || typeColor.value
-
-      if (buttonColor) {
-        const shadeBgColor = new TinyColor(buttonColor).shade(10).toString()
-        if (props.plain) {
-          styles = {
-            '--el-button-bg-color': new TinyColor(buttonColor)
-              .tint(90)
-              .toString(),
-            '--el-button-text-color': buttonColor,
-            '--el-button-hover-text-color': 'var(--el-color-white)',
-            '--el-button-hover-bg-color': buttonColor,
-            '--el-button-hover-border-color': buttonColor,
-            '--el-button-active-bg-color': shadeBgColor,
-            '--el-button-active-text-color': 'var(--el-color-white)',
-            '--el-button-active-border-color': shadeBgColor,
-          }
-        } else {
-          const tintBgColor = new TinyColor(buttonColor).tint(20).toString()
-          styles = {
-            '--el-button-bg-color': buttonColor,
-            '--el-button-border-color': buttonColor,
-            '--el-button-hover-bg-color': tintBgColor,
-            '--el-button-hover-border-color': tintBgColor,
-            '--el-button-active-bg-color': shadeBgColor,
-            '--el-button-active-border-color': shadeBgColor,
-          }
-        }
-
-        if (buttonDisabled.value) {
-          const disabledButtonColor = new TinyColor(buttonColor)
-            .tint(50)
-            .toString()
-          styles['--el-button-disabled-bg-color'] = disabledButtonColor
-          styles['--el-button-disabled-border-color'] = disabledButtonColor
-        }
-      }
-
-      return styles
-    })
-
-    const handleClick = (evt: MouseEvent) => {
-      if (props.nativeType === 'reset') {
-        form?.resetFields()
-      }
-      emit('click', evt)
-    }
-
-    return {
-      buttonRef,
-      buttonStyle,
-
-      buttonSize,
-      buttonType,
-      buttonDisabled,
-
-      shouldAddSpace,
-
-      handleClick,
-
-      ns,
-    }
-  },
 })
+
+const props = defineProps(buttonProps)
+const emit = defineEmits(buttonEmits)
+const slots = useSlots()
+
+const buttonGroupContext = inject(buttonGroupContextKey, undefined)
+const globalConfig = useGlobalConfig('button')
+const ns = useNamespace('button')
+const { form } = useFormItem()
+const buttonSize = useSize(computed(() => buttonGroupContext?.size))
+const buttonDisabled = useDisabled()
+
+const buttonRef = ref()
+const autoInsertSpace = computed(
+  () => props.autoInsertSpace ?? globalConfig.value?.autoInsertSpace ?? false
+)
+
+// add space between two characters in Chinese
+const shouldAddSpace = computed(() => {
+  const defaultSlot = slots.default?.()
+  if (autoInsertSpace.value && defaultSlot?.length === 1) {
+    const slot = defaultSlot[0]
+    if (slot?.type === Text) {
+      const text = slot.children
+      return /^\p{Unified_Ideograph}{2}$/u.test(text as string)
+    }
+  }
+  return false
+})
+const buttonType = computed(() => props.type || buttonGroupContext?.type || '')
+
+// calculate hover & active color by color
+const typeColor = computed(() => useCssVar(`--el-color-${props.type}`).value)
+const buttonStyle = computed(() => {
+  let styles: Record<string, string> = {}
+
+  const buttonColor = props.color || typeColor.value
+
+  if (buttonColor) {
+    const color = new TinyColor(buttonColor)
+    const shadeBgColor = color.shade(10).toString()
+    if (props.plain) {
+      styles = {
+        '--el-button-bg-color': color.tint(90).toString(),
+        '--el-button-text-color': buttonColor,
+        '--el-button-hover-text-color': 'var(--el-color-white)',
+        '--el-button-hover-bg-color': buttonColor,
+        '--el-button-hover-border-color': buttonColor,
+        '--el-button-active-bg-color': shadeBgColor,
+        '--el-button-active-text-color': 'var(--el-color-white)',
+        '--el-button-active-border-color': shadeBgColor,
+      }
+    } else {
+      const tintBgColor = color.tint(20).toString()
+      styles = {
+        '--el-button-bg-color': buttonColor,
+        '--el-button-border-color': buttonColor,
+        '--el-button-hover-bg-color': tintBgColor,
+        '--el-button-hover-border-color': tintBgColor,
+        '--el-button-active-bg-color': shadeBgColor,
+        '--el-button-active-border-color': shadeBgColor,
+      }
+    }
+
+    if (buttonDisabled.value) {
+      const disabledButtonColor = color.tint(50).toString()
+      styles['--el-button-disabled-bg-color'] = disabledButtonColor
+      styles['--el-button-disabled-border-color'] = disabledButtonColor
+    }
+  }
+
+  return styles
+})
+
+const handleClick = (evt: MouseEvent) => {
+  if (props.nativeType === 'reset') {
+    form?.resetFields()
+  }
+  emit('click', evt)
+}
 </script>
