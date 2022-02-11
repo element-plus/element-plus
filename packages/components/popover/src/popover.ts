bf520480f32dab2f7d34bbaf2130ab64fcb810fb
xiaoxian521 <1923740402@qq.com>
2022-02-11 11:54:01 +0800

    fix: replenish popover missing types

--- packages/components/popover/src/popover.ts
+++ packages/components/popover/src/popover.ts
@@ -1,7 +1,29 @@
 import { buildProps } from '@element-plus/utils-v2'
-import { useTooltipContentProps } from '@element-plus/components/tooltip'
+import {
+  useTooltipContentProps,
+  useTooltipTriggerProps,
+  useTooltipProps,
+} from '@element-plus/components/tooltip'
+import { dropdownProps } from '@element-plus/components/dropdown'
+
+import type { PropType } from 'vue'
+
+const triggers = ['click', 'focus', 'hover', 'manual'] as const
+
+export type Trigger = typeof triggers[number]
 
 export const usePopoverProps = {
+  trigger: {
+    type: [String, Array] as PropType<Trigger | Trigger[]>,
+    default: 'click',
+  },
+  placement: dropdownProps.placement,
+  disabled: useTooltipTriggerProps.disabled,
+  visible: useTooltipContentProps.visible,
+  transition: useTooltipContentProps.transition,
+  showArrow: useTooltipProps.showArrow,
+  popperOptions: dropdownProps.popperOptions,
+  tabindex: dropdownProps.tabindex,
   appendToBody: { type: Boolean, default: undefined },
   content: useTooltipContentProps.content,
   popperStyle: useTooltipContentProps.popperStyle,
@@ -21,6 +43,22 @@ export const usePopoverProps = {
     width: {
       type: [String, Number],
       default: 150,
+    },
+    offset: {
+      type: Number,
+      default: 0,
+    },
+    showAfter: {
+      type: Number,
+      default: 0,
+    },
+    hideAfter: {
+      type: Number,
+      default: 200,
+    },
+    autoClose: {
+      type: Number,
+      default: 0,
     },
   }),
 }
