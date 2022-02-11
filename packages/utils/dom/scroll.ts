55c92f0659b259ba6ac421128edcb0b11d1a3302
JeremyWuuuuu <15975785+JeremyWuuuuu@users.noreply.github.com>
2022-02-11 17:51:33 +0800

    Move scrollIntoView into utils/scroll.ts

--- packages/utils/dom/scroll.ts
+++ packages/utils/dom/scroll.ts
@@ -60,3 +60,38 @@ export const getScrollBarWidth = (): number => {
 
   return scrollBarWidth
 }
+
+export function scrollIntoView(
+  container: HTMLElement,
+  selected: HTMLElement
+): void {
+  if (!isClient) return
+
+  if (!selected) {
+    container.scrollTop = 0
+    return
+  }
+
+  const offsetParents: HTMLElement[] = []
+  let pointer = selected.offsetParent
+  while (
+    pointer !== null &&
+    container !== pointer &&
+    container.contains(pointer)
+  ) {
+    offsetParents.push(pointer as HTMLElement)
+    pointer = (pointer as HTMLElement).offsetParent
+  }
+  const top =
+    selected.offsetTop +
+    offsetParents.reduce((prev, curr) => prev + curr.offsetTop, 0)
+  const bottom = top + selected.offsetHeight
+  const viewRectTop = container.scrollTop
+  const viewRectBottom = viewRectTop + container.clientHeight
+
+  if (top < viewRectTop) {
+    container.scrollTop = top
+  } else if (bottom > viewRectBottom) {
+    container.scrollTop = bottom - container.clientHeight
+  }
+}
