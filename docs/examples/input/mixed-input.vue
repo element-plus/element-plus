1540d8921bc8d42db044d00991bd9aa75f06bd79
msidolphin <msidolphin@outlook.com>
2022-02-16 09:43:07 +0800

    fix: select position

--- docs/examples/input/mixed-input.vue
+++ docs/examples/input/mixed-input.vue
@@ -27,6 +27,24 @@
       </template>
     </el-input>
   </div>
+  <div class="mt-4">
+    <el-input
+      v-model="input3"
+      placeholder="Please input"
+      class="input-with-select"
+    >
+      <template #prepend>
+        <el-button :icon="Search"></el-button>
+      </template>
+      <template #append>
+        <el-select v-model="select" placeholder="Select" style="width: 110px">
+          <el-option label="Restaurant" value="1"></el-option>
+          <el-option label="Order No." value="2"></el-option>
+          <el-option label="Tel" value="3"></el-option>
+        </el-select>
+      </template>
+    </el-input>
+  </div>
 </template>
 
 <script setup lang="ts">
