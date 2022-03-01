f38c28a89a1d8912b58bb2dd15227ac70ca33add
三咲智子 <sxzz@sxzz.moe>
2022-03-01 21:59:38 +0800

    refactor(components): [check-tag] switch to script-setup syntax (#6296)

--- packages/components/check-tag/src/check-tag.vue
+++ packages/components/check-tag/src/check-tag.vue
@@ -1,32 +1,24 @@
 <template>
-  <span :class="[ns.b(), ns.is('checked', checked)]" @click="onChange">
+  <span :class="[ns.b(), ns.is('checked', checked)]" @click="handleChange">
     <slot></slot>
   </span>
 </template>
-<script lang="ts">
-import { defineComponent } from 'vue'
+
+<script lang="ts" setup>
 import { useNamespace } from '@element-plus/hooks'
 import { checkTagProps, checkTagEmits } from './check-tag'
 
-export default defineComponent({
+defineOptions({
   name: 'ElCheckTag',
-
-  props: checkTagProps,
-  emits: checkTagEmits,
-
-  setup(props, { emit }) {
-    const ns = useNamespace('check-tag')
-
-    const onChange = () => {
-      const checked = !props.checked
-      emit('change', checked)
-      emit('update:checked', checked)
-    }
-
-    return {
-      ns,
-      onChange,
-    }
-  },
 })
+const props = defineProps(checkTagProps)
+const emit = defineEmits(checkTagEmits)
+
+const ns = useNamespace('check-tag')
+
+const handleChange = () => {
+  const checked = !props.checked
+  emit('change', checked)
+  emit('update:checked', checked)
+}
 </script>
