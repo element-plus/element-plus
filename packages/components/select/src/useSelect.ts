c28d0919676afec086d937f2954102084ffe12a6
gongjunfei <1491180173@qq.com>
2022-03-07 21:24:10 +0800

    fix(components):[select] (#6446)

--- packages/components/select/src/useSelect.ts
+++ packages/components/select/src/useSelect.ts
@@ -265,7 +265,7 @@ export const useSelect = (props, states: States, ctx) => {
             input.value?.focus()
           } else {
             if (states.selectedLabel) {
-              states.currentPlaceholder = states.selectedLabel
+              states.currentPlaceholder = `${states.selectedLabel}`
               states.selectedLabel = ''
             }
           }
