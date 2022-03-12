525f61f8dadd0b733b6045b44353797dd06d5602
Zapic <45936772+KawaiiZapic@users.noreply.github.com>
2022-03-12 20:44:17 +0800

    fix(component): [el-tabs] the initial pane should only render for once (#6563)
    * fix(component): [el-tabs] the initial pane should only render for once
    
    * test(component): [el-tabs] improve test of lazy load pane
    
    * test(component): [el-tabs] fix lint error
    
    Co-authored-by: KawaiiZapic <kawaiizapic@zapic.moe>

--- packages/components/tabs/src/tab-pane.vue
+++ packages/components/tabs/src/tab-pane.vue
@@ -38,11 +38,11 @@ export default defineComponent({
     if (!tabsRoot) throwError(COMPONENT_NAME, `must use with ElTabs`)
 
     const index = ref<string>()
-    const loaded = ref(false)
     const isClosable = computed(() => props.closable || tabsRoot.props.closable)
     const active = eagerComputed(
       () => tabsRoot.currentName.value === (props.name || index.value)
     )
+    const loaded = ref(active.value)
     const paneName = computed(() => props.name || index.value)
     const shouldBeRender = eagerComputed(
       () => !props.lazy || loaded.value || active.value
