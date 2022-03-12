6f47ac44b70a3a4ee6162f41c531bdee2c2178bc
JeremyWuuuuu <15975785+JeremyWuuuuu@users.noreply.github.com>
2022-03-12 23:44:20 +0800

    Skip commenting when no files have been changed

--- scripts/build-table.ts
+++ scripts/build-table.ts
@@ -17,7 +17,7 @@ async function main() {
     .map((s) => s.split(':'))
 
   if (fileDiffs.length === 0) {
-    output = 'ℹ️ No changes be found'
+    output = ''
   } else {
     const table = fileDiffs.reduce(
       (prev, [source, filename]) => {
