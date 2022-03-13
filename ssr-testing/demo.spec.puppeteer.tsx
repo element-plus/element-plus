390807670cce90747592dea06ac9d7c1f7214b21
JeremyWuuuuu <15975785+JeremyWuuuuu@users.noreply.github.com>
2022-03-13 18:32:59 +0800

    Implement puppeteer with screenshots

--- ssr-testing/demo.spec.puppeteer.tsx
+++ ssr-testing/demo.spec.puppeteer.tsx
@@ -1,66 +1,55 @@
+import path from 'path'
 import { createApp } from 'vue'
 import { renderToString } from '@vue/server-renderer'
-import {
-  afterAll,
-  beforeAll,
-  beforeEach,
-  afterEach,
-  describe,
-  expect,
-  it,
-} from 'vitest'
+import { beforeAll, beforeEach, afterEach, describe, expect, it } from 'vitest'
 import puppeteer from 'puppeteer'
+import glob from 'fast-glob'
 import ElementPlus from '../dist/element-plus'
 
-import type { DefineComponent } from 'vue'
 import type { Browser, Page } from 'puppeteer'
 
-type ESModule = {
-  default: DefineComponent
-}
-
-type ParsedDemo = {
-  filename: string
-  Module: DefineComponent
-}
-
-const demoPrefix = '../../docs/examples'
-const rootDir = `${process.cwd()}/ssr-testing`
-let demos: ParsedDemo[]
+const projectRoot = process.cwd()
+const testRoot = `${projectRoot}/ssr-testing`
+const exampleRoot = path.resolve(projectRoot, 'docs/examples')
 describe('Cypress Button', () => {
   let browser: Browser
-  let page: Page
-
   beforeAll(async () => {
     browser = await puppeteer.launch()
-    demos = Object.entries(
-      (import.meta as any).globEager('../../docs/examples/button/*.vue')
-    ).map(([file, module]) => {
-      return {
-        filename: file.slice(demoPrefix.length),
-        Module: (module as ESModule).default,
-      }
-    })
-  })
-
-  beforeEach(async () => {
-    page = await browser.newPage()
   })
 
   describe('when initialized', () => {
-    console.log(demos)
-    it.each(demos)(`render correctly`, async ({ Module, filename }) => {
-      page.goto('file//:${process.cwd()}/ssr-testing/index.html')
-      const app = createApp(<Module />).use(ElementPlus)
+    const demos = glob
+      .sync(`${projectRoot}/docs/examples/**/*.vue`)
+      .map((demo) => demo.slice(exampleRoot.length + 1))
+    it.each(demos)(`render %s correctly`, async (demoPath) => {
+      const page = await browser.newPage()
+      await page.goto(`file://${projectRoot}/ssr-testing/index.html`)
+      await page.addStyleTag({
+        path: path.join(
+          projectRoot,
+          'dist',
+          'element-plus',
+          'dist',
+          'index.css'
+        ),
+      })
+      const { default: Demo } = await import(path.join(exampleRoot, demoPath))
+      const app = createApp(<Demo />).use(ElementPlus)
       const html = await renderToString(app)
 
       await page.evaluate((innerHTML) => {
         document.querySelector('#root')!.innerHTML = innerHTML
       }, html)
 
+      const screenshotPath = demoPath
+        .split('/')
+        .join('-')
+        .replace(/\.vue$/, '.png')
       await page.screenshot({
-        path: `${rootDir}/screenshots/${filename}`,
+        path: path.join(testRoot, 'screenshots', screenshotPath),
+        fullPage: true,
       })
+      await page.close()
       expect(true).toBe(true)
     })
   })
