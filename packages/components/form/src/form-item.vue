a261e7ccdb8194c24f65e286213510e930439179
liyu <23229437@qq.com>
2022-03-16 15:26:45 +0800

    fix(components): [form-item] emit validate when passed not triggered

--- packages/components/form/src/form-item.vue
+++ packages/components/form/src/form-item.vue
@@ -234,6 +234,7 @@ const doValidate = async (rules: RuleItem[]): Promise<true> => {
     .validate({ [modelName]: fieldValue.value }, { firstFields: true })
     .then(() => {
       setValidationState('success')
+      formContext.emit('validate', props.prop!, true, '')
       return true as const
     })
     .catch((err: FormValidateFailure) => {
