c2cf94eeff4b292bf77170f2cbbf04e0b4edf1b7
plainheart <yhen@all-my-life.cn>
2022-03-17 15:43:22 +0800

    fix(components): [table] fix wrong table width is set to resizeState

--- packages/components/table/src/table-layout.ts
+++ packages/components/table/src/table-layout.ts
@@ -261,7 +261,6 @@ class TableLayout<T> {
       }
 
       this.bodyWidth.value = Math.max(bodyMinWidth, bodyWidth)
-      this.table.state.resizeState.value.width = this.bodyWidth.value
     } else {
       flattenColumns.forEach((column) => {
         if (!column.width && !column.minWidth) {
