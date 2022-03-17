2db8da2a459d2a351d5819c6bbff67c3cf2ea7a7
JeremyWuuuuu <15975785+JeremyWuuuuu@users.noreply.github.com>
2022-03-17 12:21:30 +0800

    Update card, carousel cascader test cases

---
+++ ssr-testing/cases/carousel.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="block">
+    <span class="demonstration"
+      >Switch when indicator is hovered (default)</span
+    >
+    <el-carousel height="150px">
+      <el-carousel-item v-for="item in 4" :key="item">
+        <h3 class="small">{{ item }}</h3>
+      </el-carousel-item>
+    </el-carousel>
+  </div>
+  <div class="block">
+    <span class="demonstration">Switch when indicator is clicked</span>
+    <el-carousel trigger="click" height="150px">
+      <el-carousel-item v-for="item in 4" :key="item">
+        <h3 class="small">{{ item }}</h3>
+      </el-carousel-item>
+    </el-carousel>
+  </div>
+</template>
