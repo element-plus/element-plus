6b74660a20050f05c40be557aaed2f46a9af9dfe
JeremyWuuuuu <15975785+JeremyWuuuuu@users.noreply.github.com>
2022-03-17 22:20:25 +0800

    chore(components): [el-carousel] code style refactor (#6693)
    * chore(components): [el-carousel] code style refactor
    
    - Remove duplicated code
    - Simplified the code for components
    
    * Wrap resize handle into shallowRef
    
    * Export CourouselItemState type
    
    * Enhance itemInStage method
    
    * Refine code

--- packages/tokens/carousel.ts
+++ packages/tokens/carousel.ts
@@ -1,30 +1,29 @@
-import type { InjectionKey, Ref, ToRefs, UnwrapRef } from 'vue'
+import type { InjectionKey, Ref } from 'vue'
 
 import type { CarouselItemProps } from '@element-plus/components/carousel'
 
-export type CarouselItemContext = CarouselItemProps &
-  ToRefs<{
-    hover: boolean
-    translate: number
-    scale: number
-    active: boolean
-    ready: boolean
-    inStage: boolean
-    animating: boolean
-  }> & {
-    uid: number | undefined
-    translateItem: (
-      index: number,
-      activeIndex: number,
-      oldIndex: number
-    ) => void
-  }
+export type CarouselItemStates = {
+  hover: boolean
+  translate: number
+  scale: number
+  active: boolean
+  ready: boolean
+  inStage: boolean
+  animating: boolean
+}
+
+export type CarouselItemContext = {
+  props: CarouselItemProps
+  states: CarouselItemStates
+  uid: number | undefined
+  translateItem: (index: number, activeIndex: number, oldIndex?: number) => void
+}
 
 export type CarouselContext = {
   root: Ref<HTMLElement | undefined>
-  direction: string
-  type: string
-  items: Ref<UnwrapRef<CarouselItemContext[]>>
+  items: Ref<CarouselItemContext[]>
+  isCardType: Ref<boolean>
+  isVertical: Ref<boolean>
   loop: boolean
   addItem: (item: CarouselItemContext) => void
   removeItem: (uid: number | undefined) => void
