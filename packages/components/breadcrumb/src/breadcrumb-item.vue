991e562784e2b987498328deeb452f5d9fa28118
JeremyWuuuuu <15975785+JeremyWuuuuu@users.noreply.github.com>
2022-03-18 23:30:02 +0800

    chore(components): [el-breadcrumb] enhancement
    - Remove some unnecessary code for breadcrumb
    - Update tests

--- packages/components/breadcrumb/src/breadcrumb-item.vue
+++ packages/components/breadcrumb/src/breadcrumb-item.vue
@@ -1,6 +1,11 @@
 <template>
   <span :class="ns.e('item')">
-    <span ref="link" :class="[ns.e('inner'), ns.is('link', !!to)]" role="link">
+    <span
+      ref="link"
+      :class="[ns.e('inner'), ns.is('link', !!to)]"
+      role="link"
+      @click="onClick"
+    >
       <slot />
     </span>
     <el-icon v-if="separatorIcon" :class="ns.e('separator')">
@@ -13,13 +18,14 @@
 </template>
 
 <script lang="ts" setup>
-import { inject, ref, onMounted, getCurrentInstance } from 'vue'
+import { inject, ref, getCurrentInstance } from 'vue'
 import ElIcon from '@element-plus/components/icon'
 import { breadcrumbKey } from '@element-plus/tokens'
 import { useNamespace } from '@element-plus/hooks'
 import { breadcrumbItemProps } from './breadcrumb-item'
 
 import type { Router } from 'vue-router'
+import type { BreadcrumbProps } from './breadcrumb'
 
 defineOptions({
   name: 'ElBreadcrumbItem',
@@ -29,18 +35,16 @@ const props = defineProps(breadcrumbItemProps)
 
 const instance = getCurrentInstance()!
 const router = instance.appContext.config.globalProperties.$router as Router
-const parent = inject(breadcrumbKey, undefined)
+const breadcrumbInjection = inject(breadcrumbKey, {} as BreadcrumbProps)!
+
 const ns = useNamespace('breadcrumb')
 
-const { separator, separatorIcon } = parent ?? {}
+const { separator, separatorIcon } = breadcrumbInjection
 
 const link = ref<HTMLSpanElement>()
 
-onMounted(() => {
-  link.value!.setAttribute('role', 'link')
-  link.value!.addEventListener('click', () => {
-    if (!props.to || !router) return
-    props.replace ? router.replace(props.to) : router.push(props.to)
-  })
-})
+const onClick = () => {
+  if (!props.to || !router) return
+  props.replace ? router.replace(props.to) : router.push(props.to)
+}
 </script>
