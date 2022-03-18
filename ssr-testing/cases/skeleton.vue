6f159e95d9cdf687779fee6ab776e5681f619860
JeremyWuuuuu <15975785+JeremyWuuuuu@users.noreply.github.com>
2022-03-18 15:52:39 +0800

    test(ssr): add ssr testing cases
    - Add more cases for running ssr tests

---
+++ ssr-testing/cases/skeleton.vue
@@ -0,0 +1,9 @@
+<template>
+  <el-skeleton />
+  <br />
+  <el-skeleton style="--el-skeleton-circle-size: 100px">
+    <template #template>
+      <el-skeleton-item variant="circle" />
+    </template>
+  </el-skeleton>
+</template>
---
+++ ssr-testing/cases/steps.vue
@@ -0,0 +1,19 @@
+<template>
+  <el-steps :active="active" finish-status="success">
+    <el-step title="Step 1" />
+    <el-step title="Step 2" />
+    <el-step title="Step 3" />
+  </el-steps>
+
+  <el-button style="margin-top: 12px" @click="next">Next step</el-button>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const active = ref(0)
+
+const next = () => {
+  if (active.value++ > 2) active.value = 0
+}
+</script>
