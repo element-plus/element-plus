bb8609ecc635e38b841773355c4ce13e12b9bcb3
三咲智子 <sxzz@sxzz.moe>
2022-03-22 17:16:46 +0800

    refactor(docs): switch to unocss (#6728)

---
+++ docs/unocss.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig, presetUno, presetAttributify, presetIcons } from 'unocss'
+
+export default defineConfig({
+  presets: [presetUno(), presetAttributify(), presetIcons()],
+  include: [`${__dirname}/**/*`],
+  exclude: [`${__dirname}/node_modules/**/*`],
+  theme: {
+    colors: {
+      primary: {
+        DEFAULT: '#2563eb',
+        deep: '#1d4ed8',
+      },
+    },
+  },
+})
