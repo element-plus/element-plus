71c1b3fa8c6283bc0b6762eb6bebf79bba5121aa
YunYouJun <me@yunyoujun.cn>
2022-03-23 18:29:34 +0800

    refactor(theme-chalk): [button] remove default size class & simplify css

--- packages/constants/size.ts
+++ packages/constants/size.ts
@@ -1,2 +1,2 @@
-export const componentSizes = ['default', 'small', 'large'] as const
+export const componentSizes = ['', 'default', 'small', 'large'] as const
 export type ComponentSize = typeof componentSizes[number]
