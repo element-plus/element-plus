71b4c65b137c8a4640fa28f8c728e367bcd556b4
ElementPlusBot <hello@element-plus.org>
2022-03-24 03:23:16 +0000

    website deploy

--- assets/chunks/controllability.279ef5b1.js
+++
@@ -1,1 +0,0 @@
-var a="/images/consistency.png",s="/images/feedback.png",i="/images/efficiency.png",e="/images/controllability.png";export{a as _,s as a,i as b,e as c};
---
+++ assets/index.md.c50a06f6.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a}from"./app.c8b56773.js";const l='{"title":"A Vue 3 UI Framework","description":"","frontmatter":{"title":"A Vue 3 UI Framework","lang":"en-US","page":true},"headers":[],"relativePath":"index.md","lastUpdated":1648092045000}',r={};function o(n,s,c,d,i,p){return a(),t("div")}var m=e(r,[["render",o]]);export{l as __pageData,m as default};
