b4b7668ec6a09415c2fb06ca841cc27b3968fd26
yujinpan <1192878390@qq.com>
2022-03-26 16:12:16 +0800

    docs(ElTreeSelect): add component docs

---
+++ docs/examples/tree-select/lazy.vue
@@ -0,0 +1,32 @@
+<template>
+  <el-tree-select v-model="value" lazy :load="load" :props="props" />
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const value = ref()
+
+const props = {
+  label: 'label',
+  children: 'children',
+  isLeaf: 'isLeaf',
+}
+
+const load = (node, resolve) => {
+  setTimeout(() => {
+    resolve([
+      {
+        value: Date.now(),
+        label: 'lazy load node',
+      },
+      {
+        value: Date.now() + 1,
+        label: 'lazy load node',
+        isLeaf: true,
+      },
+    ])
+  }, 400)
+}
+</script>
+```
