7b166ed7fdd9f391505e2db5d1d14fdc27679419
JeremyWuuuuu <15975785+JeremyWuuuuu@users.noreply.github.com>
2022-03-26 18:53:56 +0800

    feat(components): [tooltip-v2] trigger implementation (#6844)
    - Implement TooltipV2Trigger
    - Implement TooltipV2Root
    - Fix a potential mem leak issue in OnlyChild
    - Add token for tooltip v2

--- packages/components/tooltip-v2/src/trigger.ts
+++ packages/components/tooltip-v2/src/trigger.ts
@@ -1,9 +1,19 @@
-import { buildProps } from '@element-plus/utils'
+import { buildProps, definePropType } from '@element-plus/utils'
 
 import type { ExtractPropTypes } from 'vue'
 
+const EventHandler = {
+  type: definePropType<(e: Event) => boolean | void>(Function),
+} as const
+
 export const tooltipTriggerV2Props = buildProps({
   asChild: Boolean,
+  onBlur: EventHandler,
+  onClick: EventHandler,
+  onFocus: EventHandler,
+  onMouseDown: EventHandler,
+  onMouseEnter: EventHandler,
+  onMouseLeave: EventHandler,
 } as const)
 
 export type TooltipTriggerV2Props = ExtractPropTypes<
--- packages/components/tooltip-v2/src/trigger.vue
+++ packages/components/tooltip-v2/src/trigger.vue
@@ -8,13 +8,80 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { inject, onBeforeUnmount, ref, watch } from 'vue'
+import { composeEventHandlers } from '@element-plus/utils'
+import { tooltipV2RootKey } from '@element-plus/tokens'
 import OnlyChild from './only-child'
 import { tooltipTriggerV2Props } from './trigger'
 
-defineProps(tooltipTriggerV2Props)
+const props = defineProps(tooltipTriggerV2Props)
+
+/**
+ * onOpen opens the tooltip instantly, onTrigger acts a lil bit differently,
+ * it will check if delayDuration is set to greater than 0 and based on that result,
+ * if true, it opens the tooltip after delayDuration, otherwise it opens it instantly.
+ */
+const { onClose, onOpen, onDelayOpen } = inject(tooltipV2RootKey)!
+
+let isMousedown = false
+
 const triggerRef = ref<HTMLElement | null>(null)
 const setTriggerRef = (el: HTMLElement | null) => {
   triggerRef.value = el
 }
+
+const onMouseenter = composeEventHandlers(props.onMouseEnter, onDelayOpen)
+
+const onMouseleave = composeEventHandlers(props.onMouseLeave, onClose)
+
+const onMouseup = () => {
+  isMousedown = false
+}
+
+const onMousedown = composeEventHandlers(props.onMouseDown, () => {
+  onClose()
+  isMousedown = true
+  document.addEventListener('mouseup', onMouseup, { once: true })
+})
+
+const onFocus = composeEventHandlers(props.onFocus, () => {
+  if (!isMousedown) onOpen()
+})
+
+const onBlur = composeEventHandlers(props.onBlur, onClose)
+
+const onClick = composeEventHandlers(props.onClick, (e) => {
+  if ((e as MouseEvent).detail === 0) onClose()
+})
+
+const events = {
+  blur: onBlur,
+  click: onClick,
+  focus: onFocus,
+  mousedown: onMousedown,
+  mouseenter: onMouseenter,
+  mouseleave: onMouseleave,
+}
+
+const setEvents = <T extends (e: Event) => void>(
+  el: HTMLElement | null | undefined,
+  events: Record<string, T>,
+  type: 'addEventListener' | 'removeEventListener'
+) => {
+  if (el) {
+    Object.entries(events).forEach(([name, handler]) => {
+      el[type](name, handler)
+    })
+  }
+}
+
+watch(triggerRef, (triggerEl, previousTriggerEl) => {
+  setEvents(triggerEl, events, 'addEventListener')
+  setEvents(previousTriggerEl, events, 'removeEventListener')
+})
+
+onBeforeUnmount(() => {
+  setEvents(triggerRef.value, events, 'removeEventListener')
+  document.removeEventListener('mouseup', onMouseup)
+})
 </script>
