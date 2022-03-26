9919e0a8678331f7a540f5fe283a7922da07d32a
JeremyWuuuuu <15975785+JeremyWuuuuu@users.noreply.github.com>
2022-03-26 13:33:24 +0800

    feat(components): [tooltip-v2] add new component (#6838)
    * feat(components): [tooltip-v2] add new component
    
    - Init component
    
    * Implement trigger and only child
    
    * Fix typing issue

---
+++ packages/components/tooltip-v2/src/only-child.tsx
@@ -0,0 +1,38 @@
+import { Fragment, defineComponent, ref } from 'vue'
+import {
+  buildProps,
+  composeRefs,
+  definePropType,
+  ensureOnlyChild,
+} from '@element-plus/utils'
+
+import type { ExtractPropTypes, VNodeArrayChildren } from 'vue'
+
+export type RefSetter = (el: HTMLElement | null) => void
+
+export const forwardRefProps = buildProps({
+  setRef: { type: definePropType<RefSetter>(Function), required: true },
+} as const)
+
+export type ForwardRefProps = ExtractPropTypes<typeof forwardRefProps>
+
+// TODO: consider make this component a reusable component without the only child feature.
+export default defineComponent({
+  props: forwardRefProps,
+  setup(props, { slots }) {
+    const fragmentRef = ref()
+    const setRef = composeRefs(fragmentRef, (el) => {
+      // vue fragments is represented as a text element.
+      // The first element sibling should be the first element children of fragment.
+      // This is how we get the element.
+      props.setRef((el as HTMLElement).nextElementSibling as HTMLElement | null)
+    })
+    return () => {
+      const [firstChild] = slots.default?.() || []
+      const child = ensureOnlyChild(firstChild.children as VNodeArrayChildren)
+      // Dunno why the ref for jsx complains about the typing issue which was not
+      // in template
+      return <Fragment ref={setRef as any}>{child}</Fragment>
+    }
+  },
+})
---
+++ packages/components/tooltip-v2/src/trigger.vue
@@ -0,0 +1,20 @@
+<template>
+  <only-child v-if="asChild" :set-ref="setTriggerRef">
+    <slot />
+  </only-child>
+  <button v-else ref="triggerRef">
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import OnlyChild from './only-child'
+import { tooltipTriggerV2Props } from './trigger'
+
+defineProps(tooltipTriggerV2Props)
+const triggerRef = ref<HTMLElement | null>(null)
+const setTriggerRef = (el: HTMLElement | null) => {
+  triggerRef.value = el
+}
+</script>
