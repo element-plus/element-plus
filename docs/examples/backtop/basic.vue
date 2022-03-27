element-plus/element-plus

commit b365fe627852402bcfe970c80d247341fb871cd8

refactor(components): refactor backtop (#3486)

packages/components/backtop/src/backtop.vue
@@ -1,0 +1,89 @@
+<template>
+  <transition name="el-fade-in">
+    <div
+      v-if="visible"
+      :style="{
+        right: styleRight,
+        bottom: styleBottom,
+      }"
+      class="el-backtop"
+      @click.stop="handleClick"
+    >
+      <slot>
+        <i class="el-icon-caret-top"></i>
+      </slot>
+    </div>
+  </transition>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, computed, onMounted, shallowRef } from 'vue'
+import { useEventListener, useThrottleFn } from '@vueuse/core'
+import { easeInOutCubic } from '@element-plus/utils/animation'
+import { throwError } from '@element-plus/utils/error'
+
+import { backtopEmits, backtopProps } from './backtop'
+
+const COMPONENT_NAME = 'ElBacktop'
+
+export default defineComponent({
+  name: COMPONENT_NAME,
+
+  props: backtopProps,
+  emits: backtopEmits,
+
+  setup(props, { emit }) {
+    const el = shallowRef<HTMLElement | undefined>(document.documentElement)
+    const container = shallowRef<Document | HTMLElement>(document)
+    const visible = ref(false)
+    const styleBottom = computed(() => `${props.bottom}px`)
+    const styleRight = computed(() => `${props.right}px`)
+
+    const scrollToTop = () => {
+      if (!el.value) return
+      const beginTime = Date.now()
+      const beginValue = el.value.scrollTop
+      const frameFunc = () => {
+        if (!el.value) return
+        const progress = (Date.now() - beginTime) / 500
+        if (progress < 1) {
+          el.value.scrollTop = beginValue * (1 - easeInOutCubic(progress))
+          requestAnimationFrame(frameFunc)
+        } else {
+          el.value.scrollTop = 0
+        }
+      }
+      requestAnimationFrame(frameFunc)
+    }
+    const handleScroll = () => {
+      if (el.value) visible.value = el.value.scrollTop >= props.visibilityHeight
+    }
+    const handleClick = (event: MouseEvent) => {
+      scrollToTop()
+      emit('click', event)
+    }
+
+    const handleScrollThrottled = useThrottleFn(handleScroll, 300)
+
+    onMounted(() => {
+      if (props.target) {
+        el.value =
+          document.querySelector<HTMLElement>(props.target) ?? undefined
+        if (!el.value) {
+          throwError(COMPONENT_NAME, `target is not existed: ${props.target}`)
+        }
+        container.value = el.value
+      }
+
+      useEventListener(container, 'scroll', handleScrollThrottled)
+    })
+
+    return {
+      visible,
+      styleBottom,
+      styleRight,
+      handleClick,
+    }
+  },
+})
+</script>

packages/components/backtop/__tests__/backtop.spec.ts
@@ -1,5 +1,6 @@
+import { nextTick } from 'vue'
 import { mount } from '@vue/test-utils'
-import Backtop from '../src/index.vue'
+import Backtop from '../src/backtop.vue'
 
 const _mount = (template: string) =>
   mount(
@@ -21,6 +22,8 @@ describe('Backtop.vue', () => {
         </div>
       </div>
     `)
+    await nextTick()
+
     expect(wrapper.find('.el-backtop').exists()).toBe(false)
     wrapper.element.scrollTop = 2000
     await wrapper.trigger('scroll')

packages/components/backtop/index.ts
@@ -1,13 +1,8 @@
-import Backtop from './src/index.vue'
-import type { App } from 'vue'
+import { withInstall } from '@element-plus/utils/with-install'
 
-import type { SFCWithInstall } from '@element-plus/utils/types'
+import Backtop from './src/backtop.vue'
 
-Backtop.install = (app: App): void => {
-  app.component(Backtop.name, Backtop)
-}
+export const ElBacktop = withInstall(Backtop)
+export default ElBacktop
 
-const _Backtop = Backtop as SFCWithInstall<typeof Backtop>
-
-export default _Backtop
-export const ElBacktop = _Backtop
+export * from './src/backtop'

packages/components/backtop/src/backtop.ts
@@ -1,0 +1,26 @@
+import type { ExtractPropTypes } from 'vue'
+
+export const backtopProps = {
+  visibilityHeight: {
+    type: Number,
+    default: 200,
+  },
+  target: {
+    type: String,
+    default: '',
+  },
+  right: {
+    type: Number,
+    default: 40,
+  },
+  bottom: {
+    type: Number,
+    default: 40,
+  },
+} as const
+export type BacktopProps = ExtractPropTypes<typeof backtopProps>
+
+export const backtopEmits = {
+  click: (evt: MouseEvent) => evt instanceof MouseEvent,
+}
+export type BacktopEmits = typeof backtopEmits

packages/components/backtop/src/index.vue
@@ -1,114 +1,0 @@
-<template>
-  <transition name="el-fade-in">
-    <div
-      v-if="visible"
-      :style="{
-        right: styleRight,
-        bottom: styleBottom,
-      }"
-      class="el-backtop"
-      @click.stop="handleClick"
-    >
-      <slot>
-        <i class="el-icon-caret-top"></i>
-      </slot>
-    </div>
-  </transition>
-</template>
-
-<script lang="ts">
-import { defineComponent, ref, computed, onMounted, onBeforeUnmount } from 'vue'
-import throttle from 'lodash/throttle'
-import { on, off } from '@element-plus/utils/dom'
-import { easeInOutCubic } from '@element-plus/utils/animation'
-import { throwError } from '@element-plus/utils/error'
-
-interface IElBacktopProps {
-  visibilityHeight: number
-  target: string
-  right: number
-  bottom: number
-}
-
-export default defineComponent({
-  name: 'ElBacktop',
-  props: {
-    visibilityHeight: {
-      type: Number,
-      default: 200,
-    },
-    target: {
-      type: String,
-      default: '',
-    },
-    right: {
-      type: Number,
-      default: 40,
-    },
-    bottom: {
-      type: Number,
-      default: 40,
-    },
-  },
-  emits: ['click'],
-  setup(props: IElBacktopProps, ctx) {
-    const el = ref(null)
-    const container = ref(null)
-    const visible = ref(false)
-    const styleBottom = computed(() => `${props.bottom}px`)
-    const styleRight = computed(() => `${props.right}px`)
-    const scope = 'ElBackTop'
-
-    const scrollToTop = () => {
-      const beginTime = Date.now()
-      const beginValue = el.value.scrollTop
-      const rAF =
-        window.requestAnimationFrame || ((func) => setTimeout(func, 16))
-      const frameFunc = () => {
-        const progress = (Date.now() - beginTime) / 500
-        if (progress < 1) {
-          el.value.scrollTop = beginValue * (1 - easeInOutCubic(progress))
-          rAF(frameFunc)
-        } else {
-          el.value.scrollTop = 0
-        }
-      }
-      rAF(frameFunc)
-    }
-    const onScroll = () => {
-      visible.value = el.value.scrollTop >= props.visibilityHeight
-    }
-    const handleClick = (event) => {
-      scrollToTop()
-      ctx.emit('click', event)
-    }
-
-    const throttledScrollHandler = throttle(onScroll, 300)
-
-    onMounted(() => {
-      container.value = document
-      el.value = document.documentElement
-      if (props.target) {
-        el.value = document.querySelector(props.target)
-        if (!el.value) {
-          throwError(scope, `target is not existed: ${props.target}`)
-        }
-        container.value = el.value
-      }
-      on(container.value, 'scroll', throttledScrollHandler)
-    })
-    onBeforeUnmount(() => {
-      off(container.value, 'scroll', throttledScrollHandler)
-    })
-
-    return {
-      el,
-      container,
-      visible,
-      styleBottom,
-      styleRight,
-      handleClick,
-    }
-  },
-})
-</script>

commit 31382714600c8e1c4e8d72acbef5643a85e3108a

fix(theme-chalk): [backtop] caused by #3486

docs/examples/backtop/basic.vue
@@ -1,4 +1,4 @@
 <template>
   Scroll down to see the bottom-right button.
-  <el-backtop />
+  <el-backtop :right="100" :bottom="100" />
 </template>

docs/examples/backtop/custom.vue
@@ -6,7 +6,7 @@
         height: 100%;
         width: 100%;
         background-color: #f2f5f6;
-        box-shadow: 0 0 6px rgba(0, 0, 0, 0.12);
+        box-shadow: var(--el-box-shadow-lighter);
         text-align: center;
         line-height: 40px;
         color: #1989fa;

packages/components/backtop/src/backtop.vue
@@ -2,10 +2,7 @@
   <transition :name="`${ns.namespace.value}-fade-in`">
     <div
       v-if="visible"
-      :style="{
-        right: styleRight,
-        bottom: styleBottom,
-      }"
+      :style="backTopStyle"
       :class="ns.b()"
       @click.stop="handleClick"
     >
@@ -39,8 +36,11 @@ const ns = useNamespace('backtop')
 const el = shallowRef<HTMLElement>()
 const container = shallowRef<Document | HTMLElement>()
 const visible = ref(false)
-const styleBottom = computed(() => `${props.bottom}px`)
-const styleRight = computed(() => `${props.right}px`)
+
+const backTopStyle = computed(() => ({
+  right: `${props.right}px`,
+  bottom: `${props.bottom}px`,
+}))
 
 const scrollToTop = () => {
   if (!el.value) return
@@ -69,6 +69,9 @@ const handleClick = (event: MouseEvent) => {
 const handleScrollThrottled = useThrottleFn(handleScroll, 300)
 
 onMounted(() => {
+  container.value = document
+  el.value = document.documentElement
+
   if (props.target) {
     el.value = document.querySelector<HTMLElement>(props.target) ?? undefined
     if (!el.value) {

packages/theme-chalk/src/backtop.scss
@@ -6,11 +6,11 @@
   @include set-component-css-var('backtop', $backtop);
 
   position: fixed;
-  background-color: var(--el-backtop-bg-color);
+  background-color: getCssVar('backtop', 'bg-color');
   width: 40px;
   height: 40px;
   border-radius: 50%;
-  color: var(--el-backtop-text-color);
+  color: getCssVar('backtop', 'text-color');
   display: flex;
   align-items: center;
   justify-content: center;
@@ -20,7 +20,7 @@
   z-index: 5;
 
   &:hover {
-    background-color: var(--el-backtop-hover-bg-color);
+    background-color: getCssVar('backtop', 'hover-bg-color');
   }
 
   @include e(icon) {