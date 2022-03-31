5d0ee3ab3482682c5a47f54e8270a6d304cd765e
云游君 <me@yunyoujun.cn>
2022-03-31 10:12:52 +0800

    fix: remove default size warn by set componetSizes (#6908)
    * fix: remove default size warn by set componetSizes
    
    * fix: default avatar size

--- packages/components/tag/src/tag.ts
+++ packages/components/tag/src/tag.ts
@@ -1,4 +1,5 @@
 import { buildProps } from '@element-plus/utils'
+import { componentSizes } from '@element-plus/constants'
 import type Tag from './tag.vue'
 
 import type { ExtractPropTypes } from 'vue'
@@ -18,7 +19,8 @@ export const tagProps = buildProps({
   },
   size: {
     type: String,
-    values: ['large', 'default', 'small'],
+    values: componentSizes,
+    default: '',
   },
   effect: {
     type: String,
