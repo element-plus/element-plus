337622ed95b031a803932c4cd537b766716679e5
JeremyWuuuuu <15975785+JeremyWuuuuu@users.noreply.github.com>
2022-03-31 17:02:12 +0800

    feat(components): [tag] add new API for rounded
    - Add new api `round` for rounded tag.
    - Add documentation for rounded tag.
    - Update document styles.

--- packages/components/tag/src/tag.ts
+++ packages/components/tag/src/tag.ts
@@ -27,6 +27,7 @@ export const tagProps = buildProps({
     values: ['dark', 'light', 'plain'],
     default: 'light',
   },
+  round: Boolean,
 } as const)
 export type TagProps = ExtractPropTypes<typeof tagProps>
 
