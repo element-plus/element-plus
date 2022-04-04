4a8cb8578ce7ac6bfdf9fbd908a79973cb8f9592
YunYouJun <me@yunyoujun.cn>
2022-04-04 19:24:11 +0800

    feat(theme-chalk): dark palette & common color mixins

--- docs/.vitepress/vitepress/components/navbar/vp-search.vue
+++ docs/.vitepress/vitepress/components/navbar/vp-search.vue
@@ -176,6 +176,7 @@ function initialize(userOptions: any) {
   --docsearch-footer-background: var(--bg-color);
   --docsearch-footer-shadow: 0 -1px 0 0 #e0e3e8,
     0 -3px 6px 0 rgba(69, 98, 155, 0.12);
+  --docsearch-searchbox-background: var(--bg-color);
   --docsearch-searchbox-focus-background: var(--bg-color-mute);
   --docsearch-muted-color: var(--text-color-lighter);
   --docsearch-text-color: var(--text-color-light);
