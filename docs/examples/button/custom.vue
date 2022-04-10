3bc07ce552e66430e671375848778e2d6919edb8
YunYouJun <me@yunyoujun.cn>
2022-04-10 23:38:38 +0800

    fix: custom button placeholder

--- docs/examples/button/custom.vue
+++ docs/examples/button/custom.vue
@@ -4,12 +4,12 @@ import { isDark } from '~/composables/dark'
 
 <template>
   <div class="flex">
-    <el-button color="#626aef" :dark="isDark">Light</el-button>
-    <el-button color="#626aef" :dark="isDark" plain>Light</el-button>
+    <el-button color="#626aef" :dark="isDark">Default</el-button>
+    <el-button color="#626aef" :dark="isDark" plain>Plain</el-button>
 
     <el-button color="#626aef" :dark="isDark" disabled>Disabled</el-button>
     <el-button color="#626aef" :dark="isDark" disabled plain
-      >Disabled</el-button
+      >Disabled Plain</el-button
     >
   </div>
 </template>
