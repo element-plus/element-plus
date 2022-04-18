f0fde733bca59509a5f4c261649d3cfa81c7c492
ElementPlusBot <hello@element-plus.org>
2022-04-18 15:57:36 +0000

    website deploy

--- assets/zh-CN_resource_index.md.c64f7bd3.js
+++
@@ -1,1 +0,0 @@
-import{c as t,a,r as o,o as r}from"./app.02fe6a17.js";const p='{"title":"","description":"","frontmatter":{"page":true,"lang":"zh-CN"},"headers":[],"relativePath":"zh-CN/resource/index.md","lastUpdated":null}',s={},u=Object.assign(s,{setup(n){return(c,l)=>{const e=o("Resource");return r(),t("div",null,[a(e)])}}});export{p as __pageData,u as default};
