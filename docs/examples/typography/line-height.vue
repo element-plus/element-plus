d26fa7342098879c0696cb14c75426cdf0ff1626
iamkun <kunhello@outlook.com>
2022-04-23 22:22:39 +0800

    docs: update typography to support dark mode (#7342)

--- docs/examples/typography/line-height.vue
+++ docs/examples/typography/line-height.vue
@@ -1,6 +1,15 @@
+<script lang="ts" setup>
+import { isDark } from '~/composables/dark'
+</script>
+
 <template>
   <div>
-    <img class="lineH-left" src="/images/typography.png" />
+    <img
+      v-if="isDark"
+      class="lineH-left"
+      src="/images/typography/line-height-dark.png"
+    />
+    <img v-else class="lineH-left" src="/images/typography/line-height.png" />
     <ul class="lineH-right">
       <li>line-height:1 <span>No line height</span></li>
       <li>line-height:1.3 <span>Compact</span></li>
