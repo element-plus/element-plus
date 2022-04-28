8303c58d3744ec928bb890e343028aaff010ca90
ElementPlusBot <hello@element-plus.org>
2022-04-28 06:09:17 +0000

    website deploy

---
+++ assets/en-US_resource_index.md.73d812b6.lean.js
@@ -0,0 +1,1 @@
+import{c as t,a,r as o,o as r}from"./app.168e005b.js";const p='{"title":"","description":"","frontmatter":{"page":true,"lang":"en-US"},"headers":[],"relativePath":"en-US/resource/index.md","lastUpdated":1651125791000}',n={},d=Object.assign(n,{setup(s){return(c,_)=>{const e=o("Resource");return r(),t("div",null,[a(e)])}}});export{p as __pageData,d as default};
