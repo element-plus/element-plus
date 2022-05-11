30a22c5fe2b636249320269be66a1b5ca85991b6
chenxch <124118265@qq.com>
2022-05-11 23:44:04 +0800

    fix: date-pick format error

--- packages/components/time-picker/src/common/date-utils.ts
+++ packages/components/time-picker/src/common/date-utils.ts
@@ -3,13 +3,22 @@ export const rangeArr = (n: number) =>
 
 export const extractDateFormat = (format: string) => {
   return format
-    .replace(/\W?m{1,2}|\W?ZZ/g, '')
-    .replace(/\W?h{1,2}|\W?s{1,3}|\W?a/gi, '')
+    .replace(/\W?m{1,2}.*|\W?ZZ.*/g, '')
+    .replace(/\W?h{1,2}.*|\W?s{1,3}.*|\W?a.*/gi, '')
     .trim()
 }
 
 export const extractTimeFormat = (format: string) => {
-  return format
-    .replace(/\W?D{1,2}|\W?Do|\W?d{1,4}|\W?M{1,4}|\W?Y{2,4}/g, '')
+  let result = format
+    .replace(
+      /(\W?D{1,2}|\W?Do|\W?d{1,4}|\W?M{1,4}|\W?Y{2,4}).*(\W?h{1,2}|\W?H{1,2}|\W?m{1,2}|\W?s{1,3}|\W?Z{1,2}|\W?A{1,2}|\W?a{1,2})/g,
+      '$2'
+    )
     .trim()
+  let end = format.indexOf(result) - 1
+  while (end > 0 && format[end] === result[0]) {
+    result = `${format[end]}${result}`
+    end--
+  }
+  return result
 }
