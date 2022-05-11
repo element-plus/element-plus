16910e0f2db5b7f3d3f3ef3af95a7e70894d62e7
chenxch <124118265@qq.com>
2022-05-12 00:32:31 +0800

    fix: date-utils time format

--- packages/components/time-picker/src/common/date-utils.ts
+++ packages/components/time-picker/src/common/date-utils.ts
@@ -3,22 +3,18 @@ export const rangeArr = (n: number) =>
 
 export const extractDateFormat = (format: string) => {
   return format
-    .replace(/\W?m{1,2}.*|\W?ZZ.*/g, '')
+    .replace(/\W?m{1,2}.*|\W?Z{1,2}.*/g, '')
     .replace(/\W?h{1,2}.*|\W?s{1,3}.*|\W?a.*/gi, '')
     .trim()
 }
 
 export const extractTimeFormat = (format: string) => {
   let result = format
-    .replace(
-      /(\W?D{1,2}|\W?Do|\W?d{1,4}|\W?M{1,4}|\W?Y{2,4}).*(\W?h{1,2}|\W?H{1,2}|\W?m{1,2}|\W?s{1,3}|\W?Z{1,2}|\W?A{1,2}|\W?a{1,2})/g,
-      '$2'
-    )
+    .replace(/\W?D{1,2}|\W?Do|\W?d{1,4}|\W?M{1,4}|\W?Y{2,4}/g, '')
     .trim()
-  let end = format.indexOf(result) - 1
-  while (end > 0 && format[end] === result[0]) {
-    result = `${format[end]}${result}`
-    end--
+  const timeChart = ['H', 'h', 'm', 's', 'Z', 'A', 'a']
+  while (result && !timeChart.includes(result[0])) {
+    result = result.slice(1)
   }
   return result
 }
