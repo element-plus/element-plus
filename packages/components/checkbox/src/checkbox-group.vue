cdeb83234244298c1c6a5b1da385817f2e02bba7
chenxch <124118265@qq.com>
2022-05-21 16:47:38 +0800

    refactor(checkbox-group): switch to script-setup synatx

--- packages/components/checkbox/src/checkbox-group.vue
+++ packages/components/checkbox/src/checkbox-group.vue
@@ -1,14 +1,18 @@
-<script lang="ts">
-import {
-  computed,
-  defineComponent,
-  h,
-  nextTick,
-  provide,
-  renderSlot,
-  toRefs,
-  watch,
-} from 'vue'
+<template>
+  <component
+    :is="tag"
+    :id="groupId"
+    :class="ns.b('group')"
+    role="group"
+    :aria-label="!isLabeledByFormItem ? label || 'checkbox-group' : undefined"
+    :aria-labelledby="isLabeledByFormItem ? elFormItem.labelId : undefined"
+  >
+    <slot />
+  </component>
+</template>
+
+<script lang="ts" setup>
+import { computed, nextTick, provide, toRefs, watch } from 'vue'
 import { UPDATE_MODEL_EVENT } from '@element-plus/constants'
 import { debugWarn } from '@element-plus/utils'
 import { useNamespace, useSize } from '@element-plus/hooks'
@@ -18,68 +22,48 @@ import {
   useCheckboxGroupProps,
 } from './checkbox'
 
-export default defineComponent({
+defineOptions({
   name: 'ElCheckboxGroup',
+})
 
-  props: useCheckboxGroupProps,
+const props = defineProps(useCheckboxGroupProps)
+const emit = defineEmits([UPDATE_MODEL_EVENT, 'change'])
 
-  emits: [UPDATE_MODEL_EVENT, 'change'],
+const { elFormItem } = useCheckboxGroup()
+const { groupId, isLabeledByFormItem } = useCheckboxGroupId(props, {
+  elFormItem,
+})
+const checkboxGroupSize = useSize()
+const ns = useNamespace('checkbox')
 
-  setup(props, { emit, slots }) {
-    const { elFormItem } = useCheckboxGroup()
-    const { groupId, isLabeledByFormItem } = useCheckboxGroupId(props, {
-      elFormItem,
-    })
-    const checkboxGroupSize = useSize()
-    const ns = useNamespace('checkbox')
+const changeEvent = (value) => {
+  emit(UPDATE_MODEL_EVENT, value)
+  nextTick(() => {
+    emit('change', value)
+  })
+}
 
-    const changeEvent = (value) => {
-      emit(UPDATE_MODEL_EVENT, value)
-      nextTick(() => {
-        emit('change', value)
-      })
-    }
-
-    const modelValue = computed({
-      get() {
-        return props.modelValue
-      },
-      set(val) {
-        changeEvent(val)
-      },
-    })
-
-    provide('CheckboxGroup', {
-      name: 'ElCheckboxGroup',
-      modelValue,
-      ...toRefs(props),
-      checkboxGroupSize,
-      changeEvent,
-    })
-
-    watch(
-      () => props.modelValue,
-      () => {
-        elFormItem.validate?.('change').catch((err) => debugWarn(err))
-      }
-    )
-    return () => {
-      return h(
-        props.tag,
-        {
-          id: groupId.value,
-          class: ns.b('group'),
-          role: 'group',
-          'aria-label': !isLabeledByFormItem.value
-            ? props.label || 'checkbox-group'
-            : undefined,
-          'aria-labelledby': isLabeledByFormItem.value
-            ? elFormItem.labelId
-            : undefined,
-        },
-        [renderSlot(slots, 'default')]
-      )
-    }
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const modelValue = computed({
+  get() {
+    return props.modelValue
+  },
+  set(val) {
+    changeEvent(val)
   },
 })
+
+provide('CheckboxGroup', {
+  name: 'ElCheckboxGroup',
+  ...toRefs(props),
+  checkboxGroupSize,
+  changeEvent,
+})
+
+watch(
+  () => props.modelValue,
+  () => {
+    elFormItem.validate?.('change').catch((err) => debugWarn(err))
+  }
+)
 </script>
