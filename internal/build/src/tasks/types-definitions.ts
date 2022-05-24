7ff199c60fcf30032e8dd1d88c340026abf6854a
三咲智子 <sxzz@sxzz.moe>
2022-05-24 22:54:34 +0800

    fix(build): enhance type check (#7880)
    * fix(build): enhance type check
    
    * feat: stricter ts check

--- internal/build/src/tasks/types-definitions.ts
+++ internal/build/src/tasks/types-definitions.ts
@@ -3,9 +3,9 @@ import path from 'path'
 import fs from 'fs/promises'
 import consola from 'consola'
 import * as vueCompiler from 'vue/compiler-sfc'
-import { Project } from 'ts-morph'
 import glob from 'fast-glob'
 import chalk from 'chalk'
+import { Project } from 'ts-morph'
 import {
   buildOutput,
   epRoot,
@@ -15,105 +15,41 @@ import {
 } from '@element-plus/build-utils'
 import { pathRewriter } from '../utils'
 import typeUnsafe from '../type-unsafe.json'
-
-import type { SourceFile } from 'ts-morph'
+import typeUnsafeStricter from '../type-unsafe-stricter.json'
+import type { CompilerOptions, SourceFile } from 'ts-morph'
 
 const TSCONFIG_PATH = path.resolve(projRoot, 'tsconfig.web.json')
 const outDir = path.resolve(buildOutput, 'types')
-
-// Type unsafe list. The TS errors are not all fixed yet, so we need a list of which files are not fixed with TS errors to prevent accidental TS errors.
-const typeUnsafePaths = typeUnsafe.map((_path) => {
-  let paths = path.resolve(projRoot, _path)
-  if (_path.endsWith('/')) paths += path.sep
-  return paths
-})
 
 /**
  * fork = require( https://github.com/egoist/vue-dts-gen/blob/main/src/index.ts
  */
 export const generateTypesDefinitions = async () => {
+  const compilerOptions: CompilerOptions = {
+    emitDeclarationOnly: true,
+    outDir,
+    baseUrl: projRoot,
+    preserveSymlinks: true,
+    skipLibCheck: true,
+    noImplicitAny: false,
+  }
   const project = new Project({
-    compilerOptions: {
-      emitDeclarationOnly: true,
-      outDir,
-      baseUrl: projRoot,
-      paths: {
-        '@element-plus/*': ['packages/*'],
-      },
-      preserveSymlinks: true,
-      types: [
-        path.resolve(projRoot, 'typings/env'),
-        'unplugin-vue-define-options',
-      ],
-    },
+    compilerOptions,
     tsConfigFilePath: TSCONFIG_PATH,
     skipAddingFilesFromTsConfig: true,
   })
 
-  const globAnyFile = '**/*.{js?(x),ts?(x),vue}'
-  const filePaths = excludeFiles(
-    await glob([globAnyFile, '!element-plus/**/*'], {
-      cwd: pkgRoot,
-      absolute: true,
-      onlyFiles: true,
-    })
-  )
-  const epPaths = excludeFiles(
-    await glob(globAnyFile, {
-      cwd: epRoot,
-      onlyFiles: true,
-    })
-  )
+  const sourceFiles = await addSourceFiles(project)
+  consola.success('Added source files')
 
-  const sourceFiles: SourceFile[] = []
-  await Promise.all([
-    ...filePaths.map(async (file) => {
-      if (file.endsWith('.vue')) {
-        const content = await fs.readFile(file, 'utf-8')
-        const sfc = vueCompiler.parse(content)
-        const { script, scriptSetup } = sfc.descriptor
-        if (script || scriptSetup) {
-          let content = script?.content ?? ''
+  typeCheck(project, typeUnsafeStricter)
+  consola.success('Stricter type check passed!')
 
-          if (scriptSetup) {
-            const compiled = vueCompiler.compileScript(sfc.descriptor, {
-              id: 'xxx',
-            })
-            content += compiled.content
-          }
+  compilerOptions.noImplicitAny = false
+  project.compilerOptions.set(compilerOptions)
 
-          const lang = scriptSetup?.lang || script?.lang || 'js'
-          const sourceFile = project.createSourceFile(
-            `${path.relative(process.cwd(), file)}.${lang}`,
-            content
-          )
-          sourceFiles.push(sourceFile)
-        }
-      } else {
-        const sourceFile = project.addSourceFileAtPath(file)
-        sourceFiles.push(sourceFile)
-      }
-    }),
-    ...epPaths.map(async (file) => {
-      const content = await fs.readFile(path.resolve(epRoot, file), 'utf-8')
-      sourceFiles.push(
-        project.createSourceFile(path.resolve(pkgRoot, file), content)
-      )
-    }),
-  ])
-
-  const diagnostics = project.getPreEmitDiagnostics().filter((diagnostic) => {
-    const filePath = diagnostic.getSourceFile()?.getFilePath()!
-    if (!filePath) return false
-    const file = path.normalize(filePath)
-    return !typeUnsafePaths.some((safePath) => file.startsWith(safePath))
-  })
-  if (diagnostics.length > 0) {
-    consola.error(project.formatDiagnosticsWithColorAndContext(diagnostics))
-    const err = new Error('Failed to generate dts.')
-    consola.error(err)
-    throw err
-  }
+  typeCheck(project, typeUnsafe)
+  consola.success('Type check passed!')
 
   await project.emit({
     emitOnlyDtsFiles: true,
@@ -156,4 +92,85 @@ export const generateTypesDefinitions = async () => {
   })
 
   await Promise.all(tasks)
+}
+
+async function addSourceFiles(project: Project) {
+  project.addSourceFileAtPath(path.resolve(projRoot, 'typings/env.d.ts'))
+
+  const globSourceFile = '**/*.{js?(x),ts?(x),vue}'
+  const filePaths = excludeFiles(
+    await glob([globSourceFile, '!element-plus/**/*'], {
+      cwd: pkgRoot,
+      absolute: true,
+      onlyFiles: true,
+    })
+  )
+  const epPaths = excludeFiles(
+    await glob(globSourceFile, {
+      cwd: epRoot,
+      onlyFiles: true,
+    })
+  )
+
+  const sourceFiles: SourceFile[] = []
+  await Promise.all([
+    ...filePaths.map(async (file) => {
+      if (file.endsWith('.vue')) {
+        const content = await fs.readFile(file, 'utf-8')
+        const sfc = vueCompiler.parse(content)
+        const { script, scriptSetup } = sfc.descriptor
+        if (script || scriptSetup) {
+          let content = script?.content ?? ''
+
+          if (scriptSetup) {
+            const compiled = vueCompiler.compileScript(sfc.descriptor, {
+              id: 'xxx',
+            })
+            content += compiled.content
+          }
+
+          const lang = scriptSetup?.lang || script?.lang || 'js'
+          const sourceFile = project.createSourceFile(
+            `${path.relative(process.cwd(), file)}.${lang}`,
+            content
+          )
+          sourceFiles.push(sourceFile)
+        }
+      } else {
+        const sourceFile = project.addSourceFileAtPath(file)
+        sourceFiles.push(sourceFile)
+      }
+    }),
+    ...epPaths.map(async (file) => {
+      const content = await fs.readFile(path.resolve(epRoot, file), 'utf-8')
+      sourceFiles.push(
+        project.createSourceFile(path.resolve(pkgRoot, file), content)
+      )
+    }),
+  ])
+
+  return sourceFiles
+}
+
+function typeCheck(project: Project, paths: string[]) {
+  // Type unsafe list. The TS errors are not all fixed yet, so we need a list of which files are not fixed with TS errors to prevent accidental TS errors.
+  const typeUnsafePaths = paths.map((_path) => {
+    let paths = path.resolve(projRoot, _path)
+    if (_path.endsWith('/')) paths += path.sep
+    return paths
+  })
+
+  const diagnostics = project.getPreEmitDiagnostics().filter((diagnostic) => {
+    const filePath = diagnostic.getSourceFile()?.getFilePath()!
+    if (!filePath) return false
+    const file = path.normalize(filePath)
+    return !typeUnsafePaths.some((safePath) => file.startsWith(safePath))
+  })
+
+  if (diagnostics.length > 0) {
+    consola.error(project.formatDiagnosticsWithColorAndContext(diagnostics))
+    const err = new Error('Failed to generate dts.')
+    consola.error(err)
+    throw err
+  }
 }
