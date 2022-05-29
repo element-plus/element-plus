76e5e24efad23b06562709a51529169873f3b111
三咲智子 <sxzz@sxzz.moe>
2022-05-29 09:14:51 +0800

    refactor: improve code

--- packages/components/calendar/src/calendar.ts
+++ packages/components/calendar/src/calendar.ts
@@ -1,4 +1,9 @@
-import { buildProps, definePropType } from '@element-plus/utils'
+import {
+  buildProps,
+  definePropType,
+  isArray,
+  isDate,
+} from '@element-plus/utils'
 import { INPUT_EVENT, UPDATE_MODEL_EVENT } from '@element-plus/constants'
 import type { ExtractPropTypes } from 'vue'
 import type Calendar from './calendar.vue'
@@ -11,9 +16,7 @@ export type CalendarDateType =
   | 'today'
 
 const isValidRange = (range: unknown): range is [Date, Date] =>
-  Array.isArray(range) &&
-  range.length === 2 &&
-  range.every((item) => item instanceof Date)
+  isArray(range) && range.length === 2 && range.every((item) => isDate(item))
 
 export const calendarProps = buildProps({
   modelValue: {
@@ -27,8 +30,8 @@ export const calendarProps = buildProps({
 export type CalendarProps = ExtractPropTypes<typeof calendarProps>
 
 export const calendarEmits = {
-  [UPDATE_MODEL_EVENT]: (value: Date) => value instanceof Date,
-  [INPUT_EVENT]: (value: Date) => value instanceof Date,
+  [UPDATE_MODEL_EVENT]: (value: Date) => isDate(value),
+  [INPUT_EVENT]: (value: Date) => isDate(value),
 }
 export type CalendarEmits = typeof calendarEmits
 
