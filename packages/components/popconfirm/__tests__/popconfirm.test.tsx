a9842c26c21482a8f6dd80680fec48ad12f9b368
Xc <124118265@qq.com>
2022-05-30 09:39:31 +0800

    refactor(components): [popconfirm] use JSX in Unit test (#7969)

--- packages/components/popconfirm/__tests__/popconfirm.test.tsx
+++ packages/components/popconfirm/__tests__/popconfirm.test.tsx
@@ -1,29 +1,12 @@
-import { h, nextTick } from 'vue'
+import { nextTick } from 'vue'
 import { mount } from '@vue/test-utils'
 import { afterEach, describe, expect, it } from 'vitest'
 import { rAF } from '@element-plus/test-utils/tick'
 import { POPPER_CONTAINER_SELECTOR } from '@element-plus/hooks'
-import ElPopconfirm from '../src/popconfirm.vue'
+import Popconfirm from '../src/popconfirm.vue'
 
 const AXIOM = 'rem is the best girl'
 const selector = '.el-popper'
-const _mount = (props: any = {}) =>
-  mount(ElPopconfirm, {
-    props: {
-      ...props,
-    },
-    slots: {
-      reference: () =>
-        h(
-          'div',
-          {
-            class: 'reference',
-          },
-          [AXIOM]
-        ),
-    },
-    attachTo: 'body',
-  })
 
 describe('Popconfirm.vue', () => {
   afterEach(() => {
@@ -31,17 +14,23 @@ describe('Popconfirm.vue', () => {
   })
 
   it('render test', async () => {
-    const wrapper = _mount()
+    const wrapper = mount(() => (
+      <>
+        <Popconfirm
+          attachTo="body"
+          v-slots={{
+            reference: () => <div class="reference">{AXIOM}</div>,
+          }}
+        />
+      </>
+    ))
     await nextTick()
-    const trigger = wrapper.find('.reference')
 
     expect(document.querySelector(selector)!.getAttribute('style')).toContain(
       'display: none'
     )
 
-    await trigger.trigger('click', {
-      button: 0,
-    })
+    await wrapper.find('.reference').trigger('click')
 
     await nextTick()
     await rAF()
@@ -54,7 +43,16 @@ describe('Popconfirm.vue', () => {
   describe('teleported API', () => {
     it('should mount on popper container', async () => {
       expect(document.body.innerHTML).toBe('')
-      _mount()
+      mount(() => (
+        <>
+          <Popconfirm
+            attachTo="body"
+            v-slots={{
+              reference: () => <div class="reference">{AXIOM}</div>,
+            }}
+          />
+        </>
+      ))
 
       await nextTick()
       expect(
@@ -64,7 +62,17 @@ describe('Popconfirm.vue', () => {
 
     it('should not mount on the popper container', async () => {
       expect(document.body.innerHTML).toBe('')
-      _mount({ teleported: false })
+      mount(() => (
+        <>
+          <Popconfirm
+            attachTo="body"
+            teleported={false}
+            v-slots={{
+              reference: () => <div class="reference">{AXIOM}</div>,
+            }}
+          />
+        </>
+      ))
 
       await nextTick()
       expect(
