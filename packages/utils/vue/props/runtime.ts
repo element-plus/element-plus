2f8e410bcd26facf8555f4e9b62e6fdd84abc583
三咲智子 <sxzz@sxzz.moe>
2022-06-01 00:53:13 +0800

    chore: add comments

--- packages/utils/vue/props/runtime.ts
+++ packages/utils/vue/props/runtime.ts
@@ -22,6 +22,26 @@ export const definePropType = <T>(val: any): PropType<T> => val
 export const isEpProp = (val: unknown): val is EpProp<any, any, any> =>
   isObject(val) && !!(val as any)[epPropKey]
 
+/**
+ * @description Build prop. It can better optimize prop types
+ * @description 生成 prop，能更好地优化类型
+ * @example
+  // limited options
+  // the type will be PropType<'light' | 'dark'>
+  buildProp({
+    type: String,
+    values: ['light', 'dark'],
+  } as const)
+  * @example
+  // limited options and other types
+  // the type will be PropType<'small' | 'large' | number>
+  buildProp({
+    type: [String, Number],
+    values: ['small', 'large'],
+    validator: (val: unknown): val is number => typeof val === 'number',
+  } as const)
+  @link see more: https://github.com/element-plus/element-plus/pull/3341
+ */
 export const buildProp = <
   Type = never,
   Value = never,
