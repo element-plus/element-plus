0a465d6c004e57bde10a19c32bdcda88f46d8f36
JeremyWuuuuu <15975785+JeremyWuuuuu@users.noreply.github.com>
2022-05-31 21:15:07 +0800

    chore: modify

--- packages/components/container/__tests__/container.test.tsx
+++ packages/components/container/__tests__/container.test.tsx
@@ -11,102 +11,104 @@ import Footer from '../src/footer.vue'
 const AXIOM = 'Rem is the best girl'
 
 describe('Container.vue', () => {
-  test('render test', () => {
-    const wrapper = mount(() => (
-      <Container
-        v-slots={{
-          default: () => AXIOM,
-        }}
-      />
-    ))
-
+  test('container render test', () => {
+    const wrapper = mount(Container, {
+      slots: {
+        default: AXIOM,
+      },
+    })
     expect(wrapper.text()).toEqual(AXIOM)
   })
 
   test('vertical', () => {
-    const wrapper = mount(() => (
-      <Container>
-        <Header></Header>
-        <Main></Main>
-      </Container>
-    ))
+    const TestComponent = {
+      template: `
+        <el-container>
+          <el-header></el-header>
+          <el-main></el-main>
+        </el-container>`,
+      components: {
+        'el-container': Container,
+        'el-header': Header,
+        'el-main': Main,
+      },
+    }
+
+    const wrapper = mount(TestComponent)
     expect(wrapper.classes('is-vertical')).toBe(true)
   })
 
-  test('direction', async () => {
-    const wrapper = mount(
-      {
-        render() {
-          return (
-            <Container direction={this.direction}>
-              <Header></Header>
-              <Main></Main>
-            </Container>
-          )
-        },
-      },
-      {
-        data() {
-          return {
-            direction: 'horizontal',
-          }
-        },
-      }
+  test('direction', () => {
+    const TestComponent = () => (
+      <Container direction={direction.value}>
+        <Header></Header>
+        <Main></Main>
+      </Container>
     )
+    const wrapper = mount(TestComponent)
     expect(wrapper.vm.$el.classList.contains('is-vertical')).toBe(false)
-    wrapper.setData({
-      direction: 'vertical',
+    wrapper.vm.direction = 'vertical'
+    wrapper.vm.$nextTick(() => {
+      expect(wrapper.vm.$el.classList.contains('is-vertical')).toBe(true)
     })
-    await wrapper.vm.$nextTick()
-    expect(wrapper.vm.$el.classList.contains('is-vertical')).toBe(true)
   })
 })
 
 describe('Header', () => {
   test('create header', () => {
-    const wrapper = mount(() => <Header />)
+    const wrapper = mount(Header)
     expect(wrapper.classes()).toContain('el-header')
   })
 
   test('header height', () => {
-    const height = '100px'
-    const wrapper = mount(() => <Header height={height} />)
-
-    expect(getCssVariable(wrapper.vm.$el, '--el-header-height')).toEqual(height)
+    const wrapper = mount(Header, {
+      props: {
+        height: '100px',
+      },
+    })
+    const vm = wrapper.vm
+    expect(getCssVariable(vm.$el, '--el-header-height')).toEqual('100px')
   })
 })
 
 describe('Aside', () => {
   test('aside create', () => {
-    const wrapper = mount(() => <Aside />)
+    const wrapper = mount(Aside)
     expect(wrapper.classes()).toContain('el-aside')
   })
 
   test('aside width', () => {
-    const width = '200px'
-    const wrapper = mount(() => <Aside width={width} />)
+    const wrapper = mount(Aside, {
+      props: {
+        width: '200px',
+      },
+    })
 
-    expect(getCssVariable(wrapper.vm.$el, '--el-aside-width')).toEqual(width)
+    const vm = wrapper.vm
+    expect(getCssVariable(vm.$el, '--el-aside-width')).toEqual('200px')
   })
 })
 
 describe('Main', () => {
   test('main create', () => {
-    const wrapper = mount(() => <Main />)
+    const wrapper = mount(Main)
     expect(wrapper.classes()).toContain('el-main')
   })
 })
 
 describe('Footer', () => {
   test('footer create', () => {
-    const wrapper = mount(() => <Footer />)
+    const wrapper = mount(Footer)
     expect(wrapper.classes()).toContain('el-footer')
   })
 
   test('footer height', () => {
-    const height = '100px'
-    const wrapper = mount(() => <Footer height={height} />)
-
-    expect(getCssVariable(wrapper.vm.$el, '--el-footer-height')).toEqual(height)
+    const wrapper = mount(Footer, {
+      props: {
+        height: '100px',
+      },
+    })
+    const vm = wrapper.vm
+    expect(getCssVariable(vm.$el, '--el-footer-height')).toEqual('100px')
   })
 })
