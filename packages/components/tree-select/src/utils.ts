ebba322c36b2ac01db797e4c8bf66075f3f13980
yujinpan <1192878390@qq.com>
2022-06-05 12:59:53 +0800

    feat(components): [TreeSelect] `treeFind` support find parent node

--- packages/components/tree-select/src/utils.ts
+++ packages/components/tree-select/src/utils.ts
@@ -1,3 +1,5 @@
+import type { TreeNodeData } from '@element-plus/components/tree/src/tree.type'
+
 export function isValidValue(val: any) {
   return val || val === 0
 }
@@ -10,18 +12,48 @@ export function toValidArray(val: any) {
   return Array.isArray(val) ? val : isValidValue(val) ? [val] : []
 }
 
-export function treeFind<T extends Record<string, any>>(
+type TreeCallback<T extends TreeNodeData, R> = (
+  data: T,
+  index: number,
+  array: T[],
+  parent?: T
+) => R
+
+type TreeFindCallback<T extends TreeNodeData> = TreeCallback<T, boolean>
+
+export function treeFind<T extends TreeNodeData>(
   treeData: T[],
-  findCallback: (data: T) => boolean,
-  getChildren: (data: T) => any
-): T | undefined {
-  for (const data of treeData) {
-    if (findCallback(data)) {
-      return data
+  findCallback: TreeFindCallback<T>,
+  getChildren: (data: T) => T[]
+): T | undefined
+export function treeFind<T extends TreeNodeData, R>(
+  treeData: T[],
+  findCallback: TreeFindCallback<T>,
+  getChildren: (data: T) => T[],
+  resultCallback?: TreeCallback<T, R>,
+  parent?: T
+): R | undefined
+export function treeFind<T extends TreeNodeData, R>(
+  treeData: T[],
+  findCallback: TreeFindCallback<T>,
+  getChildren: (data: T) => T[],
+  resultCallback?: TreeCallback<T, R>,
+  parent?: T
+): T | R | undefined {
+  for (let i = 0; i < treeData.length; i++) {
+    const data = treeData[i]
+    if (findCallback(data, i, treeData, parent)) {
+      return resultCallback ? resultCallback(data, i, treeData, parent) : data
     } else {
       const children = getChildren(data)
       if (isValidArray(children)) {
-        const find = treeFind(children, findCallback, getChildren)
+        const find = treeFind(
+          children,
+          findCallback,
+          getChildren,
+          resultCallback,
+          data
+        )
         if (find) return find
       }
     }
