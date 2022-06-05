bf97c0fa25f2d7855ecd6501cb441389b28996f6
Zhengqbbb <1074059947@qq.com>
2022-06-05 21:01:09 +0800

    chore: add commitizen dev dep

--- packages/components/dialog/index.ts
+++ packages/components/dialog/index.ts
@@ -3,6 +3,6 @@ import Dialog from './src/dialog.vue'
 
 export const ElDialog = withInstall(Dialog)
 export default ElDialog
-
+console.log(1111)
 export * from './src/use-dialog'
 export * from './src/dialog'
