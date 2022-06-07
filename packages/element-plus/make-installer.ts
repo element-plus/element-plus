edaa6f6bc9736753574c049a919d4d9bc4b29b1a
三咲智子 <sxzz@sxzz.moe>
2022-06-07 13:44:57 +0800

    fix: vue app typing (#8137)
    closes #8122

--- packages/element-plus/make-installer.ts
+++ packages/element-plus/make-installer.ts
@@ -1,15 +1,8 @@
 import { provideGlobalConfig } from '@element-plus/hooks'
+import { INSTALLED_KEY } from '@element-plus/constants'
 import { version } from './version'
 import type { App, Plugin } from '@vue/runtime-core'
 import type { ConfigProviderContext } from '@element-plus/tokens'
-
-const INSTALLED_KEY = Symbol('INSTALLED_KEY')
-
-declare module '@vue/runtime-core' {
-  interface App {
-    [INSTALLED_KEY]?: boolean
-  }
-}
 
 export const makeInstaller = (components: Plugin[] = []) => {
   const install = (app: App, options?: ConfigProviderContext) => {
