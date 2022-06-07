90f33da2454feb48221623bb132c7813a32a37b6
Faizal Andyka <logustra@gmail.com>
2022-06-07 12:08:21 +0700

    refactor(components): [link] use JSX in Unit test (#8140)

--- packages/components/link/__tests__/link.test.tsx
+++ packages/components/link/__tests__/link.test.tsx
@@ -6,47 +6,36 @@ const AXIOM = 'Rem is the best girl'
 
 describe('Link.vue', () => {
   it('render test', () => {
-    const wrapper = mount(Link, {
-      slots: {
-        default: AXIOM,
-      },
-    })
+    const wrapper = mount(() => <Link>{AXIOM}</Link>)
+
     expect(wrapper.text()).toEqual(AXIOM)
   })
 
   it('it should handle click event when link is not disabled', async () => {
-    const wrapper = mount(Link, {
-      slots: {
-        default: AXIOM,
-      },
-    })
+    const wrapper = mount(() => <Link>{AXIOM}</Link>)
 
-    await wrapper.find('.el-link').trigger('click')
+    await wrapper.trigger('click')
     expect(wrapper.emitted('click')).toHaveLength(1)
   })
 
   it('it should disable click when link is disabled', async () => {
-    const wrapper = mount(Link, {
-      slots: {
-        default: AXIOM,
-      },
-      props: {
-        disabled: true,
-      },
-    })
+    const wrapper = mount(() => <Link disabled>{AXIOM}</Link>)
 
-    await wrapper.find('.el-link').trigger('click')
-    expect(wrapper.emitted('click')).toBeUndefined()
+    expect(wrapper.classes()).toContain('is-disabled')
+    expect(wrapper.attributes('href')).toBeUndefined()
   })
 
   it('icon slots', () => {
     const linkName = 'test link'
-    const wrapper = mount(Link, {
-      slots: {
-        default: linkName,
-        icon: AXIOM,
-      },
-    })
+
+    const wrapper = mount(() => (
+      <Link
+        v-slots={{
+          default: () => linkName,
+          icon: () => AXIOM,
+        }}
+      />
+    ))
     expect(wrapper.text()).toContain(linkName)
     expect(wrapper.text()).toContain(AXIOM)
   })
