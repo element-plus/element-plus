5202fecfd70c4c6b7169fbc087720dbe653f2410
gongjunfei <1491180173@qq.com>
2022-06-09 10:06:03 +0800

    refactor(components): [visual-hidden] visual-hidden (#6114)

---
+++ packages/components/visual-hidden/src/visual-hidden.ts
@@ -0,0 +1,8 @@
+import { buildProps, definePropType } from '@element-plus/utils'
+import type { StyleValue } from 'vue'
+
+export const visualHiddenProps = buildProps({
+  style: {
+    type: definePropType<StyleValue>([String, Object, Array]),
+  },
+})
