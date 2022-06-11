2faac935aedb4dfe3f013255ce4efbb5ba31bf20
0song <82012629+0song@users.noreply.github.com>
2022-06-12 00:09:01 +0800

    chore: update img style (#8223)

--- docs/.vitepress/vitepress/components/sponsors/right-logo-small-list.vue
+++ docs/.vitepress/vitepress/components/sponsors/right-logo-small-list.vue
@@ -9,23 +9,37 @@ const onItemClick = (item: any) => {
 
 <template>
   <div class="m-t-16px flex flex-wrap justify-between">
-    <a
+    <template
       v-for="item in rightLogoSmallSponsors.concat([{} as any])"
       :key="item.name"
-      :href="item.url"
-      :title="`${item.name_cn || item.name} - ${item.slogan_cn || item.slogan}`"
-      target="_blank"
-      @click="onItemClick(item)"
     >
       <div
+        v-if="!item.url"
         :class="[
-          isDark && '!bg-#262729',
+          isDark && '!bg-#262729 color-$text-color-placeholder',
           'flex bg-#F9F9F9 rd-0px h-40px w-89px justify-center items-center',
         ]"
       >
-        <div v-if="!item.url" class="color-#ddd text-13px">Your logo</div>
-        <img v-else class="w-65px h-15px" :src="item.imgL" :alt="item.name" />
+        <div class="color-#ddd text-13px cursor-default">Your logo</div>
       </div>
-    </a>
+      <a
+        v-else
+        :href="item.url"
+        :title="`${item.name_cn || item.name} - ${
+          item.slogan_cn || item.slogan
+        }`"
+        target="_blank"
+        @click="onItemClick(item)"
+      >
+        <div
+          :class="[
+            isDark && '!bg-#262729',
+            'flex bg-#F9F9F9 rd-0px h-40px w-89px justify-center items-center',
+          ]"
+        >
+          <img :src="item.imgL" :alt="item.name" />
+        </div>
+      </a>
+    </template>
   </div>
 </template>
