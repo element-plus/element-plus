c4d8de24cad9ac1ac30214b95f01176cf6f9b408
0song <82012629+0song@users.noreply.github.com>
2022-06-11 16:35:10 +0800

    chore: update readme thanks (#8219)

---
+++ docs/.vitepress/vitepress/components/sponsors/right-logo-small-list.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { rightLogoSmallSponsors } from '../../../config/sponsors'
+import { sendEvent } from '../../../config/analytics'
+import { isDark } from '../../composables/dark'
+const onItemClick = (item: any) => {
+  sendEvent('sp_click', item.name, 'right_logo_small')
+}
+</script>
+
+<template>
+  <div class="m-t-16px flex flex-wrap justify-between">
+    <a
+      v-for="item in rightLogoSmallSponsors.concat([{} as any])"
+      :key="item.name"
+      :href="item.url"
+      :title="`${item.name_cn || item.name} - ${item.slogan_cn || item.slogan}`"
+      target="_blank"
+      @click="onItemClick(item)"
+    >
+      <div
+        :class="[
+          isDark && '!bg-#262729',
+          'flex bg-#F9F9F9 rd-0px h-40px w-89px justify-center items-center',
+        ]"
+      >
+        <div v-if="!item.url" class="color-#ddd text-13px">Your logo</div>
+        <img v-else class="w-65px h-15px" :src="item.imgL" :alt="item.name" />
+      </div>
+    </a>
+  </div>
+</template>
