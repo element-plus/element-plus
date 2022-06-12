1aa9d431292b3f76f44a11aac6375b1b863092a1
Carter Li <CarterLi@users.noreply.github.com>
2022-06-12 22:40:53 +0800

    refactor(components): [switch] Simplify color handling (#8199)
    1. Use CSS variables to set `(in)activeColor` in order not to modify DOM
    2. Deprecate property `(in)activeColor` in favor of CSS variables

--- docs/examples/switch/extended-value-types.vue
+++ docs/examples/switch/extended-value-types.vue
@@ -2,8 +2,7 @@
   <el-tooltip :content="'Switch value: ' + value" placement="top">
     <el-switch
       v-model="value"
-      active-color="#13ce66"
-      inactive-color="#ff4949"
+      style="--el-switch-on-color: #13ce66; --el-switch-off-color: #ff4949"
       active-value="100"
       inactive-value="0"
     />
