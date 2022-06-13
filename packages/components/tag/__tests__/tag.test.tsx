3b315b384d723fbaaa75a02a550db71d6e36ff5e
류한경 <hangyeong.ryu@gmail.com>
2022-06-13 16:02:36 +0900

    refactor(components): [tag] use JSX in Unit test (#8233)

--- packages/components/tag/__tests__/tag.test.tsx
+++ packages/components/tag/__tests__/tag.test.tsx
@@ -6,11 +6,13 @@ const AXIOM = 'Rem is the best girl'
 
 describe('Tag.vue', () => {
   test('render text & class', () => {
-    const wrapper = mount(Tag, {
-      slots: {
-        default: AXIOM,
-      },
-    })
+    const wrapper = mount(() => (
+      <Tag
+        v-slots={{
+          default: () => AXIOM,
+        }}
+      />
+    ))
     expect(wrapper.text()).toEqual(AXIOM)
 
     const vm = wrapper.vm
@@ -22,64 +24,41 @@ describe('Tag.vue', () => {
   })
 
   test('type', () => {
-    const wrapper = mount(Tag, {
-      props: {
-        type: 'success',
-      },
-    })
+    const wrapper = mount(() => <Tag type="success" />)
     const vm = wrapper.vm
     expect(vm.$el.classList.contains('el-tag--success')).toEqual(true)
   })
 
   test('hit', () => {
-    const wrapper = mount(Tag, {
-      props: {
-        hit: true,
-      },
-    })
+    const wrapper = mount(() => <Tag hit={true} />)
     const vm = wrapper.vm
     expect(vm.$el.classList.contains('is-hit')).toEqual(true)
   })
 
   test('closable', async () => {
-    const wrapper = mount(Tag, {
-      props: {
-        closable: true,
-      },
-    })
-    const closeBtn = wrapper.find('.el-tag .el-tag__close')
+    const wrapper = mount(() => <Tag closable={true} />)
+    const comp = wrapper.getComponent(Tag)
+    const closeBtn = comp.find('.el-tag .el-tag__close')
     expect(closeBtn.exists()).toBe(true)
 
     await closeBtn.trigger('click')
-    expect(wrapper.emitted().close).toBeTruthy()
+    expect(comp.emitted().close).toBeTruthy()
   })
 
   test('closeTransition', () => {
-    const wrapper = mount(Tag, {
-      props: {
-        closeTransition: true,
-      },
-    })
+    const wrapper = mount(() => <Tag closeTransition={true} />)
     const vm = wrapper.vm
     expect(vm.$el.classList.contains('md-fade-center')).toEqual(false)
   })
 
   test('color', () => {
-    const wrapper = mount(Tag, {
-      props: {
-        color: 'rgb(0, 0, 0)',
-      },
-    })
+    const wrapper = mount(() => <Tag color="rgb(0, 0, 0)" />)
     const vm = wrapper.vm
     expect(vm.$el.style.backgroundColor).toEqual('rgb(0, 0, 0)')
   })
 
   test('theme', () => {
-    const wrapper = mount(Tag, {
-      props: {
-        effect: 'dark',
-      },
-    })
+    const wrapper = mount(() => <Tag effect="dark" />)
     const vm = wrapper.vm
     const el = vm.$el
     expect(el.className.includes('el-tag--dark')).toEqual(true)
@@ -89,11 +68,7 @@ describe('Tag.vue', () => {
 
   // should also support large size
   test('size', () => {
-    const wrapper = mount(Tag, {
-      props: {
-        size: 'large',
-      },
-    })
+    const wrapper = mount(() => <Tag size="large" />)
     const vm = wrapper.vm
     const el = vm.$el
     expect(el.className.includes('el-tag--large')).toEqual(true)
