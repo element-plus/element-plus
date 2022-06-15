58db250f6e4a39081372777ce54baaf6a51a6961
LinZhanMing <510210521@qq.com>
2022-06-15 22:28:08 +0800

    refactor(components): [tooltip] switch to script-setup syntax

--- packages/components/tooltip/src/tooltip.vue
+++ packages/components/tooltip/src/tooltip.vue
@@ -48,30 +48,20 @@
 </template>
 
 <script lang="ts">
-import {
-  computed,
-  defineComponent,
-  provide,
-  readonly,
-  ref,
-  toRef,
-  unref,
-  watch,
-} from 'vue'
-import {
-  ElPopper,
-  ElPopperArrow,
-  usePopperArrowProps,
-  usePopperProps,
-} from '@element-plus/components/popper'
-
-import { debugWarn, isBoolean, isUndefined } from '@element-plus/utils'
+import { computed, provide, readonly, ref, toRef, unref, watch } from 'vue'
 import {
   createModelToggleComposable,
   useDelayedToggle,
   useId,
   usePopperContainer,
 } from '@element-plus/hooks'
+import {
+  ElPopper,
+  ElPopperArrow,
+  usePopperArrowProps,
+  usePopperProps,
+} from '@element-plus/components/popper'
+import { debugWarn, isBoolean, isUndefined } from '@element-plus/utils'
 import ElTooltipContent from './content.vue'
 import ElTooltipTrigger from './trigger.vue'
 import {
@@ -84,141 +74,139 @@ import { TOOLTIP_INJECTION_KEY } from './tokens'
 const { useModelToggleProps, useModelToggle, useModelToggleEmits } =
   createModelToggleComposable('visible' as const)
 
-export default defineComponent({
+export default {
   name: 'ElTooltip',
-  components: {
-    ElPopper,
-    ElPopperArrow,
-    ElTooltipContent,
-    ElTooltipTrigger,
-  },
-  props: {
-    ...usePopperProps,
-    ...useModelToggleProps,
-    ...useTooltipContentProps,
-    ...useTooltipTriggerProps,
-    ...usePopperArrowProps,
-    ...useTooltipProps,
-  },
-  emits: [
-    ...useModelToggleEmits,
-    'before-show',
-    'before-hide',
-    'show',
-    'hide',
-    'open',
-    'close',
-  ],
-  setup(props, { emit }) {
-    usePopperContainer()
-    const compatShowAfter = computed(() => {
-      if (!isUndefined(props.openDelay)) {
-        debugWarn(
-          'ElTooltip',
-          'open-delay is about to be deprecated in the next major version, please use `show-after` instead'
-        )
-      }
-      return props.openDelay || (props.showAfter as number)
-    })
-    const compatShowArrow = computed(() => {
-      if (!isUndefined(props.visibleArrow)) {
-        debugWarn(
-          'ElTooltip',
-          '`visible-arrow` is about to be deprecated in the next major version, please use `show-arrow` instead'
-        )
-      }
-      return isBoolean(props.visibleArrow)
-        ? props.visibleArrow
-        : props.showArrow
-    })
+}
+</script>
+<script lang="ts" setup>
+/* eslint-disable vue/valid-define-props */
+const props = defineProps({
+  ...usePopperProps,
+  ...useModelToggleProps,
+  ...useTooltipContentProps,
+  ...useTooltipTriggerProps,
+  ...usePopperArrowProps,
+  ...useTooltipProps,
+})
+/* eslint-enable vue/valid-define-props */
+/* eslint-disable vue/valid-define-emits */
+const emit = defineEmits([
+  ...useModelToggleEmits,
+  'before-show',
+  'before-hide',
+  'show',
+  'hide',
+  'open',
+  'close',
+])
+/* eslint-enable vue/valid-define-emits */
 
-    const id = useId()
-    const popperRef = ref<InstanceType<typeof ElPopper> | null>(null)
-    const contentRef = ref<InstanceType<typeof ElTooltipContent> | null>(null)
-
-    const updatePopper = () => {
-      const popperComponent = unref(popperRef)
-      if (popperComponent) {
-        popperComponent.popperInstanceRef?.update()
-      }
-    }
-    const open = ref(false)
-    const toggleReason = ref<Event | undefined>(undefined)
-
-    const { show, hide } = useModelToggle({
-      indicator: open,
-      toggleReason,
-    })
-
-    const { onOpen, onClose } = useDelayedToggle({
-      showAfter: compatShowAfter,
-      hideAfter: toRef(props, 'hideAfter'),
-      open: show,
-      close: hide,
-    })
-
-    const controlled = computed(() => isBoolean(props.visible))
-
-    provide(TOOLTIP_INJECTION_KEY, {
-      controlled,
-      id,
-      open: readonly(open),
-      trigger: toRef(props, 'trigger'),
-      onOpen: (event?: Event) => {
-        onOpen(event)
-      },
-      onClose: (event?: Event) => {
-        onClose(event)
-      },
-      onToggle: (event?: Event) => {
-        if (unref(open)) {
-          onClose(event)
-        } else {
-          onOpen(event)
-        }
-      },
-      onShow: () => {
-        emit('show', toggleReason.value)
-      },
-      onHide: () => {
-        emit('hide', toggleReason.value)
-      },
-      onBeforeShow: () => {
-        emit('before-show', toggleReason.value)
-      },
-      onBeforeHide: () => {
-        emit('before-hide', toggleReason.value)
-      },
-      updatePopper,
-    })
-
-    watch(
-      () => props.disabled,
-      (disabled) => {
-        if (disabled && open.value) {
-          open.value = false
-        }
-      }
+usePopperContainer()
+const compatShowAfter = computed(() => {
+  if (!isUndefined(props.openDelay)) {
+    debugWarn(
+      'ElTooltip',
+      'open-delay is about to be deprecated in the next major version, please use `show-after` instead'
     )
+  }
+  return props.openDelay || (props.showAfter as number)
+})
+const compatShowArrow = computed(() => {
+  if (!isUndefined(props.visibleArrow)) {
+    debugWarn(
+      'ElTooltip',
+      '`visible-arrow` is about to be deprecated in the next major version, please use `show-arrow` instead'
+    )
+  }
+  return isBoolean(props.visibleArrow) ? props.visibleArrow : props.showArrow
+})
 
-    const isFocusInsideContent = () => {
-      const popperContent: HTMLElement | undefined =
-        contentRef.value?.contentRef?.popperContentRef
-      return popperContent && popperContent.contains(document.activeElement)
-    }
+const id = useId()
+const popperRef = ref<InstanceType<typeof ElPopper> | null>(null)
+const contentRef = ref<InstanceType<typeof ElTooltipContent> | null>(null)
 
-    return {
-      compatShowAfter,
-      compatShowArrow,
-      popperRef,
-      contentRef,
-      open,
-      hide,
-      isFocusInsideContent,
-      updatePopper,
-      onOpen,
-      onClose,
+const updatePopper = () => {
+  const popperComponent = unref(popperRef)
+  if (popperComponent) {
+    popperComponent.popperInstanceRef?.update()
+  }
+}
+const open = ref(false)
+const toggleReason = ref<Event | undefined>(undefined)
+
+const { show, hide } = useModelToggle({
+  indicator: open,
+  toggleReason,
+})
+
+const { onOpen, onClose } = useDelayedToggle({
+  showAfter: compatShowAfter,
+  hideAfter: toRef(props, 'hideAfter'),
+  open: show,
+  close: hide,
+})
+
+const controlled = computed(() => isBoolean(props.visible))
+
+provide(TOOLTIP_INJECTION_KEY, {
+  controlled,
+  id,
+  open: readonly(open),
+  trigger: toRef(props, 'trigger'),
+  onOpen: (event?: Event) => {
+    onOpen(event)
+  },
+  onClose: (event?: Event) => {
+    onClose(event)
+  },
+  onToggle: (event?: Event) => {
+    if (unref(open)) {
+      onClose(event)
+    } else {
+      onOpen(event)
     }
   },
+  onShow: () => {
+    emit('show', toggleReason.value)
+  },
+  onHide: () => {
+    emit('hide', toggleReason.value)
+  },
+  onBeforeShow: () => {
+    emit('before-show', toggleReason.value)
+  },
+  onBeforeHide: () => {
+    emit('before-hide', toggleReason.value)
+  },
+  updatePopper,
+})
+
+watch(
+  () => props.disabled,
+  (disabled) => {
+    if (disabled && open.value) {
+      open.value = false
+    }
+  }
+)
+
+const isFocusInsideContent = () => {
+  const popperContent: HTMLElement | undefined =
+    contentRef.value?.contentRef?.popperContentRef
+  return popperContent && popperContent.contains(document.activeElement)
+}
+
+defineExpose({
+  disabled: props.disabled,
+  compatShowAfter,
+  compatShowArrow,
+  popperRef,
+  contentRef,
+  open,
+  hide,
+  isFocusInsideContent,
+  updatePopper,
+  onOpen,
+  onClose,
 })
 </script>
--- packages/components/tooltip/src/trigger.vue
+++ packages/components/tooltip/src/trigger.vue
@@ -17,8 +17,8 @@
   </el-popper-trigger>
 </template>
 
-<script lang="ts">
-import { defineComponent, inject, ref, toRef, unref } from 'vue'
+<script lang="ts" setup>
+import { inject, ref, toRef, unref } from 'vue'
 import { ElPopperTrigger } from '@element-plus/components/popper'
 import { composeEventHandlers } from '@element-plus/utils'
 import { useNamespace } from '@element-plus/hooks'
@@ -28,86 +28,83 @@ import { whenTrigger } from './utils'
 
 import type { OnlyChildExpose } from '@element-plus/components/slot'
 
-export default defineComponent({
+const props = defineProps(useTooltipTriggerProps)
+
+defineOptions({
   name: 'ElTooltipTrigger',
-  components: {
-    ElPopperTrigger,
-  },
-  props: useTooltipTriggerProps,
-  setup(props) {
-    const ns = useNamespace('tooltip')
-    const { controlled, id, open, onOpen, onClose, onToggle } = inject(
-      TOOLTIP_INJECTION_KEY,
-      undefined
-    )!
-    const triggerRef = ref<OnlyChildExpose | null>(null)
+})
 
-    const stopWhenControlledOrDisabled = () => {
-      if (unref(controlled) || props.disabled) {
-        return true
-      }
+const ns = useNamespace('tooltip')
+const { controlled, id, open, onOpen, onClose, onToggle } = inject(
+  TOOLTIP_INJECTION_KEY,
+  undefined
+)!
+const triggerRef = ref<OnlyChildExpose | null>(null)
+
+const stopWhenControlledOrDisabled = () => {
+  if (unref(controlled) || props.disabled) {
+    return true
+  }
+}
+const trigger = toRef(props, 'trigger')
+const onMouseenter = composeEventHandlers(
+  stopWhenControlledOrDisabled,
+  whenTrigger(trigger, 'hover', onOpen)
+)
+const onMouseleave = composeEventHandlers(
+  stopWhenControlledOrDisabled,
+  whenTrigger(trigger, 'hover', onClose)
+)
+const onClick = composeEventHandlers(
+  stopWhenControlledOrDisabled,
+  whenTrigger(trigger, 'click', (e) => {
+    // distinguish left click
+    if ((e as MouseEvent).button === 0) {
+      onToggle(e)
     }
-    const trigger = toRef(props, 'trigger')
-    const onMouseenter = composeEventHandlers(
-      stopWhenControlledOrDisabled,
-      whenTrigger(trigger, 'hover', onOpen)
-    )
-    const onMouseleave = composeEventHandlers(
-      stopWhenControlledOrDisabled,
-      whenTrigger(trigger, 'hover', onClose)
-    )
-    const onClick = composeEventHandlers(
-      stopWhenControlledOrDisabled,
-      whenTrigger(trigger, 'click', (e) => {
-        // distinguish left click
-        if ((e as MouseEvent).button === 0) {
-          onToggle(e)
-        }
-      })
-    )
+  })
+)
 
-    const onFocus = composeEventHandlers(
-      stopWhenControlledOrDisabled,
-      whenTrigger(trigger, 'focus', onOpen)
-    )
+const onFocus = composeEventHandlers(
+  stopWhenControlledOrDisabled,
+  whenTrigger(trigger, 'focus', onOpen)
+)
 
-    const onBlur = composeEventHandlers(
-      stopWhenControlledOrDisabled,
-      whenTrigger(trigger, 'focus', onClose)
-    )
+const onBlur = composeEventHandlers(
+  stopWhenControlledOrDisabled,
+  whenTrigger(trigger, 'focus', onClose)
+)
 
-    const onContextMenu = composeEventHandlers(
-      stopWhenControlledOrDisabled,
-      whenTrigger(trigger, 'contextmenu', (e: Event) => {
-        e.preventDefault()
-        onToggle(e)
-      })
-    )
+const onContextMenu = composeEventHandlers(
+  stopWhenControlledOrDisabled,
+  whenTrigger(trigger, 'contextmenu', (e: Event) => {
+    e.preventDefault()
+    onToggle(e)
+  })
+)
 
-    const onKeydown = composeEventHandlers(
-      stopWhenControlledOrDisabled,
-      (e: KeyboardEvent) => {
-        const { code } = e
-        if (props.triggerKeys.includes(code)) {
-          e.preventDefault()
-          onToggle(e)
-        }
-      }
-    )
-
-    return {
-      onBlur,
-      onContextMenu,
-      onFocus,
-      onMouseenter,
-      onMouseleave,
-      onClick,
-      onKeydown,
-      open,
-      id,
-      triggerRef,
-      ns,
+const onKeydown = composeEventHandlers(
+  stopWhenControlledOrDisabled,
+  (e: KeyboardEvent) => {
+    const { code } = e
+    if (props.triggerKeys.includes(code)) {
+      e.preventDefault()
+      onToggle(e)
     }
-  },
+  }
+)
+
+defineExpose({
+  onBlur,
+  onContextMenu,
+  onFocus,
+  onMouseenter,
+  onMouseleave,
+  onClick,
+  onKeydown,
+  open,
+  id,
+  triggerRef,
+  ns,
 })
 </script>
