2ee3992be02996da6fae187fd0c11a6f263cbb58
三咲智子 <sxzz@sxzz.moe>
2022-06-22 14:07:50 +0800

    chore: upgrade

--- internal/build/src/plugins/ast.ts
+++ internal/build/src/plugins/ast.ts
@@ -1,23 +1,7 @@
 import AST from 'unplugin-ast/rollup'
-import type { CallExpression } from '@babel/types'
-import type { Transformer } from 'unplugin-ast'
-
-export const removeWrapperFunction: Transformer<CallExpression> = {
-  filterNode: (node) =>
-    node.type === 'CallExpression' &&
-    node.callee.type === 'Identifier' &&
-    (node.callee.name === 'definePropType' || node.callee.name === 'mutable'),
-  transform: (node, code) => {
-    const [arg] = node.arguments
-    code.overwrite(
-      node.start!,
-      node.end!,
-      `(${code.slice(arg.start!, arg.end!)})`
-    )
-  },
-}
+import { RemoveWrapperFunction } from 'unplugin-ast/presets'
 
 export const ASTPlugin = () =>
   AST({
-    transformer: [removeWrapperFunction],
+    transformer: [RemoveWrapperFunction(['definePropType', 'mutable'])],
   })
