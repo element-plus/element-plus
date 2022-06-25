7ed11ba12fd4d3612ce5a997415e92e4810fd4bc
LinZhanMing <510210521@qq.com>
2022-06-25 22:45:07 +0800

    refactor: withInstall

--- packages/components/select-v2/index.ts
+++ packages/components/select-v2/index.ts
@@ -1,13 +1,7 @@
+import { withInstall } from '@element-plus/utils'
 import Select from './src/select.vue'
 
-import type { App } from 'vue'
-import type { SFCWithInstall } from '@element-plus/utils'
-
-Select.install = (app: App): void => {
-  app.component(Select.name, Select)
-}
-
-const _Select = Select as SFCWithInstall<typeof Select>
+const _Select = withInstall(Select)
 
 export default _Select
 export const ElSelectV2 = _Select
