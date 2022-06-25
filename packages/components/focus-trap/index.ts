3c0d06862e66b14e98725adda98f3456f17763df
LinZhanMing <510210521@qq.com>
2022-06-25 17:38:05 +0800

    refactor: do not export tokens

--- packages/components/focus-trap/index.ts
+++ packages/components/focus-trap/index.ts
@@ -1,7 +1,6 @@
 import ElFocusTrap from './src/focus-trap.vue'
 
 export default ElFocusTrap
-export * from '../../tokens/focus-trap'
 export * from './src/utils'
 
 export * from './src/focus-trap'
