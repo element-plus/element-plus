299cf928b22c158dbec5d44eacaa959203074170
tolking <qw13131wang@gmail.com>
2022-06-30 21:17:13 +0800

    fix(components): [tabs] types

--- packages/components/tabs/src/tab-bar.vue
+++ packages/components/tabs/src/tab-bar.vue
@@ -5,8 +5,8 @@
     :style="barStyle"
   />
 </template>
+
 <script lang="ts" setup>
-// @ts-nocheck
 import { getCurrentInstance, inject, nextTick, ref, watch } from 'vue'
 import { useResizeObserver } from '@vueuse/core'
 import { capitalize, isNumber, throwError } from '@element-plus/utils'
