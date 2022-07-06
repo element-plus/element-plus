e2cbfb44b20e42afac39bcf192c0d23eceeb573d
0song <0song@gmail.com>
2022-07-06 12:17:17 +0800

    chore: update readme

--- docs/.vitepress/config/sponsors.ts
+++ docs/.vitepress/config/sponsors.ts
@@ -1,11 +1,11 @@
 export const rightRichTextSponsors = [
-  // {
-  //   name: 'Fantastic-admin',
-  //   img: '/images/sponsors/fantasticadmin.png',
-  //   url: 'https://fantastic-admin.netlify.app/?from=element-plus',
-  //   slogan: 'An out-of-the-box backend framework',
-  //   slogan_cn: '开箱即用的 Vue 后台管理框架',
-  // },
+  {
+    name: 'Fantastic-admin',
+    img: '/images/sponsors/fantasticadmin.png',
+    url: 'https://hooray.gitee.io/fantastic-admin/?from=element-plus',
+    slogan: 'An out-of-the-box backend framework',
+    slogan_cn: '开箱即用的管理系统框架为开发提供舒适体验',
+  },
 ]
 
 export const rightLogoSmallSponsors = [
