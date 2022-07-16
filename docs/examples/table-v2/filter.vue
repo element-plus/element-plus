ba426c069f440aafbb75b2a46dd5d2540b76d7f4
KimYangOfCat <hi@kimyang.cn>
2022-07-17 00:12:29 +0800

    fix(docs): fix table v2 filter demo style error

--- docs/examples/table-v2/filter.vue
+++ docs/examples/table-v2/filter.vue
@@ -70,7 +70,7 @@ columns[0].headerCellRenderer = (props: HeaderCellSlotProps) => {
   return (
     <div class="flex items-center justify-center">
       <span class="mr-2 text-xs">{props.column.title}</span>
-      <ElPopover ref={popoverRef} trigger="click">
+      <ElPopover ref={popoverRef} trigger="click" width="180">
         {{
           default: () => (
             <div class="filter-wrapper">
