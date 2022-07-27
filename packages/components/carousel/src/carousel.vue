f61827765438c32c02ded697bd068d96f384ee2c
btea <2356281422@qq.com>
2022-07-27 17:54:45 +0800

    fix(components): [carousel] dynamic set interval

--- packages/components/carousel/src/carousel.vue
+++ packages/components/carousel/src/carousel.vue
@@ -104,7 +104,6 @@ const items = ref<Array<CarouselItemContext>>([])
 const arrowDisplay = computed(
   () => props.arrow !== 'never' && !unref(isVertical)
 )
-const interval = computed(() => props.interval)
 
 const hasLabel = computed(() => {
   return items.value.some((item) => item.props.label.toString().length > 0)
@@ -153,8 +152,8 @@ function pauseTimer() {
 }
 
 function startTimer() {
-  if (interval.value <= 0 || !props.autoplay || timer.value) return
-  timer.value = setInterval(() => playSlides(), interval.value)
+  if (props.interval <= 0 || !props.autoplay || timer.value) return
+  timer.value = setInterval(() => playSlides(), props.interval)
 }
 
 const playSlides = () => {
@@ -297,6 +296,13 @@ watch(
   () => props.loop,
   () => {
     setActiveItem(activeIndex.value)
+  }
+)
+watch(
+  () => props.interval,
+  () => {
+    pauseTimer()
+    startTimer()
   }
 )
 
