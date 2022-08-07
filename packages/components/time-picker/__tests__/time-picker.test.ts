ec3aaed36fdc8641702ff02f3695f6b365daa2bf
baiwusanyu <740132583@qq.com>
2022-08-07 13:17:33 +0800

    feat: [timer-pick] modify the unit test

--- packages/components/time-picker/__tests__/time-picker.test.ts
+++ packages/components/time-picker/__tests__/time-picker.test.ts
@@ -470,7 +470,7 @@ describe('TimePicker', () => {
     )
   })
 
-  it.only('can auto skip when arrow control', async () => {
+  it('can auto skip when disabled', async () => {
     const disabledHoursArr = [
       0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 23,
     ]
