6482a9cc7fb04152d5774c22e329f7333ec3f635
holazz <2418184580@qq.com>
2022-08-23 12:31:12 +0800

    feat(components): [tree-v-2] `setExpandKeys` instead of `setExpendKeys`

--- packages/components/tree-v2/src/tree.vue
+++ packages/components/tree-v2/src/tree.vue
@@ -93,7 +93,7 @@ export default defineComponent({
       getNode,
       expandNode,
       collapseNode,
-      setExpendKeys,
+      setExpandKeys,
     } = useTree(props, ctx.emit)
 
     ctx.expose({
@@ -111,7 +111,7 @@ export default defineComponent({
       getNode,
       expandNode,
       collapseNode,
-      setExpendKeys,
+      setExpandKeys,
     })
 
     return {
