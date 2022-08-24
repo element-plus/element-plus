e1bc244bcba9a9965e26653e491bae001c74616e
Neil Guo <kissme3131@gmail.com>
2022-08-24 13:05:45 +0800

    fix(hooks): [use-z-index] update currentZIndex mechanism

--- packages/hooks/use-z-index/index.ts
+++ packages/hooks/use-z-index/index.ts
@@ -1,14 +1,32 @@
 import { computed, ref } from 'vue'
 import { useGlobalConfig } from '../use-global-config'
 
+const maximumValueOfZIndexAtDocument = () => {
+  const minimumValueOfZIndex = 1
+  let result = minimumValueOfZIndex
+  // Check if it is server-side rendering or not
+  if (typeof window !== 'undefined') {
+    // In client side
+    result = Math.max(
+      minimumValueOfZIndex,
+      ...Array.from(window.document.querySelectorAll('*'))
+        .map((el) => getComputedStyle(el).zIndex)
+        .filter((v) => !Number.isNaN(Number.parseInt(v[1])))
+        .map((o) => Number.parseInt(o))
+    )
+  }
+  return result
+}
 const zIndex = ref(0)
 
 export const useZIndex = () => {
   const initialZIndex = useGlobalConfig('zIndex', 2000) // TODO: move to @element-plus/constants
-  const currentZIndex = computed(() => initialZIndex.value + zIndex.value)
+  const currentZIndex = computed(() =>
+    Math.max(initialZIndex.value, zIndex.value)
+  )
 
   const nextZIndex = () => {
-    zIndex.value++
+    zIndex.value = maximumValueOfZIndexAtDocument()
     return currentZIndex.value
   }
 
