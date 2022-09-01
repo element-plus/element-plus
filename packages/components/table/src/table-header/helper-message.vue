71765c26a1fc4d6aaec9580c9a210af433d66432
laizuan <1032299142@qq.com>
2022-09-01 18:06:21 +0800

    Merge remote-tracking branch 'element-plus/dev' into dev

---
+++ packages/components/table/src/table-header/helper-message.vue
@@ -0,0 +1,35 @@
+<template>
+  <el-tooltip :content="helperMessage" :placement="helperPlacement">
+    <el-icon :style="{ color: helperIconColor }" :class="ns.e('helper-icon')">
+      <component :is="helperIcon" />
+    </el-icon>
+  </el-tooltip>
+</template>
+
+<script lang="ts" setup>
+import { ElIcon } from '@element-plus/components/icon'
+import ElTooltip from '@element-plus/components/tooltip'
+import { iconPropType } from '@element-plus/utils'
+import { QuestionFilled } from '@element-plus/icons-vue'
+import { useNamespace } from '@element-plus/hooks'
+import type { PropType } from 'vue'
+import type { Placement } from '@element-plus/components/popper'
+defineOptions({
+  name: 'ElTableHeaderHelperMessage',
+})
+
+defineProps({
+  helperMessage: String,
+  helperPlacement: {
+    type: String as PropType<Placement>,
+    default: 'right-start',
+  },
+  helperIconColor: String,
+  helperIcon: {
+    type: iconPropType,
+    default: () => QuestionFilled,
+  },
+})
+
+const ns = useNamespace('table-column')
+</script>
