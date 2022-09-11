6fde5a4de69519fbc8b4720be025f8fcab0b1f70
三咲智子 <sxzz@sxzz.moe>
2022-09-11 17:14:32 +0800

    fix: dts build

--- internal/build/src/tasks/types-definitions.ts
+++ internal/build/src/tasks/types-definitions.ts
@@ -91,7 +91,7 @@ async function addSourceFiles(project: Project) {
 
   const globSourceFile = '**/*.{js?(x),ts?(x),vue}'
   const filePaths = excludeFiles(
-    await glob([globSourceFile, '!element-plus/**/*'], {
+    await glob([globSourceFile, '!element-plus/**/*', '!node', '!web'], {
       cwd: pkgRoot,
       absolute: true,
       onlyFiles: true,
