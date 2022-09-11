d8cc58b18a2342cce1c7b5cbcbfd25f80d925d4e
三咲智子 <sxzz@sxzz.moe>
2022-09-11 17:11:33 +0800

    feat: add node-constants

---
+++ packages/node/node-constants/build.config.ts
@@ -0,0 +1,5 @@
+import { defineConfig } from '@element-plus/build-published/config'
+
+export default defineConfig({
+  platform: 'node',
+})
---
+++ packages/node/node-constants/src/index.ts
@@ -0,0 +1,3 @@
+export * from './build'
+export * from './pkg'
+export * from './repo'
