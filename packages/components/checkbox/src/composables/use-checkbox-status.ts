95cf106110b87cbbe385163bc7921f0eb1bf928f
MonsterPi13 <rpishappynow@gmail.com>
2022-09-14 08:40:50 +0800

    refactor(components): [checkbox]convert to common util function

--- packages/components/checkbox/src/composables/use-checkbox-status.ts
+++ packages/components/checkbox/src/composables/use-checkbox-status.ts
@@ -1,6 +1,6 @@
 import { computed, inject, ref, toRaw } from 'vue'
-import { toTypeString } from '@vue/shared'
 import { useSize } from '@element-plus/hooks'
+import { isArray, isBoolean } from '@element-plus/utils'
 import { checkboxGroupContextKey } from '@element-plus/tokens/checkbox'
 
 import type { ComponentInternalInstance } from 'vue'
@@ -16,9 +16,9 @@ export const useCheckboxStatus = (
   const isFocused = ref(false)
   const isChecked = computed<boolean>(() => {
     const value = model!.value
-    if (toTypeString(value) === '[object Boolean]') {
+    if (isBoolean(value)) {
       return value
-    } else if (Array.isArray(value)) {
+    } else if (isArray(value)) {
       return value.map(toRaw).includes(props.label)
     } else if (value !== null && value !== undefined) {
       return value === props.trueLabel
