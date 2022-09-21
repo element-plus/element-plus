8529fad614421428d88aa9ede655b186813bac80
三咲智子 <sxzz@sxzz.moe>
2022-09-21 14:28:02 +0800

    refactor(directives): [repeat-click] refactor

--- packages/directives/repeat-click/index.ts
+++ packages/directives/repeat-click/index.ts
@@ -1,39 +1,54 @@
-import type { DirectiveBinding, ObjectDirective } from 'vue'
+import type { ObjectDirective } from 'vue'
 
 export const REPEAT_INTERVAL = 100
 export const REPEAT_DELAY = 600
 
-const RepeatClick: ObjectDirective = {
-  beforeMount(el: HTMLElement, binding: DirectiveBinding) {
-    let interval: ReturnType<typeof setInterval> | null = null
-    let delay: ReturnType<typeof setTimeout> | null = null
+export interface RepeatClickOptions {
+  interval?: number
+  delay?: number
+  handler: (...args: unknown[]) => unknown
+}
 
-    const handler = () => binding.value && binding.value()
+export const vRepeatClick: ObjectDirective<
+  HTMLElement,
+  RepeatClickOptions | RepeatClickOptions['handler']
+> = {
+  beforeMount(el, binding) {
+    const value = binding.value
+    const { interval = REPEAT_INTERVAL, delay = REPEAT_DELAY } =
+      typeof value === 'object' ? value : {}
+
+    let intervalId: ReturnType<typeof setInterval> | undefined
+    let delayId: ReturnType<typeof setTimeout> | undefined
+
+    const handler = () =>
+      typeof value === 'object' ? value.handler() : value()
 
     const clear = () => {
-      if (delay) {
-        clearTimeout(delay)
-        delay = null
+      if (delayId) {
+        clearTimeout(delayId)
+        delayId = undefined
       }
-      if (interval) {
-        clearInterval(interval)
-        interval = null
+      if (intervalId) {
+        clearInterval(intervalId)
+        intervalId = undefined
       }
     }
 
-    el.addEventListener('mousedown', (e: MouseEvent) => {
-      if (e.button !== 0) return
+    el.addEventListener('mousedown', (evt: MouseEvent) => {
+      if (evt.button !== 0) return
+      clear()
       handler()
 
-      document.addEventListener('mouseup', clear, { once: true })
-      clear()
-      delay = setTimeout(() => {
-        interval = setInterval(() => {
+      document.addEventListener('mouseup', () => clear(), {
+        once: true,
+      })
+
+      delayId = setTimeout(() => {
+        intervalId = setInterval(() => {
           handler()
-        }, REPEAT_INTERVAL)
-      }, REPEAT_DELAY)
+        }, interval)
+      }, delay)
     })
   },
 }
-
-export default RepeatClick
