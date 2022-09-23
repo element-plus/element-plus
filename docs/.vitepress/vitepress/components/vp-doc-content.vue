5d8b2065d0786e7c6bab7361c7d5ef74d7a433c5
qiang <qw13131wang@gmail.com>
2022-09-23 10:03:04 +0800

    docs: refactor the external link icon (#8975)

--- docs/.vitepress/vitepress/components/vp-doc-content.vue
+++ docs/.vitepress/vitepress/components/vp-doc-content.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
 import { useData } from 'vitepress'
-import { insertLinkIcon, insertTableWrapper } from '../utils'
+import { insertTableWrapper } from '../utils'
 import VPPageFooter from './doc-content/vp-page-footer.vue'
 import VPPageNav from './doc-content/vp-page-nav.vue'
 import VPTableOfContent from './doc-content/vp-table-of-content.vue'
@@ -10,7 +10,6 @@ const { page } = useData()
 const content = ref<{ $el: HTMLElement }>()
 
 function updateDom() {
-  insertLinkIcon(content)
   insertTableWrapper(content)
 }
 </script>
