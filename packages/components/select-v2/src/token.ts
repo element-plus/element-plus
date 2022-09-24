2c15fba72e1abc084ddfe3dd1023a23cbcc50eee
btea <2356281422@qq.com>
2022-09-24 14:36:32 +0800

    chore: update type

--- packages/components/select-v2/src/token.ts
+++ packages/components/select-v2/src/token.ts
@@ -1,12 +1,12 @@
 import type { OptionProps, SelectProps } from './defaults'
-import type { ExtractPropTypes, InjectionKey } from 'vue'
+import type { ExtractPropTypes, InjectionKey, Ref } from 'vue'
 import type { Option } from './select.types'
 import type { TooltipInstance } from '@element-plus/components/tooltip'
 
 export interface SelectV2Context {
   props: ExtractPropTypes<typeof SelectProps>
   expanded: boolean
-  popper: TooltipInstance
+  popper: Ref<TooltipInstance>
   onSelect: (option: Option<any>, index: number, byClick?: boolean) => void
   onHover: (idx: number) => void
   onKeyboardNavigate: (direction: 'forward' | 'backward') => void
