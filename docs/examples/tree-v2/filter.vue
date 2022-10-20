2ccb25d7a14ae74e000578d8bedaf90ff3906ce0
snowingfox <1503401882@qq.com>
2022-10-20 09:55:50 +0800

    fix: complete path

--- docs/examples/tree-v2/filter.vue
+++ docs/examples/tree-v2/filter.vue
@@ -15,7 +15,7 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
 import { ElTreeV2 } from 'element-plus'
-import type { TreeNode } from 'element-plus/es/components/tree-v2'
+import type { TreeNode } from 'element-plus/es/components/tree-v2/src/types'
 
 interface Tree {
   id: string
