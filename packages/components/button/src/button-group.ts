bbdc69db9ab77ceaa51d1c3272941b648d582597
jw-foss <15975785+jw-foss@users.noreply.github.com>
2022-10-20 13:55:48 +0800

    chore: remove export of button group instance

--- packages/components/button/src/button-group.ts
+++ packages/components/button/src/button-group.ts
@@ -1,12 +1,9 @@
 import { buttonProps } from './button'
 
 import type { ExtractPropTypes } from 'vue'
-import type buttonGroup from './button-group.vue'
 
 export const buttonGroupProps = {
   size: buttonProps.size,
   type: buttonProps.type,
 } as const
 export type ButtonGroupProps = ExtractPropTypes<typeof buttonGroupProps>
-
-export type ButtonGroupInstance = InstanceType<typeof buttonGroup>
