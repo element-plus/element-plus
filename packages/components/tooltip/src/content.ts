1d508db9bc169c20f639d5f1415da20f7ac79d03
Simon He <13917107469@163.com>
2022-10-26 23:23:15 +0800

    chore: update

--- packages/components/tooltip/src/content.ts
+++ packages/components/tooltip/src/content.ts
@@ -38,6 +38,10 @@ export const useTooltipContentProps = buildProps({
     type: String,
     default: `${ns.namespace.value}-fade-in-linear`,
   },
+  teleported: {
+    type: Boolean,
+    default: true,
+  },
   disabled: {
     type: Boolean,
   },
