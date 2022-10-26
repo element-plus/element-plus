525cba57da4d22e64d0cff46524497b17d335eb0
Judith Umbach <judith.umbach@akra.de>
2022-10-26 16:12:48 +0200

    fix(components): [tree-node-content] bracket bug coming from #9613 (slot inherit styles)

--- packages/components/tree/src/tree-node-content.vue
+++ packages/components/tree/src/tree-node-content.vue
@@ -24,11 +24,11 @@ export default defineComponent({
       const { data, store } = node
       return props.renderContent
         ? props.renderContent(h, { _self: nodeInstance, node, data, store })
-        : h('span', { class: ns.be('node', 'label') }, [
+        : h('span', { class: ns.be('node', 'label') }), [
             tree.ctx.slots.default
               ? tree.ctx.slots.default({ node, data })
               : node.label,
-          ])
+          ]
     }
   },
 })
