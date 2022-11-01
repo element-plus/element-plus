7677a11938f249aeea52cfd0518096626fb3851e
jw-foss <15975785+jw-foss@users.noreply.github.com>
2022-11-01 17:01:15 +0800

    feat(docs): vite-press new component
    * Add a new component for prompting typing.

---
+++ docs/.vitepress/vitepress/components/globals/vp-api-typing.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { Warning } from '@element-plus/icons-vue'
+
+defineProps({
+  type: String,
+  details: String,
+})
+</script>
+
+<template>
+  <span class="flex items-center">
+    <span class="api-typing">
+      {{ type }}
+    </span>
+    <el-tooltip effect="light" trigger="click">
+      <el-button text :icon="Warning" class="p-2 text-5" />
+      <template #content>
+        <div class="m-1">
+          <code>
+            {{ details }}
+          </code>
+        </div>
+      </template>
+    </el-tooltip>
+  </span>
+</template>
