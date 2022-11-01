231709b8cfe00d9725aa48980dac85a886ebb280
Jeremy <15975785+jw-foss@users.noreply.github.com>
2022-11-01 20:31:39 +0800

    feat(docs): api typing shortcuts (#10353)
    * Add `FunctionType` and `EnumType` as typing shortcuts.
    
    Co-authored-by: JeremyWuuuuu <15975785+JeremyWuuuuu@users.noreply.github.com>

---
+++ docs/.vitepress/vitepress/components/globals/vp-api-function.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import ApiTyping from './vp-api-typing.vue'
+
+const props = defineProps({
+  params: {
+    type: String,
+    default: '',
+  },
+  returns: {
+    type: String,
+    default: 'void',
+  },
+})
+
+const details = computed(() => `(${props.params}) => ${props.returns}`)
+</script>
+
+<template>
+  <api-typing type="Function" :details="details" />
+</template>
