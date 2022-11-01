9596ccc8aba5515e7dfadf428bd5b1897cc9ee04
Xc <124118265@qq.com>
2022-11-01 23:41:12 +0800

    docs(components): [input]

--- docs/.vitepress/vitepress/components/globals/vp-api-external.vue
+++ docs/.vitepress/vitepress/components/globals/vp-api-external.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import VpLink from '../common/vp-link.vue'
+
 defineProps({
   text: {
     type: String,
@@ -16,20 +18,16 @@ defineProps({
 </script>
 
 <template>
-  <span v-if="prefix" class="mr-4">{{ prefix }}</span>
-  <a :href="url" class="vp-link" target="_blank" rel="noopener noreferrer">
-    {{ text }}
-    <svg
-      preserveAspectRatio="xMidYMid meet"
-      viewBox="0 0 24 24"
-      width="1.2em"
-      height="1.2em"
-      class="link-icon"
-    >
-      <path
-        fill="currentColor"
-        d="M10 6v2H5v11h11v-5h2v6a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h6zm11-3v8h-2V6.413l-7.793 7.794l-1.414-1.414L17.585 5H13V3h8z"
-      />
-    </svg>
-  </a>
+  <slot>
+    <span v-if="prefix" class="mr-1">{{ prefix }}</span>
+    <VpLink :href="url">
+      {{ text }}
+    </VpLink>
+  </slot>
 </template>
+
+<style scoped>
+:deep(.el-icon) {
+  font-size: 18px;
+}
+</style>
