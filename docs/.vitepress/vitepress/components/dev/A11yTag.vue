4e3a136e8181de981f0c74483aa122a9963fda83
Xc <124118265@qq.com>
2022-11-03 22:52:19 +0800

    docs(components): [a11y] modify color

--- docs/.vitepress/vitepress/components/dev/A11yTag.vue
+++ docs/.vitepress/vitepress/components/dev/A11yTag.vue
@@ -1,9 +1,13 @@
 <template>
-  <el-tag size="small" effect="plain" hit round>a11y</el-tag>
+  <el-tag size="small" effect="plain" hit round class="ml-8px">a11y</el-tag>
 </template>
 <style scoped>
 .el-tag {
-  color: #8d4ddb;
-  border-color: #8d4ddb;
+  color: #6222c2;
+  border-color: #9065db;
+}
+.dark .el-tag {
+  color: #9065db;
+  border-color: #6222c2;
 }
 </style>
