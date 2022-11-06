1ec01130d8b8c440aca0d065253d292660dbccba
Jeremy <15975785+jw-foss@users.noreply.github.com>
2022-11-06 14:46:35 +0800

    fix(docs): [vp-api-typing] (#10467)
    * Add ClientOnly to wrap tooltip to prevent hydration issue.
    
    Co-authored-by: JeremyWuuuuu <15975785+JeremyWuuuuu@users.noreply.github.com>

--- docs/.vitepress/vitepress/components/globals/vp-api-typing.vue
+++ docs/.vitepress/vitepress/components/globals/vp-api-typing.vue
@@ -12,22 +12,24 @@ defineProps({
     <code class="api-typing mr-1">
       {{ type }}
     </code>
-    <el-tooltip v-if="details" effect="light" trigger="click">
-      <el-button text :icon="Warning" class="p-2 text-4" />
-      <template #content>
-        <slot>
-          <div class="m-1">
-            <code
-              style="
-                color: var(--code-tooltip-color);
-                background-color: var(--code-tooltip-bg-color);
-              "
-            >
-              {{ details }}
-            </code>
-          </div>
-        </slot>
-      </template>
-    </el-tooltip>
+    <ClientOnly>
+      <ElTooltip v-if="details" effect="light" trigger="click">
+        <ElButton text :icon="Warning" class="p-2 text-4" />
+        <template #content>
+          <slot>
+            <div class="m-1">
+              <code
+                style="
+                  color: var(--code-tooltip-color);
+                  background-color: var(--code-tooltip-bg-color);
+                "
+              >
+                {{ details }}
+              </code>
+            </div>
+          </slot>
+        </template>
+      </ElTooltip>
+    </ClientOnly>
   </span>
 </template>
