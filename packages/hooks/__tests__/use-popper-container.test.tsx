4cf244665f4d5ca1257b46ce0c0135058e28de36
tolking <qw13131wang@gmail.com>
2022-11-09 18:19:34 +0800

    test(hooks): add test

--- packages/hooks/__tests__/use-popper-container.test.tsx
+++ packages/hooks/__tests__/use-popper-container.test.tsx
@@ -1,11 +1,12 @@
 import { nextTick } from 'vue'
-import { shallowMount } from '@vue/test-utils'
-import { afterEach, describe, expect, it, vi } from 'vitest'
+import { config, mount, shallowMount } from '@vue/test-utils'
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
 import * as vueuse from '@vueuse/core'
 import {
   usePopperContainer,
   usePopperContainerId,
 } from '../use-popper-container'
+import { ID_INJECTION_KEY } from '../use-id'
 
 const AXIOM = 'rem is the best girl'
 
@@ -41,5 +42,52 @@ describe('usePopperContainer', () => {
     await nextTick()
     const { selector } = usePopperContainerId()
     expect(document.body.querySelector(selector.value)).toBeNull()
+  })
+})
+
+describe('no injection value', () => {
+  afterEach(() => {
+    document.body.innerHTML = ''
+  })
+
+  it('usePopperContainerId', () => {
+    const wrapper = mount({
+      setup() {
+        const data = usePopperContainerId()
+        return data
+      },
+    })
+
+    expect(wrapper.vm.id).toMatch(/^el-popper-container-\d{0,4}$/)
+    expect(wrapper.vm.selector).toMatch(/^#el-popper-container-\d{0,4}$/)
+    expect(wrapper.vm.selector).toBe(`#${wrapper.vm.id}`)
+  })
+})
+
+describe('with injection value', () => {
+  beforeEach(() => {
+    config.global.provide = {
+      [ID_INJECTION_KEY as symbol]: {
+        prefix: 1024,
+        current: 0,
+      },
+    }
+  })
+
+  afterEach(() => {
+    document.body.innerHTML = ''
+    config.global.provide = {}
+  })
+
+  it('usePopperContainerId', () => {
+    const wrapper = mount({
+      setup() {
+        const data = usePopperContainerId()
+        return data
+      },
+    })
+
+    expect(wrapper.vm.id).toBe('el-popper-container-1024')
+    expect(wrapper.vm.selector).toBe('#el-popper-container-1024')
   })
 })
