d003c7fac95816de05724488d8927deae21d0e4f
ElementPlusBot <hello@element-plus.org>
2022-11-11 16:21:14 +0000

    website deploy

--- assets/index.md.f355bf18.js
+++
@@ -1,1 +0,0 @@
-import{o as e,c as t}from"./app.fce3f284.js";const _='{"title":"A Vue 3 UI Framework","description":"","frontmatter":{"title":"A Vue 3 UI Framework","lang":"en-US","page":true},"headers":[],"relativePath":"index.md","lastUpdated":1667717195000}',a={},c=Object.assign(a,{__name:"index",setup(r){return(n,o)=>(e(),t("div"))}});export{_ as __pageData,c as default};
