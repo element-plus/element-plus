e43e28416451373f226fe4e4125150f0345ccbb6
yang <29636098325@qq.com>
2022-11-13 15:12:58 +0800

    ci(components): [statistic]

--- packages/components/statistic/__tests__/statistic.spec.ts
+++ packages/components/statistic/__tests__/statistic.spec.ts
@@ -1,15 +1,64 @@
 import { mount } from '@vue/test-utils'
+import { describe, expect, it } from 'vitest'
 import Statistic from '../src/index.vue'
-
-const AXIOM = 'Rem is the best girl'
-
+// const _mount = (template: string, data: any = () => ({})) =>
+//   mount(
+//     {
+//       template,
+//       data,
+//       // ...otherObj,
+//     },
+//     {
+//       attachTo: 'body',
+//     }
+//   )
 describe('Statistic.vue', () => {
-  test('render test', () => {
+  it('render test', async () => {
     const wrapper = mount(Statistic, {
-      slots: {
-        default: AXIOM,
+      propsData: {
+        value: 57454157,
+        title: 'test',
       },
     })
-    expect(wrapper.text()).toEqual(AXIOM)
+    await wrapper.vm.$nextTick()
+    expect(wrapper.text()).toBe('test57,454,157.00')
   })
+  it('Front and rear', async () => {
+    const wrapper = mount(Statistic, {
+      propsData: {
+        value: 57454157,
+        prefix: 'Front',
+        suffix: 'rear',
+      },
+    })
+    await wrapper.vm.$nextTick()
+    expect(wrapper.text()).toBe('Front57,454,157.00rear')
+  })
+  it('Accuracy check', async () => {
+    const wrapper = mount(Statistic, {
+      propsData: {
+        value: 1,
+        precision: 4,
+      },
+    })
+    await wrapper.vm.$nextTick()
+    expect(wrapper.text()).toBe('1.0000')
+  })
+
+  it('should work', async () =>
+    new Promise((done) => {
+      const wrapper = mount(Statistic, {
+        propsData: {
+          value: Date.now() + 200,
+          timeIndices: true,
+          finish: () => {
+            console.log('end')
+          },
+        },
+      })
+      setTimeout(() => {
+        expect(wrapper.emitted().finish).toBeTruthy()
+        done()
+      }, 300)
+    }))
 })
--- packages/components/statistic/src/index.vue
+++ packages/components/statistic/src/index.vue
@@ -1,30 +1,27 @@
 <template>
-  <div>
-    <!-- :style="statisticStyle" -->
-    <div :class="[ns.b()]">
-      <div class="head">
-        <slot name="title">
-          <span class="title">
-            {{ title }}
-          </span>
+  <div :class="[ns.b()]">
+    <div class="head">
+      <slot name="title">
+        <span class="title">
+          {{ title }}
+        </span>
+      </slot>
+    </div>
+    <div class="con">
+      <span class="prefix">
+        <slot name="prefix">
+          {{ prefix }}
         </slot>
-      </div>
-      <div class="con">
-        <span class="prefix">
-          <slot name="prefix">
-            {{ prefix }}
-          </slot>
-        </span>
+      </span>
 
-        <span class="number" :style="valueStyle">
-          <slot name="formatter"> {{ disposeValue }}</slot>
-        </span>
-        <span class="suffix">
-          <slot name="suffix">
-            {{ suffix }}
-          </slot>
-        </span>
-      </div>
+      <span class="number" :style="valueStyle">
+        <slot name="formatter"> {{ disposeValue }}</slot>
+      </span>
+      <span class="suffix">
+        <slot name="suffix">
+          {{ suffix }}
+        </slot>
+      </span>
     </div>
   </div>
 </template>
@@ -40,7 +37,6 @@ import {
   statisticProps,
 } from './statistic'
 const emit = defineEmits(['finish'])
-// import { useStatisticCustomStyle } from './statistic-custom'
 const props = defineProps(statisticProps)
 defineOptions({
   name: 'ElStatistic',
@@ -60,11 +56,7 @@ watch(
   }
 )
 function branch() {
-  if (props.timeIndices) {
-    countDown()
-  } else {
-    dispose()
-  }
+  props.timeIndices ? countDown() : dispose()
 }
 
 const dispose = function (): number {
@@ -77,7 +69,6 @@ const dispose = function (): number {
   } = props
   let value: any = Pvalue
   if (precision) value = ceil(value, precision)
-  // console.log('value', value)
   let integer: any = String(value).split('.')[0]
   const decimals =
     String(value).split('.')[1] ||
