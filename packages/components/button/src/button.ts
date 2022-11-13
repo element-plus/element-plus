fedf1e7f339f889d80658188903d97392e3a9159
Xc <124118265@qq.com>
2022-11-13 22:14:48 +0800

    docs(components): [button]

--- packages/components/button/src/button.ts
+++ packages/components/button/src/button.ts
@@ -20,35 +20,86 @@ export const buttonTypes = [
 export const buttonNativeTypes = ['button', 'submit', 'reset'] as const
 
 export const buttonProps = buildProps({
+  /**
+   * @description button size
+   */
   size: useSizeProp,
+  /**
+   * @description disable the button
+   */
   disabled: Boolean,
+  /**
+   * @description button type
+   */
   type: {
     type: String,
     values: buttonTypes,
     default: '',
   },
+  /**
+   * @description icon component
+   */
   icon: {
     type: iconPropType,
   },
+  /**
+   * @description native button type
+   */
   nativeType: {
     type: String,
     values: buttonNativeTypes,
     default: 'button',
   },
+  /**
+   * @description determine whether it's loading
+   */
   loading: Boolean,
+  /**
+   * @description customize loading icon component
+   */
   loadingIcon: {
     type: iconPropType,
     default: () => Loading,
   },
+  /**
+   * @description determine whether it's a plain button
+   */
   plain: Boolean,
+  /**
+   * @description determine whether it's a text button
+   */
   text: Boolean,
+  /**
+   * @description determine whether it's a link button
+   */
   link: Boolean,
+  /**
+   * @description determine whether the text button background color is always on
+   */
   bg: Boolean,
+  /**
+   * @description native button autofocus
+   */
   autofocus: Boolean,
+  /**
+   * @description determine whether it's a round button
+   */
   round: Boolean,
+  /**
+   * @description determine whether it's a circle button
+   */
   circle: Boolean,
+  /**
+   * @description custom button color, automatically calculate `hover` and `active` color
+   */
   color: String,
+  /**
+   * @description dark mode, which automatically converts `color` to dark mode colors
+   */
   dark: Boolean,
+  /**
+   * @description automatically insert a space between two chinese characters
+   */
   autoInsertSpace: {
     type: Boolean,
     default: undefined,
