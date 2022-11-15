ab7fec8f33ccdcf8c223410803712c24efb9d371
Hongbusi <coderhbs@gmail.com>
2022-11-15 15:35:50 +0800

    chore: format

--- docs/examples/button/group.vue
+++ docs/examples/button/group.vue
@@ -1,17 +1,17 @@
 <template>
   <el-space wrap>
     <el-button-group>
-    <el-button type="primary" :icon="ArrowLeft">Previous Page</el-button>
-    <el-button type="primary">
-      Next Page<el-icon class="el-icon--right"><ArrowRight /></el-icon>
-    </el-button>
-  </el-button-group>
+      <el-button type="primary" :icon="ArrowLeft">Previous Page</el-button>
+      <el-button type="primary">
+        Next Page<el-icon class="el-icon--right"><ArrowRight /></el-icon>
+      </el-button>
+    </el-button-group>
 
-  <el-button-group>
-    <el-button type="primary" :icon="Edit" />
-    <el-button type="primary" :icon="Share" />
-    <el-button type="primary" :icon="Delete" />
-  </el-button-group>
+    <el-button-group>
+      <el-button type="primary" :icon="Edit" />
+      <el-button type="primary" :icon="Share" />
+      <el-button type="primary" :icon="Delete" />
+    </el-button-group>
   </el-space>
 </template>
 
