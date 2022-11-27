f17c8f3a2e2135e505c9170ad8711073ce0b8cfd
Jeremy <15975785+jw-foss@users.noreply.github.com>
2022-11-27 22:35:13 +0800

    docs(components): [badge] enhancement (#10789)
    * docs(components): [badge] enhancement
    
    * Update badge docs per new syntax.
    * Reorganize code per readability wise purpose.
    
    * chore: remove component name prefix
    
    Co-authored-by: JeremyWuuuuu <15975785+JeremyWuuuuu@users.noreply.github.com>

--- packages/components/badge/src/badge.ts
+++ packages/components/badge/src/badge.ts
@@ -1,18 +1,32 @@
 import { buildProps } from '@element-plus/utils'
 import type { ExtractPropTypes } from 'vue'
-import type Badge from './badge.vue'
 
 export const badgeProps = buildProps({
+  /**
+   * @description display value.
+   */
   value: {
     type: [String, Number],
     default: '',
   },
+  /**
+   * @description maximum value, shows `{max}+` when exceeded. Only works if value is a number.
+   */
   max: {
     type: Number,
     default: 99,
   },
+  /**
+   * @description if a little dot is displayed.
+   */
   isDot: Boolean,
+  /**
+   * @description hidden badge.
+   */
   hidden: Boolean,
+  /**
+   * @description badge type.
+   */
   type: {
     type: String,
     values: ['primary', 'success', 'warning', 'info', 'danger'],
@@ -20,5 +34,3 @@ export const badgeProps = buildProps({
   },
 } as const)
 export type BadgeProps = ExtractPropTypes<typeof badgeProps>
-
-export type BadgeInstance = InstanceType<typeof Badge>
