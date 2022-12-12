2e172b68f7ff1f09540d974f4d15d612b3db0cd0
Xc <124118265@qq.com>
2022-12-12 22:56:05 +0800

    docs(components): [config-provider] (#10881)
    * docs(components): [config-provider]
    * Adjust API style

--- packages/components/config-provider/src/config-provider.ts
+++ packages/components/config-provider/src/config-provider.ts
@@ -11,38 +11,55 @@ import type { MessageConfigContext } from '@element-plus/components/message'
 export const messageConfig: MessageConfigContext = {}
 
 export const configProviderProps = buildProps({
-  // Controlling if the users want a11y features.
+  /**
+   * @description Controlling if the users want a11y features
+   */
   a11y: {
     type: Boolean,
     default: true,
   },
-
+  /**
+   * @description Locale Object
+   */
   locale: {
     type: definePropType<Language>(Object),
   },
-
+  /**
+   * @description global component size
+   */
   size: useSizeProp,
-
+  /**
+   * @description button related configuration, [see the following table](#button-attributes)
+   */
   button: {
     type: definePropType<ButtonConfigContext>(Object),
   },
-
+  /**
+   * @description features at experimental stage to be added, all features are default to be set to false                                                                                | ^[object]
+   */
   experimentalFeatures: {
     type: definePropType<ExperimentalFeatures>(Object),
   },
-
-  // Controls if we should handle keyboard navigation
+  /**
+   * @description Controls if we should handle keyboard navigation
+   */
   keyboardNavigation: {
     type: Boolean,
     default: true,
   },
-
+  /**
+   * @description message related configuration, [see the following table](#message-attributes)
+   */
   message: {
     type: definePropType<MessageConfigContext>(Object),
   },
-
+  /**
+   * @description global Initial zIndex
+   */
   zIndex: Number,
-
+  /**
+   * @description global component className prefix (cooperated with [$namespace](https://github.com/element-plus/element-plus/blob/dev/packages/theme-chalk/src/mixins/config.scss#L1)) | ^[string]
+   */
   namespace: {
     type: String,
     default: 'el',
