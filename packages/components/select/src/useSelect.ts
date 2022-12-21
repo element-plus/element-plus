81d4f94f71f4113dcf676a74125c5b675bb63440
sleepyShen1989 <lucky-boy-2007@163.com>
2022-12-21 23:18:45 +0800

    fix(components): [select] fix placeholder display
    closed # 11049

--- packages/components/select/src/useSelect.ts
+++ packages/components/select/src/useSelect.ts
@@ -210,6 +210,15 @@ export const useSelect = (props, states: States, ctx) => {
     () => props.placeholder,
     (val) => {
       states.cachedPlaceHolder = states.currentPlaceholder = val
+
+      const hasValue =
+        props.multiple &&
+        Array.isArray(props.modelValue) &&
+        props.modelValue.length > 0
+
+      if (hasValue) {
+        states.currentPlaceholder = ''
+      }
     }
   )
 
