66f4f73b9f0ad8f8e73b73a5e6bcbf3cc74b4ccc
zhoulixiang <18366276315@163.com>
2023-02-08 19:37:44 +0800

    fix: select multiple
    resolve the height of select when using multiple and filterable at the same time.

--- packages/components/select/src/select.vue
+++ packages/components/select/src/select.vue
@@ -152,6 +152,7 @@
                 flexGrow: 1,
                 width: `${inputLength / (inputWidth - 32)}%`,
                 maxWidth: `${inputWidth - 42}px`,
+                minWidth: '4px',
               }"
               @focus="handleFocus"
               @blur="handleBlur"
