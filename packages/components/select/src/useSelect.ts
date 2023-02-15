5b35ace954368a573302df320be898b31ce98564
acyza <101238421+acyza@users.noreply.github.com>
2023-02-16 00:21:48 +0800

    Update useSelect.ts

--- packages/components/select/src/useSelect.ts
+++ packages/components/select/src/useSelect.ts
@@ -749,6 +749,7 @@ export const useSelect = (props, states: States, ctx) => {
 
   const handleMenuLeave = () => {
     if (!states.visible && props.filterable && states.previousQuery) {
+      states.previousQuery = ''
       if (isFunction(props.filterMethod)) {
         props.filterMethod('')
       }
