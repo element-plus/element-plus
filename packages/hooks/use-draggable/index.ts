99e6f1a7007860d547428236415af12317210d7d
xianyongwen <xianyongwen@163.com>
2023-02-17 20:29:36 +0800

    fix(components): [Dialog & MessageBox] Adjust after window resize

--- packages/hooks/use-draggable/index.ts
+++ packages/hooks/use-draggable/index.ts
@@ -12,9 +12,7 @@ export const useDraggable = (
     offsetY: 0,
   }
 
-  const onMousedown = (e: MouseEvent) => {
-    const downX = e.clientX
-    const downY = e.clientY
+  const getRectLimit = () => {
     const { offsetX, offsetY } = transform
 
     const targetRect = targetRef.value!.getBoundingClientRect()
@@ -26,10 +24,19 @@ export const useDraggable = (
     const clientWidth = document.documentElement.clientWidth
     const clientHeight = document.documentElement.clientHeight
 
-    const minLeft = -targetLeft + offsetX
-    const minTop = -targetTop + offsetY
-    const maxLeft = clientWidth - targetLeft - targetWidth + offsetX
-    const maxTop = clientHeight - targetTop - targetHeight + offsetY
+    return {
+      minLeft: -targetLeft + offsetX,
+      minTop: -targetTop + offsetY,
+      maxLeft: clientWidth - targetLeft - targetWidth + offsetX,
+      maxTop: clientHeight - targetTop - targetHeight + offsetY,
+    }
+  }
+
+  const onMousedown = (e: MouseEvent) => {
+    const downX = e.clientX
+    const downY = e.clientY
+    const { offsetX, offsetY } = transform
+    const { minLeft, minTop, maxLeft, maxTop } = getRectLimit()
 
     const onMousemove = (e: MouseEvent) => {
       const moveX = Math.min(
@@ -59,15 +66,52 @@ export const useDraggable = (
     document.addEventListener('mouseup', onMouseup)
   }
 
+  const useWindowResizeObserver = (fn: () => any) => {
+    let resizeTimeout: ReturnType<typeof setTimeout> | null
+    function resizeThrottler() {
+      if (!resizeTimeout) {
+        resizeTimeout = setTimeout(() => {
+          resizeTimeout = null
+          fn()
+        }, 66)
+      }
+    }
+    window.addEventListener('resize', resizeThrottler, false)
+    return {
+      clear: () => {
+        window.removeEventListener('resize', resizeThrottler)
+      },
+    }
+  }
+
+  let windowResizeObserver: ReturnType<typeof useWindowResizeObserver> | null
+  const windowResizeHandle = () => {
+    const { offsetX, offsetY } = transform
+    const { minLeft, minTop, maxLeft, maxTop } = getRectLimit()
+    const moveX = Math.min(Math.max(offsetX, minLeft), maxLeft)
+    const moveY = Math.min(Math.max(offsetY, minTop), maxTop)
+
+    transform = {
+      offsetX: moveX,
+      offsetY: moveY,
+    }
+    targetRef.value!.style.transform = `translate(${addUnit(moveX)}, ${addUnit(
+      moveY
+    )})`
+  }
   const onDraggable = () => {
     if (dragRef.value && targetRef.value) {
       dragRef.value.addEventListener('mousedown', onMousedown)
+      windowResizeObserver = useWindowResizeObserver(windowResizeHandle)
     }
   }
 
   const offDraggable = () => {
     if (dragRef.value && targetRef.value) {
       dragRef.value.removeEventListener('mousedown', onMousedown)
+    }
+    if (windowResizeObserver) {
+      windowResizeObserver.clear()
     }
   }
 
