1acbcc0836d67e05775d1231e87d51ec8e066433
jw-foss <15975785+jw-foss@users.noreply.github.com>
2023-02-24 22:14:10 +0800

    feat(build): [monorepo] element-plus
    * Move files to element-plus/src
    * Update package.json
    * Update .gitignore for version.ts
    * Add new tsconfig for packages/element-plus

--- packages/element-plus/src/make-installer.ts
+++ packages/element-plus/src/make-installer.ts
@@ -1,15 +1,14 @@
 import { provideGlobalConfig } from '@element-plus/hooks'
 import { INSTALLED_KEY } from '@element-plus/constants'
-import { version } from './version'
+import { version } from '../version'
 
-import type { App, Plugin } from '@vue/runtime-core'
+import type { App, Plugin } from 'vue'
 import type { ConfigProviderContext } from '@element-plus/tokens'
 
 export const makeInstaller = (components: Plugin[] = []) => {
   const install = (app: App, options?: ConfigProviderContext) => {
-    if (app[INSTALLED_KEY]) return
-
-    app[INSTALLED_KEY] = true
+    if ((app as any)[INSTALLED_KEY]) return
+    ;(app as any)[INSTALLED_KEY] = true
     components.forEach((c) => app.use(c))
 
     if (options) provideGlobalConfig(options, app, true)
