4d2fef11ed9e4ee4526aa52f020d43a78a9a6a40
jw-foss <15975785+jw-foss@users.noreply.github.com>
2023-02-28 17:12:06 +0800

    refactor(tokens): removing tokens/dialog

--- packages/components/dialog/src/constants.ts
+++ packages/components/dialog/src/constants.ts
@@ -1,11 +1,11 @@
 import type { CSSProperties, ComputedRef, InjectionKey, Ref } from 'vue'
-import type { useNamespace } from '@element-plus/hooks'
+import type { UseNamespaceReturn } from '@element-plus/hooks'
 
 export type DialogContext = {
   dialogRef: Ref<HTMLElement | undefined>
   headerRef: Ref<HTMLElement | undefined>
   bodyId: Ref<string>
-  ns: ReturnType<typeof useNamespace>
+  ns: UseNamespaceReturn
   rendered: Ref<boolean>
   style: ComputedRef<CSSProperties>
 }
