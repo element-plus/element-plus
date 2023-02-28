8263b26ec90d97248ab3d403b12da7ab79e8bf77
gimjin <17204288@qq.com>
2023-02-28 21:01:58 +0800

    feat(components): [text] el-text implementation

---
+++ packages/components/text/src/text.vue
@@ -0,0 +1,34 @@
+<template>
+  <render />
+</template>
+
+<script lang="ts" setup>
+import { h, useSlots } from 'vue'
+import { useNamespace } from '@element-plus/hooks'
+import { useFormSize } from '@element-plus/components/form'
+import { textProps } from './text'
+
+defineOptions({
+  name: 'ElText',
+})
+const props = defineProps(textProps)
+const slots = useSlots()
+
+const textSize = useFormSize()
+const ns = useNamespace('text')
+
+const render = () => {
+  return h(
+    props.as,
+    {
+      class: [
+        ns.b(),
+        ns.m(props.type),
+        ns.m(textSize.value),
+        ns.is('truncated', props.truncated),
+      ],
+    },
+    slots.default?.()
+  )
+}
+</script>
