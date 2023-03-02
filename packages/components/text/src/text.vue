fc9ceea6112387f94e3843a19151538e0f3afb8a
gimjin <17204288@qq.com>
2023-03-02 16:31:35 +0800

    fix(components): [text] use template and render function together

--- packages/components/text/src/text.vue
+++ packages/components/text/src/text.vue
@@ -1,9 +1,11 @@
 <template>
-  <render />
+  <component :is="tag" :class="textKls">
+    <slot />
+  </component>
 </template>
 
 <script lang="ts" setup>
-import { h, useSlots } from 'vue'
+import { computed } from 'vue'
 import { useNamespace } from '@element-plus/hooks'
 import { useFormSize } from '@element-plus/components/form'
 import { textProps } from './text'
@@ -11,24 +13,16 @@ import { textProps } from './text'
 defineOptions({
   name: 'ElText',
 })
+
 const props = defineProps(textProps)
-const slots = useSlots()
 
 const textSize = useFormSize()
 const ns = useNamespace('text')
 
-const render = () => {
-  return h(
-    props.tag,
-    {
-      class: [
-        ns.b(),
-        ns.m(props.type),
-        ns.m(textSize.value),
-        ns.is('truncated', props.truncated),
-      ],
-    },
-    slots.default?.()
-  )
-}
+const textKls = computed(() => [
+  ns.b(),
+  ns.m(props.type),
+  ns.m(textSize.value),
+  ns.is('truncated', props.truncated),
+])
 </script>
