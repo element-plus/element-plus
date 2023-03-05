0f25857abc171aff53d6117950fb5cce80922983
chenxch <124118265@qq.com>
2023-03-05 21:53:26 +0800

    docs(components): [popconfirm] * Update popconfirm docs with new syntax. * Supplement hide-after. * Remove the event event from props.

--- packages/components/popconfirm/src/popconfirm.ts
+++ packages/components/popconfirm/src/popconfirm.ts
@@ -6,48 +6,92 @@ import type { ExtractPropTypes } from 'vue'
 import type Popconfirm from './popconfirm.vue'
 
 export const popconfirmProps = buildProps({
+  /**
+   * @description Title
+   */
   title: String,
+  /**
+   * @description Confirm button text
+   */
   confirmButtonText: String,
+  /**
+   * @description Cancel button text
+   */
   cancelButtonText: String,
+  /**
+   * @description Confirm button type
+   */
   confirmButtonType: {
     type: String,
     values: buttonTypes,
     default: 'primary',
   },
+  /**
+   * @description Cancel button type
+   */
   cancelButtonType: {
     type: String,
     values: buttonTypes,
     default: 'text',
   },
+  /**
+   * @description Icon Component
+   */
   icon: {
     type: iconPropType,
     default: () => QuestionFilled,
   },
+  /**
+   * @description Icon color
+   */
   iconColor: {
     type: String,
     default: '#f90',
   },
+  /**
+   * @description is hide Icon
+   */
   hideIcon: {
     type: Boolean,
     default: false,
   },
+  /**
+   * @description delay of disappear, in millisecond
+   */
   hideAfter: {
     type: Number,
     default: 200,
   },
-  onConfirm: {
-    type: definePropType<(e: Event) => Promise<void> | void>(Function),
-  },
-  onCancel: {
-    type: definePropType<(e: Event) => Promise<void> | void>(Function),
-  },
+  /**
+   * @description whether popconfirm is teleported to the body
+   */
   teleported: useTooltipContentProps.teleported,
+  /**
+   * @description when popconfirm inactive and `persistent` is `false` , popconfirm will be destroyed
+   */
   persistent: useTooltipContentProps.persistent,
+  /**
+   * @description popconfirm width, min width 150px
+   */
   width: {
     type: [String, Number],
     default: 150,
   },
 } as const)
+
+export const popconfirmEmits = {
+  /**
+   * @description triggers when click confirm button
+   */
+  confirm: (e: MouseEvent) => e instanceof MouseEvent,
+  /**
+   * @description triggers when click cancel button
+   */
+  cancel: (e: MouseEvent) => e instanceof MouseEvent,
+}
+
+export type PopconfirmEmits = typeof popconfirmEmits
+
 export type PopconfirmProps = ExtractPropTypes<typeof popconfirmProps>
 
 export type PopconfirmInstance = InstanceType<typeof Popconfirm>
