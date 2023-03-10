a01d060cbbde54bac222cdbaf0fa4e5c2d87baa4
jw-foss <15975785+jw-foss@users.noreply.github.com>
2023-03-10 23:11:21 +0800

    refactor: extract textarea into separate component

---
+++ packages/components/input/src/textarea.vue
@@ -0,0 +1,23 @@
+<template>
+  <textarea v-bind="$attrs" ref="inputRef" :class="ns.e('inner')" />
+  <slot />
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useNamespace } from '@element-plus/hooks'
+
+defineOptions({
+  name: 'ElInputTextarea',
+})
+
+const ns = useNamespace('textarea')
+const inputRef = ref()
+
+defineExpose({
+  /**
+   * @description textarea DOM ref
+   */
+  inputRef,
+})
+</script>
