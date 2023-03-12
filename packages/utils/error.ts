64a61b530c00ffc1243625232a3772a594b394ba
ERKELSOT <1256029807@qq.com>
2023-03-12 11:13:29 +0800

    fix: utils type error

--- packages/utils/error.ts
+++ packages/utils/error.ts
@@ -15,7 +15,7 @@ export function debugWarn(err: Error): void
 export function debugWarn(scope: string, message: string): void
 export function debugWarn(scope: string | Error, message?: string): void {
   if (process.env.NODE_ENV !== 'production') {
-    const error: Error = isString(scope)
+    const error: string | Error = isString(scope)
       ? new ElementPlusError(`[${scope}] ${message}`)
       : scope
     // eslint-disable-next-line no-console
