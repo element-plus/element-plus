c48bf47c0163c77700d8bdf9269d2dab31d15ae3
jw-foss <15975785+jw-foss@users.noreply.github.com>
2023-03-13 19:29:21 +0800

    refactor(components): [input] state-icon
    * Extract state icon into a separate component.

---
+++ packages/components/input/src/state-icon.vue
@@ -0,0 +1,15 @@
+<template>
+  <el-icon>
+    <component :is="ValidateComponentsMap[validateState]" />
+  </el-icon>
+</template>
+
+<script setup lang="ts">
+import { ValidateComponentsMap } from '@element-plus/utils'
+
+import type { FormItemContext } from '@element-plus/components/form'
+
+defineProps<{
+  validateState: Exclude<FormItemContext['validateState'], ''>
+}>()
+</script>
