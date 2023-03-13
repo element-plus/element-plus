2c5dabee878fcb7fbcde145a5421b4ff9aeed3fa
jw-foss <15975785+jw-foss@users.noreply.github.com>
2023-03-13 17:20:06 +0800

    refactor(components): [input] use-input-state
    * Extract state related logic into a separate file.

---
+++ packages/components/input/src/hooks/use-input-state.ts
@@ -0,0 +1,117 @@
+import { computed, ref, unref, useSlots } from 'vue'
+import { isNil } from 'lodash-unified'
+import { useAttrs } from '@element-plus/hooks'
+import {
+  useFormDisabled,
+  useFormItem,
+  useFormItemInputId,
+} from '@element-plus/components/form'
+
+import type { InputProps } from '../input'
+
+/**
+ * @description Initializing input related states for the UI.
+ */
+export const useInputStates = (
+  props: InputProps,
+  rawAttrs: Record<string, unknown>
+) => {
+  const inputDisabled = useFormDisabled()
+  const { form, formItem } = useFormItem()
+  const { inputId } = useFormItemInputId(props, {
+    formItemContext: formItem,
+  })
+
+  const attrs = useAttrs({
+    excludeKeys: computed<string[]>(() => {
+      return Object.keys(unref(containerAttrs))
+    }),
+  })
+  const slots = useSlots()
+
+  const focused = ref(false)
+  const hovering = ref(false)
+  const isComposing = ref(false)
+  const passwordVisible = ref(false)
+  const containerAttrs = computed(() => {
+    const comboBoxAttrs: Record<string, unknown> = {}
+    if (props.containerRole === 'combobox') {
+      comboBoxAttrs['aria-haspopup'] = rawAttrs['aria-haspopup']
+      comboBoxAttrs['aria-owns'] = rawAttrs['aria-owns']
+      comboBoxAttrs['aria-expanded'] = rawAttrs['aria-expanded']
+    }
+    return comboBoxAttrs
+  })
+  const nativeInputValue = computed(() =>
+    isNil(props.modelValue) ? '' : String(props.modelValue)
+  )
+  // using maxLength instead of maxlength to avoid conflict with native attr
+  const maxLength = computed(() => Number(unref(attrs).maxlength))
+  const isPlainText = computed(() => ['text', 'textarea'].includes(props.type))
+  const showClear = computed(
+    () =>
+      props.clearable &&
+      !unref(inputDisabled) &&
+      !props.readonly &&
+      !!unref(nativeInputValue) &&
+      (unref(focused) || unref(hovering))
+  )
+  const showPwdVisible = computed(
+    () =>
+      props.showPassword &&
+      !unref(inputDisabled) &&
+      !props.readonly &&
+      !!unref(nativeInputValue) &&
+      (!!unref(nativeInputValue) || unref(focused))
+  )
+  const isWordLimitVisible = computed(
+    () =>
+      props.showWordLimit &&
+      unref(maxLength) > 0 &&
+      unref(isPlainText) &&
+      !unref(inputDisabled) &&
+      !props.readonly &&
+      !props.showPassword
+  )
+
+  const textLength = computed(() => unref(nativeInputValue).length)
+  const needStatusIcon = computed(() => form?.statusIcon ?? false)
+  const validateState = computed(() => formItem?.validateState || '')
+
+  // NOTE: while porting this it was changed from `inputExceed` to `inputExceeded`
+  const inputExceeded = computed(
+    () =>
+      // show exceed style if length of initial value greater then maxlength
+      !!unref(isWordLimitVisible) && textLength.value > unref(maxLength)
+  )
+  const suffixVisible = computed(
+    () =>
+      !!slots.suffix ||
+      !!props.suffixIcon ||
+      unref(showClear) ||
+      props.showPassword ||
+      unref(isWordLimitVisible) ||
+      (!!unref(validateState) && unref(needStatusIcon))
+  )
+  const inputType = computed(() => {
+    if (!props.showPassword) return props.type
+    return unref(passwordVisible) ? 'text' : 'password'
+  })
+
+  return {
+    inputId,
+    inputDisabled,
+    containerAttrs,
+    focused,
+    hovering,
+    isComposing,
+    passwordVisible,
+    showClear,
+    showPwdVisible,
+    isWordLimitVisible,
+    isPlainText,
+    inputExceeded,
+    inputType,
+    suffixVisible,
+  }
+}
