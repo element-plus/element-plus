60134ad19fe39d4aeb5e6a0c9c3242b101d75410
ElementPlusBot <hello@element-plus.org>
2023-03-20 15:09:58 +0000

    website deploy

---
+++ assets/chunks/a11y-tag.2aa7c883.js
@@ -0,0 +1,1 @@
+import{f as _,r as t,o as a,g as o,w as s,d as n}from"../app.eec746eb.js";const c={},r=n("a11y");function l(p,d){const e=t("el-tag");return a(),o(e,{size:"small",effect:"plain",hit:"",round:"",class:"ml-2"},{default:s(()=>[r]),_:1})}var f=_(c,[["render",l],["__scopeId","data-v-ab284018"]]);export{f as _};
