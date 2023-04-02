efcdc27aecb6091028ff38716ed8c431e7d8c58b
Boran <993967177@qq.com>
2023-04-02 19:13:27 +0800

    fix(components): [cascader] reactive size change

--- packages/components/cascader/src/cascader.vue
+++ packages/components/cascader/src/cascader.vue
@@ -654,6 +654,11 @@ const handleInput = (val: string, e?: KeyboardEvent) => {
   val ? handleFilter() : hideSuggestionPanel()
 }
 
+const getInputInnerHeight = (inputInner: HTMLElement): number =>
+  Number.parseFloat(
+    useCssVar(nsInput.cssVarName('input-height'), inputInner).value
+  ) - 2
+
 watch(filtering, updatePopperPosition)
 
 watch([checkedNodes, isDisabled], calculatePresentTags)
@@ -662,15 +667,18 @@ watch(presentTags, () => {
   nextTick(() => updateStyle())
 })
 
+watch(realSize, async () => {
+  await nextTick()
+  const inputInner = input.value!.input!
+  inputInitialHeight = getInputInnerHeight(inputInner) || inputInitialHeight
+})
+
 watch(presentText, syncPresentTextValue, { immediate: true })
 
 onMounted(() => {
   const inputInner = input.value!.input!
 
-  const inputInnerHeight =
-    Number.parseFloat(
-      useCssVar(nsInput.cssVarName('input-height'), inputInner).value
-    ) - 2
+  const inputInnerHeight = getInputInnerHeight(inputInner)
 
   inputInitialHeight = inputInner.offsetHeight || inputInnerHeight
   useResizeObserver(inputInner, updateStyle)
