6afd4daf5ab4ce3684797d2e278e0a6178ba2e68
lzl <develop0304@outlook.com>
2023-04-03 23:29:17 +0800

    fix(components): [select] fix find created option error
    closed #12302

--- packages/components/select/src/useSelect.ts
+++ packages/components/select/src/useSelect.ts
@@ -166,7 +166,9 @@ export const useSelect = (props, states: States, ctx) => {
         newList.push(list[index])
       }
     })
-    return newList.length ? newList : list
+    return newList.length
+      ? newList.concat(list.filter((item) => item.created))
+      : list
   })
 
   const cachedOptionsArray = computed(() =>
