ce76bdc91d1d1baa549c015e0a1a2fcca4111bb7
Xc <124118265@qq.com>
2023-04-06 20:56:09 +0800

    Revert "fix(components): [popover] default trigger (#12321)"
    This reverts commit bda7d9a57c4559bddb1991127717e034854a3e28.

--- packages/components/popover/src/popover.ts
+++ packages/components/popover/src/popover.ts
@@ -8,6 +8,7 @@ import type { ExtractPropTypes, PropType } from 'vue'
 import type Popover from './popover.vue'
 
 export const popoverProps = buildProps({
+  trigger: useTooltipTriggerProps.trigger,
   placement: dropdownProps.placement,
   disabled: useTooltipTriggerProps.disabled,
   visible: useTooltipContentProps.visible,
@@ -17,10 +18,6 @@ export const popoverProps = buildProps({
   content: useTooltipContentProps.content,
   popperStyle: useTooltipContentProps.popperStyle,
   popperClass: useTooltipContentProps.popperClass,
-  trigger: {
-    ...useTooltipTriggerProps.trigger,
-    default: 'click',
-  },
   enterable: {
     ...useTooltipContentProps.enterable,
     default: true,
