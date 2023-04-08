8ba21e9a7739930f4f43ae0607fddc53338f15ab
wzc520pyfm <1528857653@qq.com>
2023-04-08 16:19:44 +0800

    docs(components): [progress] refactor `striped-progress` example

--- docs/examples/progress/striped-progress.vue
+++ docs/examples/progress/striped-progress.vue
@@ -24,16 +24,32 @@
       striped-flow
       :duration="duration"
     />
-    <el-button @click="percentage -= 10">-</el-button>
-    <el-button @click="percentage += 10">+</el-button>
+    <el-button-group>
+      <el-button :icon="Minus" @click="decrease" />
+      <el-button :icon="Plus" @click="increase" />
+    </el-button-group>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { computed, ref } from 'vue'
+import { Minus, Plus } from '@element-plus/icons-vue'
 
 const percentage = ref<number>(70)
 const duration = computed(() => Math.floor(percentage.value / 10))
+
+const increase = () => {
+  percentage.value += 10
+  if (percentage.value > 100) {
+    percentage.value = 100
+  }
+}
+const decrease = () => {
+  percentage.value -= 10
+  if (percentage.value < 0) {
+    percentage.value = 0
+  }
+}
 </script>
 
 <style scoped>
