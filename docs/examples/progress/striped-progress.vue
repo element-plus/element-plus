0a822fe8d5499834a810b14eb5c089be5daa7d5c
wzc520pyfm <1528857653@qq.com>
2023-04-08 15:33:18 +0800

    docs(components): [progress] add `striped-flow` attribute
    add `striped-flow` attribute
    
    update `duration` attribute description

--- docs/examples/progress/striped-progress.vue
+++ docs/examples/progress/striped-progress.vue
@@ -2,20 +2,39 @@
   <div class="demo-progress">
     <el-progress :percentage="50" :stroke-width="15" striped />
     <el-progress
+      :percentage="30"
+      :stroke-width="15"
+      status="warning"
+      striped
+      striped-flow
+    />
+    <el-progress
       :percentage="100"
       :stroke-width="15"
       status="success"
       striped
+      striped-flow
+      :duration="10"
     />
-    <el-progress :percentage="70" :stroke-width="15" status="warning" striped />
     <el-progress
-      :percentage="40"
+      :percentage="percentage"
       :stroke-width="15"
       status="exception"
       striped
+      striped-flow
+      :duration="duration"
     />
+    <el-button @click="percentage -= 10">-</el-button>
+    <el-button @click="percentage += 10">+</el-button>
   </div>
 </template>
+
+<script lang="ts" setup>
+import { computed, ref } from 'vue'
+
+const percentage = ref<number>(70)
+const duration = computed(() => Math.floor(percentage.value / 10))
+</script>
 
 <style scoped>
 .demo-progress .el-progress--line {
