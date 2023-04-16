eac3393e6022aabefbecb3731a584609c402455d
Zourunfa <473023989@qq.com>
2023-04-16 08:29:10 +0800

    fix(docs): modify after review

--- docs/examples/tooltip/poper-locations.vue
+++ docs/examples/tooltip/poper-locations.vue
@@ -15,7 +15,7 @@
 </template>
 
 <script setup lang="ts">
-import { reactive } from '@vue/reactivity'
+import { reactive } from 'vue'
 const popperOptions = reactive({
   modifiers: [
     {
@@ -61,23 +61,3 @@ const popperOptions = reactive({
   ],
 })
 </script>
-
-<style lang="scss">
-html,
-body {
-  width: 100vw;
-  height: 100vh;
-  margin: 0;
-  #play {
-    height: 100%;
-    width: 100%;
-    .play-container {
-      height: 100%;
-      width: 100%;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-    }
-  }
-}
-</style>
