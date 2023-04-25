2f740bd12af1769d0aa2e6db4eb3d22e7a6b5d39
btea <2356281422@qq.com>
2023-04-25 19:30:02 +0800

    fix: use localMd

--- docs/.vitepress/vitepress/components/doc-content/vp-table-of-content.vue
+++ docs/.vitepress/vitepress/components/doc-content/vp-table-of-content.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { computed, ref } from 'vue'
+import MarkdownIt from 'markdown-it'
 import { useToc } from '../../composables/use-toc'
 import { useActiveSidebarLinks } from '../../composables/active-bar'
 
@@ -8,42 +9,16 @@ import { useLang } from '../../composables/lang'
 import SponsorsButton from '../sponsors/sponsors-button.vue'
 import SponsorRightTextList from '../sponsors/right-richtext-list.vue'
 import SponsorRightLogoSmallList from '../sponsors/right-logo-small-list.vue'
+import tag from '../../../plugins/tag'
 // import SponsorLarge from '../vp-sponsor-large.vue'
 
+const localMd = MarkdownIt().use(tag)
 const headers = useToc()
 const marker = ref()
 const container = ref()
 useActiveSidebarLinks(container, marker)
 const lang = useLang()
 const sponsor = computed(() => sponsorLocale[lang.value])
-
-const tagRegExp = /\^\(([^)]*)\)/
-type Header = {
-  link: string
-  text: string
-  tag?: string
-  children?: Header[]
-  className?: string
-}
-const convertHeader = (item: Header) => {
-  const tag = item.text.match(tagRegExp)
-  if (tag) {
-    const text = item.text.replace(tagRegExp, '')
-    item.text = text
-    item.tag = tag[1]
-    item.className = 'vp-tag'
-    if (['beta', 'deprecated', 'a11y'].includes(tag[1])) {
-      item.className += ` ${tag[1]}`
-    }
-    if (item.children) {
-      item.children = item.children.map(convertHeader)
-    }
-  }
-  return item
-}
-const convertHeaders = computed(() => {
-  return headers.value.map(convertHeader)
-})
 </script>
 
 <template>
@@ -52,31 +27,28 @@ const convertHeaders = computed(() => {
       <h3 class="toc-content__heading">Contents</h3>
       <ul class="toc-items">
         <li
-          v-for="{ link, text, children, tag, className } in convertHeaders"
+          v-for="{ link, text, children } in headers"
           :key="link"
           class="toc-item"
         >
-          <a class="toc-link" :href="link" :title="text">
-            {{ text }}
-            <span v-if="tag" :class="className">{{ tag }}</span>
-          </a>
+          <a
+            class="toc-link"
+            :href="link"
+            :title="text"
+            v-html="localMd.render(text)"
+          />
           <ul v-if="children">
             <li
-              v-for="{
-                link: childLink,
-                text: childText,
-                tag: childTag,
-                className: childClassName,
-              } in children"
+              v-for="{ link: childLink, text: childText } in children"
               :key="childLink"
               class="toc-item"
             >
-              <a class="toc-link subitem" :href="childLink" :title="text">
-                {{ childText }}
-                <span v-if="childTag" :class="childClassName">{{
-                  childTag
-                }}</span>
-              </a>
+              <a
+                class="toc-link subitem"
+                :href="childLink"
+                :title="text"
+                v-html="localMd.render(childText)"
+              />
             </li>
           </ul>
         </li>
