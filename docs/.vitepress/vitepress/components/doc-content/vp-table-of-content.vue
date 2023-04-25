7fb240ab679c0aa1d9145a74740e68e41ee39a6b
btea <2356281422@qq.com>
2023-04-25 15:22:44 +0800

    feat(docs): right menu tag convert

--- docs/.vitepress/vitepress/components/doc-content/vp-table-of-content.vue
+++ docs/.vitepress/vitepress/components/doc-content/vp-table-of-content.vue
@@ -16,6 +16,34 @@ const container = ref()
 useActiveSidebarLinks(container, marker)
 const lang = useLang()
 const sponsor = computed(() => sponsorLocale[lang.value])
+
+const tagRegExp = /\^\(([^)]*)\)/
+type Header = {
+  link: string
+  text: string
+  tag?: string
+  children?: Header[]
+  className?: string
+}
+const convertHeader = (item: Header) => {
+  const tag = item.text.match(tagRegExp)
+  if (tag) {
+    const text = item.text.replace(tagRegExp, '')
+    item.text = text
+    item.tag = tag[1]
+    item.className = 'vp-tag'
+    if (['beta', 'deprecated', 'a11y'].includes(tag[1])) {
+      item.className += ` ${tag[1]}`
+    }
+    if (item.children) {
+      item.children = item.children.map(convertHeader)
+    }
+  }
+  return item
+}
+const convertHeaders = computed(() => {
+  return headers.value.map(convertHeader)
+})
 </script>
 
 <template>
@@ -24,20 +52,31 @@ const sponsor = computed(() => sponsorLocale[lang.value])
       <h3 class="toc-content__heading">Contents</h3>
       <ul class="toc-items">
         <li
-          v-for="{ link, text, children } in headers"
+          v-for="{ link, text, children, tag, className } in convertHeaders"
           :key="link"
           class="toc-item"
         >
-          <a class="toc-link" :href="link" :title="text">{{ text }}</a>
+          <a class="toc-link" :href="link" :title="text">
+            {{ text }}
+            <span v-if="tag" :class="className">{{ tag }}</span>
+          </a>
           <ul v-if="children">
             <li
-              v-for="{ link: childLink, text: childText } in children"
+              v-for="{
+                link: childLink,
+                text: childText,
+                tag: childTag,
+                className: childClassName,
+              } in children"
               :key="childLink"
               class="toc-item"
             >
-              <a class="toc-link subitem" :href="childLink" :title="text">{{
-                childText
-              }}</a>
+              <a class="toc-link subitem" :href="childLink" :title="text">
+                {{ childText }}
+                <span v-if="childTag" :class="childClassName">{{
+                  childTag
+                }}</span>
+              </a>
             </li>
           </ul>
         </li>
