101cd9c6fc5c7a5faf3622cc71b64ef5da7e5b67
ModyQyW <wurui-dev@foxmail.com>
2023-05-11 10:50:08 +0800

    fix(components): [form] improve FormRules type, support object and array

--- packages/components/form/src/types.ts
+++ packages/components/form/src/types.ts
@@ -20,11 +20,52 @@ export type FormLabelWidthContext = ReturnType<typeof useFormLabelWidth>
 export interface FormItemRule extends RuleItem {
   trigger?: Arrayable<string>
 }
+
+// Reference https://stackoverflow.com/questions/58434389/typescript-deep-keyof-of-a-nested-object/58436959
+// Concatenates two strings with a dot in the middle, unless the last string is empty
+// Join<"a","b"> => "a.b"
+// Join<"a",""> => "a"
+type Join<K, P> = K extends string | number
+  ? P extends string | number
+    ? `${K}${'' extends P ? '' : '.'}${P}`
+    : never
+  : never
+// Limit the recursion to avoid typescript error
+type Prev = [
+  never,
+  0,
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10,
+  11,
+  12,
+  13,
+  14,
+  15,
+  16,
+  17,
+  18,
+  19,
+  20,
+  ...0[]
+]
+type Leaves<T, D extends number = 10> = [D] extends [never]
+  ? never
+  : T extends object
+  ? { [K in keyof T]-?: Join<K, Leaves<T[K], Prev[D]>> }[keyof T]
+  : ''
 export type FormRules<
   T extends MaybeRef<Record<string, any> | string> = string
 > = Partial<
   Record<
-    UnwrapRef<T> extends string ? UnwrapRef<T> : keyof UnwrapRef<T>,
+    UnwrapRef<T> extends string ? UnwrapRef<T> : Leaves<UnwrapRef<T>>,
     Arrayable<FormItemRule>
   >
 >
