50afe8405b5d439d7c2efb8cde6697890f087ebc
zhengsixsix <1833496831@qq.com>
2023-05-23 09:29:59 +0800

    fix(components): add test
    closed #12887

--- packages/components/table/__tests__/table.test.ts
+++ packages/components/table/__tests__/table.test.ts
@@ -1554,6 +1554,74 @@ describe('Table.vue', () => {
     await doubleWait()
     expect(wrapper.vm.selected.length).toEqual(3)
   })
+  // test issues #12887
+  it('First call getSelectionRows', () => {
+    beforeEach(() => {
+      const wrapper = mount({
+        components: {
+          ElTable,
+          ElTableColumn,
+          ElCheckboxGroup,
+          ElCheckbox,
+        },
+        template: `<div>
+          <el-table
+          ref="multipleTableRef"
+          :data="tableData"
+          style="width: 100%"
+          @selection-change="handleSelectionChange"
+        >
+          <el-table-column type="selection" width="55" />
+          <el-table-column label="Date" width="120">
+            <template #default="scope">{{ scope.row.date }}</template>
+          </el-table-column>
+          <el-table-column property="name" label="Name" width="120" />
+          <el-table-column property="address" label="Address" show-overflow-tooltip />
+        </el-table>
+        </div>`,
+        data() {
+          return {
+            tableData: [
+              {
+                date: '2016-05-03',
+                name: 'Tom',
+                address: 'No. 189, Grove St, Los Angeles',
+              },
+              {
+                date: '2016-05-02',
+                name: 'Tom',
+                address: 'No. 189, Grove St, Los Angeles',
+              },
+              {
+                date: '2016-05-04',
+                name: 'Tom',
+                address: 'No. 189, Grove St, Los Angeles',
+              },
+            ],
+            multipleSelection: [],
+          }
+        },
+        methods: {
+          handleSelectionChange(val) {
+            this.multipleSelection = val
+          },
+          getAllDateWhenNoRowSelected() {
+            const multipleTableRef = this.$refs.multipleTableRef
+            let val = multipleTableRef.getSelectionRows()
+            if (val.length === 0) {
+              multipleTableRef.toggleAllSelection()
+              val = multipleTableRef.getSelectionRows()
+              this.multipleSelection = val
+            }
+          },
+        },
+        mounted() {
+          this.getAllDateWhenNoRowSelected()
+        },
+      })
+      expect(wrapper.vm.multipleSelection.length).toBe(3)
+    })
+  })
   it('change columns order when use v-for & key to render table', async () => {
     const wrapper = mount({
       components: {
