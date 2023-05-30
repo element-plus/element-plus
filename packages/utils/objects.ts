e92ffb250e9b896cc9e4b7fb3bda01247b19f2a3
jinhaitao <836334258@qq.com>
2023-05-30 17:13:06 +0800

    refactor: type narrowing

--- packages/utils/objects.ts
+++ packages/utils/objects.ts
@@ -2,8 +2,10 @@ import { get, set } from 'lodash-unified'
 import type { Entries } from 'type-fest'
 import type { Arrayable } from '.'
 
-export const keysOf = <T>(arr: T) => Object.keys(arr) as Array<keyof T>
-export const entriesOf = <T>(arr: T) => Object.entries(arr) as Entries<T>
+export const keysOf = <T extends object>(arr: T) =>
+  Object.keys(arr) as Array<keyof T>
+export const entriesOf = <T extends object>(arr: T) =>
+  Object.entries(arr) as Entries<T>
 export { hasOwn } from '@vue/shared'
 
 export const getProp = <T = any>(
