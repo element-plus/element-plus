233bce3a2568b170970fdd15f0ca783323ca66a8
kamesan <1335951429@qq.com>
2023-07-01 01:53:40 +0800

    fix(components): [select] fix the only first option can be added by enter error

--- packages/components/select/src/useSelect.ts
+++ packages/components/select/src/useSelect.ts
@@ -164,7 +164,7 @@ export const useSelect = (props, states: States, ctx) => {
         newList.push(list[index])
       }
     })
-    return newList.length ? newList : list
+    return newList.length >= list.length ? newList : list
   })
 
   const cachedOptionsArray = computed(() =>
