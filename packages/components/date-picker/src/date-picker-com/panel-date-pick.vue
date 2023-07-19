637535613e2244d7cf1ec830885d05a6daac54fa
btea <2356281422@qq.com>
2023-07-19 16:14:05 +0800

    fix(components): [date-picker] disbale button if now is disabled

--- packages/components/date-picker/src/date-picker-com/panel-date-pick.vue
+++ packages/components/date-picker/src/date-picker-com/panel-date-pick.vue
@@ -168,6 +168,7 @@
         text
         size="small"
         :class="ppNs.e('link-btn')"
+        :disabled="disabledNow"
         @click="changeToNow"
       >
         {{ t('el.datepicker.now') }}
@@ -176,6 +177,7 @@
         plain
         size="small"
         :class="ppNs.e('link-btn')"
+        :disabled="disabledConfirm"
         @click="onConfirm"
       >
         {{ t('el.datepicker.confirm') }}
@@ -437,6 +439,11 @@ const footerVisible = computed(() => {
   return showTime.value || selectionMode.value === 'dates'
 })
 
+const disabledConfirm = computed(() => {
+  if (!disabledDate) return false
+  if (!props.parsedValue) return true
+  return disabledDate(props.parsedValue)
+})
 const onConfirm = () => {
   if (selectionMode.value === 'dates') {
     emit(props.parsedValue as Dayjs[])
@@ -456,6 +463,10 @@ const onConfirm = () => {
   }
 }
 
+const disabledNow = computed(() => {
+  if (!disabledDate) return false
+  return disabledDate(dayjs().locale(lang.value).toDate())
+})
 const changeToNow = () => {
   // NOTE: not a permanent solution
   //       consider disable "now" button in the future
