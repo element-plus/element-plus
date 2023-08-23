708b4d8be4752b4b2ddf0653ff23f05ebf7aaea4
Hefty <yeyuqiudeng@gmail.com>
2023-08-23 17:34:05 +0800

    refactor(components): [upload] remove useless useSlot (#14053)
    refactor(components): [upload] remove useless useSlots

--- packages/components/upload/src/upload.vue
+++ packages/components/upload/src/upload.vue
@@ -13,8 +13,8 @@
       </template>
       <template #append>
         <upload-content ref="uploadRef" v-bind="uploadContentProps">
-          <slot v-if="slots.trigger" name="trigger" />
-          <slot v-if="!slots.trigger && slots.default" />
+          <slot v-if="$slots.trigger" name="trigger" />
+          <slot v-if="!$slots.trigger && $slots.default" />
         </upload-content>
       </template>
     </upload-list>
@@ -24,8 +24,8 @@
       ref="uploadRef"
       v-bind="uploadContentProps"
     >
-      <slot v-if="slots.trigger" name="trigger" />
-      <slot v-if="!slots.trigger && slots.default" />
+      <slot v-if="$slots.trigger" name="trigger" />
+      <slot v-if="!$slots.trigger && $slots.default" />
     </upload-content>
 
     <slot v-if="$slots.trigger" />
@@ -46,14 +46,7 @@
 </template>
 
 <script lang="ts" setup>
-import {
-  computed,
-  onBeforeUnmount,
-  provide,
-  shallowRef,
-  toRef,
-  useSlots,
-} from 'vue'
+import { computed, onBeforeUnmount, provide, shallowRef, toRef } from 'vue'
 import { useFormDisabled } from '@element-plus/components/form'
 import { uploadContextKey } from './constants'
 import UploadList from './upload-list.vue'
@@ -72,7 +65,6 @@ defineOptions({
 
 const props = defineProps(uploadProps)
 
-const slots = useSlots()
 const disabled = useFormDisabled()
 
 const uploadRef = shallowRef<UploadContentInstance>()
