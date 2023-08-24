b4f91335dba10a1e456ee35195156a3839b73a95
King <103868381+kinggq@users.noreply.github.com>
2023-08-24 10:40:31 +0800

    Update packages/components/collapse/src/collapse-item.vue
    Co-authored-by: btea <2356281422@qq.com>

--- packages/components/collapse/src/collapse-item.vue
+++ packages/components/collapse/src/collapse-item.vue
@@ -12,7 +12,7 @@
         role="button"
         :tabindex="disabled ? -1 : 0"
         @click="handleHeaderClick"
-        @keypress.space.enter.stop.prevent.self="handleEnterClick"
+        @keypress.space.enter.self.stop.prevent="handleEnterClick"
         @focus="handleFocus"
         @blur="focusing = false"
       >
