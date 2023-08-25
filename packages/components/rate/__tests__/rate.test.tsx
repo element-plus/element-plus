1a9dc30fb885f0dd750fad4246495a69d84b4341
zepeng <75007029+yj-liuzepeng@users.noreply.github.com>
2023-08-25 11:04:34 +0800

    add test case

--- packages/components/rate/__tests__/rate.test.tsx
+++ packages/components/rate/__tests__/rate.test.tsx
@@ -56,6 +56,18 @@ describe('Rate.vue', () => {
     expect(text.textContent).toMatchInlineSnapshot('"4"')
   })
 
+  it('text color', () => {
+    const wrapper = mount(Rate, {
+      props: {
+        showText: true,
+        modelValue: 1,
+        textColor: '#d74209'
+      },
+    })
+    const text = wrapper.find('.el-rate__text').element as HTMLDivElement
+    expect(text.style.color).toEqual('#d74209')
+  })
+  
   it('value change', async () => {
     const wrapper = mount(Rate, {
       props: {
