298dde9d7216950ad2b3c7ffe290fc9e241cd503
sv-98-maxin <58161021+sv-98-maxin@users.noreply.github.com>
2023-09-05 16:09:28 +0800

    revert(components): [tree]

--- packages/components/tree/src/tree.vue
+++ packages/components/tree/src/tree.vue
@@ -329,9 +329,9 @@ export default defineComponent({
       if (!props.nodeKey)
         throw new Error('[Tree] nodeKey is required in setCurrentKey')
 
-      handleCurrentChange(store, ctx.emit, () => {
+      handleCurrentChange(store, ctx.emit, () =>
         store.value.setCurrentNodeKey(key, shouldAutoExpandParent)
-      })
+      )
     }
 
     const getNode = (data: TreeKey | TreeNodeData): Node => {
