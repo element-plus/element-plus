3599bf1b4d803335fc2a52bdfe3386c78e6d1d3b
zhen <770226915@qq.com>
2023-09-16 15:38:09 +0800

    docs(components): update image watermark example
    update the image for dark mode

--- docs/examples/watermark/image.vue
+++ docs/examples/watermark/image.vue
@@ -1,5 +1,9 @@
 <template>
-  <el-watermark :width="50" :height="50" image="/apple-touch-icon.png">
+  <el-watermark
+    :width="130"
+    :height="30"
+    image="https://element-plus.org/images/element-plus-logo.svg"
+  >
     <div style="height: 500px" />
   </el-watermark>
 </template>
