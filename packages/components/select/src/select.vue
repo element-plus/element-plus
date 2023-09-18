9dacf4de38c242fcdc87c62822dcec1025a6b747
Karolis_Stoncius_Sneakybox <karolis.stoncius@sneakybox.biz>
2023-09-18 11:04:02 +0300

    feat(components): [select] append select dropdown to element

--- packages/components/select/src/select.vue
+++ packages/components/select/src/select.vue
@@ -22,6 +22,7 @@
       :stop-popper-mouse-event="false"
       :gpu-acceleration="false"
       :persistent="persistent"
+      :append-to="appendTo"
       @show="handleMenuEnter"
     >
       <template #default>
@@ -532,6 +533,13 @@ export default defineComponent({
      * @description native input aria-label
      */
     ariaLabel: {
+      type: String,
+      default: undefined,
+    },
+    /**
+     * @description which element the selection dropdown appends to
+     */
+    appendTo: {
       type: String,
       default: undefined,
     },
