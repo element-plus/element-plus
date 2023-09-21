9a173d6c6534b4ab16794c9663c9946886c094db
Zhengyi Ma <1204056646@qq.com>
2023-09-21 17:09:11 +0800

    fix(components): [date-picker] el-date-picker cannot display date

--- packages/components/date-picker/src/date-picker-com/basic-cell-render.tsx
+++ packages/components/date-picker/src/date-picker-com/basic-cell-render.tsx
@@ -14,7 +14,10 @@ export default defineComponent({
       if (slots.default) {
         const list = slots.default(cell).filter((item) => {
           return (
-            item.patchFlag !== -2 && item.type.toString() !== 'Symbol(Comment)'
+            item.patchFlag !== -2 &&
+            item.type.toString() !== 'Symbol(Comment)' &&
+            item.type.toString() !== 'Symbol(v-cmt)' &&
+            item.type.toString() !== 'template'
           )
         })
         if (list.length) {
