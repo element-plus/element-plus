1a7f9b6d3aea0f7eac00064f38d8bea69266457f
Icey Wu <66096254+IceyWu@users.noreply.github.com>
2023-10-12 23:23:48 +0800

    feat(components): [image-viewer] add `minScale` and `maxScale` (#14120)
    * fix(components): [image-viewer] add scale props
    
    * docs(components): [image] add min-scale and max-scale  properties
    
    * feat(components): [image] add minScale and maxScale properties
    
    * docs(components): [image] add example properties
    
    * docs(components): [image] add docs  properties
    
    * docs(components): [image] add docs properties
    
    * docs(components): [image] update  version tag

--- packages/components/image-viewer/src/image-viewer.ts
+++ packages/components/image-viewer/src/image-viewer.ts
@@ -64,6 +64,20 @@ export const imageViewerProps = buildProps({
     type: Number,
     default: 1.2,
   },
+  /**
+   * @description the min scale of the image viewer zoom event.
+   */
+  minScale: {
+    type: Number,
+    default: 0.2,
+  },
+  /**
+   * @description the max scale of the image viewer zoom event.
+   */
+  maxScale: {
+    type: Number,
+    default: 7,
+  },
 } as const)
 export type ImageViewerProps = ExtractPropTypes<typeof imageViewerProps>
 
