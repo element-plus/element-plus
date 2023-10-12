e0f47610e435343e19385c05bba3dac40d6d3320
btea <2356281422@qq.com>
2023-10-12 21:33:26 +0800

    feat(docs): theme switch use startViewTransition api (#14489)
    * feat(docs): theme switch use startViewTransition api
    
    * chore: format
    
    * feat: update

--- docs/.vitepress/vitepress/components/common/vp-theme-toggler.vue
+++ docs/.vitepress/vitepress/components/common/vp-theme-toggler.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, watch } from 'vue'
+import { nextTick, ref, watch } from 'vue'
 import { isDark, toggleDark } from '../../composables/dark'
 import DarkIcon from '../icons/dark.vue'
 import LightIcon from '../icons/light.vue'
@@ -12,13 +12,60 @@ watch(
     toggleDark()
   }
 )
+
+let resolveFn: (value: boolean | PromiseLike<boolean>) => void
+const switchTheme = (event: MouseEvent) => {
+  const isAppearanceTransition =
+    // @ts-expect-error
+    document.startViewTransition &&
+    !window.matchMedia('(prefers-reduced-motion: reduce)').matches
+  if (!isAppearanceTransition || !event) {
+    resolveFn(true)
+    return
+  }
+  const x = event.clientX
+  const y = event.clientY
+  const endRadius = Math.hypot(
+    Math.max(x, innerWidth - x),
+    Math.max(y, innerHeight - y)
+  )
+  // @ts-expect-error: Transition API
+  const transition = document.startViewTransition(async () => {
+    resolveFn(true)
+    await nextTick()
+  })
+  transition.ready.then(() => {
+    const clipPath = [
+      `circle(0px at ${x}px ${y}px)`,
+      `circle(${endRadius}px at ${x}px ${y}px)`,
+    ]
+    document.documentElement.animate(
+      {
+        clipPath: isDark.value ? [...clipPath].reverse() : clipPath,
+      },
+      {
+        duration: 400,
+        easing: 'ease-in',
+        pseudoElement: isDark.value
+          ? '::view-transition-old(root)'
+          : '::view-transition-new(root)',
+      }
+    )
+  })
+}
+const beforeChange = (): Promise<boolean> => {
+  return new Promise((resolve) => {
+    resolveFn = resolve
+  })
+}
 </script>
 
 <template>
-  <div>
+  <div @click.stop="switchTheme">
     <ClientOnly>
       <el-switch
         v-model="darkMode"
+        :before-change="beforeChange"
         :active-action-icon="DarkIcon"
         :inactive-action-icon="LightIcon"
       />
