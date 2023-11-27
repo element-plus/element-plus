b18e50b3bc825e23ab7f7e12b7321ea64b4ff9b6
0song <82012629+0song@users.noreply.github.com>
2023-11-27 21:42:47 +0800

    chore: update readme (#14982)

--- docs/components.d.ts
+++ docs/components.d.ts
@@ -47,6 +47,7 @@ declare module '@vue/runtime-core' {
     Playground: typeof import('./.vitepress/vitepress/components/icons/playground.vue')['default']
     Resource: typeof import('./.vitepress/vitepress/components/globals/resource.vue')['default']
     ResourceSvg: typeof import('./.vitepress/vitepress/components/home/svg/resource-svg.vue')['default']
+    RightBigLogoList: typeof import('./.vitepress/vitepress/components/sponsors/right-big-logo-list.vue')['default']
     RightLayerSvg: typeof import('./.vitepress/vitepress/components/home/svg/right-layer-svg.vue')['default']
     RightLogoSmallList: typeof import('./.vitepress/vitepress/components/sponsors/right-logo-small-list.vue')['default']
     RightRichtextList: typeof import('./.vitepress/vitepress/components/sponsors/right-richtext-list.vue')['default']
