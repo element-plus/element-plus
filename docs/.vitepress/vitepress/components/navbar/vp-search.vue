915098efe7c6149b71e1670c0b2ee65ab589b411
plainheart <yhen@all-my-life.cn>
2023-12-17 19:06:31 +0800

    feat(docs): add translations for doc search

--- docs/.vitepress/vitepress/components/navbar/vp-search.vue
+++ docs/.vitepress/vitepress/components/navbar/vp-search.vue
@@ -6,6 +6,7 @@ import docsearch from '@docsearch/js'
 import { isClient } from '@vueuse/core'
 import { useLang } from '../../composables/lang'
 // import type { DefaultTheme } from '../config'
+import searchLocale from '../../../i18n/component/search.json'
 import type { DocSearchHit } from '@docsearch/react/dist/esm/types'
 
 const props = defineProps<{
@@ -58,11 +59,14 @@ function initialize(userOptions: any) {
   // if the user has multiple locales, the search results should be filtered
   // based on the language
   const facetFilters = props.multilang ? [`language:${lang.value}`] : []
+  const algoliaLocale = searchLocale[lang.value].algolia
 
   docsearch(
     Object.assign({}, userOptions, {
       container: '#docsearch',
       indexName: 'element-plus',
+      placeholder: algoliaLocale.placeholder,
+      translations: algoliaLocale.translations,
       searchParameters: Object.assign({}, userOptions.searchParameters, {
         // pass a custom lang facetFilter to allow multiple language search
         // https://github.com/algolia/docsearch-configs/pull/3942
