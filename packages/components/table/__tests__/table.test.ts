54dc76d74564715680bb9a8112497a2560d462ce
dwaynewdong <dwaynewdong@tencent.com>
2024-01-23 22:17:51 +0800

    fix(components): [table]
    use getTestData is better

--- packages/components/table/__tests__/table.test.ts
+++ packages/components/table/__tests__/table.test.ts
@@ -514,43 +514,7 @@ describe('Table.vue', () => {
       `,
         data() {
           return {
-            testData: [
-              {
-                id: '12987122',
-                name: 'Tom',
-                amount1: '234',
-                amount2: '3.2',
-                amount3: 10,
-              },
-              {
-                id: '12987123',
-                name: 'Tom',
-                amount1: '165',
-                amount2: '4.43',
-                amount3: 12,
-              },
-              {
-                id: '12987124',
-                name: 'Tom',
-                amount1: '324',
-                amount2: '1.9',
-                amount3: 9,
-              },
-              {
-                id: '12987125',
-                name: 'Tom',
-                amount1: '621',
-                amount2: '2.2',
-                amount3: 17,
-              },
-              {
-                id: '12987126',
-                name: 'Tom',
-                amount1: '539',
-                amount2: '4.1',
-                amount3: 15,
-              },
-            ],
+            testData: getTestData(),
           }
         },
         methods: {
@@ -1070,43 +1034,7 @@ describe('Table.vue', () => {
       `,
       data() {
         return {
-          testData: [
-            {
-              id: '12987122',
-              name: 'Tom',
-              amount1: '234',
-              amount2: '3.2',
-              amount3: 10,
-            },
-            {
-              id: '12987123',
-              name: 'Tom',
-              amount1: '165',
-              amount2: '4.43',
-              amount3: 12,
-            },
-            {
-              id: '12987124',
-              name: 'Tom',
-              amount1: '324',
-              amount2: '1.9',
-              amount3: 9,
-            },
-            {
-              id: '12987125',
-              name: 'Tom',
-              amount1: '621',
-              amount2: '2.2',
-              amount3: 17,
-            },
-            {
-              id: '12987126',
-              name: 'Tom',
-              amount1: '539',
-              amount2: '4.1',
-              amount3: 15,
-            },
-          ],
+          testData: getTestData(),
         }
       },
       methods: {
