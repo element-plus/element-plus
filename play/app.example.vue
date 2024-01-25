0c27c1a4520935bd7fb517fd6a49a420504e555e
btea <2356281422@qq.com>
2024-01-25 17:34:52 +0800

    feat(play): custom namespace

--- play/app.example.vue
+++ play/app.example.vue
@@ -1,14 +1,16 @@
 <template>
-  <div class="play-container">
-    <el-icon color="#409EFC">
-      <el-icon-check />
-    </el-icon>
-    <el-icon>
-      <el-icon-menu />
-    </el-icon>
-    <el-button>test</el-button>
-    <div v-loading="true" />
-  </div>
+  <el-config-provider namespace="ep">
+    <div class="play-container">
+      <el-icon color="#409EFC">
+        <el-icon-check />
+      </el-icon>
+      <el-icon>
+        <el-icon-menu />
+      </el-icon>
+      <el-button>test</el-button>
+      <div v-loading="true" />
+    </div>
+  </el-config-provider>
 </template>
 
 <script setup lang="ts">
@@ -21,9 +23,11 @@ body {
   width: 100vw;
   height: 100vh;
   margin: 0;
+
   #play {
     height: 100%;
     width: 100%;
+
     .play-container {
       height: 100%;
       width: 100%;
