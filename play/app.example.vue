9709bc6328291d5a1cd84cfc0662226610984d57
btea <2356281422@qq.com>
2024-01-31 12:43:17 +0800

    chore: add comment

--- play/app.example.vue
+++ play/app.example.vue
@@ -1,16 +1,17 @@
 <template>
-  <el-config-provider namespace="ep">
-    <div class="play-container">
-      <el-icon color="#409EFC">
-        <el-icon-check />
-      </el-icon>
-      <el-icon>
-        <el-icon-menu />
-      </el-icon>
-      <el-button>test</el-button>
-      <div v-loading="true" />
-    </div>
-  </el-config-provider>
+  <!-- If you want to apply a custom namespace, uncomment the following and apply the css configuration in `vite.config.ts`. -->
+  <!-- <el-config-provider namespace="ep"> -->
+  <div class="play-container">
+    <el-icon color="#409EFC">
+      <el-icon-check />
+    </el-icon>
+    <el-icon>
+      <el-icon-menu />
+    </el-icon>
+    <el-button>test</el-button>
+    <div v-loading="true" />
+  </div>
+  <!-- </el-config-provider> -->
 </template>
 
 <script setup lang="ts">
