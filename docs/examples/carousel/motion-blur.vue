23725787e49daffe266997dc26dd600cdc1cd38d
Jed <510374040@qq.com>
2024-02-28 21:28:02 +0800

    update(components): [Carousel] docs/examples/carousel/motion-blur.vue
    Co-authored-by: kooriookami <38392315+kooriookami@users.noreply.github.com>

--- docs/examples/carousel/motion-blur.vue
+++ docs/examples/carousel/motion-blur.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="block text-center">
     <span class="demonstration">Motion blur the switch (default)</span>
-    <el-carousel height="200px" :motion-blur="true">
+    <el-carousel height="200px" motion-blur>
       <el-carousel-item v-for="item in 4" :key="item">
         <h3 class="small justify-center" text="2xl">{{ item }}</h3>
       </el-carousel-item>
