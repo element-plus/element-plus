847cd26cb1f7a6ed0bd792d1448f425976942d33
tolking <qw13131wang@gmail.com>
2024-03-20 00:08:16 +0800

    fix(hooks): SSR hydration error caused by z-index

--- packages/hooks/use-z-index/index.ts
+++ packages/hooks/use-z-index/index.ts
@@ -1,29 +1,56 @@
-import { computed, getCurrentInstance, inject, ref, unref } from 'vue'
-import { isNumber } from '@element-plus/utils'
+import { computed, getCurrentInstance, inject, unref } from 'vue'
+import { debugWarn, isClient, isNumber } from '@element-plus/utils'
 
 import type { InjectionKey, Ref } from 'vue'
 
-const zIndex = ref(0)
+export interface ZIndexInjection {
+  current: number
+}
+
+const initial: ZIndexInjection = {
+  current: 0,
+}
+
 export const defaultInitialZIndex = 2000
+
+// For SSR
+export const ZINDEX_INJECTION_KEY: InjectionKey<ZIndexInjection> =
+  Symbol('elZIndexContextKey')
 
 export const zIndexContextKey: InjectionKey<Ref<number | undefined>> =
   Symbol('zIndexContextKey')
 
 export const useZIndex = (zIndexOverrides?: Ref<number>) => {
+  const increasingInjection = getCurrentInstance()
+    ? inject(ZINDEX_INJECTION_KEY, initial)
+    : initial
+
   const zIndexInjection =
     zIndexOverrides ||
     (getCurrentInstance() ? inject(zIndexContextKey, undefined) : undefined)
+
   const initialZIndex = computed(() => {
     const zIndexFromInjection = unref(zIndexInjection)
     return isNumber(zIndexFromInjection)
       ? zIndexFromInjection
       : defaultInitialZIndex
   })
-  const currentZIndex = computed(() => initialZIndex.value + zIndex.value)
+
+  const currentZIndex = computed(() => {
+    return initialZIndex.value + increasingInjection.current
+  })
 
   const nextZIndex = () => {
-    zIndex.value++
+    increasingInjection.current++
     return currentZIndex.value
+  }
+
+  if (!isClient && !inject(ZINDEX_INJECTION_KEY)) {
+    debugWarn(
+      'ZIndexInjection',
+      `Looks like you are using server rendering, you must provide a z-index provider to ensure the hydration process to be succeed
+usage: app.provide(ZINDEX_INJECTION_KEY, { current: 0 })`
+    )
   }
 
   return {
