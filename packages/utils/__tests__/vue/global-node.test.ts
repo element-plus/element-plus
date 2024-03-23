686c5ab1dca4190cbfa0dfff8681a98262da3108
wzc520pyfm <1528857653@qq.com>
2024-03-23 15:22:06 +0800

    test(utils): [vue] add icon,install,vnode and global-node test

--- packages/utils/__tests__/vue/global-node.test.ts
+++ packages/utils/__tests__/vue/global-node.test.ts
@@ -38,7 +38,17 @@ describe('global-nodes', () => {
     expect(el.parentElement).toBe(document.body)
 
     changeGlobalNodesTarget(target)
+    changeGlobalNodesTarget(target)
 
     expect(el.parentElement).toBe(target)
+  })
+
+  it('should create node with id', () => {
+    const myId = 'my-id'
+    const el = createGlobalNode(myId)
+
+    expect(el).not.toBeNull()
+
+    expect(el.getAttribute('id')).toBe(myId)
   })
 })
