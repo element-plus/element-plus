9d2c4f46e14b69728372d8d27e156002f80db12b
Electrolux <3451613934@qq.com>
2024-04-10 19:09:24 +0800

    fix(table-body-hover-trigger): fix the tooltip count way  when trigger the table

--- packages/components/table/src/table-body/events-helper.ts
+++ packages/components/table/src/table-body/events-helper.ts
@@ -134,6 +134,7 @@ function useEvents<T>(props: Partial<TableBodyProps<T>>) {
     let rangeWidth = range.getBoundingClientRect().width
     let rangeHeight = range.getBoundingClientRect().height
     const offsetWidth = rangeWidth - Math.floor(rangeWidth)
+    let {width:cellChildWidth,height:cellChildHeight} = cellChild.getBoundingClientRect()
     if (offsetWidth < 0.001) {
       rangeWidth = Math.floor(rangeWidth)
     }
@@ -146,9 +147,9 @@ function useEvents<T>(props: Partial<TableBodyProps<T>>) {
     const horizontalPadding = left + right
     const verticalPadding = top + bottom
     if (
-      rangeWidth + horizontalPadding > cellChild.offsetWidth ||
-      rangeHeight + verticalPadding > cellChild.offsetHeight ||
-      cellChild.scrollWidth > cellChild.offsetWidth
+      rangeWidth + horizontalPadding > cellChildWidth ||
+      rangeHeight + verticalPadding > cellChildHeight ||
+      cellChild.scrollWidth > cellChildWidth
     ) {
       createTablePopper(
         tooltipOptions,
