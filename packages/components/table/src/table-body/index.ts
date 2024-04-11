28dac4760773aa5732b7da09a337fcc76a45e248
dwaynewdong <dwaynewdong@tencent.com>
2024-04-11 17:08:11 +0800

    fix(components): [table]
    table column hover style error #16439

--- packages/components/table/src/table-body/index.ts
+++ packages/components/table/src/table-body/index.ts
@@ -40,10 +40,12 @@ export default defineComponent({
       const childNodes = rows[rowNum]?.childNodes
       if (childNodes?.length) {
         const indexes = Array.from(childNodes).reduce((acc, item, index) => {
+          let control = 0
           // drop colsSpan
-          const pre = childNodes[index - 1]?.colSpan > 1
-          const next = childNodes[index + 1]?.colSpan > 1
-          if (item.nodeName !== 'TD' && !pre && !next) {
+          if (childNodes[index]?.colSpan > 1) {
+            control = childNodes[index]?.colSpan - 1
+          }
+          if (item.nodeName !== 'TD' && control === 0) {
             acc.push(index)
           }
           return acc
