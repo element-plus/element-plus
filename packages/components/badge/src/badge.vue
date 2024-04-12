dc6972916102626528a0527a125c9bbfc70c7fcc
bhwa23 <404174262@qq.com>
2024-04-12 14:35:55 +0800

    feat(components): [badge] add dotClass、dotStyle、offset props (#16434)
    * feat: 🎸 [badge] add color prop
    
    add color prop to set badge background color
    
    * feat: 🎸 [badge] add dotClass dotStyle offset  props
    
    * docs: ✏️ [badge] add dotClass dotStyle offset props
    
    * feat: 🎸 [badge] unify style
    
    * style: 💄 [badge] format
    
    * docs: ✏️ update badge.md
    
    * docs: ✏️ update badge props desc
    
    * Update docs/en-US/component/badge.md
    
    Co-authored-by: kooriookami <38392315+kooriookami@users.noreply.github.com>
    
    * refactor: 💡 [badge] refactor style
    
    * fix: 🐛 [badge] dotStyle has higher precedence than color
    
    ---------
    
    Co-authored-by: kooriookami <38392315+kooriookami@users.noreply.github.com>

--- packages/components/badge/src/badge.vue
+++ packages/components/badge/src/badge.vue
@@ -9,8 +9,9 @@
           ns.em('content', type),
           ns.is('fixed', !!$slots.default),
           ns.is('dot', isDot),
+          dotClass,
         ]"
-        :style="{ backgroundColor: color }"
+        :style="style"
         v-text="content"
       />
     </transition>
@@ -20,8 +21,9 @@
 <script lang="ts" setup>
 import { computed } from 'vue'
 import { useNamespace } from '@element-plus/hooks'
-import { isNumber } from '@element-plus/utils'
+import { addUnit, isNumber } from '@element-plus/utils'
 import { badgeProps } from './badge'
+import type { StyleValue } from 'vue'
 
 defineOptions({
   name: 'ElBadge',
@@ -40,6 +42,16 @@ const content = computed<string>(() => {
     return props.value === 0 && !props.showZero ? '' : `${props.value}`
   }
   return `${props.value}`
+})
+const style = computed<StyleValue>(() => {
+  return [
+    {
+      backgroundColor: props.color,
+      marginRight: addUnit(-(props.offset?.[0] ?? 0)),
+      marginTop: addUnit(props.offset?.[1] ?? 0),
+    },
+    props.dotStyle ?? {},
+  ]
 })
 
 defineExpose({
