bd936c3ea37cca848af42043d325c093ce6a6804
刘硕子 <93781659+liushuozi@users.noreply.github.com>
2024-04-15 18:29:38 +0800

    Update basic.vue

--- docs/examples/tabs/basic.vue
+++ docs/examples/tabs/basic.vue
@@ -1,6 +1,6 @@
 <template>
   <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
-    <el-tab-pane label="User" name="first">User</el-tab-pane>
+    <el-tab-pane label="User" name="first">User111</el-tab-pane>
     <el-tab-pane label="Config" name="second">Config</el-tab-pane>
     <el-tab-pane label="Role" name="third">Role</el-tab-pane>
     <el-tab-pane label="Task" name="fourth">Task</el-tab-pane>
