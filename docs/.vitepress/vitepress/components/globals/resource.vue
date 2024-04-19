4fd52ac9ba75c7926ce3f5e320b9d8a2e98dd03a
kooriookami <38392315+kooriookami@users.noreply.github.com>
2024-04-19 17:00:32 +0800

    docs: add mastergo ui kit (#16583)
    * docs: add mastergo ui kit
    
    * feat: update

--- docs/.vitepress/vitepress/components/globals/resource.vue
+++ docs/.vitepress/vitepress/components/globals/resource.vue
@@ -10,6 +10,7 @@ import SketchTemplateSvg from './resources/sketch-template-svg.vue'
 import FigmaTemplateSvg from './resources/figma-template-svg.vue'
 import FigmaVariablesSvg from './resources/figma-variables-svg.vue'
 import FigmaUiKitSvg from './resources/figma-ui-kit-svg.vue'
+import MasterGoUiKitSvg from './resources/master-go-ui-kit-svg.vue'
 
 const mirrorUrl = 'element-plus.gitee.io'
 const isMirrorUrl = () => {
@@ -59,6 +60,14 @@ const resourceCards = computed(() => [
     icon: FigmaTemplateSvg,
     intro: resourceLang.value.figmaIntro,
     url: 'https://www.figma.com/community/file/1021254029764378306',
+  },
+  {
+    key: '2024-master-go-ui-kit',
+    title: resourceLang.value.masterGo2024,
+    description: '2024 MasterGo UI Kit',
+    icon: MasterGoUiKitSvg,
+    intro: resourceLang.value.masterGo2024Intro,
+    url: 'https://mastergo.com/community/resource/124855257836266',
   },
   {
     key: 'sketch',
