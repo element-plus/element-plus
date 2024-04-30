98ee5519c3f856ac004ac9a46744ffaf1c934873
dengzhengcheng <lananabc@163.com>
2024-05-01 00:30:05 +0800

    feat(components): add validation for the Container component properties
    - I found that the orientation attribute of the Container component was not validated.
    
    - So I added enumeration value validation to the component

--- packages/components/container/src/container.vue
+++ packages/components/container/src/container.vue
@@ -6,6 +6,7 @@
 <script lang="ts" setup>
 import { computed, useSlots } from 'vue'
 import { useNamespace } from '@element-plus/hooks'
+import { isDirectionType } from '@element-plus/utils'
 
 import type { Component, VNode } from 'vue'
 
@@ -18,6 +19,7 @@ const props = defineProps({
    */
   direction: {
     type: String,
+    validator: isDirectionType,
   },
 })
 const slots = useSlots()
