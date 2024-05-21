7ab4413ee701dbdd83a3ea66829c614abdcaf340
warmthsea <2586244885@qq.com>
2024-05-21 08:51:56 +0800

    test: add delete keydown test and format alignment string template

--- packages/components/select/__tests__/select.test.ts
+++ packages/components/select/__tests__/select.test.ts
@@ -2583,24 +2583,29 @@ describe('Select', () => {
         label: 'Option2',
         disable: false,
       },
+      {
+        value: 'Option3',
+        label: 'Option3',
+        disable: false,
+      },
     ]
-    const value = ['Option2', 'Option1']
+    const value = ['Option3', 'Option2', 'Option1']
     const wrapper = _mount(
       `
-          <el-select v-model="value"
-            multiple
-            filterable
+        <el-select v-model="value"
+          multiple
+          filterable
+        >
+          <el-option
+            v-for="option in options"
+            :key="option.value"
+            :value="option.value"
+            :label="option.label"
+            :disabled="option.disable"
           >
-            <el-option
-              v-for="option in options"
-              :key="option.value"
-              :value="option.value"
-              :label="option.label"
-              :disabled="option.disable"
-            >
-            </el-option>
-          </el-select>
-        `,
+          </el-option>
+        </el-select>
+      `,
       () => ({
         value,
         options,
@@ -2608,8 +2613,14 @@ describe('Select', () => {
     )
     await nextTick()
     const selectInput = wrapper.find('.el-select__input')
-    expect(wrapper.findAll('.el-tag').length).toBe(2)
+    expect(wrapper.findAll('.el-tag').length).toBe(3)
     // after deletion, an el-tag will be deleted
+    await selectInput.trigger('keydown', {
+      code: EVENT_CODE.delete,
+      key: EVENT_CODE.delete,
+    })
+    await nextTick()
+    expect(wrapper.findAll('.el-tag').length).toBe(2)
     await selectInput.trigger('keydown', {
       code: EVENT_CODE.backspace,
       key: EVENT_CODE.backspace,
