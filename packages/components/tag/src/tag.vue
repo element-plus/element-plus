46ed87f483c8d2c7b70bef43e9a8bc002fe277d2
tianfeng09 <tianfeng09@baidu.com>
2024-06-20 10:18:09 +0800

    fix(components): [tag] fix #17255
    修复使用splice等数组方法移除el-tag过渡动画无法应用的问题
    
    closed #17255

--- packages/components/tag/src/tag.vue
+++ packages/components/tag/src/tag.vue
@@ -14,6 +14,7 @@
   </span>
   <transition v-else :name="`${ns.namespace.value}-zoom-in-center`" appear>
     <span
+      v-if="isVisible"
       :class="containerKls"
       :style="{ backgroundColor: color }"
       @click="handleClick"
@@ -29,7 +30,7 @@
 </template>
 
 <script lang="ts" setup>
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 import ElIcon from '@element-plus/components/icon'
 import { Close } from '@element-plus/icons-vue'
 import { useNamespace } from '@element-plus/hooks'
@@ -58,9 +59,14 @@ const containerKls = computed(() => {
   ]
 })
 
+const isVisible = ref(true)
+
 // methods
 const handleClose = (event: MouseEvent) => {
-  emit('close', event)
+  isVisible.value = false
+  setTimeout(() => {
+    emit('close', event)
+  }, 0)
 }
 
 const handleClick = (event: MouseEvent) => {
