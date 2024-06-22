5b3441b468b487400bd5b8db854aa9446c61e5d8
warmthsea <2586244885@qq.com>
2024-06-22 13:07:10 +0800

    chore: delete /

--- packages/utils/arrays.ts
+++ packages/utils/arrays.ts
@@ -8,14 +8,3 @@ export const ensureArray = <T>(arr: Many<T>): T[] => {
 }
 
 export { castArray } from 'lodash-unified'
-
-// // TODO: rename to `ensureArray`
-// /** like `_.castArray`, except falsy value returns empty array. */
-// export const castArray = <T>(arr: Many<T>): T[] => {
-//   if (!arr && (arr as any) !== 0) return []
-//   return Array.isArray(arr) ? arr : [arr]
-// }
-
-// TODO: remove import alias
-// avoid naming conflicts
-// export { castArray as ensureArray } from 'lodash-unified'
