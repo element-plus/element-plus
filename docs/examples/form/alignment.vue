3775e232eeb76b35c2d5dca93bc730343b73edd4
kooriookami <bingshuanglingluo@163.com>
2024-06-24 17:01:08 +0800

    docs(components): beautify the demo

--- docs/examples/form/alignment.vue
+++ docs/examples/form/alignment.vue
@@ -1,37 +1,35 @@
 <template>
-  <div style="margin: 20px">
-    <span>Form Align</span>
-    <el-radio-group v-model="labelPosition" aria-label="label position">
-      <el-radio-button value="left">Left</el-radio-button>
-      <el-radio-button value="right">Right</el-radio-button>
-      <el-radio-button value="top">Top</el-radio-button>
-    </el-radio-group>
-  </div>
-  <div>
-    <span>First Form Item Align</span>
-    <el-radio-group
-      v-model="itemLabelPosition"
-      aria-label="form item label position"
-    >
-      <el-radio-button value="left">Left</el-radio-button>
-      <el-radio-button value="right">Right</el-radio-button>
-      <el-radio-button value="top">Top</el-radio-button>
-    </el-radio-group>
-  </div>
-  <div style="margin: 20px" />
   <el-form
     :label-position="labelPosition"
     label-width="auto"
     :model="formLabelAlign"
     style="max-width: 600px"
   >
+    <el-form-item label="Form Align" label-position="right">
+      <el-radio-group v-model="labelPosition" aria-label="label position">
+        <el-radio-button value="left">Left</el-radio-button>
+        <el-radio-button value="right">Right</el-radio-button>
+        <el-radio-button value="top">Top</el-radio-button>
+      </el-radio-group>
+    </el-form-item>
+    <el-form-item label="Form Item Align" label-position="right">
+      <el-radio-group
+        v-model="itemLabelPosition"
+        aria-label="item label position"
+      >
+        <el-radio-button value="">Empty</el-radio-button>
+        <el-radio-button value="left">Left</el-radio-button>
+        <el-radio-button value="right">Right</el-radio-button>
+        <el-radio-button value="top">Top</el-radio-button>
+      </el-radio-group>
+    </el-form-item>
     <el-form-item label="Name" :label-position="itemLabelPosition">
       <el-input v-model="formLabelAlign.name" />
     </el-form-item>
-    <el-form-item label="Activity zone">
+    <el-form-item label="Activity zone" :label-position="itemLabelPosition">
       <el-input v-model="formLabelAlign.region" />
     </el-form-item>
-    <el-form-item label="Activity form">
+    <el-form-item label="Activity form" :label-position="itemLabelPosition">
       <el-input v-model="formLabelAlign.type" />
     </el-form-item>
   </el-form>
@@ -39,10 +37,10 @@
 
 <script lang="ts" setup>
 import { reactive, ref } from 'vue'
-import type { FormProps } from 'element-plus'
+import type { FormItemProps, FormProps } from 'element-plus'
 
 const labelPosition = ref<FormProps['labelPosition']>('right')
-const itemLabelPosition = ref<FormProps['labelPosition']>('right')
+const itemLabelPosition = ref<FormItemProps['labelPosition']>('')
 const formLabelAlign = reactive({
   name: '',
   region: '',
