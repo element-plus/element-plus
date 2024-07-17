baf6975715977919d68a667b7350284779ad96ed
dopamine <coderzyou@gmail.com>
2024-07-17 17:03:15 +0800

    reduce inline type declaration

--- packages/components/dialog/index.ts
+++ packages/components/dialog/index.ts
@@ -1,7 +1,8 @@
 import { withInstall } from '@element-plus/utils'
 import Dialog from './src/dialog.vue'
+import type { SFCWithInstall } from '@element-plus/utils'
 
-export const ElDialog = withInstall(Dialog)
+export const ElDialog: SFCWithInstall<typeof Dialog> = withInstall(Dialog)
 export default ElDialog
 
 export * from './src/use-dialog'
