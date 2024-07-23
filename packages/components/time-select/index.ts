6c7fbd1f7b5fef8e27b600dfe1538edce66507ec
qiang <qw13131wang@gmail.com>
2024-07-23 16:09:47 +0800

    chore: [time-select] use withInstall

--- packages/components/time-select/index.ts
+++ packages/components/time-select/index.ts
@@ -1,15 +1,8 @@
+import { withInstall } from '@element-plus/utils'
+
 import TimeSelect from './src/time-select.vue'
 
-import type { App } from 'vue'
-import type { SFCWithInstall } from '@element-plus/utils'
-
-TimeSelect.install = (app: App): void => {
-  app.component(TimeSelect.name, TimeSelect)
-}
-
-const _TimeSelect = TimeSelect as SFCWithInstall<typeof TimeSelect>
-
-export default _TimeSelect
-export const ElTimeSelect = _TimeSelect
+export const ElTimeSelect = withInstall(TimeSelect)
+export default ElTimeSelect
 
 export * from './src/time-select'
