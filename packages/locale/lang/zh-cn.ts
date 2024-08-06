526f0b10903ee641d8e5bc22dbaebc2d97892a7d
warmthsea <2586244885@qq.com>
2024-08-06 09:08:32 +0800

    chore: fix zh-cn

--- packages/locale/lang/zh-cn.ts
+++ packages/locale/lang/zh-cn.ts
@@ -139,7 +139,7 @@ const zhCn: Language = {
     slider: {
       defaultLabel: '滑块介于 {min} 至 {max}',
       defaultRangeStartLabel: '选择起始值',
-      defaultRangeEndLabel: '选择結束值',
+      defaultRangeEndLabel: '选择结束值',
     },
     table: {
       emptyText: '暂无数据',
