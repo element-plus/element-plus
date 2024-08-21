d08b6bfbb2ffd8729174488e81ed4b3bfd6032f2
wuzhiqing <gtawu12138@gmail.com>
2024-08-21 08:18:49 +0000

    feat(docs): remove dependency on `npm:element-plus@latest`

--- docs/examples/config-provider/usage.vue
+++ docs/examples/config-provider/usage.vue
@@ -12,8 +12,7 @@
 
 <script lang="ts" setup>
 import { computed, ref } from 'vue'
-import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
-import en from 'element-plus/dist/locale/en.mjs'
+import { en, zhCn } from '@element-plus/locale/index'
 
 const language = ref('zh-cn')
 const locale = computed(() => (language.value === 'zh-cn' ? zhCn : en))
