7a7515bae842067427454c4aebe0f76866cfcce4
btea <2356281422@qq.com>
2024-08-26 20:19:05 +0800

    Update packages/hooks/__tests__/use-focus-controller.test.tsx
    Co-authored-by: qiang <qw13131wang@gmail.com>

--- packages/hooks/__tests__/use-focus-controller.test.tsx
+++ packages/hooks/__tests__/use-focus-controller.test.tsx
@@ -117,7 +117,6 @@ describe('useFocusController', () => {
 
     await nextTick()
     expect(wrapper.find('span').text()).toBe('false')
-    expect(wrapper.find('div').attributes('tabindex')).toBe(undefined)
     expect(focusHandler).not.toHaveBeenCalled()
     expect(blurHandler).not.toHaveBeenCalled()
 
