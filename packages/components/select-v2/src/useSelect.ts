aeffae7ad0150699e256241fdd1257fda5a3f404
btea <2356281422@qq.com>
2024-09-03 10:58:04 +0800

    fix(components): [select-v2] label display error when re-rendering

--- packages/components/select-v2/src/useSelect.ts
+++ packages/components/select-v2/src/useSelect.ts
@@ -716,7 +716,7 @@ const useSelect = (props: ISelectV2Props, emit) => {
     menuRef.value.scrollToItem(index)
   }
 
-  const getOption = (value) => {
+  const getOption = (value: any, cachedOptions?: Option[]) => {
     // match the option with the given value, if not found, create a new option
     const selectValue = getValueKey(value)
 
@@ -725,20 +725,31 @@ const useSelect = (props: ISelectV2Props, emit) => {
 
       return option
     }
+    let label = value
+    if (cachedOptions && cachedOptions.length) {
+      const option = cachedOptions.find(
+        (option) => getValueKey(getValue(option)) === selectValue
+      )
+      if (option) {
+        label = getLabel(option)
+      }
+    }
+
     return {
       [aliasProps.value.value]: value,
-      [aliasProps.value.label]: value,
+      [aliasProps.value.label]: label,
     }
   }
 
   const initStates = () => {
     if (props.multiple) {
       if ((props.modelValue as Array<any>).length > 0) {
+        const cachedOptions = states.cachedOptions.slice()
         states.cachedOptions.length = 0
         states.previousValue = props.modelValue.toString()
 
         for (const value of props.modelValue) {
-          const option = getOption(value)
+          const option = getOption(value, cachedOptions)
           states.cachedOptions.push(option)
         }
       } else {
