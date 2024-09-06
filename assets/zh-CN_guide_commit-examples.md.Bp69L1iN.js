85f6d8a59abbfe8e569ab28d7d38745753fc1a0d
ElementPlusBot <element-plus@outlook.com>
2024-09-06 11:02:20 +0000

    website deploy

---
+++ assets/zh-CN_guide_commit-examples.md.Bp69L1iN.js
@@ -0,0 +1,50 @@
+import{_ as a}from"./chunks/theme.czabJi79.js";import{_ as t,o as e,c as h,a as i,X as s,y as n}from"./chunks/framework.DcRevfCx.js";const C=JSON.parse('{"title":"提交示例","description":"","frontmatter":{"title":"提交示例","lang":"zh-CN"},"headers":[{"level":2,"title":"为什么存在这个章节?","slug":"为什么存在这个章节","link":"#为什么存在这个章节","children":[{"level":3,"title":"写入提交消息的规则","slug":"写入提交消息的规则","link":"#写入提交消息的规则","children":[]},{"level":3,"title":"提交消息的模板","slug":"提交消息的模板","link":"#提交消息的模板","children":[]},{"level":3,"title":"相关链接","slug":"相关链接","link":"#相关链接","children":[]}]}],"relativePath":"zh-CN/guide/commit-examples.md","filePath":"zh-CN/guide/commit-examples.md","lastUpdated":null}'),k={name:"zh-CN/guide/commit-examples.md"};function o(d,p,g,r,c,b){const l=a;return e(),h("div",null,[i("h1",{id:"提交示例",tabindex:"-1"},[s("提交示例 "),i("a",{class:"header-anchor vp-link",href:"#提交示例","aria-label":'Permalink to "提交示例"'},"​")]),i("h2",{id:"为什么存在这个章节",tabindex:"-1"},[s("为什么存在这个章节? "),i("a",{class:"header-anchor vp-link",href:"#为什么存在这个章节","aria-label":'Permalink to "为什么存在这个章节?"'},"​")]),i("p",null,[s("详情请参阅 "),i("a",{href:"https://www.conventionalcommits.org/",class:"vp-link",target:"_blank",rel:"noreferrer"},[s("通用惯例"),n(l,{class:"link-icon"})]),s("。")]),i("p",null,"一个好的 Git 提交信息能够使我们："),i("ol",null,[i("li",null,"了解贡献者试图做什么"),i("li",null,"自动生成更改日志")]),i("h3",{id:"写入提交消息的规则",tabindex:"-1"},[s("写入提交消息的规则 "),i("a",{class:"header-anchor vp-link",href:"#写入提交消息的规则","aria-label":'Permalink to "写入提交消息的规则"'},"​")]),i("div",{class:"language-md vp-adaptive-theme"},[i("button",{title:"Copy Code",class:"copy"}),i("span",{class:"lang"},"md"),i("pre",{class:"shiki shiki-themes github-light github-dark vp-code",tabindex:"0"},[i("code",null,[i("span",{class:"line"},[i("span",{style:{"--shiki-light":"#005CC5","--shiki-dark":"#79B8FF","--shiki-light-font-weight":"bold","--shiki-dark-font-weight":"bold"}},"# (如果通过, 当前提交会实现...) <"),i("span",{style:{"--shiki-light":"#22863A","--shiki-dark":"#85E89D","--shiki-light-font-weight":"bold","--shiki-dark-font-weight":"bold"}},"subject"),i("span",{style:{"--shiki-light":"#005CC5","--shiki-dark":"#79B8FF","--shiki-light-font-weight":"bold","--shiki-dark-font-weight":"bold"}},"> (最多 72 个字符)")]),s(`
+`),i("span",{class:"line"}),s(`
+`),i("span",{class:"line"},[i("span",{style:{"--shiki-light":"#005CC5","--shiki-dark":"#79B8FF","--shiki-light-font-weight":"bold","--shiki-dark-font-weight":"bold"}},"# |<---- 使用最多 72 个字符 ---->|")]),s(`
+`),i("span",{class:"line"}),s(`
+`),i("span",{class:"line"},[i("span",{style:{"--shiki-light":"#005CC5","--shiki-dark":"#79B8FF","--shiki-light-font-weight":"bold","--shiki-dark-font-weight":"bold"}},"# 解释清楚为什么要作出此修改")]),s(`
+`),i("span",{class:"line"}),s(`
+`),i("span",{class:"line"},[i("span",{style:{"--shiki-light":"#005CC5","--shiki-dark":"#79B8FF","--shiki-light-font-weight":"bold","--shiki-dark-font-weight":"bold"}},"# |<---- 尝试将每一行控制在 72 个字符以内 ---->|")]),s(`
+`),i("span",{class:"line"}),s(`
+`),i("span",{class:"line"},[i("span",{style:{"--shiki-light":"#005CC5","--shiki-dark":"#79B8FF","--shiki-light-font-weight":"bold","--shiki-dark-font-weight":"bold"}},"# 提供链接、相关标签关键词、文章或其他资源信息")]),s(`
+`),i("span",{class:"line"}),s(`
+`),i("span",{class:"line"},[i("span",{style:{"--shiki-light":"#005CC5","--shiki-dark":"#79B8FF","--shiki-light-font-weight":"bold","--shiki-dark-font-weight":"bold"}},"# 使用issue和合并请求的全链接地址替代部分链接")]),s(`
+`),i("span",{class:"line"}),s(`
+`),i("span",{class:"line"},[i("span",{style:{"--shiki-light":"#005CC5","--shiki-dark":"#79B8FF","--shiki-light-font-weight":"bold","--shiki-dark-font-weight":"bold"}},"# 因为它们在GitLab之外显示为纯文本")]),s(`
+`),i("span",{class:"line"}),s(`
+`),i("span",{class:"line"},[i("span",{style:{"--shiki-light":"#005CC5","--shiki-dark":"#79B8FF","--shiki-light-font-weight":"bold","--shiki-dark-font-weight":"bold"}},"# --- 提交结束 ---")]),s(`
+`),i("span",{class:"line"}),s(`
+`),i("span",{class:"line"},[i("span",{style:{"--shiki-light":"#005CC5","--shiki-dark":"#79B8FF","--shiki-light-font-weight":"bold","--shiki-dark-font-weight":"bold"}},"# --------------------")]),s(`
+`),i("span",{class:"line"}),s(`
+`),i("span",{class:"line"},[i("span",{style:{"--shiki-light":"#005CC5","--shiki-dark":"#79B8FF","--shiki-light-font-weight":"bold","--shiki-dark-font-weight":"bold"}},"# 不要忘记")]),s(`
+`),i("span",{class:"line"}),s(`
+`),i("span",{class:"line"},[i("span",{style:{"--shiki-light":"#005CC5","--shiki-dark":"#79B8FF","--shiki-light-font-weight":"bold","--shiki-dark-font-weight":"bold"}},"# 将主题行大写(英文标题)")]),s(`
+`),i("span",{class:"line"}),s(`
+`),i("span",{class:"line"},[i("span",{style:{"--shiki-light":"#005CC5","--shiki-dark":"#79B8FF","--shiki-light-font-weight":"bold","--shiki-dark-font-weight":"bold"}},"# 主题行使用命令式语气")]),s(`
+`),i("span",{class:"line"}),s(`
+`),i("span",{class:"line"},[i("span",{style:{"--shiki-light":"#005CC5","--shiki-dark":"#79B8FF","--shiki-light-font-weight":"bold","--shiki-dark-font-weight":"bold"}},"# 不要在主题行末尾带有一段时间")]),s(`
+`),i("span",{class:"line"}),s(`
+`),i("span",{class:"line"},[i("span",{style:{"--shiki-light":"#005CC5","--shiki-dark":"#79B8FF","--shiki-light-font-weight":"bold","--shiki-dark-font-weight":"bold"}},"# 主题行至少包含 3 个单词")]),s(`
+`),i("span",{class:"line"}),s(`
+`),i("span",{class:"line"},[i("span",{style:{"--shiki-light":"#005CC5","--shiki-dark":"#79B8FF","--shiki-light-font-weight":"bold","--shiki-dark-font-weight":"bold"}},"# 主题和空行之间使用空行隔开")]),s(`
+`),i("span",{class:"line"}),s(`
+`),i("span",{class:"line"},[i("span",{style:{"--shiki-light":"#005CC5","--shiki-dark":"#79B8FF","--shiki-light-font-weight":"bold","--shiki-dark-font-weight":"bold"}},"# 当提交的更改行数超过30行或至少3个文件需要发生修改时")]),s(`
+`),i("span",{class:"line"}),s(`
+`),i("span",{class:"line"},[i("span",{style:{"--shiki-light":"#005CC5","--shiki-dark":"#79B8FF","--shiki-light-font-weight":"bold","--shiki-dark-font-weight":"bold"}},"# 应在提交的主体信息中说明修改的详情")]),s(`
+`),i("span",{class:"line"}),s(`
+`),i("span",{class:"line"},[i("span",{style:{"--shiki-light":"#005CC5","--shiki-dark":"#79B8FF","--shiki-light-font-weight":"bold","--shiki-dark-font-weight":"bold"}},"# 不要使用 Emojis 表情符号")]),s(`
+`),i("span",{class:"line"}),s(`
+`),i("span",{class:"line"},[i("span",{style:{"--shiki-light":"#005CC5","--shiki-dark":"#79B8FF","--shiki-light-font-weight":"bold","--shiki-dark-font-weight":"bold"}},"# 在主体信息中解释提交的是什么内容, 为什么提交, 如何实现")]),s(`
+`),i("span",{class:"line"}),s(`
+`),i("span",{class:"line"},[i("span",{style:{"--shiki-light":"#005CC5","--shiki-dark":"#79B8FF","--shiki-light-font-weight":"bold","--shiki-dark-font-weight":"bold"}},'# 每行以 "-" 开头, 写入多行可描述信息要点')]),s(`
+`),i("span",{class:"line"}),s(`
+`),i("span",{class:"line"},[i("span",{style:{"--shiki-light":"#005CC5","--shiki-dark":"#79B8FF","--shiki-light-font-weight":"bold","--shiki-dark-font-weight":"bold"}},"# 详见: https://chris.beams.io/posts/git-commit/")]),s(`
+`),i("span",{class:"line"}),s(`
+`),i("span",{class:"line"},[i("span",{style:{"--shiki-light":"#005CC5","--shiki-dark":"#79B8FF","--shiki-light-font-weight":"bold","--shiki-dark-font-weight":"bold"}},"# --------------------")])])])]),i("h3",{id:"提交消息的模板",tabindex:"-1"},[s("提交消息的模板 "),i("a",{class:"header-anchor vp-link",href:"#提交消息的模板","aria-label":'Permalink to "提交消息的模板"'},"​")]),i("p",null,"下面是供参考的模板提交消息。"),i("div",{class:"language-md vp-adaptive-theme"},[i("button",{title:"Copy Code",class:"copy"}),i("span",{class:"lang"},"md"),i("pre",{class:"shiki shiki-themes github-light github-dark vp-code",tabindex:"0"},[i("code",null,[i("span",{class:"line"},[i("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},"feat(components): ["),i("span",{style:{"--shiki-light":"#032F62","--shiki-dark":"#DBEDFF","--shiki-light-text-decoration":"underline","--shiki-dark-text-decoration":"underline"}},"button"),i("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},"] 我用按钮实现了某些功能")]),s(`
+`),i("span",{class:"line"}),s(`
+`),i("span",{class:"line"},[i("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},"主体行和主体内容之间用空白行隔开(可以有预期时间)")]),s(`
+`),i("span",{class:"line"},[i("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},"通过一行或多行描述你的修改信息")]),s(`
+`),i("span",{class:"line"},[i("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},"每一行的首字母大写")]),s(`
+`),i("span",{class:"line"},[i("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},"且每一行的总字符数限制在72个以内最优, 超过了将不易于他人理解")]),s(`
+`),i("span",{class:"line"}),s(`
+`),i("span",{class:"line"},[i("span",{style:{"--shiki-light":"#E36209","--shiki-dark":"#FFAB70"}},"-"),i("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}}," 你也可以通过添加子项列表符号来为内容布局")])])])]),i("p",null,"主题标题的格式是："),i("div",{class:"language- vp-adaptive-theme"},[i("button",{title:"Copy Code",class:"copy"}),i("span",{class:"lang"}),i("pre",{class:"shiki shiki-themes github-light github-dark vp-code",tabindex:"0"},[i("code",null,[i("span",{class:"line"},[i("span",null,"[type](scope 域): [messages]")])])])]),i("p",null,[s("关于"),i("strong",null,"type"),s(" 和 "),i("strong",null,"scope"),s("属性允许的值可以在 "),i("a",{href:"https://github.com/element-plus/element-plus/blob/c2ee36a7fc72b17742d43ecdff4e2912c416141d/commitlint.config.js#L57",class:"vp-link",target:"_blank",rel:"noreferrer"},[s("commitlint.config.js"),n(l,{class:"link-icon"})]),s(" 文件内查看")]),i("h3",{id:"相关链接",tabindex:"-1"},[s("相关链接 "),i("a",{class:"header-anchor vp-link",href:"#相关链接","aria-label":'Permalink to "相关链接"'},"​")]),i("p",null,[i("a",{href:"https://about.gitlab.com/blog/2018/06/07/keeping-git-commit-history-clean/",class:"vp-link",target:"_blank",rel:"noreferrer"},[s("保持 git 提交历史简洁"),n(l,{class:"link-icon"})])])])}const u=t(k,[["render",o]]);export{C as __pageData,u as default};
