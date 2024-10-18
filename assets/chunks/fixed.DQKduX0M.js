6620b5cab74cab7c1277754ebad595ae236eca8e
ElementPlusBot <element-plus@outlook.com>
2024-10-18 11:48:52 +0000

    website deploy

--- assets/chunks/fixed.DQKduX0M.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,q as t,o as a,t as c,v as e,y as f,X as _}from"./framework.DcRevfCx.js";const r={};function p(i,x){const o=t("el-button"),n=t("el-affix");return a(),c(n,{position:"bottom",offset:20},{default:e(()=>[f(o,{type:"primary"},{default:e(()=>[_("Offset bottom 20px")]),_:1})]),_:1})}const m=s(r,[["render",p]]);export{m as E};
