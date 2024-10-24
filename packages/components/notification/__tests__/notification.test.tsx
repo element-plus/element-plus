ba3e80ce0e41c451823a159b1c8e2d625b1c2f2d
geforseN <geforsenjs@gmail.com>
2024-10-24 13:33:07 +0700

    test(components): [notification] refactor
    - Style
    - Remove commented code.
    - Reduce boilerplate with `__mount`.
    - Hide implementation details with `isClosed` and `isOpen`.
    - Add `keydown` utility.

--- packages/components/notification/__tests__/notification.test.tsx
+++ packages/components/notification/__tests__/notification.test.tsx
@@ -1,30 +1,15 @@
 import { nextTick } from 'vue'
-import { mount } from '@vue/test-utils'
 import { describe, expect, test, vi } from 'vitest'
 import { TypeComponentsMap } from '@element-plus/utils'
 import { EVENT_CODE } from '@element-plus/constants'
 import { notificationTypes } from '../src/notification'
-import Notification from '../src/notification.vue'
-
-import type { VNode } from 'vue'
-import type { VueWrapper } from '@vue/test-utils'
-import type { MockInstance } from 'vitest'
-import type {
-  NotificationInstance,
-  NotificationProps,
-} from '../src/notification'
+import { __mount, _mount, isClosed, isOpen } from './wrapper.utils'
+import type { NotificationVueWrapper } from './wrapper.utils'
 
 const AXIOM = 'Rem is the best girl'
 
-const onClose = vi.fn()
-
-const _mount = ({
-  props,
-  slots,
-}: {
-  props?: Partial<NotificationProps>
-  slots?: Record<'default', () => string | VNode>
-}) => mount(<Notification {...{ onClose, ...props }} v-slots={slots} />)
+const findNotification = (wrapper: NotificationVueWrapper) =>
+  wrapper.find('[role=alert]')
 
 describe('Notification.vue', () => {
   describe('render', () => {
@@ -34,12 +19,11 @@ describe('Notification.vue', () => {
           default: () => AXIOM,
         },
       })
-
       expect(wrapper.text()).toEqual(AXIOM)
-      expect(wrapper.vm.visible).toBe(true)
-      expect(wrapper.vm.iconComponent).toBeUndefined()
-      expect(wrapper.vm.horizontalClass).toBe('right')
-      expect(wrapper.vm.positionStyle).toEqual(
+      expect(wrapper).toSatisfy(isOpen)
+      expect((wrapper.vm as any).iconComponent).toBeUndefined()
+      expect((wrapper.vm as any).horizontalClass).toBe('right')
+      expect((wrapper.vm as any).positionStyle).toEqual(
         expect.objectContaining({
           top: '0px',
         })
@@ -52,43 +36,31 @@ describe('Notification.vue', () => {
           default: () => <span class="text-node">{AXIOM}</span>,
         },
       })
-
       expect(wrapper.find('.text-node').exists()).toBe(true)
     })
 
     test('should be able to render raw HTML tag with dangerouslyUseHTMLString flag', () => {
       const tagClass = 'test-class'
-      const HTMLWrapper = _mount({
-        props: {
-          dangerouslyUseHTMLString: true,
-          message: `<strong class=${tagClass}>${AXIOM}</strong>`,
-        },
+      const HTMLWrapper = __mount({
+        dangerouslyUseHTMLString: true,
+        message: `<strong class=${tagClass}>${AXIOM}</strong>`,
       })
-
       expect(HTMLWrapper.find(`.${tagClass}`).exists()).toBe(true)
     })
 
     test('should not be able to render raw HTML tag without dangerouslyUseHTMLString flag', () => {
       const tagClass = 'test-class'
-      const HTMLWrapper = _mount({
-        props: {
-          dangerouslyUseHTMLString: false,
-          message: `<strong class=${tagClass}>${AXIOM}</strong>`,
-        },
+      const HTMLWrapper = __mount({
+        dangerouslyUseHTMLString: false,
+        message: `<strong class=${tagClass}>${AXIOM}</strong>`,
       })
-
       expect(HTMLWrapper.find(`.${tagClass}`).exists()).toBe(false)
     })
 
     test('should be able to render z-index style with zIndex flag', async () => {
-      const wrapper = _mount({
-        props: {
-          zIndex: 9999,
-        },
-      })
+      const wrapper = __mount({ zIndex: 9999 })
       await nextTick()
-
-      expect(wrapper.vm.positionStyle).toEqual(
+      expect((wrapper.vm as any).positionStyle).toEqual(
         expect.objectContaining({
           top: '0px',
           zIndex: 9999,
@@ -98,40 +70,35 @@ describe('Notification.vue', () => {
   })
 
   describe('Notification.type', () => {
-    test('should be able to render typed notification', () => {
-      let wrapper: VueWrapper<NotificationInstance>
-
-      for (const type of notificationTypes) {
-        wrapper = _mount({
-          props: {
-            type,
-          },
-        })
-        expect(wrapper.findComponent(TypeComponentsMap[type]).exists()).toBe(
-          true
-        )
+    test.for(notificationTypes)(
+      "should be able to render typed notification when { type: '%s' }",
+      (type) => {
+        const wrapper = __mount({ type })
+        const typeComponent = wrapper.findComponent(TypeComponentsMap[type])
+        expect(typeComponent.exists()).toBe(true)
       }
-    })
+    )
+
+    const findIcon = (wrapper: NotificationVueWrapper) =>
+      wrapper.find('.el-notification__icon')
 
     test('should not be able to render invalid type icon', () => {
-      vi.spyOn(console, 'warn').mockImplementation(() => vi.fn)
-
-      const type = 'some-type'
-      const wrapper = _mount({
-        props: {
-          // @ts-expect-error
-          type,
-        },
-      })
-
-      expect(wrapper.find('.el-notification__icon').exists()).toBe(false)
-      expect(console.warn).toHaveBeenCalled()
-      ;(console.warn as any as MockInstance).mockRestore()
+      const consoleWarn = vi
+        .spyOn(console, 'warn')
+        .mockImplementation(() => vi.fn)
+      // @ts-expect-error
+      const wrapper = __mount({ type: 'some-type' })
+      expect(findIcon(wrapper).exists()).toBe(false)
+      expect(consoleWarn).toHaveBeenCalled()
+      consoleWarn.mockRestore()
     })
   })
 
+  const findCloseButton = (wrapper: NotificationVueWrapper) =>
+    wrapper.find('.el-notification__closeBtn')
+
   describe('event handlers', () => {
-    test('it should be able to close the notification by clicking close button', async () => {
+    test('should be able to close the notification by clicking close button', async () => {
       const onClose = vi.fn()
       const wrapper = _mount({
         slots: {
@@ -140,78 +107,51 @@ describe('Notification.vue', () => {
         props: { onClose },
       })
       await nextTick()
-
-      const closeBtn = wrapper.find('.el-notification__closeBtn')
-      expect(closeBtn.exists()).toBe(true)
-      await closeBtn.trigger('click')
+      const closeButton = findCloseButton(wrapper)
+      expect(closeButton.exists()).toBe(true)
+      await closeButton.trigger('click')
       expect(onClose).toHaveBeenCalled()
     })
 
     test('should be able to close after duration', async () => {
       vi.useFakeTimers()
-      const duration = 100
-      const wrapper = _mount({
-        props: {
-          duration,
-        },
-      })
+      const wrapper = __mount({ duration: 100 })
       vi.runAllTimers()
+      expect(wrapper).toSatisfy(isClosed)
       vi.useRealTimers()
-
-      expect(wrapper.vm.visible).toBe(false)
     })
 
     test('should be able to prevent close itself when hover over', async () => {
       vi.useFakeTimers()
-      const duration = 100
-      const wrapper = _mount({
-        props: {
-          duration,
-        },
-      })
+      const wrapper = __mount({ duration: 100 })
       vi.advanceTimersByTime(50)
-
-      await wrapper.find('[role=alert]').trigger('mouseenter')
+      await findNotification(wrapper).trigger('mouseenter')
       vi.advanceTimersByTime(5000)
-      expect(wrapper.vm.visible).toBe(true)
-
-      await wrapper.find('[role=alert]').trigger('mouseleave')
-      // expect(wrapper.vm.timer).not.toBe(null)
-      expect(wrapper.vm.visible).toBe(true)
-      // expect(wrapper.vm.closed).toBe(false)
+      expect(wrapper).toSatisfy(isOpen)
+      await findNotification(wrapper).trigger('mouseleave')
+      expect(wrapper).toSatisfy(isOpen)
       vi.runAllTimers()
-      expect(wrapper.vm.visible).toBe(false)
-      // expect(wrapper.vm.timer).toBe(null)
-      // expect(wrapper.vm.closed).toBe(true)
+      expect(wrapper).toSatisfy(isClosed)
       vi.useRealTimers()
     })
 
     test('should not be able to close when duration is set to 0', async () => {
       vi.useFakeTimers()
-      const duration = 0
-      const wrapper = _mount({
-        props: {
-          duration,
-        },
-      })
-
+      const wrapper = __mount({ duration: 0 })
       vi.runAllTimers()
-      expect(wrapper.vm.visible).toBe(true)
+      expect(wrapper).toSatisfy(isOpen)
       vi.useRealTimers()
     })
 
     test('should be able to handle click event', async () => {
       const onClick = vi.fn()
-      const wrapper = _mount({
-        props: {
-          duration: 0,
-          onClick,
-        },
-      })
-
+      const wrapper = __mount({ duration: 0, onClick })
       await wrapper.trigger('click')
       expect(onClick).toHaveBeenCalledTimes(1)
     })
+
+    const keydown = (options: KeyboardEventInit) =>
+      new KeyboardEvent('keydown', { ...options })
 
     test('should be able to delete timer when press delete', async () => {
       vi.useFakeTimers()
@@ -220,15 +160,11 @@ describe('Notification.vue', () => {
           default: () => AXIOM,
         },
       })
-
-      const event = new KeyboardEvent('keydown', {
-        code: EVENT_CODE.backspace,
-        bubbles: true,
-      })
-      document.dispatchEvent(event)
-
+      document.dispatchEvent(
+        keydown({ code: EVENT_CODE.backspace, bubbles: true })
+      )
       vi.runAllTimers()
-      expect(wrapper.vm.visible).toBe(true)
+      expect(wrapper).toSatisfy(isOpen)
       vi.useRealTimers()
     })
 
@@ -242,15 +178,9 @@ describe('Notification.vue', () => {
           default: () => AXIOM,
         },
       })
-
-      // Same as above
-      const event = new KeyboardEvent('keydown', {
-        code: EVENT_CODE.esc,
-      })
-
-      document.dispatchEvent(event)
+      document.dispatchEvent(keydown({ code: EVENT_CODE.esc }))
       vi.runAllTimers()
-      expect(wrapper.vm.visible).toBe(false)
+      expect(wrapper).toSatisfy(isClosed)
       vi.useRealTimers()
     })
   })
