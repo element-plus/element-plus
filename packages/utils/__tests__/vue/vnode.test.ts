d184dd3003c69e765dc40618ce8e219c878ddc5a
zsd <80782170@qq.com>
2024-10-28 19:55:07 +0800

    test(utils): [vue] fix tests's lint errors(element-plus:#18649)

--- packages/utils/__tests__/vue/vnode.test.ts
+++ packages/utils/__tests__/vue/vnode.test.ts
@@ -20,104 +20,94 @@ describe('ensureOnlyChild', () => {
   })
 })
 
-describe("flattedChildren", () => {
-  it("Component vnode has no child nodes.", () => {
+describe('flattedChildren', () => {
+  it('Component vnode has no child nodes.', () => {
     const vnode = {
       __v_isVNode: true,
       type: {
-        __name: "Comp",
-        setup() { },
-        render() { },
+        __name: 'Comp',
       },
       component: {
         subTree: {
-          type: "div",
-          children: "text",
+          type: 'div',
+          children: 'text',
         },
       },
-    };
-    const flattedVNodes = flattedChildren(vnode);
-    expect(flattedVNodes).toContain(vnode);
-  });
-  it("Component VNode has child nodes.", () => {
+    }
+    const flattedVNodes = flattedChildren(vnode)
+    expect(flattedVNodes).toContain(vnode)
+  })
+  it('Component VNode has child nodes.', () => {
     const childVNode = {
       __v_isVNode: true,
       type: {
-        __name: "Child",
-        setup() { },
-        render() { },
+        __name: 'Child',
       },
       component: {
         subTree: {
-          type: "div",
-          children: "text",
+          type: 'div',
+          children: 'text',
         },
       },
-    };
+    }
     const parentVNode = {
       __v_isVNode: true,
       type: {
-        __name: "Parent",
-        setup() { },
-        render() { },
+        __name: 'Parent',
       },
       component: {
         subTree: childVNode,
       },
-    };
+    }
 
-    const flattedVNodes = flattedChildren(parentVNode);
-    expect(flattedVNodes).toContain(parentVNode);
-    expect(flattedVNodes).toContain(childVNode);
-  });
-  it("Component VNode has a slot, and the children passed in through the h function are object.", () => {
+    const flattedVNodes = flattedChildren(parentVNode)
+    expect(flattedVNodes).toContain(parentVNode)
+    expect(flattedVNodes).toContain(childVNode)
+  })
+  it('Component VNode has a slot, and the children passed in through the h function are object.', () => {
     const slotVNode = {
       __v_isVNode: true,
-      type: "div",
-      children: "text",
-    };
+      type: 'div',
+      children: 'text',
+    }
     const vnode = {
       __v_isVNode: true,
       type: {
-        __name: "Child",
-        setup() { },
-        render() { },
+        __name: 'Child',
       },
       component: {
         subTree: slotVNode,
       },
       children: {
         default() {
-          return slotVNode;
+          return slotVNode
         },
       },
-    };
+    }
 
-    const flattedVNodes = flattedChildren(vnode);
-    expect(flattedVNodes).toContain(vnode);
-    expect(flattedVNodes).toContain(slotVNode);
-  });
-  it("Component VNode has a slot, and the children passed in through the h function are array.", () => {
+    const flattedVNodes = flattedChildren(vnode)
+    expect(flattedVNodes).toContain(vnode)
+    expect(flattedVNodes).toContain(slotVNode)
+  })
+  it('Component VNode has a slot, and the children passed in through the h function are array.', () => {
     const slotVNode = {
       __v_isVNode: true,
-      type: "div",
-      children: "text",
-    };
+      type: 'div',
+      children: 'text',
+    }
     const vnode = {
       __v_isVNode: true,
       type: {
-        __name: "Child",
-        setup() { },
-        render() { },
+        __name: 'Child',
       },
       component: {
         subTree: slotVNode,
       },
       children: [slotVNode],
-    };
+    }
 
-    const flattedVNodes = flattedChildren(vnode);
-    expect(flattedVNodes).toContain(vnode);
-    expect(flattedVNodes).toContain(slotVNode);
-  });
-});
+    const flattedVNodes = flattedChildren(vnode)
+    expect(flattedVNodes).toContain(vnode)
+    expect(flattedVNodes).toContain(slotVNode)
+  })
+})
