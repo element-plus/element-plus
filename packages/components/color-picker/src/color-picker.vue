239b76ce7772053fed34190087d094f25c857e0e
lujiamu <2262754355@qq.com>
2024-11-12 09:19:01 +0800

    fix(components): [color-picker] Optimize the flickering issue

--- packages/components/color-picker/src/color-picker.vue
+++ packages/components/color-picker/src/color-picker.vue
@@ -16,7 +16,10 @@
     @hide="setShowPicker(false)"
   >
     <template #content>
-      <div v-click-outside="handleClickOutside" @keydown.esc="handleEsc">
+      <div
+        v-click-outside:[triggerRef]="handleClickOutside"
+        @keydown.esc="handleEsc"
+      >
         <div :class="ns.be('dropdown', 'main-wrapper')">
           <hue-slider ref="hue" class="hue-slider" :color="color" vertical />
           <sv-panel ref="sv" :color="color" />
@@ -244,8 +247,8 @@ function setShowPicker(value: boolean) {
   showPicker.value = value
 }
 
+// const debounceSetShowPicker = debounce(setShowPicker, 100, { leading: true })
 const debounceSetShowPicker = debounce(setShowPicker, 100, { leading: true })
-
 function show() {
   if (colorDisabled.value) return
   setShowPicker(true)
@@ -271,6 +274,9 @@ function resetColor() {
 
 function handleTrigger() {
   if (colorDisabled.value) return
+  if (showPicker.value) {
+    resetColor()
+  }
   debounceSetShowPicker(!showPicker.value)
 }
 
