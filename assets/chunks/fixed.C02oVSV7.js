88c801eac0ad8d20c12d0c63f8c9d51bec1e7bc8
ElementPlusBot <element-plus@outlook.com>
2024-11-15 10:42:33 +0000

    website deploy

---
+++ assets/chunks/fixed.C02oVSV7.js
@@ -0,0 +1,1 @@
+import{_ as s,q as t,o as a,t as c,v as e,y as f,W as _}from"./framework.SZaum9Aq.js";const r={};function p(i,x){const o=t("el-button"),n=t("el-affix");return a(),c(n,{position:"bottom",offset:20},{default:e(()=>[f(o,{type:"primary"},{default:e(()=>[_("Offset bottom 20px")]),_:1})]),_:1})}const m=s(r,[["render",p]]);export{m as E};
