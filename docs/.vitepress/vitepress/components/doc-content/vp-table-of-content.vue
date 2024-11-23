88d96835b92eacc343b53b99c929fbf7888659c6
JiaxiangZhang <zjx9987@gmail.com>
2024-11-23 23:26:35 +0800

    chore: optimize docs style

--- docs/.vitepress/vitepress/components/doc-content/vp-table-of-content.vue
+++ docs/.vitepress/vitepress/components/doc-content/vp-table-of-content.vue
@@ -20,30 +20,28 @@ const removeTag = (str: string) => str.replace(/<span.*<\/span>/g, '')
   <aside ref="container" class="toc-wrapper">
     <nav class="toc-content">
       <h3 class="toc-content__heading">Contents</h3>
-      <el-anchor :offset="70" :bound="120">
-        <el-anchor-link
-          v-for="{ link, text, children } in headers"
-          :key="link"
-          :href="link"
-          :title="text"
-        >
-          <div :title="removeTag(text)" v-html="text" />
-          <template v-if="children" #sub-link>
-            <el-anchor-link
-              v-for="{ link: childLink, text: childText } in children"
-              :key="childLink"
-              :href="childLink"
-              :title="text"
-            >
-              <div :title="removeTag(childText)" v-html="childText" />
-            </el-anchor-link>
-          </template>
-        </el-anchor-link>
-      </el-anchor>
-      <!-- <SponsorLarge
-        class="mt-8 toc-ads flex flex-col"
-        item-style="width: 180px; height: 55px;"
-      /> -->
+      <el-scrollbar max-height="calc(100vh - 110px)">
+        <el-anchor :offset="70" :bound="120" type="underline">
+          <el-anchor-link
+            v-for="{ link, text, children } in headers"
+            :key="link"
+            :href="link"
+            :title="text"
+          >
+            <div :title="removeTag(text)" v-html="text" />
+            <template v-if="children" #sub-link>
+              <el-anchor-link
+                v-for="{ link: childLink, text: childText } in children"
+                :key="childLink"
+                :href="childLink"
+                :title="text"
+              >
+                <div :title="removeTag(childText)" v-html="childText" />
+              </el-anchor-link>
+            </template>
+          </el-anchor-link>
+        </el-anchor>
+      </el-scrollbar>
       <p class="text-14px font-300 color-$text-color-secondary">
         {{ sponsor.sponsoredBy }}
       </p>
