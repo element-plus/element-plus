26e28a63709632a5d036f7589fbf89b6ace38cb3
btea <2356281422@qq.com>
2024-11-28 22:04:28 +0800

    Update packages/components/input-number/src/input-number.vue
    Co-authored-by: qiang <qw13131wang@gmail.com>

--- packages/components/input-number/src/input-number.vue
+++ packages/components/input-number/src/input-number.vue
@@ -293,13 +293,11 @@ const handleFocus = (event: MouseEvent | FocusEvent) => {
 
 const handleBlur = (event: MouseEvent | FocusEvent) => {
   data.userInput = null
-  if (isFirefox()) {
-    // This is a Firefox-specific problem. When non-numeric content is entered into a numeric input box,
-    // the content displayed on the page is not cleared after the value is cleared. #18533
-    // https://bugzilla.mozilla.org/show_bug.cgi?id=1398528
-    if (data.currentValue === null && input.value?.input) {
-      input.value.input.value = ''
-    }
+  // This is a Firefox-specific problem. When non-numeric content is entered into a numeric input box,
+  // the content displayed on the page is not cleared after the value is cleared. #18533
+  // https://bugzilla.mozilla.org/show_bug.cgi?id=1398528
+  if (isFirefox() && data.currentValue === null && input.value?.input) {
+    input.value.input.value = ''
   }
   emit('blur', event)
   if (props.validateEvent) {
