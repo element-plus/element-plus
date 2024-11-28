18cf9db1f3bac97b48f31d136cce94b06a42912c
btea <2356281422@qq.com>
2024-11-28 21:32:22 +0800

    chore: [input-number] add comment

--- packages/components/input-number/src/input-number.vue
+++ packages/components/input-number/src/input-number.vue
@@ -84,6 +84,7 @@ import { vRepeatClick } from '@element-plus/directives'
 import { useLocale, useNamespace } from '@element-plus/hooks'
 import {
   debugWarn,
+  isFirefox,
   isNumber,
   isString,
   isUndefined,
@@ -292,8 +293,13 @@ const handleFocus = (event: MouseEvent | FocusEvent) => {
 
 const handleBlur = (event: MouseEvent | FocusEvent) => {
   data.userInput = null
-  if (data.currentValue === null && input.value?.input) {
-    input.value.input.value = ''
+  if (isFirefox()) {
+    // This is a Firefox-specific problem. When non-numeric content is entered into a numeric input box,
+    // the content displayed on the page is not cleared after the value is cleared. #18533
+    // https://bugzilla.mozilla.org/show_bug.cgi?id=1398528
+    if (data.currentValue === null && input.value?.input) {
+      input.value.input.value = ''
+    }
   }
   emit('blur', event)
   if (props.validateEvent) {
