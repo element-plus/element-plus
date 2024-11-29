3303561d933dc69f6a0906c1d62fa59927a3f808
wuzhiqing <gtawu12138@gmail.com>
2024-11-29 03:29:16 +0000

    feat: remove required, use debugWarn to notify

--- packages/components/menu/src/menu-item.vue
+++ packages/components/menu/src/menu-item.vue
@@ -46,7 +46,7 @@ import {
   toRef,
 } from 'vue'
 import ElTooltip from '@element-plus/components/tooltip'
-import { throwError } from '@element-plus/utils'
+import { debugWarn, isPropAbsent, throwError } from '@element-plus/utils'
 import { useNamespace } from '@element-plus/hooks'
 import useMenu from './use-menu'
 import { menuItemEmits, menuItemProps } from './menu-item'
@@ -59,6 +59,12 @@ defineOptions({
 })
 const props = defineProps(menuItemProps)
 const emit = defineEmits(menuItemEmits)
+
+isPropAbsent(props.index) &&
+  debugWarn(
+    COMPONENT_NAME,
+    "The 'index' prop will be required in the next major version"
+  )
 
 const instance = getCurrentInstance()!
 const rootMenu = inject<MenuProvider>('rootMenu')
