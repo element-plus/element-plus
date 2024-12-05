738a22decf93f8ce96899a0b5177ae14ae3031c6
coderchang <coderchang0105@gmail.com>
2024-12-05 10:28:03 +0800

    fix: update

--- packages/components/cascader-panel/src/store.ts
+++ packages/components/cascader-panel/src/store.ts
@@ -50,19 +50,19 @@ export default class Store {
 
     if (!parentNode) this.nodes.push(node)
 
-    this.append2AllNodesAndLeafNodes(node)
+    this.appendAllNodesAndLeafNodes(node)
   }
 
   appendNodes(nodeDataList: CascaderOption[], parentNode: Node) {
     nodeDataList.forEach((nodeData) => this.appendNode(nodeData, parentNode))
   }
 
-  append2AllNodesAndLeafNodes(node: Node) {
+  appendAllNodesAndLeafNodes(node: Node) {
     this.allNodes.push(node)
     node.isLeaf && this.leafNodes.push(node)
     if (node.children) {
       node.children.forEach((subNode) => {
-        this.append2AllNodesAndLeafNodes(subNode)
+        this.appendAllNodesAndLeafNodes(subNode)
       })
     }
   }
