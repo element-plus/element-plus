d198ffeb406ff6a5062f910abadbd1837f67bd00
betavs <betavs@qq.com>
2024-12-07 00:15:42 +0800

    fix(components): [select] selected label is not updated when remote is true

--- packages/components/select/src/useSelect.ts
+++ packages/components/select/src/useSelect.ts
@@ -353,6 +353,8 @@ export const useSelect = (props: ISelectProps, emit) => {
     // If you want to control it by condition, write here
     if (states.isBeforeHide) return
     updateOptions()
+    if (props.multiple || !props.remote) return
+    nextTick(() => setSelected())
   })
 
   const handleQueryChange = (val: string) => {
