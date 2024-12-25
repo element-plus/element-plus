ef0e054162c3cb5bd3ff9c5fbf4e98993d544f44
JiaxiangZhang <zjx9987@gmail.com>
2024-12-26 00:54:35 +0800

    chore: [table] use script-setup syntax refactor

--- packages/components/table/src/table.vue
+++ packages/components/table/src/table.vue
@@ -93,7 +93,7 @@
               @set-drag-visible="setDragVisible"
             />
             <table-body
-              :context="context"
+              :context="table"
               :highlight="highlightCurrentRow"
               :row-class-name="rowClassName"
               :tooltip-effect="tooltipEffect"
@@ -168,17 +168,10 @@
   </div>
 </template>
 
-<script lang="ts">
-// @ts-nocheck
-import {
-  computed,
-  defineComponent,
-  getCurrentInstance,
-  onBeforeUnmount,
-  provide,
-} from 'vue'
+<script lang="ts" setup>
+import { computed, getCurrentInstance, onBeforeUnmount, provide } from 'vue'
 import { debounce } from 'lodash-unified'
-import { Mousewheel } from '@element-plus/directives'
+import { Mousewheel as vMousewheel } from '@element-plus/directives'
 import { useLocale, useNamespace } from '@element-plus/hooks'
 import ElScrollbar from '@element-plus/components/scrollbar'
 import { createStore } from './store/helper'
@@ -190,7 +183,7 @@ import useUtils from './table/utils-helper'
 import { convertToRows } from './table-header/utils-helper'
 import useStyle from './table/style-helper'
 import useKeyRender from './table/key-render-helper'
-import defaultProps from './table/defaults'
+import defaultProps, { tableEmits } from './table/defaults'
 import { TABLE_INJECTION_KEY } from './tokens'
 import { hColgroup } from './h-helper'
 import { useScrollbar } from './composables/use-scrollbar'
@@ -198,210 +191,156 @@ import { useScrollbar } from './composables/use-scrollbar'
 import type { Table } from './table/defaults'
 
 let tableIdSeed = 1
-export default defineComponent({
-  name: 'ElTable',
-  directives: {
-    Mousewheel,
-  },
-  components: {
-    TableHeader,
-    TableBody,
-    TableFooter,
-    ElScrollbar,
-    hColgroup,
-  },
-  props: defaultProps,
-  emits: [
-    'select',
-    'select-all',
-    'selection-change',
-    'cell-mouse-enter',
-    'cell-mouse-leave',
-    'cell-contextmenu',
-    'cell-click',
-    'cell-dblclick',
-    'row-click',
-    'row-contextmenu',
-    'row-dblclick',
-    'header-click',
-    'header-contextmenu',
-    'sort-change',
-    'filter-change',
-    'current-change',
-    'header-dragend',
-    'expand-change',
-    'scroll',
-  ],
-  setup(props) {
-    type Row = typeof props.data[number]
-    const { t } = useLocale()
-    const ns = useNamespace('table')
-    const table = getCurrentInstance() as Table<Row>
-    provide(TABLE_INJECTION_KEY, table)
-    const store = createStore<Row>(table, props)
-    table.store = store
-    const layout = new TableLayout<Row>({
-      store: table.store,
-      table,
-      fit: props.fit,
-      showHeader: props.showHeader,
-    })
-    table.layout = layout
+const COMPONENT_NAME = 'ElTable'
+defineOptions({
+  name: COMPONENT_NAME,
+  inheritAttrs: false,
+})
 
-    const isEmpty = computed(() => (store.states.data.value || []).length === 0)
+const props = defineProps(defaultProps)
+defineEmits(tableEmits)
 
-    /**
-     * open functions
-     */
-    const {
-      setCurrentRow,
-      getSelectionRows,
-      toggleRowSelection,
-      clearSelection,
-      clearFilter,
-      toggleAllSelection,
-      toggleRowExpansion,
-      clearSort,
-      sort,
-      updateKeyChildren,
-    } = useUtils<Row>(store)
-    const {
-      isHidden,
-      renderExpanded,
-      setDragVisible,
-      isGroup,
-      handleMouseLeave,
-      handleHeaderFooterMousewheel,
-      tableSize,
-      emptyBlockStyle,
-      handleFixedMousewheel,
-      resizeProxyVisible,
-      bodyWidth,
-      resizeState,
-      doLayout,
-      tableBodyStyles,
-      tableLayout,
-      scrollbarViewStyle,
-      scrollbarStyle,
-    } = useStyle<Row>(props, layout, store, table)
+type Row = typeof props.data[number]
+const { t } = useLocale()
+const ns = useNamespace('table')
+const table = getCurrentInstance() as Table<Row>
 
-    const { scrollBarRef, scrollTo, setScrollLeft, setScrollTop } =
-      useScrollbar()
+provide(TABLE_INJECTION_KEY, table)
 
-    const debouncedUpdateLayout = debounce(doLayout, 50)
+const store = createStore<Row>(table, props)
+table.store = store
+const layout = new TableLayout<Row>({
+  store: table.store,
+  table,
+  fit: props.fit,
+  showHeader: props.showHeader,
+})
+table.layout = layout
 
-    const tableId = `${ns.namespace.value}-table_${tableIdSeed++}`
-    table.tableId = tableId
-    table.state = {
-      isGroup,
-      resizeState,
-      doLayout,
-      debouncedUpdateLayout,
-    }
-    const computedSumText = computed(
-      () => props.sumText ?? t('el.table.sumText')
-    )
+const isEmpty = computed(() => (store.states.data.value || []).length === 0)
 
-    const computedEmptyText = computed(() => {
-      return props.emptyText ?? t('el.table.emptyText')
-    })
+/**
+ * open functions
+ */
+const {
+  setCurrentRow,
+  getSelectionRows,
+  toggleRowSelection,
+  clearSelection,
+  clearFilter,
+  toggleAllSelection,
+  toggleRowExpansion,
+  clearSort,
+  sort,
+  updateKeyChildren,
+} = useUtils<Row>(store)
 
-    const columns = computed(() => {
-      return convertToRows(store.states.originColumns.value)[0]
-    })
+const {
+  isHidden,
+  setDragVisible,
+  isGroup,
+  handleMouseLeave,
+  handleHeaderFooterMousewheel,
+  tableSize,
+  emptyBlockStyle,
+  resizeProxyVisible,
+  bodyWidth,
+  resizeState,
+  doLayout,
+  tableBodyStyles,
+  tableLayout,
+  scrollbarViewStyle,
+  scrollbarStyle,
+} = useStyle<Row>(props, layout, store, table)
 
-    useKeyRender(table)
+const { scrollBarRef, scrollTo, setScrollLeft, setScrollTop } = useScrollbar()
 
-    onBeforeUnmount(() => {
-      debouncedUpdateLayout.cancel()
-    })
+const debouncedUpdateLayout = debounce(doLayout, 50)
 
-    return {
-      ns,
-      layout,
-      store,
-      columns,
-      handleHeaderFooterMousewheel,
-      handleMouseLeave,
-      tableId,
-      tableSize,
-      isHidden,
-      isEmpty,
-      renderExpanded,
-      resizeProxyVisible,
-      resizeState,
-      isGroup,
-      bodyWidth,
-      tableBodyStyles,
-      emptyBlockStyle,
-      debouncedUpdateLayout,
-      handleFixedMousewheel,
-      /**
-       * @description used in single selection Table, set a certain row selected. If called without any parameter, it will clear selection
-       */
-      setCurrentRow,
-      /**
-       * @description returns the currently selected rows
-       */
-      getSelectionRows,
-      /**
-       * @description used in multiple selection Table, toggle if a certain row is selected. With the second parameter, you can directly set if this row is selected
-       */
-      toggleRowSelection,
-      /**
-       * @description used in multiple selection Table, clear user selection
-       */
-      clearSelection,
-      /**
-       * @description clear filters of the columns whose `columnKey` are passed in. If no params, clear all filters
-       */
-      clearFilter,
-      /**
-       * @description used in multiple selection Table, toggle select all and deselect all
-       */
-      toggleAllSelection,
-      /**
-       * @description used in expandable Table or tree Table, toggle if a certain row is expanded. With the second parameter, you can directly set if this row is expanded or collapsed
-       */
-      toggleRowExpansion,
-      /**
-       * @description clear sorting, restore data to the original order
-       */
-      clearSort,
-      /**
-       * @description refresh the layout of Table. When the visibility of Table changes, you may need to call this method to get a correct layout
-       */
-      doLayout,
-      /**
-       * @description sort Table manually. Property `prop` is used to set sort column, property `order` is used to set sort order
-       */
-      sort,
-      /**
-       * @description used in lazy Table, must set `rowKey`, update key children
-       */
-      updateKeyChildren,
-      t,
-      setDragVisible,
-      context: table,
-      computedSumText,
-      computedEmptyText,
-      tableLayout,
-      scrollbarViewStyle,
-      scrollbarStyle,
-      scrollBarRef,
-      /**
-       * @description scrolls to a particular set of coordinates
-       */
-      scrollTo,
-      /**
-       * @description set horizontal scroll position
-       */
-      setScrollLeft,
-      /**
-       * @description set vertical scroll position
-       */
-      setScrollTop,
-    }
-  },
+const tableId = `${ns.namespace.value}-table_${tableIdSeed++}`
+table.tableId = tableId
+table.state = {
+  isGroup,
+  resizeState,
+  doLayout,
+  debouncedUpdateLayout,
+}
+const computedSumText = computed(() => props.sumText ?? t('el.table.sumText'))
+
+const computedEmptyText = computed(
+  () => props.emptyText ?? t('el.table.emptyText')
+)
+
+const columns = computed(
+  () => convertToRows(store.states.originColumns.value)[0]
+)
+
+useKeyRender(table)
+
+onBeforeUnmount(() => {
+  debouncedUpdateLayout.cancel()
+})
+
+defineExpose({
+  /**
+   * @description used in single selection Table, set a certain row selected. If called without any parameter, it will clear selection
+   */
+  setCurrentRow,
+  /**
+   * @description returns the currently selected rows
+   */
+  getSelectionRows,
+  /**
+   * @description used in multiple selection Table, toggle if a certain row is selected. With the second parameter, you can directly set if this row is selected
+   */
+  toggleRowSelection,
+  /**
+   * @description used in multiple selection Table, clear user selection
+   */
+  clearSelection,
+  /**
+   * @description clear filters of the columns whose `columnKey` are passed in. If no params, clear all filters
+   */
+  clearFilter,
+  /**
+   * @description used in multiple selection Table, toggle select all and deselect all
+   */
+  toggleAllSelection,
+  /**
+   * @description used in expandable Table or tree Table, toggle if a certain row is expanded. With the second parameter, you can directly set if this row is expanded or collapsed
+   */
+  toggleRowExpansion,
+  /**
+   * @description clear sorting, restore data to the original order
+   */
+  clearSort,
+  /**
+   * @description refresh the layout of Table. When the visibility of Table changes, you may need to call this method to get a correct layout
+   */
+  doLayout,
+  /**
+   * @description sort Table manually. Property `prop` is used to set sort column, property `order` is used to set sort order
+   */
+  sort,
+  /**
+   * @description used in lazy Table, must set `rowKey`, update key children
+   */
+  updateKeyChildren,
+  /**
+   * @description scrolls to a particular set of coordinates
+   */
+  scrollTo,
+  /**
+   * @description set horizontal scroll position
+   */
+  setScrollLeft,
+  /**
+   * @description set vertical scroll position
+   */
+  setScrollTop,
+  /**
+   * @description get table columns context
+   */
+  columns,
 })
 </script>
