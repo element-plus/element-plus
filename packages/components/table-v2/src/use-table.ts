d26902a8db30f7880592a1b38201dc197198aa42
ZhangYang <yang.zhang@linkkids.cn>
2024-12-30 16:23:29 +0800

    fix(components): [table-v2] format code for better readability in use-table.ts

--- packages/components/table-v2/src/use-table.ts
+++ packages/components/table-v2/src/use-table.ts
@@ -152,7 +152,10 @@ function useTable(props: TableV2Props) {
 
     if (
       unref(lastRenderedRowIndex) >= 0 &&
-      Math.abs(_totalHeight - (scrollTop + unref(mainTableHeight) - unref(headerHeight))) < 0.001
+      Math.abs(
+        _totalHeight -
+        (scrollTop + unref(mainTableHeight) - unref(headerHeight))
+      ) < 0.001
     ) {
       onEndReached(heightUntilEnd)
     }
