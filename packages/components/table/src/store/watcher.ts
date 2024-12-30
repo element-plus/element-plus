2114f6694a1886428e9fbdf80619cac77ab25828
qiang <qw13131wang@gmail.com>
2024-12-30 11:44:56 +0800

    perf(components): [table] use rowKey to determine whether it is selected

--- packages/components/table/src/store/watcher.ts
+++ packages/components/table/src/store/watcher.ts
@@ -1,5 +1,5 @@
 // @ts-nocheck
-import { getCurrentInstance, ref, toRefs, unref, watch } from 'vue'
+import { computed, getCurrentInstance, ref, toRefs, unref, watch } from 'vue'
 import { isEqual } from 'lodash-unified'
 import { hasOwn, isArray, isString, isUndefined } from '@element-plus/utils'
 import {
@@ -76,6 +76,10 @@ function useWatcher<T>() {
   const sortProp = ref(null)
   const sortOrder = ref(null)
   const hoverRow = ref(null)
+
+  const selectedMap = computed(() => {
+    return rowKey.value ? getKeysMap(selection.value, rowKey.value) : undefined
+  })
 
   watch(
     data,
@@ -184,8 +188,12 @@ function useWatcher<T>() {
   }
 
   // 选择
-  const isSelected = (row) => {
-    return selection.value.some((item) => isEqual(item, row))
+  const isSelected = (row: DefaultRow) => {
+    if (selectedMap.value) {
+      return !!selectedMap.value[getRowIdentity(row, rowKey.value)]
+    } else {
+      return selection.value.some((item) => isEqual(item, row))
+    }
   }
 
   const clearSelection = () => {
@@ -201,15 +209,16 @@ function useWatcher<T>() {
     let deleted
     if (rowKey.value) {
       deleted = []
-      const selectedMap = getKeysMap(selection.value, rowKey.value)
       const dataMap = getKeysMap(data.value, rowKey.value)
-      for (const key in selectedMap) {
-        if (hasOwn(selectedMap, key) && !dataMap[key]) {
-          deleted.push(selectedMap[key].row)
+      for (const key in selectedMap.value) {
+        if (hasOwn(selectedMap.value, key) && !dataMap[key]) {
+          deleted.push(selectedMap.value[key].row)
         }
       }
     } else {
-      deleted = selection.value.filter((item) => !data.value.includes(item))
+      deleted = selection.value.filter((item) => {
+        return !data.value.some((row) => isEqual(row, item))
+      })
     }
     if (deleted.length) {
       const newSelection = selection.value.filter(
@@ -295,10 +304,9 @@ function useWatcher<T>() {
   }
 
   const updateSelectionByRowKey = () => {
-    const selectedMap = getKeysMap(selection.value, rowKey.value)
     data.value.forEach((row) => {
       const rowId = getRowIdentity(row, rowKey.value)
-      const rowInfo = selectedMap[rowId]
+      const rowInfo = selectedMap.value![rowId]
       if (rowInfo) {
         selection.value[rowInfo.index] = row
       }
@@ -313,20 +321,9 @@ function useWatcher<T>() {
     }
 
     const { childrenColumnName } = instance.store.states
-    const selectedMap = rowKey.value
-      ? getKeysMap(selection.value, rowKey.value)
-      : undefined
-
     let rowIndex = 0
     let selectedCount = 0
 
-    const isSelected = (row: DefaultRow) => {
-      if (selectedMap) {
-        return !!selectedMap[getRowIdentity(row, rowKey.value)]
-      } else {
-        return selection.value.includes(row)
-      }
-    }
     const checkSelectedStatus = (data: DefaultRow[]) => {
       for (const row of data) {
         const isRowSelectable =
