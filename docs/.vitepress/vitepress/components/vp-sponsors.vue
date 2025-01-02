1f42bfe93e58f7d25f6d04ee143b199d539fdb38
0song <0song@gmail.com>
2025-01-02 19:39:18 +0800

    chore: update doc

--- docs/.vitepress/vitepress/components/vp-sponsors.vue
+++ docs/.vitepress/vitepress/components/vp-sponsors.vue
@@ -10,7 +10,7 @@ const sponsor = computed(() => sponsorLocale[lang.value])
 </script>
 
 <template>
-  <div class="page-sidebar-table">
+  <div class="page-sidebar-panel">
     <p class="title">{{ sponsor.sponsoredBy }}</p>
     <VPSponsorLarge />
     <VPSponsorSmall />
@@ -18,7 +18,7 @@ const sponsor = computed(() => sponsorLocale[lang.value])
 </template>
 
 <style lang="scss" scoped>
-.page-sidebar-table {
+.page-sidebar-panel {
   padding-bottom: 10px;
   padding-top: 0;
   .title {
