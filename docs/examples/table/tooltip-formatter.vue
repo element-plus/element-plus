bd5744c5ef6a3dc80641028a665fcc66341a9914
LiuZhi <zhixiaotong.me@qq.com>
2025-01-08 09:52:50 +0800

    docs: simpify tooltip demo

--- docs/examples/table/tooltip-formatter.vue
+++ docs/examples/table/tooltip-formatter.vue
@@ -1,53 +1,35 @@
 <template>
-  <el-row :gutter="20">
-    <el-col :lg="8" :sm="8">
-      <p>simple formatter</p>
-      <el-table :data="tableData" style="width: 100%">
-        <el-table-column
-          property="address"
-          label="simple formatter"
-          width="240"
-          show-overflow-tooltip
-          :tooltip-formatter="(row) => `${row.address}: formatter`"
-        />
-      </el-table>
-    </el-col>
-    <el-col :lg="8" :sm="8">
-      <p>formatter object</p>
-      <el-table :data="tableData2" style="width: 100%">
-        <el-table-column
-          property="tags"
-          label="format tags"
-          width="240"
-          show-overflow-tooltip
-          :tooltip-formatter="(row) => row.tags.join(', ')"
+  <el-table :data="tableData" show-overflow-tooltip style="width: 100%">
+    <el-table-column
+      property="address"
+      label="simple formatter"
+      width="240"
+      :tooltip-formatter="(row) => `${row.address}: formatter`"
+    />
+    <el-table-column
+      property="tags"
+      label="formatter object"
+      width="240"
+      :tooltip-formatter="(row) => row.tags.join(', ')"
+    >
+      <template #default="{ row }">
+        <el-tag
+          v-for="tag in row.tags"
+          :key="tag"
+          class="tag-item"
+          type="primary"
         >
-          <template #default="{ row }">
-            <el-tag
-              v-for="tag in row.tags"
-              :key="tag"
-              class="tag-item"
-              type="primary"
-            >
-              {{ tag }}
-            </el-tag>
-          </template>
-        </el-table-column>
-      </el-table>
-    </el-col>
-    <el-col :lg="8" :sm="8">
-      <p>with vnode</p>
-      <el-table :data="tableData3" style="width: 100%">
-        <el-table-column
-          property="url"
-          label="element-plus github"
-          width="240"
-          show-overflow-tooltip
-          :tooltip-formatter="withVNode"
-        />
-      </el-table>
-    </el-col>
-  </el-row>
+          {{ tag }}
+        </el-tag>
+      </template>
+    </el-table-column>
+    <el-table-column
+      property="url"
+      label="with vnode"
+      width="240"
+      :tooltip-formatter="withVNode"
+    />
+  </el-table>
 </template>
 
 <script lang="ts" setup>
@@ -55,38 +37,24 @@ import { h } from 'vue'
 import { ElLink, ElTable } from 'element-plus'
 
 const tableData = [
-  { address: 'Lohrbergstr. 86c, Süd Lilli, Saarland' },
-  { address: '760 A Street, South Frankfield, Illinois' },
-  { address: 'Arnold-Ohletz-Str. 41a, Alt Malinascheid, Thüringen' },
-  { address: '23618 Windsor Drive, West Ricardoview, Idaho' },
-]
-
-const tableData2 = [
   {
+    address: 'Lohrbergstr. 86c, Süd Lilli, Saarland',
     tags: ['Office', 'Home', 'Park', 'Garden'],
-  },
-  {
-    tags: ['error', 'warning', 'success', 'info'],
-  },
-  {
-    tags: ['one', 'two', 'three', 'four', 'five'],
-  },
-  {
-    tags: ['blue', 'white', 'dark', 'gray', 'red', 'bright'],
-  },
-]
-
-const tableData3 = [
-  {
     url: 'https://github.com/element-plus/element-plus/issues',
   },
   {
+    address: '760 A Street, South Frankfield, Illinois',
+    tags: ['error', 'warning', 'success', 'info'],
     url: 'https://github.com/element-plus/element-plus/pulls',
   },
   {
+    address: 'Arnold-Ohletz-Str. 41a, Alt Malinascheid, Thüringen',
+    tags: ['one', 'two', 'three', 'four', 'five'],
     url: 'https://github.com/element-plus/element-plus/discussions',
   },
   {
+    address: '23618 Windsor Drive, West Ricardoview, Idaho',
+    tags: ['blue', 'white', 'dark', 'gray', 'red', 'bright'],
     url: 'https://github.com/element-plus/element-plus/actions',
   },
 ]
