e3837f59c407aaf4bbfcdcf30864ffbb4b5ce404
JiaxiangZhang <zjx9987@gmail.com>
2025-01-20 23:01:27 +0800

    test: [image-viewer] update tests to handle image loading failures

--- packages/components/image-viewer/__tests__/image-viewer.test.tsx
+++ packages/components/image-viewer/__tests__/image-viewer.test.tsx
@@ -1,7 +1,7 @@
 import { nextTick } from 'vue'
 import { mount } from '@vue/test-utils'
 import { describe, expect, test } from 'vitest'
-import { IMAGE_SUCCESS } from '@element-plus/test-utils/mock'
+import { IMAGE_FAIL, IMAGE_SUCCESS } from '@element-plus/test-utils/mock'
 import ImageViewer from '../src/image-viewer.vue'
 
 async function doubleWait() {
@@ -40,22 +40,20 @@ describe('<image-viewer />', () => {
   })
 
   test('manually switch image', async () => {
-    const wrapper = mount(
-      <ImageViewer urlList={[IMAGE_SUCCESS, IMAGE_SUCCESS]} />
-    )
+    const wrapper = mount(<ImageViewer urlList={[IMAGE_SUCCESS, IMAGE_FAIL]} />)
 
     await doubleWait()
     const viewer = wrapper.find('.el-image-viewer__wrapper')
     expect(viewer.exists()).toBe(true)
 
     const imgList = wrapper.findAll('.el-image-viewer__img')
-    expect(imgList[0].attributes('style')).not.contains('display: none;')
-    expect(imgList[1].attributes('style')).contains('display: none;')
+    expect(imgList[0].attributes('src')).toBe(IMAGE_SUCCESS)
 
     wrapper.vm.setActiveItem(1)
     await doubleWait()
-    expect(imgList[0].attributes('style')).contains('display: none;')
-    expect(imgList[1].attributes('style')).not.contains('display: none;')
+    expect(viewer.findAll('.el-image-viewer__img')[0].attributes('src')).toBe(
+      IMAGE_FAIL
+    )
     wrapper.unmount()
   })
 
