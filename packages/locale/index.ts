724044745a5d6d021d5ece782003e19fd62dfda5
btea <2356281422@qq.com>
2025-01-30 20:38:50 +0800

    feat(locale): export zhHk/zhMo

--- packages/locale/index.ts
+++ packages/locale/index.ts
@@ -54,6 +54,8 @@ export { default as uzUz } from './lang/uz-uz'
 export { default as vi } from './lang/vi'
 export { default as zhCn } from './lang/zh-cn'
 export { default as zhTw } from './lang/zh-tw'
+export { default as zhHk } from './lang/zh-hk'
+export { default as zhMo } from './lang/zh-mo'
 
 export type TranslatePair = {
   [key: string]: string | string[] | TranslatePair
