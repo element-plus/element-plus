88e18fe279d031ccd71e4a7da5fe9a8b0e677524
Spaceman <chenjunwei517@qq.com>
2025-02-21 02:11:24 +0800

    docs: add a demo using the append-to API
    为Tooltip 组件添加 append-to API使用的示例

---
+++ docs/examples/tooltip/append-to.vue
@@ -0,0 +1,23 @@
+<template>
+  <el-tooltip
+    :append-to="targetElement"
+    trigger="click"
+    content="Append to .target"
+    placement="top"
+  >
+    <el-button class="target">Click to open tooltip</el-button>
+  </el-tooltip>
+</template>
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+
+const targetElement = ref<string>('')
+onMounted(() => {
+  targetElement.value = '.target'
+})
+</script>
+<style scoped>
+.target {
+  position: relative;
+}
+</style>
