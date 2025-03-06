b46fc201e4dc205f6ec4599c4294d0e610dcb0e3
TKSun <1037305420@qq.com>
2025-03-06 09:36:49 +0800

    fix(components): [input] `@change` event value formatter (#20000)
    * fix: change事件值没有被格式化
    
    fix: change事件值没有被格式化
    
    * Update packages/components/input/src/input.vue
    
    ---------
    
    Co-authored-by: btea <2356281422@qq.com>

--- packages/components/input/src/input.vue
+++ packages/components/input/src/input.vue
@@ -417,7 +417,12 @@ const handleInput = async (event: Event) => {
 }
 
 const handleChange = (event: Event) => {
-  emit('change', (event.target as TargetElement).value)
+  let { value } = event.target as TargetElement
+
+  if (props.formatter && props.parser) {
+    value = props.parser(value)
+  }
+  emit('change', value)
 }
 
 const {
