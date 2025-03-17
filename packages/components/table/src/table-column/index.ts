71646797e12ad41aa680a8ed3437b6822dcc3f6a
yang <2636098325@qq.com>
2025-03-17 22:10:45 +0800

    fix(components): [table] type sets showOverflowTooltip centering style problem under `selection`

--- packages/components/table/src/table-column/index.ts
+++ packages/components/table/src/table-column/index.ts
@@ -67,9 +67,13 @@ export default defineComponent({
 
       const type = props.type || 'default'
       const sortable = props.sortable === '' ? true : props.sortable
-      const showOverflowTooltip = isUndefined(props.showOverflowTooltip)
-        ? parent.props.showOverflowTooltip
-        : props.showOverflowTooltip
+      // 限制selection类型设置showOverflowTooltip，因为其自身lable标签无法使用父级的text-align属性，所以直接限制
+      const showOverflowTooltip =
+        type === 'selection'
+          ? false
+          : isUndefined(props.showOverflowTooltip)
+          ? parent.props.showOverflowTooltip
+          : props.showOverflowTooltip
       const tooltipFormatter = isUndefined(props.tooltipFormatter)
         ? parent.props.tooltipFormatter
         : props.tooltipFormatter
