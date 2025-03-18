46a7586c2d00a6de7668aaf66b7c8825fecc920b
Kylin <1159469891@qq.com>
2025-03-18 15:25:33 +0800

    refactor: update demo

--- docs/examples/input-number/hover.vue
+++ docs/examples/input-number/hover.vue
@@ -1,41 +1,65 @@
 <template>
-  <el-input-number
-    v-model="num"
-    :min="1"
-    :max="10"
-    :controls="false"
-    hover-controls
-    size="large"
-    controls-position="right"
-    @change="handleChange"
-  />
-  <el-input-number
-    v-model="num"
-    :min="1"
-    :max="10"
-    class="mx-4"
-    hover-controls
-    size="large"
-    @change="handleChange"
-  />
-  <el-input-number
-    v-model="num"
-    :min="1"
-    :max="10"
-    hover-controls
-    controls-position="right"
-    @change="handleChange"
-  />
-  <el-input-number
-    v-model="num"
-    :min="1"
-    :max="10"
-    class="mx-4"
-    hover-controls
-    size="small"
-    controls-position="right"
-    @change="handleChange"
-  />
+  <div>
+    <el-input-number
+      v-model="num"
+      :min="1"
+      :max="10"
+      hover-controls
+      @change="handleChange"
+    />
+    <el-input-number
+      v-model="num"
+      class="mx-4"
+      :min="1"
+      :max="10"
+      hover-controls
+      controls-position="right"
+      @change="handleChange"
+    />
+  </div>
+  <div class="mt-4">
+    <el-input-number v-model="num" hover-controls :min="1" :max="10">
+      <template #prefix>
+        <span>￥</span>
+      </template>
+    </el-input-number>
+    <el-input-number
+      v-model="num"
+      class="mx-4"
+      hover-controls
+      :min="1"
+      :max="10"
+    >
+      <template #suffix>
+        <span>RMB</span>
+      </template>
+    </el-input-number>
+  </div>
+  <div class="mt-4">
+    <el-input-number
+      v-model="num"
+      hover-controls
+      :min="1"
+      :max="10"
+      controls-position="right"
+    >
+      <template #prefix>
+        <span>￥</span>
+      </template>
+    </el-input-number>
+    <el-input-number
+      v-model="num"
+      class="mx-4"
+      hover-controls
+      :min="1"
+      :max="10"
+      controls-position="right"
+    >
+      <template #suffix>
+        <span>RMB</span>
+      </template>
+    </el-input-number>
+  </div>
 </template>
 
 <script lang="ts" setup>
