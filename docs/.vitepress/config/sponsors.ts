5a03fcefc759c6ce59ae2065cc9844bfc5b544fb
0song <0song@gmail.com>
2025-03-20 15:21:06 +0800

    chore: update UI

--- docs/.vitepress/config/sponsors.ts
+++ docs/.vitepress/config/sponsors.ts
@@ -57,6 +57,14 @@ export const leftCustomImgSponsors = [
     className: 'jnpf',
     banner_img: '/images/jnpfsoft.png',
   },
+  {
+    name: 'MISBoot',
+    img: '/images/misboot.png',
+    banner_img: '/images/misboot-left.png',
+    url: 'https://vue.misboot.com/#/login?from=element-plus',
+    slogan: 'Low-code development platform',
+    slogan_cn: '基于Element Plus构建的低代码开发平台',
+  },
 ]
 
 export const platinumSponsors = [
