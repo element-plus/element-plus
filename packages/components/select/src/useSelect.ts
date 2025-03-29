38cbfbb0b040544fe58d911f3564516bb2fbe2d9
dopamine <coderzyou@gmail.com>
2025-03-29 17:58:52 +0800

    chore: [select] remove @ts-nocheck directive (#19098)
    * chore: [select] remove @ts-nocheck directive
    
    * Update packages/components/select/src/option.vue
    
    Co-authored-by: qiang <qw13131wang@gmail.com>
    
    * chore: delete useless type
    
    * Update packages/components/select/src/useSelect.ts
    
    Co-authored-by: qiang <qw13131wang@gmail.com>
    
    * fix: type error
    
    * fix: type error
    
    * chore: use buildProps
    
    * chore: add type
    
    * Update packages/components/select/src/useSelect.ts
    
    Co-authored-by: btea <2356281422@qq.com>
    
    * chore: fix type
    
    * chore: fix type
    
    * chore: fix types
    
    ---------
    
    Co-authored-by: warmthsea <2586244885@qq.com>
    Co-authored-by: sea <45450994+warmthsea@users.noreply.github.com>
    Co-authored-by: qiang <qw13131wang@gmail.com>
    Co-authored-by: btea <2356281422@qq.com>

--- packages/components/select/src/useSelect.ts
+++ packages/components/select/src/useSelect.ts
@@ -1,5 +1,5 @@
-// @ts-nocheck
 import {
+  Component,
   computed,
   nextTick,
   onMounted,
@@ -49,20 +49,22 @@ import {
 } from '@element-plus/components/form'
 
 import type { TooltipInstance } from '@element-plus/components/tooltip'
-import type { ISelectProps, SelectOptionProxy } from './token'
+import type { ScrollbarInstance } from '@element-plus/components/scrollbar'
+import type { ISelectProps, SelectEmits } from './select'
+import type { OptionPublicInstance, OptionValue, SelectStates } from './type'
 
-export const useSelect = (props: ISelectProps, emit) => {
+export const useSelect = (props: ISelectProps, emit: SelectEmits) => {
   const { t } = useLocale()
   const contentId = useId()
   const nsSelect = useNamespace('select')
   const nsInput = useNamespace('input')
 
-  const states = reactive({
+  const states = reactive<SelectStates>({
     inputValue: '',
     options: new Map(),
     cachedOptions: new Map(),
-    optionValues: [] as any[], // sorted value of options
-    selected: [] as any[],
+    optionValues: [], // sorted value of options
+    selected: [],
     selectionWidth: 0,
     collapseItemWidth: 0,
     selectedLabel: '',
@@ -74,19 +76,17 @@ export const useSelect = (props: ISelectProps, emit) => {
   })
 
   // template refs
-  const selectRef = ref<HTMLElement>(null)
-  const selectionRef = ref<HTMLElement>(null)
-  const tooltipRef = ref<TooltipInstance | null>(null)
-  const tagTooltipRef = ref<TooltipInstance | null>(null)
-  const inputRef = ref<HTMLInputElement | null>(null)
-  const prefixRef = ref<HTMLElement>(null)
-  const suffixRef = ref<HTMLElement>(null)
-  const menuRef = ref<HTMLElement>(null)
-  const tagMenuRef = ref<HTMLElement>(null)
-  const collapseItemRef = ref<HTMLElement>(null)
-  const scrollbarRef = ref<{
-    handleScroll: () => void
-  } | null>(null)
+  const selectRef = ref<HTMLElement>()
+  const selectionRef = ref<HTMLElement>()
+  const tooltipRef = ref<TooltipInstance>()
+  const tagTooltipRef = ref<TooltipInstance>()
+  const inputRef = ref<HTMLInputElement>()
+  const prefixRef = ref<HTMLElement>()
+  const suffixRef = ref<HTMLElement>()
+  const menuRef = ref<HTMLElement>()
+  const tagMenuRef = ref<HTMLElement>()
+  const collapseItemRef = ref<HTMLElement>()
+  const scrollbarRef = ref<ScrollbarInstance>()
 
   const {
     isComposing,
@@ -153,12 +153,14 @@ export const useSelect = (props: ISelectProps, emit) => {
       : props.suffixIcon
   )
   const iconReverse = computed(() =>
-    nsSelect.is('reverse', iconComponent.value && expanded.value)
+    nsSelect.is('reverse', !!(iconComponent.value && expanded.value))
   )
 
   const validateState = computed(() => formItem?.validateState || '')
   const validateIcon = computed(
-    () => ValidateComponentsMap[validateState.value]
+    () =>
+      validateState.value &&
+      (ValidateComponentsMap[validateState.value] as Component)
   )
 
   const debounce = computed(() => (props.remote ? 300 : 0))
@@ -192,7 +194,7 @@ export const useSelect = (props: ISelectProps, emit) => {
 
   const optionsArray = computed(() => {
     const list = Array.from(states.options.values())
-    const newList = []
+    const newList: OptionPublicInstance[] = []
     states.optionValues.forEach((item) => {
       const index = list.findIndex((i) => i.value === item)
       if (index > -1) {
@@ -402,7 +404,7 @@ export const useSelect = (props: ISelectProps, emit) => {
     } else {
       states.selectedLabel = ''
     }
-    const result: any[] = []
+    const result: SelectStates['selected'] = []
     if (!isUndefined(props.modelValue)) {
       ensureArray(props.modelValue).forEach((value) => {
         result.push(getOption(value))
@@ -411,7 +413,7 @@ export const useSelect = (props: ISelectProps, emit) => {
     states.selected = result
   }
 
-  const getOption = (value) => {
+  const getOption = (value: OptionValue) => {
     let option
     const isObjectValue = isPlainObject(value)
 
@@ -449,12 +451,12 @@ export const useSelect = (props: ISelectProps, emit) => {
   }
 
   const resetSelectionWidth = () => {
-    states.selectionWidth = selectionRef.value.getBoundingClientRect().width
+    states.selectionWidth = selectionRef.value!.getBoundingClientRect().width
   }
 
   const resetCollapseItemWidth = () => {
     states.collapseItemWidth =
-      collapseItemRef.value.getBoundingClientRect().width
+      collapseItemRef.value!.getBoundingClientRect().width
   }
 
   const updateTooltip = () => {
@@ -472,8 +474,8 @@ export const useSelect = (props: ISelectProps, emit) => {
     handleQueryChange(states.inputValue)
   }
 
-  const onInput = (event) => {
-    states.inputValue = event.target.value
+  const onInput = (event: Event) => {
+    states.inputValue = (event.target as HTMLInputElement).value
     if (props.remote) {
       debouncedOnInputChange()
     } else {
@@ -485,22 +487,22 @@ export const useSelect = (props: ISelectProps, emit) => {
     onInputChange()
   }, debounce.value)
 
-  const emitChange = (val) => {
+  const emitChange = (val: OptionValue | OptionValue[]) => {
     if (!isEqual(props.modelValue, val)) {
       emit(CHANGE_EVENT, val)
     }
   }
 
-  const getLastNotDisabledIndex = (value) =>
+  const getLastNotDisabledIndex = (value: OptionValue[]) =>
     findLastIndex(value, (it) => {
       const option = states.cachedOptions.get(it)
       return option && !option.disabled && !option.states.groupDisabled
     })
 
-  const deletePrevTag = (e) => {
+  const deletePrevTag = (e: KeyboardEvent) => {
     if (!props.multiple) return
     if (e.code === EVENT_CODE.delete) return
-    if (e.target.value.length <= 0) {
+    if ((e.target as HTMLInputElement).value.length <= 0) {
       const value = ensureArray(props.modelValue).slice()
       const lastNotDisabledIndex = getLastNotDisabledIndex(value)
       if (lastNotDisabledIndex < 0) return
@@ -512,7 +514,10 @@ export const useSelect = (props: ISelectProps, emit) => {
     }
   }
 
-  const deleteTag = (event, tag) => {
+  const deleteTag = (
+    event: MouseEvent,
+    tag: OptionPublicInstance | SelectStates['selected'][0]
+  ) => {
     const index = states.selected.indexOf(tag)
     if (index > -1 && !selectDisabled.value) {
       const value = ensureArray(props.modelValue).slice()
@@ -525,9 +530,9 @@ export const useSelect = (props: ISelectProps, emit) => {
     focus()
   }
 
-  const deleteSelected = (event) => {
+  const deleteSelected = (event: Event) => {
     event.stopPropagation()
-    const value: string | any[] = props.multiple ? [] : valueOnClear.value
+    const value = props.multiple ? [] : valueOnClear.value
     if (props.multiple) {
       for (const item of states.selected) {
         if (item.isDisabled) value.push(item.value)
@@ -541,7 +546,7 @@ export const useSelect = (props: ISelectProps, emit) => {
     focus()
   }
 
-  const handleOptionSelect = (option) => {
+  const handleOptionSelect = (option: OptionPublicInstance) => {
     if (props.multiple) {
       const value = ensureArray(props.modelValue ?? []).slice()
       const optionIndex = getValueIndex(value, option)
@@ -573,7 +578,7 @@ export const useSelect = (props: ISelectProps, emit) => {
     })
   }
 
-  const getValueIndex = (arr: any[] = [], option) => {
+  const getValueIndex = (arr: OptionValue[], option: OptionPublicInstance) => {
     if (isUndefined(option)) return -1
     if (!isObject(option.value)) return arr.indexOf(option.value)
 
@@ -582,7 +587,12 @@ export const useSelect = (props: ISelectProps, emit) => {
     })
   }
 
-  const scrollToOption = (option) => {
+  const scrollToOption = (
+    option:
+      | OptionPublicInstance
+      | OptionPublicInstance[]
+      | SelectStates['selected']
+  ) => {
     const targetOption = isArray(option) ? option[0] : option
     let target = null
 
@@ -606,12 +616,12 @@ export const useSelect = (props: ISelectProps, emit) => {
     scrollbarRef.value?.handleScroll()
   }
 
-  const onOptionCreate = (vm: SelectOptionProxy) => {
+  const onOptionCreate = (vm: OptionPublicInstance) => {
     states.options.set(vm.value, vm)
     states.cachedOptions.set(vm.value, vm)
   }
 
-  const onOptionDestroy = (key, vm: SelectOptionProxy) => {
+  const onOptionDestroy = (key: OptionValue, vm: OptionPublicInstance) => {
     if (states.options.get(key) === vm) {
       states.options.delete(key)
     }
@@ -689,7 +699,9 @@ export const useSelect = (props: ISelectProps, emit) => {
     }
   }
 
-  const getValueKey = (item) => {
+  const getValueKey = (
+    item: OptionPublicInstance | SelectStates['selected'][0]
+  ) => {
     return isObject(item.value) ? get(item.value, props.valueKey) : item.value
   }
 
@@ -717,7 +729,7 @@ export const useSelect = (props: ISelectProps, emit) => {
       : []
   })
 
-  const navigateOptions = (direction) => {
+  const navigateOptions = (direction: 'prev' | 'next') => {
     if (!expanded.value) {
       expanded.value = true
       return
