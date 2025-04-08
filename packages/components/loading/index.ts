8f8f6744201b1a54d5a9f444417947b81d197fac
Dsaquel <noblet.ouwaysgta5@gmail.com>
2025-04-08 23:54:26 +0200

    fix(components): [loading] inherit app context in ssr mode

--- packages/components/loading/index.ts
+++ packages/components/loading/index.ts
@@ -1,13 +1,15 @@
 import { Loading } from './src/service'
 import { vLoading } from './src/directive'
 
-import type { App } from 'vue'
+import type { App, AppContext } from 'vue'
 
 // installer and everything in all
 export const ElLoading = {
+  _context: null as AppContext | null,
   install(app: App) {
     app.directive('loading', vLoading)
     app.config.globalProperties.$loading = Loading
+    ElLoading._context = app._context
   },
   directive: vLoading,
   service: Loading,
