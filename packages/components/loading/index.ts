3258a4ce85bade2b7cf54c1c3b2d7368cbca1d97
Dsaquel <noblet.ouwaysgta5@gmail.com>
2025-04-09 21:00:44 +0200

    chore: get appContext inherit in childApp

--- packages/components/loading/index.ts
+++ packages/components/loading/index.ts
@@ -1,15 +1,20 @@
-import { Loading } from './src/service'
-import { vLoading } from './src/directive'
+import Loading from './src/service'
+import vLoading from './src/directive'
 
-import type { App, AppContext } from 'vue'
+import type { App, AppContext, Directive } from 'vue'
+import type { ElementLoading, LoadingBinding } from './src/directive'
 
 // installer and everything in all
 export const ElLoading = {
-  _context: null as AppContext | null,
   install(app: App) {
+    Loading._context = app._context
+    ;(
+      vLoading as Directive<ElementLoading, LoadingBinding> & {
+        _context: AppContext | null
+      }
+    )._context = app._context
     app.directive('loading', vLoading)
     app.config.globalProperties.$loading = Loading
-    ElLoading._context = app._context
   },
   directive: vLoading,
   service: Loading,
