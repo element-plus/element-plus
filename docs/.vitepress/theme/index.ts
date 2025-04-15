0bf286fa4b065926f6b3eb22582d6685071691d5
Dsaquel <noblet.ouwaysgta5@gmail.com>
2025-04-15 23:05:36 +0200

    docs: attach nprogress on router event

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,6 +2,7 @@ import ElementPlus, {
   ID_INJECTION_KEY,
   ZINDEX_INJECTION_KEY,
 } from 'element-plus'
+import nprogress from 'nprogress'
 
 import VPApp, { NotFound, globals } from '../vitepress'
 import { define } from '../utils/types'
@@ -14,7 +15,7 @@ import type { Theme } from 'vitepress'
 export default define<Theme>({
   NotFound,
   Layout: VPApp,
-  enhanceApp: ({ app }) => {
+  enhanceApp: ({ app, router }) => {
     app.use(ElementPlus)
     app.provide(ID_INJECTION_KEY, { prefix: 1024, current: 0 })
     app.provide(ZINDEX_INJECTION_KEY, { current: 0 })
@@ -22,5 +23,7 @@ export default define<Theme>({
     globals.forEach(([name, Comp]) => {
       app.component(name, Comp)
     })
+    router.onBeforeRouteChange = nprogress.start
+    router.onAfterRouteChanged = nprogress.done
   },
 })
--- docs/.vitepress/vitepress/components/vp-app.vue
+++ docs/.vitepress/vitepress/components/vp-app.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { onMounted } from 'vue'
 // import { ElMessageBox } from 'element-plus'
-import nprogress from 'nprogress'
 // import dayjs from 'dayjs'
 import { isClient, useEventListener, useToggle } from '@vueuse/core'
 import { EVENT_CODE } from 'element-plus'
@@ -47,72 +46,44 @@ useEventListener('keydown', (e) => {
 
 onMounted(async () => {
   if (!isClient) return
-  window.addEventListener(
-    'click',
-    (e) => {
-      const link = (e.target as HTMLElement).closest('a')
-      if (!link) return
 
-      const { protocol, hostname, pathname, target } = link
-      const currentUrl = window.location
-      const extMatch = pathname.match(/\.\w+$/)
-      // only intercept inbound links
-      if (
-        !e.ctrlKey &&
-        !e.shiftKey &&
-        !e.altKey &&
-        !e.metaKey &&
-        target !== `_blank` &&
-        protocol === currentUrl.protocol &&
-        hostname === currentUrl.hostname &&
-        !(extMatch && extMatch[0] !== '.html')
-      ) {
-        e.preventDefault()
-        if (pathname !== currentUrl.pathname) {
-          nprogress.start()
-        }
-      }
-    },
-    { capture: true }
-  )
-
-  // if (lang.value === 'zh-CN') {
-  //   if (isMirrorUrl()) return
-
-  //   if (userPrefer.value) {
-  //     // no alert in the next 90 days
-  //     if (
-  //       dayjs
-  //         .unix(Number(userPrefer.value))
-  //         .add(90, 'day')
-  //         .diff(dayjs(), 'day', true) > 0
-  //     )
-  //       return
-  //   }
-  //   try {
-  //     await ElMessageBox.confirm(
-  //       '建议大陆用户访问部署在国内的站点，是否跳转？',
-  //       '提示',
-  //       {
-  //         confirmButtonText: '跳转',
-  //         cancelButtonText: '取消',
-  //       }
-  //     )
-  //     const toLang = '/zh-CN/'
-  //     location.href = `https://element-plus.gitee.io${toLang}${location.pathname.slice(
-  //       toLang.length
-  //     )}`
-  //   } catch {
-  //     userPrefer.value = String(dayjs().unix())
-  //   }
-  // }
-  // unregister sw
   navigator?.serviceWorker?.getRegistrations().then((registrations) => {
     for (const registration of registrations) {
       registration.unregister()
     }
   })
 })
+// if (lang.value === 'zh-CN') {
+//   if (isMirrorUrl()) return
+
+//   if (userPrefer.value) {
+//     // no alert in the next 90 days
+//     if (
+//       dayjs
+//         .unix(Number(userPrefer.value))
+//         .add(90, 'day')
+//         .diff(dayjs(), 'day', true) > 0
+//     )
+//       return
+//   }
+//   try {
+//     await ElMessageBox.confirm(
+//       '建议大陆用户访问部署在国内的站点，是否跳转？',
+//       '提示',
+//       {
+//         confirmButtonText: '跳转',
+//         cancelButtonText: '取消',
+//       }
+//     )
+//     const toLang = '/zh-CN/'
+//     location.href = `https://element-plus.gitee.io${toLang}${location.pathname.slice(
+//       toLang.length
+//     )}`
+//   } catch {
+//     userPrefer.value = String(dayjs().unix())
+//   }
+// }
+// unregister sw
 </script>
 
 <template>
@@ -137,7 +108,7 @@ onMounted(async () => {
         <slot name="sidebar-bottom" />
       </template>
     </VPSidebar>
-    <VPContent :is-sidebar-open="isSidebarOpen">
+    <VPContent>
       <template #content-top>
         <slot name="content-top" />
       </template>
