c787caf8d3aa94493f90cc9c9ce298ba824e3a9d
Dsaquel <noblet.ouwaysgta5@gmail.com>
2025-04-15 23:44:21 +0200

    chore: fix build

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,7 +2,7 @@ import ElementPlus, {
   ID_INJECTION_KEY,
   ZINDEX_INJECTION_KEY,
 } from 'element-plus'
-import nprogress from 'nprogress'
+import { isClient } from '@vueuse/core'
 
 import VPApp, { NotFound, globals } from '../vitepress'
 import { define } from '../utils/types'
@@ -15,7 +15,7 @@ import type { Theme } from 'vitepress'
 export default define<Theme>({
   NotFound,
   Layout: VPApp,
-  enhanceApp: ({ app, router }) => {
+  enhanceApp: async ({ app, router }) => {
     app.use(ElementPlus)
     app.provide(ID_INJECTION_KEY, { prefix: 1024, current: 0 })
     app.provide(ZINDEX_INJECTION_KEY, { current: 0 })
@@ -23,6 +23,8 @@ export default define<Theme>({
     globals.forEach(([name, Comp]) => {
       app.component(name, Comp)
     })
+    if (!isClient) return
+    const nprogress = await import('nprogress')
     router.onBeforeRouteChange = nprogress.start
     router.onAfterRouteChanged = nprogress.done
   },
