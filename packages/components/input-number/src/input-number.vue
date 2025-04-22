3a1f712a2ad8b9c664a79f3509968a4d7b282d29
Dsaquel <noblet.ouwaysgta5@gmail.com>
2025-04-22 13:15:18 +0200

    fix(components): [input-number] reset non numeric value on chromium

--- packages/components/input-number/src/input-number.vue
+++ packages/components/input-number/src/input-number.vue
@@ -85,7 +85,6 @@ import { vRepeatClick } from '@element-plus/directives'
 import { useLocale, useNamespace } from '@element-plus/hooks'
 import {
   debugWarn,
-  isFirefox,
   isNumber,
   isString,
   isUndefined,
@@ -297,7 +296,7 @@ const handleBlur = (event: MouseEvent | FocusEvent) => {
   // This is a Firefox-specific problem. When non-numeric content is entered into a numeric input box,
   // the content displayed on the page is not cleared after the value is cleared. #18533
   // https://bugzilla.mozilla.org/show_bug.cgi?id=1398528
-  if (isFirefox() && data.currentValue === null && input.value?.input) {
+  if (data.currentValue === null && input.value?.input) {
     input.value.input.value = ''
   }
   emit('blur', event)
