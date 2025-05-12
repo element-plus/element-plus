8afe594beac4284e126cfed8baf82595cc03d002
Dsaquel <noblet.ouwaysgta5@gmail.com>
2025-05-12 22:29:19 +0200

    fix(components): [date-picker] sync input value with time-picker

--- packages/components/date-picker-panel/src/date-picker-com/panel-date-range.vue
+++ packages/components/date-picker-panel/src/date-picker-com/panel-date-range.vue
@@ -59,7 +59,7 @@
                 :visible="minTimePickerVisible"
                 :format="timeFormat"
                 datetime-role="start"
-                :parsed-value="leftDate"
+                :parsed-value="minDate || leftDate"
                 @pick="handleMinTimePick"
               />
             </span>
@@ -101,7 +101,7 @@
                 datetime-role="end"
                 :visible="maxTimePickerVisible"
                 :format="timeFormat"
-                :parsed-value="rightDate"
+                :parsed-value="maxDate || rightDate"
                 @pick="handleMaxTimePick"
               />
             </span>
