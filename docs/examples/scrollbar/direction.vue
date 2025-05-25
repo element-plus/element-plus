e0da0a2d5345fd133176a5a7895f612a74c17920
Dsaquel <291874700n@gmail.com>
2025-05-26 01:02:12 +0200

    wip

--- docs/examples/scrollbar/direction.vue
+++ docs/examples/scrollbar/direction.vue
@@ -1,11 +1,6 @@
 <template>
-  <el-scrollbar
-    v-loading="loading"
-    height="400px"
-    :bottom-reached="loadMore"
-    direction="top"
-  >
-    <p v-for="item in num" :key="item" class="scrollbar-demo-item">
+  <el-scrollbar v-loading="loading" height="400px" :bottom-reached="loadMore" direction="top">
+    <p v-for="item in nums" :key="item" class="scrollbar-demo-item">
       {{ item }}
     </p>
   </el-scrollbar>
@@ -14,29 +9,29 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
 
-const num = ref(20)
-const loading = ref(false)
-function* getItems() {
-  yield 5
-  yield 5
-  yield 5
-}
-const items = getItems()
-
-const loadMore = async () => {
-  console.log('!!')
-  loading.value = true
-  await new Promise((r) => setTimeout(r, 2000))
-  try {
-    const huh = items.next().value
-    console.log(huh)
-    if (!huh) return console.log('what !')
-    console.log('kesk')
-    num.value += huh
-  } finally {
-    loading.value = false
-    console.log('end')
+const makeRange = (start: number, end: number) => {
+  const result: number[] = []
+  for (let i = start; i <= end; i++) {
+    result.push(i)
   }
+  return result
+}
+
+const nums = ref(makeRange(60, 80))
+
+const loading = ref(false)
+const db = [
+  makeRange(0, 19),
+  makeRange(20, 39),
+  makeRange(40, 59),
+]
+const loadMore = async () => {
+  const items = db.pop()
+  if(!items) return
+  loading.value = true
+  await new Promise(r => setTimeout(r, 1000))
+  nums.value.unshift(...items)
+  loading.value = false
 }
 </script>
 
