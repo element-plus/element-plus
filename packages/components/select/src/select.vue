47a576b199ded5f6315459dfe9cd5e9cbe718fd8
huangjunfeng <huangjunfeng@jilinxiangyun.com>
2025-05-26 16:15:23 +0800

    refactor(components): [select] modify `manuallyRenderSlots` execute

--- packages/components/select/src/select.vue
+++ packages/components/select/src/select.vue
@@ -300,7 +300,7 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, provide, reactive, toRefs, watch } from 'vue'
+import { computed, defineComponent, nextTick, onMounted, onUpdated, provide, reactive, toRefs } from 'vue'
 import { ClickOutside } from '@element-plus/directives'
 import ElTooltip from '@element-plus/components/tooltip'
 import ElScrollbar from '@element-plus/components/scrollbar'
@@ -382,13 +382,16 @@ export default defineComponent({
         })
       }
     }
-    watch(() => {
-      const currentSlot = slots.default?.()
-      return currentSlot
-    }, (newSlot) => {
-      manuallyRenderSlots(newSlot)
-    }, {
-      immediate: true,
+    onMounted(() => {
+      nextTick(() => {
+        manuallyRenderSlots(slots.default?.())
+      })
+    })
+
+    onUpdated(() => {
+      nextTick(() => {
+        manuallyRenderSlots(slots.default?.())
+      })
     })
 
     provide(
