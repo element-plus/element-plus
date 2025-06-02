5179597d6fd084e64b294f47924ce4f2b04ad072
伊墨 <1905672206@qq.com>
2025-06-02 18:39:53 +0800

    fix: `valueOnClear` prop

--- packages/hooks/use-empty-values/index.ts
+++ packages/hooks/use-empty-values/index.ts
@@ -1,5 +1,10 @@
 import { computed, getCurrentInstance, inject, ref } from 'vue'
-import { buildProps, debugWarn, isFunction } from '@element-plus/utils'
+import {
+  buildProps,
+  debugWarn,
+  definePropType,
+  isFunction,
+} from '@element-plus/utils'
 
 import type { ExtractPropTypes, InjectionKey, Ref } from 'vue'
 
@@ -20,7 +25,13 @@ export const useEmptyValuesProps = buildProps({
    * @description return value when cleared, if you want to set `undefined`, use `() => undefined`
    */
   valueOnClear: {
-    type: [String, Number, Boolean, Function, null],
+    type: definePropType<string | number | boolean | (() => any) | null>([
+      String,
+      Number,
+      Boolean,
+      Function,
+      null,
+    ]),
     default: undefined,
     validator: (val: any) => (isFunction(val) ? !val() : !val),
   },
