5cee7a1464978bc3e68b961047c833325537bfed
xiaohe0601 <xiaohe0601@outlook.com>
2025-06-20 10:01:20 +0800

    fix(components): [table] fix children can't be unchecked (#20995)
    * fix(components): [table] fix children can't be unchecked
    
    * test(components): [table] reserve selection

--- packages/components/table/src/util.ts
+++ packages/components/table/src/util.ts
@@ -292,11 +292,24 @@ export function toggleRowStatus<T>(
   newVal?: boolean,
   tableTreeProps?: TreeProps,
   selectable?: (row: T, index?: number) => boolean,
-  rowIndex?: number
+  rowIndex?: number,
+  rowKey?: string
 ): boolean {
   let _rowIndex = rowIndex ?? 0
   let changed = false
-  const index = statusArr.indexOf(row)
+
+  const getIndex = () => {
+    if (!rowKey) {
+      return statusArr.indexOf(row)
+    }
+
+    const id = getRowIdentity(row, rowKey)
+
+    return statusArr.findIndex((item) => getRowIdentity(item, rowKey) === id)
+  }
+
+  const index = getIndex()
+
   const included = index !== -1
   const isRowSelectable = selectable?.call(null, row, _rowIndex)
 
@@ -344,7 +357,8 @@ export function toggleRowStatus<T>(
         newVal ?? !included,
         tableTreeProps,
         selectable,
-        _rowIndex + 1
+        _rowIndex + 1,
+        rowKey
       )
       _rowIndex += getChildrenCount(item) + 1
       if (childChanged) {
