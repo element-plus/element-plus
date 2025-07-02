e11b76edd9cfd6e9199337fd8d1e103d4a63c0c2
snowbitx <WangXiaoYu_1995@outlook.com>
2025-07-02 20:27:44 +0800

    feat(components):[select] format

--- packages/components/select/src/select.vue
+++ packages/components/select/src/select.vue
@@ -317,15 +317,7 @@
 </template>
 
 <script lang="ts">
-import {
-  computed,
-  defineComponent,
-  getCurrentInstance,
-  provide,
-  reactive,
-  toRefs,
-  watch,
-} from 'vue'
+import { computed, defineComponent, getCurrentInstance, provide, reactive, toRefs, watch } from 'vue'
 import { ClickOutside } from '@element-plus/directives'
 import ElTooltip from '@element-plus/components/tooltip'
 import ElScrollbar from '@element-plus/components/scrollbar'
@@ -341,7 +333,7 @@ import { selectKey } from './token'
 import ElOptions from './options'
 import { selectProps } from './select'
 
-import type { VNode } from 'vue'
+import type { VNode } from 'vue';
 import type { Props, SelectContext } from './type'
 
 const COMPONENT_NAME = 'ElSelect'
@@ -375,12 +367,7 @@ export default defineComponent({
     instance.appContext.config.warnHandler = (...args) => {
       // Overrides warnings about slots not being executable outside of a render function.
       // We call slot below just to simulate data when persist is false, this warning message should be ignored
-      if (
-        !args[0] ||
-        args[0].includes(
-          'Slot "default" invoked outside of the render function'
-        )
-      ) {
+      if (!args[0] || args[0].includes('Slot "default" invoked outside of the render function')) {
         return
       }
       // eslint-disable-next-line no-console
@@ -416,17 +403,14 @@ export default defineComponent({
       }, [])
     }
 
-    const manuallyRenderSlots = (vnodes: VNode[] | undefined) => {
+     const manuallyRenderSlots = (vnodes: VNode[] | undefined) => {
       // After option rendering is completed, the useSelect internal state can collect the value of each option.
       // If the persistent value is false, option will not be rendered by default, so in this case,
       // manually render and load option data here.
       const children = flattedChildren(vnodes || []) as VNode[]
       children.forEach((item) => {
         // @ts-expect-error
-        if (
-          isObject(item) &&
-          (item.type.name === 'ElOption' || item.type.name === 'ElTree')
-        ) {
+        if (isObject(item) && (item.type.name === 'ElOption' || item.type.name === 'ElTree')) {
           // @ts-expect-error
           const _name = item.type.name
           if (_name === 'ElTree') {
@@ -435,36 +419,29 @@ export default defineComponent({
             const treeData = item.props?.data || []
             const flatData = flatTreeSelectData(treeData)
             flatData.forEach((treeItem: any) => {
-              treeItem.currentLabel =
-                treeItem.label ||
-                (isObject(treeItem.value) ? '' : treeItem.value)
+              treeItem.currentLabel = treeItem.label || (isObject(treeItem.value) ? '' : treeItem.value)
               API.onOptionCreate(treeItem)
             })
           } else if (_name === 'ElOption') {
             const obj = { ...item.props } as any
-            obj.currentLabel =
-              obj.label || (isObject(obj.value) ? '' : obj.value)
+            obj.currentLabel = obj.label || (isObject(obj.value) ? '' : obj.value)
             API.onOptionCreate(obj)
           }
         }
       })
     }
-    watch(
-      () => {
-        const slotsContent = slots.default?.()
-        return slotsContent
-      },
-      (newSlot) => {
-        if (props.persistent) {
-          // If persistent is true, we don't need to manually render slots.
-          return
-        }
-        manuallyRenderSlots(newSlot)
-      },
-      {
-        immediate: true,
+    watch(() => {
+      const slotsContent = slots.default?.()
+      return slotsContent
+    }, newSlot => {
+      if (props.persistent) {
+        // If persistent is true, we don't need to manually render slots.
+        return
       }
-    )
+      manuallyRenderSlots(newSlot)
+    }, {
+      immediate: true,
+    })
 
     provide(
       selectKey,
