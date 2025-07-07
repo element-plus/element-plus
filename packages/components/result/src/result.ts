861596a13ef03a884ebfcbbe89848be773b2a979
snowbitx <109521682+snowbitx@users.noreply.github.com>
2025-07-08 01:14:50 +0800

    Update result.ts

--- packages/components/result/src/result.ts
+++ packages/components/result/src/result.ts
@@ -18,7 +18,7 @@ export const IconMap = {
 } as const
 
 export const IconComponentMap: Record<
-  (typeof IconMap)[keyof typeof IconMap],
+  typeof IconMap[keyof typeof IconMap],
   Component
 > = {
   [IconMap.primary]: InfoFilled,
