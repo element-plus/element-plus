8dee4499afbc28316f64164ef92b6ef8615ea6db
snowbitx <109521682+snowbitx@users.noreply.github.com>
2025-07-11 22:19:19 +0800

    Update basic.vue

--- docs/examples/input-number/basic.vue
+++ docs/examples/input-number/basic.vue
@@ -1,11 +1,5 @@
 <template>
-  <el-input-number
-    v-model="num"
-    disable-scientific
-    :min="1"
-    :max="10"
-    @change="handleChange"
-  />
+   <el-input-number v-model="num" :min="1" :max="10" @change="handleChange" />
 </template>
 
 <script lang="ts" setup>
