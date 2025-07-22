25d9fd456b34ca10a3f844d42329479e92d7dcb5
ccg <506566603@qq.com>
2025-07-22 17:35:48 +0800

    feat(components): 添加标签折叠功能
    - 新增 collapseTags、collapseTagsTooltip 和 maxCollapseTags 属性
    - 实现标签折叠和折叠后悬停显示完整内容的功能
    - 更新文档和示例代码，展示新功能的使用方法

---
+++ docs/examples/input-tag/collapse.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="m-4">
+    <p>use collapse-tags</p>
+    <el-input-tag
+      v-model="input1"
+      collapse-tags
+      :max="3"
+      placeholder="Please input"
+      aria-label="Please click the Enter key after input"
+    />
+    <p>use collapse-tags-tooltip</p>
+    <el-input-tag
+      v-model="input2"
+      collapse-tags
+      collapse-tags-tooltip
+      placeholder="Please input"
+      aria-label="Please click the Enter key after input"
+    />
+    <p>use max-collapse-tags</p>
+    <el-input-tag
+      v-model="input3"
+      collapse-tags
+      collapse-tags-tooltip
+      :max-collapse-tags="3"
+      placeholder="Please input"
+      aria-label="Please click the Enter key after input"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const input1 = ref<string[]>()
+const input2 = ref<string[]>()
+const input3 = ref<string[]>()
+</script>
