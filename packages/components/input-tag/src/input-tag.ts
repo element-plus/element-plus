7a68beb9182bf19415be04d9de8edd9c84779185
ccg <506566603@qq.com>
2025-07-23 11:36:52 +0800

    feat(components): Add input box label folding function
    - Add collapseTags, collapseTagsTooltip, and maxCollapseTags attributes
    - Implement the function of tag folding and mouse hovering to display complete content
    - Update documentation and sample code

--- packages/components/input-tag/src/input-tag.ts
+++ packages/components/input-tag/src/input-tag.ts
@@ -128,6 +128,21 @@ export const inputTagProps = buildProps({
     default: true,
   },
   /**
+   * @description whether to collapse tags to a text
+   */
+  collapseTags: Boolean,
+  /**
+   * @description whether show all selected tags when mouse hover text of collapse-tags. To use this, `collapse-tags` must be true
+   */
+  collapseTagsTooltip: Boolean,
+  /**
+   * @description the max tags number to be shown. To use this, `collapse-tags` must be true
+   */
+  maxCollapseTags: {
+    type: Number,
+    default: 1,
+  },
+  /**
    * @description native `aria-label` attribute
    */
   ariaLabel: String,
