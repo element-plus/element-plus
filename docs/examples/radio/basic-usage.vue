bcdeed77794e46c957a74d398eb11226777b59aa
snowbitx <109521682+snowbitx@users.noreply.github.com>
2025-07-29 23:44:42 +0800

    Update basic-usage.vue

--- docs/examples/radio/basic-usage.vue
+++ docs/examples/radio/basic-usage.vue
@@ -1,4 +1,10 @@
 <template>
+  <div class="mb-2 ml-4">
+    <el-radio-group v-model="radio1">
+      <el-radio value="1" size="large">Option 1</el-radio>
+      <el-radio value="2" size="large">Option 2</el-radio>
+    </el-radio-group>
+  </div>
   <div class="my-2 ml-4">
     <el-radio-group v-model="radio2">
       <el-radio value="1">Option 1</el-radio>
