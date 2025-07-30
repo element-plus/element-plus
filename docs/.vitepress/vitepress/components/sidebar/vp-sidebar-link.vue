e6622a035c696472b174e892d1e6e54299faf16b
warmthsea <2586244885@qq.com>
2025-07-30 17:55:58 +0800

    docs: delete sidebar component version class

--- docs/.vitepress/vitepress/components/sidebar/vp-sidebar-link.vue
+++ docs/.vitepress/vitepress/components/sidebar/vp-sidebar-link.vue
@@ -38,7 +38,7 @@ watch([activeLink, sidebarItem], ([active, el]) => {
     @click="$emit('close')"
   >
     <p class="link-text">{{ item.text }}</p>
-    <VersionTag v-if="item.promotion" class="ml-2" :version="item.promotion" />
+    <VersionTag v-if="item.promotion" :version="item.promotion" />
   </a>
 </template>
 
