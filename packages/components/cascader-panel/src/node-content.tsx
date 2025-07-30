9d500fe44ff0e3ad23000fbde8aad7c41496f22a
Dsaquel <291874700n@gmail.com>
2025-07-30 15:33:02 +0200

    chore: chore

--- packages/components/cascader-panel/src/node-content.tsx
+++ packages/components/cascader-panel/src/node-content.tsx
@@ -24,7 +24,7 @@ export default defineComponent({
   setup(props, { emit }) {
     const ns = useNamespace('cascader-node')
     const { config, renderLabelFn } = inject(CASCADER_PANEL_INJECTION_KEY)!
-    const { checkOnClickNode, checkOnClickLeaf, multiple } = config
+    const { checkOnClickNode, checkOnClickLeaf } = config
     const { node, disabled } = props
     const { data, label: nodeLabel } = node
 
@@ -34,7 +34,7 @@ export default defineComponent({
     }
     function handleClick() {
       if (
-        (checkOnClickNode || (node.isLeaf && checkOnClickLeaf && multiple)) &&
+        (checkOnClickNode || (node.isLeaf && checkOnClickLeaf)) &&
         !disabled
       ) {
         emit('handleSelectCheck', !node.checked)
