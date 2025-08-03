8cb174ccb2ff146deba4e0c50dd7ad026e1ad56d
thinkasany <480968828@qq.com>
2025-08-03 22:33:32 +0800

    fix(components): fix

--- packages/components/tour/src/mask.vue
+++ packages/components/tour/src/mask.vue
@@ -58,7 +58,10 @@ const path = computed(() => {
 
 const maskStyle = computed<CSSProperties>(() => ({
   position: 'fixed',
-  inset: 0,
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
   zIndex: props.zIndex,
   pointerEvents: props.pos && props.targetAreaClickable ? 'none' : 'auto',
 }))
