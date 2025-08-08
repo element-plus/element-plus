7f3056235dd98aa0deda6dcd410d098c64b1d44f
Zhong <cszhjh@gmail.com>
2025-08-08 13:05:17 +0800

    test(ssr): add ssr testing cases

---
+++ ssr-testing/cases/statistic.vue
@@ -0,0 +1,50 @@
+<template>
+  <el-row>
+    <el-col :span="6">
+      <el-statistic title="Daily active users" :value="268500" />
+    </el-col>
+    <el-col :span="6">
+      <el-statistic :value="138">
+        <template #title>
+          <div style="display: inline-flex; align-items: center">
+            Ratio of men to women
+            <el-icon style="margin-left: 4px" :size="12">
+              <Male />
+            </el-icon>
+          </div>
+        </template>
+        <template #suffix>/100</template>
+      </el-statistic>
+    </el-col>
+    <el-col :span="6">
+      <el-statistic title="Total Transactions" :value="outputValue" />
+    </el-col>
+    <el-col :span="6">
+      <el-statistic title="Feedback number" :value="562">
+        <template #suffix>
+          <el-icon style="vertical-align: -0.125em">
+            <ChatLineRound />
+          </el-icon>
+        </template>
+      </el-statistic>
+    </el-col>
+  </el-row>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+import { useTransition } from '@vueuse/core'
+import { ChatLineRound, Male } from '@element-plus/icons-vue'
+
+const source = ref(0)
+const outputValue = useTransition(source, {
+  duration: 1500,
+})
+source.value = 172000
+</script>
+
+<style scoped>
+.el-col {
+  text-align: center;
+}
+</style>
---
+++ ssr-testing/cases/watermark.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { reactive } from 'vue'
+
+const font = reactive({
+  color: 'rgba(0, 0, 0, .15)',
+})
+</script>
+
+<template>
+  <el-watermark :font="font">
+    <div style="height: 500px" />
+  </el-watermark>
+</template>
