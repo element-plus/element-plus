992d959da8ed73a4fd7ae697ab084d031c53cd86
catanswer <564029918@qq.com>
2025-08-14 09:00:35 +0000

    Update packages/components/time-picker/src/common/picker-range-trigger.vue

--- packages/components/time-picker/src/common/picker-range-trigger.vue
+++ packages/components/time-picker/src/common/picker-range-trigger.vue
@@ -39,7 +39,7 @@
 </template>
 
 <script lang="ts" setup>
-import { computed, ref } from 'vue'
+import { computed, ref, reactive } from 'vue'
 import { useAttrs, useFocusController, useNamespace } from '@element-plus/hooks'
 import { timePickerRangeTriggerProps } from './props'
 import { useFormItem, useFormItemInputId } from '@element-plus/components/form'
