004fbc7303e0558aa9d8c53deb46778ccba5163f
Dsaquel <291874700n@gmail.com>
2025-08-21 10:29:37 +0200

    chore: apply narrow screen style
    Co-authored-by: 知晓同丶 <zhixiaotong.me@qq.com>

--- docs/examples/color-picker-panel/border.vue
+++ docs/examples/color-picker-panel/border.vue
@@ -1,19 +1,30 @@
 <template>
-  <div class="text-center">No border:</div>
-  <el-divider />
-  <div class="flex justify-center gap-4">
-    <div class="p-5">
-      <el-color-picker-panel v-model="value" :border="false" />
+  <div ref="containerRef">
+    <div class="text-center">No border:</div>
+    <el-divider />
+    <div class="flex flex-wrap justify-center gap-4">
+      <div class="p-5">
+        <el-color-picker-panel v-model="value" :border="false" />
+      </div>
+      <el-divider
+        class="h-auto"
+        :direction="isNarrow ? 'horizontal' : 'vertical'"
+      />
+      <el-card>
+        <el-color-picker-panel v-model="value" :border="false" />
+      </el-card>
     </div>
-    <el-divider class="h-auto" direction="vertical" />
-    <el-card>
-      <el-color-picker-panel v-model="value" :border="false" />
-    </el-card>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
+import { useElementSize } from '@vueuse/core'
 
 const value = ref<string>('#ff6900')
+const containerRef = ref<HTMLElement>()
+
+const { width } = useElementSize(containerRef)
+
+const isNarrow = computed(() => width.value < 815)
 </script>
