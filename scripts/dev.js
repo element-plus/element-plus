ea1c9794c1394102c727a4c51ff0c7babd9a4d57
dopamine <coderzyou@gmail.com>
2025-08-26 19:01:00 +0800

    wip

--- scripts/dev.js
+++ scripts/dev.js
@@ -1,31 +1,32 @@
-import { rolldown } from 'rolldown'
 import { parseArgs } from 'node:util'
 import { readFile } from 'node:fs/promises'
 import path from 'node:path'
+import { watch } from 'rolldown'
 
-const { positionals: targets } = parseArgs({
-    allowPositionals: true
-})
-
-
-
+const { positionals: targets } = parseArgs({ allowPositionals: true })
 
 for (const target of targets) {
-    const packageJSON = JSON.parse(await readFile(path.resolve(`packages/${target}/package.json`), 'utf8'))
-    console.log(packageJSON)
-    const bundle = await rolldown({
-        input: `packages/${target}/index.ts`,
-    })
+  const packageJSON = JSON.parse(
+    await readFile(path.resolve(`packages/${target}/package.json`), 'utf8')
+  )
+  const deps = []
+  if (packageJSON.dependencies)
+    deps.push(...Object.keys(packageJSON.dependencies))
+  if (packageJSON.devDependencies)
+    deps.push(...Object.keys(packageJSON.devDependencies))
 
-    await bundle.generate({
-        format: 'cjs',
-    })
+  const watcher = watch({
+    input: `packages/${target}/index.ts`,
+    output: {
+      file: `packages/${target}/dist/bundle.js`,
+      format: 'cjs',
+    },
+    external: deps,
+  })
 
-    await bundle.write({
-        file: `packages/${target}/dist/bundle.js`
-    })
+  watcher.on('event', (data) => {
+    if (data.code === 'BUNDLE_END') {
+      console.log(`build time:${data.duration}ms`)
+    }
+  })
 }
-
-
-console.log(performance.measure('build time'))
-
