1551099ec587aaa2c98bb1ec83d3cc9eb90ec5bb
snowbitx <109521682+snowbitx@users.noreply.github.com>
2025-08-30 02:48:51 +0800

    Update size.vue

--- docs/examples/splitter/size.vue
+++ docs/examples/splitter/size.vue
@@ -3,7 +3,6 @@
     style="height: 250px; box-shadow: var(--el-border-color-light) 0px 0px 10px"
   >
     <el-splitter
-      :key="splitKey"
       @resize-start="handleResizeStart"
       @resize-end="handleResizeEnd"
       @resize="handleResize"
@@ -11,20 +10,20 @@
       <el-splitter-panel>
         <div class="demo-panel">1</div>
       </el-splitter-panel>
-      <el-splitter-panel v-model:size="size" :max="900" :min="50">
+      <el-splitter-panel v-model:size="size" :max="200" :min="50">
         <div class="demo-panel">{{ size }}px</div>
+      </el-splitter-panel>
+      <el-splitter-panel>
+        <div class="demo-panel">3</div>
       </el-splitter-panel>
     </el-splitter>
   </div>
-
-  <el-button @click="changeSize">点我设置成100px</el-button>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
 
 const size = ref(100)
-const splitKey = ref(0)
 
 const handleResizeStart = (index: number, sizes: number[]) => {
   console.log('resizeStart', index, sizes)
@@ -36,9 +35,6 @@ const handleResize = (index: number, sizes: number[]) => {
 
 const handleResizeEnd = (index: number, sizes: number[]) => {
   console.log('resizeEnd', index, sizes)
-}
-const changeSize = () => {
-  size.value = 100
 }
 </script>
 
