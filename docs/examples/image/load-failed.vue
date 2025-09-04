8217b51f97dfdf1690987804f9d93f2d5de02f5f
JiaxiangZhang <zjx9987@gmail.com>
2025-09-04 18:02:45 +0800

    docs: perf dome

--- docs/examples/image/load-failed.vue
+++ docs/examples/image/load-failed.vue
@@ -1,31 +1,21 @@
 <template>
-  <div class="demo-image__error">
-    <div class="block">
-      <span class="demonstration">Default</span>
-      <el-image />
-    </div>
-    <div class="block">
-      <span class="demonstration">Custom</span>
-      <el-image>
-        <template #error>
-          <div class="image-viewer-slot image-slot">
-            <el-icon><icon-picture /></el-icon>
-          </div>
-        </template>
-      </el-image>
-    </div>
-    <div class="block">
-      <el-image show-progress :src="url" :preview-src-list="srcList" />
-    </div>
-    <div class="block">
-      <el-image :src="url" :preview-src-list="srcList" show-progress>
-        <template #viewer-error>
-          <div class="image-slot">
-            <el-icon><icon-picture /></el-icon>
-          </div>
-        </template>
-      </el-image>
-    </div>
+  <div class="demo-image__error" flex gap-2>
+    <el-image />
+    <el-image>
+      <template #error>
+        <div class="image-viewer-slot image-slot">
+          <el-icon><icon-picture /></el-icon>
+        </div>
+      </template>
+    </el-image>
+    <el-image show-progress :src="url" :preview-src-list="srcList" />
+    <el-image :src="url" :preview-src-list="srcList" show-progress>
+      <template #viewer-error>
+        <div class="image-slot">
+          <el-icon><icon-picture /></el-icon>
+        </div>
+      </template>
+    </el-image>
   </div>
 </template>
 
@@ -41,27 +31,10 @@ const url =
 </script>
 
 <style scoped>
-.demo-image__error .block {
-  padding: 30px 0;
-  text-align: center;
-  border-right: solid 1px var(--el-border-color);
-  display: inline-block;
-  width: 49%;
-  box-sizing: border-box;
-  vertical-align: top;
-}
-.demo-image__error .demonstration {
-  display: block;
-  color: var(--el-text-color-secondary);
-  font-size: 14px;
-  margin-bottom: 20px;
-}
 .demo-image__error .el-image {
-  padding: 0 5px;
   max-width: 300px;
   max-height: 200px;
   width: 100%;
-  height: 200px;
 }
 
 .demo-image__error .image-slot {
