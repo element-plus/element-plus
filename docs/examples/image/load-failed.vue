81be87461fca6fad35fa6844ec6d5ae30935e0f1
JiaxiangZhang <zjx9987@gmail.com>
2025-09-07 13:36:30 +0800

    feat: add image preview

--- docs/examples/image/load-failed.vue
+++ docs/examples/image/load-failed.vue
@@ -19,14 +19,42 @@
         </div>
       </template>
     </el-image>
+    <el-button @click="showPreview = true"> preview controlled </el-button>
+
+    <el-image-viewer
+      v-if="showPreview"
+      show-progress
+      :url-list="srcList"
+      @close="showPreview = false"
+    >
+      <template #viewer-error="{ activeIndex, src }">
+        <div class="image-slot">
+          <el-icon><icon-picture /></el-icon>
+          <span>
+            this is viewer-error slot. current index: {{ activeIndex }} src:
+            {{ src }}
+          </span>
+        </div>
+      </template>
+    </el-image-viewer>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 import { Picture as IconPicture } from '@element-plus/icons-vue'
 
+import type { ImageInstance } from 'element-plus'
+
+const imageRef = ref<ImageInstance>()
+const showPreview = ref(false)
+
+const handleClick = () => {
+  imageRef.value!.showPreview()
+}
+
 const srcList = [
-  'https://fuss10.elemecdn.com/1/34/19aa98b1fcb2781c4fba33d850549jpeg.jpeg',
+  'https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg',
   'https://errorSrc',
 ]
 const url =
