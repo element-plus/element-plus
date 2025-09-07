187695b719187ed0c9a14418715e88dafb230192
JiaxiangZhang <zjx9987@gmail.com>
2025-09-07 13:30:12 +0800

    fix: restore error source in image load-failed example

--- docs/examples/image/load-failed.vue
+++ docs/examples/image/load-failed.vue
@@ -26,8 +26,8 @@
 import { Picture as IconPicture } from '@element-plus/icons-vue'
 
 const srcList = [
-  'https://errorSrc',
   'https://fuss10.elemecdn.com/1/34/19aa98b1fcb2781c4fba33d850549jpeg.jpeg',
+  'https://errorSrc',
 ]
 const url =
   'https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg'
