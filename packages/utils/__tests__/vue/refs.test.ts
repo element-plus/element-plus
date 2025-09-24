4c7ed6f6115db1c52acce9fe66a23d99996b33f2
warmthsea <2586244885@qq.com>
2025-09-24 08:55:13 +0800

    Revert "refactor: remove unused code related to tooltip-v2 (#22304)"
    This reverts commit 630b66f4cc71be9628d680b0aafee3d1835c6504.

--- packages/utils/__tests__/vue/refs.test.ts
+++ packages/utils/__tests__/vue/refs.test.ts
@@ -1,14 +1,11 @@
-import { ref } from 'vue'
-import { describe, expect, it } from 'vitest'
+import { describe, expect, it, vi } from 'vitest'
 import { composeRefs } from '../..'
 
 describe('composeRefs', () => {
-  it('should assign correctly the element to the given refs', () => {
-    const element = document.createElement('div')
-    const firstRef = ref<HTMLDivElement>()
-    const secondRef = ref<HTMLDivElement>()
-    composeRefs(firstRef, secondRef)(element)
-    expect(firstRef.value).toBe(element)
-    expect(secondRef.value).toBe(element)
+  it('ref setter should be called', () => {
+    const refSetter = vi.fn()
+    const fn = composeRefs(refSetter)
+    fn(null)
+    expect(refSetter).toHaveBeenCalled()
   })
 })
