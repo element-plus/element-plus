d871117c68d3f10e28c2ef611e59b4db4c43cbb4
btea <2356281422@qq.com>
2025-10-21 15:48:33 +0800

    fix(components): [select] should not affect global config (#22544)

--- packages/components/select/src/select.vue
+++ packages/components/select/src/select.vue
@@ -386,6 +386,7 @@ export default defineComponent({
 
   setup(props, { emit, slots }) {
     const instance = getCurrentInstance()!
+    const originalWarnHandler = instance.appContext.config.warnHandler
     instance.appContext.config.warnHandler = (...args) => {
       // Overrides warnings about slots not being executable outside of a render function.
       // We call slot below just to simulate data when persist is false, this warning message should be ignored
@@ -507,7 +508,7 @@ export default defineComponent({
 
     onBeforeUnmount(() => {
       // https://github.com/element-plus/element-plus/issues/21279
-      instance.appContext.config.warnHandler = undefined
+      instance.appContext.config.warnHandler = originalWarnHandler
     })
 
     return {
