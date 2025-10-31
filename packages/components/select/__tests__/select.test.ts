a84657db97bdbd0f74382c03331241cb0c5bcc16
btea <2356281422@qq.com>
2025-10-31 22:12:35 +0800

    test: update

--- packages/components/select/__tests__/select.test.ts
+++ packages/components/select/__tests__/select.test.ts
@@ -1,4 +1,7 @@
 // @ts-nocheck
+/**
+ * @vitest-environment happy-dom
+ */
 import { defineComponent, markRaw, nextTick, ref } from 'vue'
 import { mount } from '@vue/test-utils'
 import { afterEach, describe, expect, it, test, vi } from 'vitest'
