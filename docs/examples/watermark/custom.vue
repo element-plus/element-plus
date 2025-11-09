cf0e103321b514555980abe7874cb0aa0d15e17f
JiaxiangZhang <zjx9987@gmail.com>
2025-11-09 23:50:43 +0800

    docs: [Watermark] rename class name

--- docs/examples/watermark/custom.vue
+++ docs/examples/watermark/custom.vue
@@ -25,7 +25,7 @@ const config = reactive({
       :gap="config.gap"
       :offset="config.offset"
     >
-      <div class="demo">
+      <div class="watermarkContainer">
         <h1>Element Plus</h1>
         <h2>A Vue 3 based component library for designers and developers</h2>
         <img src="/images/hamburger.png" alt="示例图片" />
@@ -84,7 +84,7 @@ const config = reactive({
   display: flex;
   flex: auto;
 }
-.demo {
+.watermarkContainer {
   flex: auto;
 }
 .form {
