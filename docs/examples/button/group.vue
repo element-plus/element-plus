ff9f7d1470959f80ad707081b4eac365dfa507c0
Dsaquel <291874700n@gmail.com>
2025-11-19 14:59:12 +0100

    docs: improve display example

--- docs/examples/button/group.vue
+++ docs/examples/button/group.vue
@@ -1,18 +1,18 @@
 <template>
-  <el-button-group>
+  <el-button-group class="mb-4">
     <el-button type="primary" :icon="ArrowLeft">Previous Page</el-button>
     <el-button type="primary">
       Next Page<el-icon class="el-icon--right"><ArrowRight /></el-icon>
     </el-button>
   </el-button-group>
+  <br />
+  <el-radio-group v-model="direction" class="mb-2">
+    <el-radio value="horizontal">Horizontal</el-radio>
+    <el-radio value="vertical">Vertical</el-radio>
+  </el-radio-group>
+  <br />
 
-  <el-button-group class="ml-4">
-    <el-button type="primary" :icon="Edit" />
-    <el-button type="primary" :icon="Share" />
-    <el-button type="primary" :icon="Delete" />
-  </el-button-group>
-
-  <el-button-group class="ml-4" direction="vertical">
+  <el-button-group :direction="direction">
     <el-button type="primary" :icon="House" />
     <el-button type="primary" :icon="Operation" />
     <el-button type="primary" :icon="Notification" />
@@ -20,14 +20,14 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 import {
   ArrowLeft,
   ArrowRight,
-  Delete,
-  Edit,
   House,
   Notification,
   Operation,
-  Share,
 } from '@element-plus/icons-vue'
+
+const direction = ref('horizontal')
 </script>
