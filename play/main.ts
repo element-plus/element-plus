3062379e1f1bf2a58ccfbfd3c7f9892d64739793
btea <2356281422@qq.com>
2025-11-23 10:01:42 +0800

    feat(play): add window error listener capture miss error

--- play/main.ts
+++ play/main.ts
@@ -4,6 +4,21 @@ import '@element-plus/theme-chalk/src/dark/css-vars.scss'
 import '@element-plus/theme-chalk/src/notification.scss'
 import '@element-plus/theme-chalk/src/message-box.scss'
 import '@element-plus/theme-chalk/src/message.scss'
+
+// #21498
+window.addEventListener('error', (e: ErrorEvent) => {
+  console.error(`%c${e.message}`, 'color: #e74c3c; font-weight: bold;')
+  console.error(e)
+})
+window.addEventListener('unhandledrejection', (e: PromiseRejectionEvent) => {
+  const err = e.reason as Error
+  console.error(
+    `%cUnhandled Promise Rejection: %c${err.message}`,
+    'color: #e74c3c; font-weight: bold;',
+    'color: #3498db;'
+  )
+  console.error(err)
+})
 ;(async () => {
   const apps = import.meta.glob<
     true,
