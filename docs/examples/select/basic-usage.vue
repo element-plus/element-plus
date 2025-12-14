96af4d9523eb2c531c9c962e319c82dac0a85703
snowbitx <109521682+snowbitx@users.noreply.github.com>
2025-12-14 18:45:57 +0800

    update
    Updated the example to use a static options array and removed dynamic option addition functionality.

--- docs/examples/select/basic-usage.vue
+++ docs/examples/select/basic-usage.vue
@@ -1,46 +1,39 @@
-<script setup lang="ts">
-import { defineComponent, h, ref } from 'vue'
-import { ElButton, ElOption, ElSelect } from 'element-plus'
-
-const options = ref([
-  { label: 'A', value: 'a' },
-  { label: 'B', value: 'b' },
-])
-
-const modelValue = ref('a')
-
-function addOption() {
-  options.value.push({
-    label: `Option ${options.value.length + 1}`,
-    value: `${options.value.length + 1}`,
-  })
-}
-</script>
-
 <template>
-  <div style="padding: 20px">
-    <el-button @click="addOption">add option</el-button>
-    <component
-      :is="
-        h(
-          ElSelect,
-          {
-            modelValue: modelValue,
-            'onUpdate:modelValue': (value) => {
-              modelValue = value
-            },
-          },
-          () =>
-            options.map((opt) =>
-              h(ElOption, {
-                key: opt.value,
-                label: opt.label,
-                value: opt.value,
-              })
-            )
-        )
-      "
-    >
-    </component>
-  </div>
+  <el-select v-model="value" placeholder="Select" style="width: 240px">
+    <el-option
+      v-for="item in options"
+      :key="item.value"
+      :label="item.label"
+      :value="item.value"
+    />
+  </el-select>
 </template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const value = ref('')
+
+const options = [
+  {
+    value: 'Option1',
+    label: 'Option1',
+  },
+  {
+    value: 'Option2',
+    label: 'Option2',
+  },
+  {
+    value: 'Option3',
+    label: 'Option3',
+  },
+  {
+    value: 'Option4',
+    label: 'Option4',
+  },
+  {
+    value: 'Option5',
+    label: 'Option5',
+  },
+]
+</script>
