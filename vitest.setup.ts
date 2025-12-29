d741452204f9eb19845367459fb4bfac7a7b9312
Joie <3082243978@qq.com>
2025-12-29 20:13:34 +0800

    revert: remove HTMLCanvasElement.getContext mock as not needed

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -4,11 +4,4 @@ import ResizeObserver from 'resize-observer-polyfill'
 
 vi.stubGlobal('ResizeObserver', ResizeObserver)
 
-// Mock HTMLCanvasElement.getContext for jsdom compatibility
-Object.defineProperty(HTMLCanvasElement.prototype, 'getContext', {
-  value: () => {
-    return {}
-  },
-})
-
 config.global.stubs = {}
