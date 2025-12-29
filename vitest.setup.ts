35ce50a58915165fed1cd44b98b9685b53855499
Joie <3082243978@qq.com>
2025-12-29 13:59:08 +0800

    test: mock HTMLCanvasElement.getContext for jsdom compatibility

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -4,4 +4,11 @@ import ResizeObserver from 'resize-observer-polyfill'
 
 vi.stubGlobal('ResizeObserver', ResizeObserver)
 
+// Mock HTMLCanvasElement.getContext for jsdom compatibility
+Object.defineProperty(HTMLCanvasElement.prototype, 'getContext', {
+  value: () => {
+    return {}
+  },
+})
+
 config.global.stubs = {}
