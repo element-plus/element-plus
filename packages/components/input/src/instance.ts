fc28ca8d5f597a69e2158f93104027743a6309e2
rzzf <cszhjh@gmail.com>
2026-01-16 10:14:16 +0800

    refactor: revert InputInstance

--- packages/components/input/src/instance.ts
+++ packages/components/input/src/instance.ts
@@ -1,6 +1,3 @@
-import type { ComponentInstance } from 'vue'
-import type { ComponentExposed } from 'vue-component-type-helpers'
 import type Input from './input.vue'
 
-export type InputInstance = ComponentInstance<typeof Input> &
-  ComponentExposed<typeof Input>
+export type InputInstance = InstanceType<typeof Input> & unknown
