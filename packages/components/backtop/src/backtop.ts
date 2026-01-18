75160ce4035521edc3a66e9cb4d165864cf5c0ae
lw56777 <535847787@qq.com>
2026-01-18 14:51:02 +0800

    refactor(components): [backtop] use type-based definitions

--- packages/components/backtop/src/backtop.ts
+++ packages/components/backtop/src/backtop.ts
@@ -1,6 +1,30 @@
-import type { ExtractPropTypes, ExtractPublicPropTypes } from 'vue'
+import { buildProps } from '@element-plus/utils'
 
-export const backtopProps = {
+import type { ExtractPublicPropTypes } from 'vue'
+
+export interface BacktopProps {
+  /**
+   * @description the button will not show until the scroll height reaches this value.
+   */
+  visibilityHeight?: number
+  /**
+   * @description the target to trigger scroll.
+   */
+  target?: string
+  /**
+   * @description right distance.
+   */
+  right?: number
+  /**
+   * @description bottom distance.
+   */
+  bottom?: number
+}
+
+/**
+ * @deprecated Removed after 3.0.0, Use `BacktopProps` instead.
+ */
+export const backtopProps = buildProps({
   /**
    * @description the button will not show until the scroll height reaches this value.
    */
@@ -29,8 +53,11 @@ export const backtopProps = {
     type: Number,
     default: 40,
   },
-} as const
-export type BacktopProps = ExtractPropTypes<typeof backtopProps>
+} as const)
+
+/**
+ * @deprecated Removed after 3.0.0, Use `BacktopProps` instead.
+ */
 export type BacktopPropsPublic = ExtractPublicPropTypes<typeof backtopProps>
 
 export const backtopEmits = {
