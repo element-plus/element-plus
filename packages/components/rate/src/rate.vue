e71993c5199680a092b2fdbca96971c84364752b
lw56777 <535847787@qq.com>
2026-01-18 17:27:06 +0800

    refactor(components): [rate] use type-based definitions (#23439)
    * refactor(components): [rate] use type-based definitions
    
    * refactor: update
    
    ---------
    
    Co-authored-by: rzzf <cszhjh@gmail.com>

--- packages/components/rate/src/rate.vue
+++ packages/components/rate/src/rate.vue
@@ -80,11 +80,13 @@ import {
   useFormSize,
 } from '@element-plus/components/form'
 import { ElIcon } from '@element-plus/components/icon'
+import { Star, StarFilled } from '@element-plus/icons-vue'
 import { useNamespace } from '@element-plus/hooks'
-import { rateEmits, rateProps } from './rate'
+import { rateEmits } from './rate'
 
 import type { CSSProperties, Component } from 'vue'
 import type { IconInstance } from '@element-plus/components/icon'
+import type { RateProps } from './rate'
 
 function getValueFromMap<T>(
   value: number,
@@ -110,7 +112,29 @@ defineOptions({
   name: 'ElRate',
 })
 
-const props = defineProps(rateProps)
+const props = withDefaults(defineProps<RateProps>(), {
+  modelValue: 0,
+  id: undefined,
+  lowThreshold: 2,
+  highThreshold: 4,
+  max: 5,
+  colors: () => ['', '', ''],
+  voidColor: '',
+  disabledVoidColor: '',
+  icons: () => [StarFilled, StarFilled, StarFilled],
+  voidIcon: () => Star,
+  disabledVoidIcon: () => StarFilled,
+  disabled: undefined,
+  textColor: '',
+  texts: () => [
+    'Extremely bad',
+    'Disappointed',
+    'Fair',
+    'Satisfied',
+    'Surprise',
+  ],
+  scoreTemplate: '{value}',
+})
 const emit = defineEmits(rateEmits)
 
 const formItemContext = inject(formItemContextKey, undefined)
