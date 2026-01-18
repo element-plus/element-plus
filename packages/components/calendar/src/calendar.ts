3333aaf6183c9efbfd908512995b08bff29e690b
zhongli <zhongli8166@163.com>
2026-01-18 14:46:00 +0800

    refactor(components): [calendar] use type-based definitions

--- packages/components/calendar/src/calendar.ts
+++ packages/components/calendar/src/calendar.ts
@@ -19,12 +19,26 @@ const isValidRange = (range: unknown): range is [Date, Date] =>
   isArray(range) && range.length === 2 && range.every((item) => isDate(item))
 
 export interface CalendarProps {
+  /**
+   * @description binding value
+   */
   modelValue?: Date
+  /** @description time range, including start time and end time.
+   *   Start time must be start day of week, end time must be end day of week, the time span cannot exceed two months.
+   */
   range?: [Date, Date]
+  /**
+   * @description type of the controller for the Calendar header
+   * */
   controllerType?: 'button' | 'select'
+  /**
+   * @description format label when `controller-type` is 'select'
+   */
   formatter?: (value: number, type: 'year' | 'month') => string | number
 }
-
+/**
+ * @deprecated Removed after 3.0.0, Use `CalendarProps` instead.
+ */
 export const calendarProps = buildProps({
   /**
    * @description binding value
@@ -57,6 +71,8 @@ export const calendarProps = buildProps({
     >(Function),
   },
 } as const)
+/** @deprecated Removed after 3.0.0, Use `CalendarProps` instead.
+ */
 export type CalendarPropsPublic = ExtractPublicPropTypes<typeof calendarProps>
 
 export const calendarEmits = {
