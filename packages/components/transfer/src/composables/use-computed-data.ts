40a885a1e221baa454e278d9eed874fcd242f57a
btea <2356281422@qq.com>
2026-01-19 14:23:32 +0800

    fix: update

--- packages/components/transfer/src/composables/use-computed-data.ts
+++ packages/components/transfer/src/composables/use-computed-data.ts
@@ -3,31 +3,28 @@ import { usePropsAlias } from './use-props-alias'
 
 import type { TransferDataItem, TransferKey, TransferProps } from '../transfer'
 
-export const useComputedData = (props: TransferProps) => {
+export const useComputedData = (props: Required<TransferProps>) => {
   const propsAlias = usePropsAlias(props)
 
   const dataObj = computed(() =>
-    props.data?.reduce(
-      (o, cur) => (o[cur[propsAlias.value.key]] = cur) && o,
-      {}
-    )
+    props.data.reduce((o, cur) => (o[cur[propsAlias.value.key]] = cur) && o, {})
   )
 
   const sourceData = computed(() =>
-    props.data?.filter(
-      (item) => !props.modelValue?.includes(item[propsAlias.value.key])
+    props.data.filter(
+      (item) => !props.modelValue.includes(item[propsAlias.value.key])
     )
   )
 
   const targetData = computed(() => {
     if (props.targetOrder === 'original') {
-      return props.data?.filter((item) =>
-        props.modelValue?.includes(item[propsAlias.value.key])
+      return props.data.filter((item) =>
+        props.modelValue.includes(item[propsAlias.value.key])
       )
     } else {
-      return props.modelValue?.reduce(
+      return props.modelValue.reduce(
         (arr: TransferDataItem[], cur: TransferKey) => {
-          const val = dataObj.value?.[cur]
+          const val = dataObj.value[cur]
           if (val) {
             arr.push(val)
           }
