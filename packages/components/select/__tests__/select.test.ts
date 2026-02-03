5694ffe04c8220ef73a4f0acedaf8f0715e3db24
lostElk <lostelkwork@163.com>
2026-02-03 13:14:12 +0800

    Update packages/components/select/__tests__/select.test.ts
    Co-authored-by: rzzf <cszhjh@gmail.com>

--- packages/components/select/__tests__/select.test.ts
+++ packages/components/select/__tests__/select.test.ts
@@ -1515,8 +1515,7 @@ describe('Select', () => {
           tooltip-append-to=".append-target"
           placeholder="请选择"
         >
-          <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value">
-          </el-option>
+          <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value" />
         </el-select>
       </div>
     `,
