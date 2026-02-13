34571582ce3d0faefb4874a0e2edf89766c5f3ca
lihuihui717 <41786752+lihuihui717@users.noreply.github.com>
2026-02-13 19:18:33 +0800

    s

--- docs/examples/table/multi-select.vue
+++ docs/examples/table/multi-select.vue
@@ -39,7 +39,7 @@ interface User {
 const multipleTableRef = ref<TableInstance>()
 const multipleSelection = ref<User[]>([])
 
-const selectable = (row: User) => ![1, 2].includes(row.id)
+const selectable = (row: User) => ![].includes(row.id)
 const toggleSelection = (rows?: User[], ignoreSelectable?: boolean) => {
   if (rows) {
     rows.forEach((row) => {
