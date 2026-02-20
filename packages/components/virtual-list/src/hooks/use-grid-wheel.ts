d65be931fd498a9d82820e99bea46513bab2d8fa
rzzf <cszhjh@gmail.com>
2026-02-20 19:19:25 +0800

    fix(components): [table-v2] navigation back triggered by trackpad scrolling

--- packages/components/virtual-list/src/hooks/use-grid-wheel.ts
+++ packages/components/virtual-list/src/hooks/use-grid-wheel.ts
@@ -19,19 +19,22 @@ export const useGridWheel = (
   let xOffset = 0
   let yOffset = 0
 
+  const hasXReachedEdge = (x: number) => {
+    return (x < 0 && atXStartEdge.value) || (x > 0 && atXEndEdge.value)
+  }
+  const hasYReachedEdge = (y: number) => {
+    return (y < 0 && atYStartEdge.value) || (y > 0 && atYEndEdge.value)
+  }
   const hasReachedEdge = (x: number, y: number) => {
-    const xEdgeReached =
-      (x < 0 && atXStartEdge.value) || (x > 0 && atXEndEdge.value)
-    const yEdgeReached =
-      (y < 0 && atYStartEdge.value) || (y > 0 && atYEndEdge.value)
-    return xEdgeReached || yEdgeReached
+    return hasXReachedEdge(x) || hasYReachedEdge(y)
   }
 
   const onWheel = (e: WheelEvent) => {
     cAF(frameHandle!)
 
-    let x = e.deltaX
-    let y = e.deltaY
+    const { deltaX, deltaY } = e
+    let x = deltaX
+    let y = deltaY
     // Simulate native behavior when using touch pad/track pad for wheeling.
     if (Math.abs(x) > Math.abs(y)) {
       y = 0
@@ -45,7 +48,18 @@ export const useGridWheel = (
       y = 0
     }
 
-    if (hasReachedEdge(x, y)) return
+    if (hasReachedEdge(x, y)) {
+      // #23524
+      if (
+        deltaX < 0 &&
+        deltaY < 0 &&
+        !hasXReachedEdge(deltaX) &&
+        hasYReachedEdge(deltaY)
+      ) {
+        e.preventDefault()
+      }
+      return
+    }
 
     xOffset += x
     yOffset += y
