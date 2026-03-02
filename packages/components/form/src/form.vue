51054e22e61c0540ab2dea279bf842936f97b185
leihaohao <2468048176@qq.com>
2026-03-02 10:49:35 +0800

    fix(components): resetFields
    closed #23705

--- packages/components/form/src/form.vue
+++ packages/components/form/src/form.vue
@@ -6,7 +6,6 @@
 
 <script lang="ts" setup>
 import { computed, provide, reactive, ref, toRefs, watch } from 'vue'
-import { cloneDeep } from 'lodash-unified'
 import {
   debugWarn,
   ensureArray,
@@ -48,7 +47,7 @@ const emit = defineEmits(formEmits)
 
 const formRef = ref<HTMLElement>()
 const fields = reactive<FormItemContext[]>([])
-const initialValues = new Map<string, any>()
+const removedFields = new Map<string, FormItemContext>()
 
 const formSize = useFormSize()
 const ns = useNamespace('form')
@@ -70,18 +69,13 @@ const getField: FormContext['getField'] = (prop) => {
 
 const addField: FormContext['addField'] = (field) => {
   fields.push(field)
-  if (field.propString) {
-    if (initialValues.has(field.propString)) {
-      field.setInitialValue(initialValues.get(field.propString))
-    } else {
-      initialValues.set(field.propString, cloneDeep(field.fieldValue))
-    }
-  }
 }
 
 const removeField: FormContext['removeField'] = (field) => {
   if (field.prop) {
     fields.splice(fields.indexOf(field), 1)
+    // When field unmounts, propString remains unchanged, so it can be used as the key
+    removedFields.set(field.propString, field)
   }
 }
 
@@ -98,14 +92,14 @@ const setInitialValues: FormContext['setInitialValues'] = (initModel) => {
     return
   }
 
-  for (const key of initialValues.keys()) {
-    initialValues.set(key, cloneDeep(getProp(initModel, key).value))
-  }
-  fields.forEach((field) => {
+  const eachCallback = (field: FormItemContext) => {
     if (field.prop) {
+      // getProp will return undefined if the prop is not found
       field.setInitialValue(getProp(initModel, field.prop).value)
     }
-  })
+  }
+  removedFields.forEach(eachCallback)
+  fields.forEach(eachCallback)
 }
 
 const resetFields: FormContext['resetFields'] = (properties = []) => {
@@ -114,22 +108,39 @@ const resetFields: FormContext['resetFields'] = (properties = []) => {
     return
   }
 
-  filterFields(fields, properties).forEach((field) => field.resetField())
-
-  const activePropStrings = new Set(
-    fields.map((f) => f.propString).filter(Boolean)
+  const isSpecificProps = properties.length > 0
+  const propStrings = ensureArray(properties).map((prop) =>
+    isArray(prop) ? prop.join('.') : prop
   )
-  const propsToCheck =
-    properties.length > 0
-      ? ensureArray(properties).map((p) => (isArray(p) ? p.join('.') : p))
-      : [...initialValues.keys()]
 
-  for (const propString of propsToCheck) {
-    if (!activePropStrings.has(propString) && initialValues.has(propString)) {
-      getProp(props.model, propString).value = cloneDeep(
-        initialValues.get(propString)
+  // First reset the existing or specified fields
+  filterFields(fields, propStrings, true).forEach((field) => {
+    field.resetField()
+
+    // The key in removedFields is fixed at unmount time, so if the same propString exists in fields,
+    // it means the field is duplicated and we need to remove the cache from removedFields
+    removedFields.delete(field.propString)
+
+    // If propStrings are provided, remove the already reset field from propStrings
+    if (isSpecificProps) {
+      const index = propStrings.indexOf(field.propString)
+      if (index > -1) {
+        propStrings.splice(index, 1)
+      }
+    }
+  })
+
+  // If propStrings are provided, search for fields not yet reset in removedFields,
+  // otherwise reset all removedFields
+  if (isSpecificProps) {
+    // propStrings.length > 0 means there are fields not yet reset, otherwise all fields have been reset
+    if (propStrings.length > 0) {
+      filterFields([...removedFields.values()], propStrings, true).forEach(
+        (field) => field.resetField()
       )
     }
+  } else {
+    removedFields.forEach((field) => field.resetField())
   }
 }
 
