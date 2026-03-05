1fa834e2f98a087d1b19d37bc0f1f02c88a4f2de
rzzf <cszhjh@gmail.com>
2026-03-05 12:09:23 +0800

    docs: fix ts error

--- docs/.vitepress/env.d.ts
+++ docs/.vitepress/env.d.ts
@@ -1,10 +1,6 @@
 /// <reference types="vite/client" />
 
 declare module 'virtual:component-changelog-data' {
-  import type { ComponentChangelogs } from './utils/changelog-parser'
-
-  const data: ComponentChangelogs
+  const data: import('./utils/changelog-parser').ComponentChangelogs
   export default data
 }
-
-export {}
