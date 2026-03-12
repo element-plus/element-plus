2184fe1aaa70fdb9257f75231f02f4916076d84f
rzzf <cszhjh@gmail.com>
2026-03-12 16:41:11 +0800

    chore: add doc

--- packages/utils/numbers.ts
+++ packages/utils/numbers.ts
@@ -1,3 +1,8 @@
+/**
+ * Due to browser rendering and calculation precision loss issues,
+ * boundary checks cannot be based solely on value equality;
+ * a certain range of fluctuation is permissible.
+ */
 export function isGreaterThan(a: number, b: number, epsilon = 0.03) {
   return a - b > epsilon
 }
