d453289a8ebbb50713d05ba5ea8818243ea50a7a
btea <2356281422@qq.com>
2026-03-13 15:18:57 +0800

    chore: make build happy

--- internal/build/src/tasks/types-definitions.ts
+++ internal/build/src/tasks/types-definitions.ts
@@ -44,7 +44,6 @@ export async function generateTypesDefinitions() {
       compilerOptions: {
         emitDeclarationOnly: true,
         declaration: true,
-        preserveSymlinks: true,
       },
     }),
     output: {
