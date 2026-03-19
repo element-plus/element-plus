956edd3486a1f8cdaa95f42a227c543dab5da836
micaiguai <1399880823@qq.com>
2026-03-19 21:41:24 +0800

    chore(utils): optimize setPropsDefaults ts suggestion

--- packages/utils/vue/typescript.ts
+++ packages/utils/vue/typescript.ts
@@ -1,4 +1,10 @@
-import type { AppContext, EmitsOptions, ObjectPlugin, SetupContext } from 'vue'
+import type {
+  AppContext,
+  EmitsOptions,
+  ObjectPlugin,
+  SetupContext,
+  VNodeProps,
+} from 'vue'
 
 export type SFCWithInstall<T> = T & ObjectPlugin & SFCWithPropsDefaultsSetter<T>
 
@@ -6,9 +12,19 @@ export type SFCInstallWithContext<T> = SFCWithInstall<T> & {
   _context: AppContext | null
 }
 
+type PickProps<T> = {
+  [P in keyof T as P extends keyof VNodeProps
+    ? never
+    : P extends `on${string}`
+      ? never
+      : P]: T[P]
+}
+
 export type SFCWithPropsDefaultsSetter<T> = T extends new (...args: any) => any
   ? {
-      setPropsDefaults: (defaults: Partial<InstanceType<T>['$props']>) => void
+      setPropsDefaults: (
+        defaults: Partial<PickProps<InstanceType<T>['$props']>>
+      ) => void
     }
   : unknown
 
