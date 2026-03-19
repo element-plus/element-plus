f4c34f7bd4e178c1711a0e125227251ee38ed1b0
micaiguai <1399880823@qq.com>
2026-03-19 21:41:59 +0800

    chore: update

--- packages/utils/vue/typescript.ts
+++ packages/utils/vue/typescript.ts
@@ -6,18 +6,18 @@ import type {
   VNodeProps,
 } from 'vue'
 
-export type SFCWithInstall<T> = T & ObjectPlugin & SFCWithPropsDefaultsSetter<T>
-
-export type SFCInstallWithContext<T> = SFCWithInstall<T> & {
-  _context: AppContext | null
-}
-
 type PickProps<T> = {
   [P in keyof T as P extends keyof VNodeProps
     ? never
     : P extends `on${string}`
       ? never
       : P]: T[P]
+}
+
+export type SFCWithInstall<T> = T & ObjectPlugin & SFCWithPropsDefaultsSetter<T>
+
+export type SFCInstallWithContext<T> = SFCWithInstall<T> & {
+  _context: AppContext | null
 }
 
 export type SFCWithPropsDefaultsSetter<T> = T extends new (...args: any) => any
