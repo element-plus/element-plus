49d06e7487d1071968112993dfda99e6fadbda63
micaiguai <1399880823@qq.com>
2026-04-09 23:13:23 +0800

    chore: keep type consistence

--- packages/components/tabs/src/tab-bar.vue
+++ packages/components/tabs/src/tab-bar.vue
@@ -84,7 +84,9 @@ const getBarStyle = (): CSSProperties => {
   }
 }
 
-const update = () => (barStyle.value = getBarStyle())
+const update = () => {
+  barStyle.value = getBarStyle()
+}
 
 const tabObservers = [] as ReturnType<typeof useResizeObserver>[]
 const observerTabs = () => {
