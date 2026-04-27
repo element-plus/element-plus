ffbbb2b57cd23de1739c8d98191245c8a4441ed8
rzzf <cszhjh@gmail.com>
2026-04-27 14:46:26 +0800

    refactor: improve type

--- packages/utils/vue/typescript.ts
+++ packages/utils/vue/typescript.ts
@@ -9,18 +9,19 @@ import type {
 } from 'vue'
 import type { ComponentEmit, ComponentProps } from 'vue-component-type-helpers'
 
-type ExtractEventNames<T> = keyof {
-  [K in keyof ComponentProps<T> as K extends `on${infer Event}`
-    ? ComponentEmit<T> extends (event: string, ...args: any[]) => any
-      ? never
-      : ComponentEmit<T> extends (
-            event: Uncapitalize<Event>,
-            ...args: any[]
-          ) => any
-        ? K
-        : never
-    : never]: unknown
-}
+type ExtractEventNames<T> =
+  ComponentEmit<T> extends (event: string, ...args: any[]) => any
+    ? never
+    : keyof {
+        [K in keyof ComponentProps<T> as K extends `on${infer Event}`
+          ? ComponentEmit<T> extends (
+              event: Uncapitalize<Event>,
+              ...args: any[]
+            ) => any
+            ? K
+            : never
+          : never]: unknown
+      }
 
 type ExcludedProps<T> =
   | ExtractEventNames<T>
