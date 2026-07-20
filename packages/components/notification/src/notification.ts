b616a8387badcbcc84aefa307eaa65bb4bb1db7b
misaka <514183295@qq.com>
2026-07-20 16:20:23 +0800

    chore(components): [notification] fix lint issues

--- packages/components/notification/src/notification.ts
+++ packages/components/notification/src/notification.ts
@@ -20,7 +20,10 @@ export const notificationTypes = [
 export type NotificationType = (typeof notificationTypes)[number] | ''
 
 export type NotificationPosition =
-  'top-right' | 'top-left' | 'bottom-right' | 'bottom-left'
+  | 'top-right'
+  | 'top-left'
+  | 'bottom-right'
+  | 'bottom-left'
 
 export interface NotificationProps {
   /**
@@ -266,7 +269,9 @@ export interface NotificationHandle {
 
 export type NotificationParams = Partial<NotificationOptions> | string | VNode
 export type NotificationParamsTyped =
-  Partial<NotificationOptionsTyped> | string | VNode
+  | Partial<NotificationOptionsTyped>
+  | string
+  | VNode
 
 export interface NotifyFn {
   (
