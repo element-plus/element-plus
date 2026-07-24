8ecc24eccd598cde6543218868a4e3a86b427c26
keeplearning66 <1256734885@qq.com>
2026-07-24 21:11:43 +0800

    chore: remove unnecessary imports

--- packages/components/notification/style/css.ts
+++ packages/components/notification/style/css.ts
@@ -1,4 +1,3 @@
 import '@element-plus/components/base/style/css'
 import '@element-plus/theme-chalk/el-notification.css'
 import '@element-plus/components/progress/style/css'
-import '@element-plus/components/icon/style/css'
--- packages/components/notification/style/index.ts
+++ packages/components/notification/style/index.ts
@@ -1,4 +1,3 @@
 import '@element-plus/components/base/style'
 import '@element-plus/theme-chalk/src/notification.scss'
 import '@element-plus/components/progress/style'
-import '@element-plus/components/icon/style'
